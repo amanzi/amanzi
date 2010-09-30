1844c08b10af9308536442f341d3a815f35d676b
Ben Andre <bandre@lbl.gov>
2010-09-29 17:31:10 -0700

    geochemistry: use an enum for verbose output flag, move into own header file.

---
+++ src/chemistry/includes/Verbosity.hpp
@@ -0,0 +1,10 @@
+/* -*-  mode: c++; c-default-style: "google-c-style"; indent-tabs-mode: nil -*- */
+#ifndef __VERBOSITY_HPP__
+
+#define __VERBOSITY_HPP__
+
+enum VERBOSITY {kSilent, kTerse, kVerbose, kDebug, kDebugBeaker, kDebugMineralKinetics};
+
+
+#endif     /* __VERBOSITY_HPP__ */
+
