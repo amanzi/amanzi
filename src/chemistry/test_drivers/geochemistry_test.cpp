dfb8fa7565987a7a5bcf53332dabaf88519f216c
Ben Andre <bandre@lbl.gov>
2010-10-04 16:10:12 -0700

    geochemistry: change verbose output enum type, whitespace in test program.

--- src/chemistry/test_drivers/geochemistry_test.cpp
+++ src/chemistry/test_drivers/geochemistry_test.cpp
@@ -11,10 +11,10 @@
 #include "ActivityModelFactory.hpp"
 #include "Verbosity.hpp"
 
-int CommandLineOptions(int argc, char **argv, VERBOSITY& verbosity, int& test);
+int CommandLineOptions(int argc, char **argv, Verbosity& verbosity, int& test);
 
 int main(int argc, char **argv) {
-  VERBOSITY verbosity = kTerse;
+  Verbosity verbosity = kTerse;
   int test = 0;
   int error = EXIT_SUCCESS;
 
@@ -25,41 +25,46 @@ int main(int argc, char **argv) {
 
   if (error == EXIT_SUCCESS) {
     switch (test) {
-    case 1:
-      // set up simple 2-species carbonate system (H,HCO3-) unit activity coefficients
-      if (verbosity >= kTerse) {
-        std::cout << "Running simple carbonate example, unit activity coefficients." << std::endl;
+      case 1: {
+        // set up simple 2-species carbonate system (H,HCO3-) unit activity coefficients
+        if (verbosity >= kTerse) {
+          std::cout << "Running simple carbonate example, unit activity coefficients." << std::endl;
+        }
+        chem = new SimpleCarbonate();
+        activity_model_name = ActivityModelFactory::unit;
+        break;
       }
-      chem = new SimpleCarbonate();
-      activity_model_name = ActivityModelFactory::unit;
-      break;
-    case 2:
-      // set up simple 2-species carbonate system (H,HCO3-), debye-huckel activity coefficients
-      if (verbosity >= kTerse) {
-        std::cout << "Running simple carbonate example, debye-huckel." << std::endl;
+      case 2: {
+        // set up simple 2-species carbonate system (H,HCO3-), debye-huckel activity coefficients
+        if (verbosity >= kTerse) {
+          std::cout << "Running simple carbonate example, debye-huckel." << std::endl;
+        }
+        chem = new SimpleCarbonate();
+        activity_model_name = ActivityModelFactory::debye_huckel;
+        break;
       }
-      chem = new SimpleCarbonate();
-      activity_model_name = ActivityModelFactory::debye_huckel;
-      break;
-    case 3:
-      // larger carbonate system, 3 components, 9 secondary, unit activity coefficients
-      if (verbosity >= kTerse) {
-        std::cout << "Running large carbonate speciation example, unit activity coefficients." << std::endl;
+      case 3: {
+        // larger carbonate system, 3 components, 9 secondary, unit activity coefficients
+        if (verbosity >= kTerse) {
+          std::cout << "Running large carbonate speciation example, unit activity coefficients." << std::endl;
+        }
+        chem = new LargeCarbonate();
+        activity_model_name = ActivityModelFactory::unit;
+        break;
       }
-      chem = new LargeCarbonate();
-      activity_model_name = ActivityModelFactory::unit;
-      break;
-    case 4:
-      // larger carbonate system, 3 components, 9 secondary, debye-huckel activity coefficients
-      if (verbosity >= kTerse) {
-        std::cout << "Running large carbonate speciation example, debye-huckel activity coefficients." << std::endl;
+      case 4: {
+        // larger carbonate system, 3 components, 9 secondary, debye-huckel activity coefficients
+        if (verbosity >= kTerse) {
+          std::cout << "Running large carbonate speciation example, debye-huckel activity coefficients." << std::endl;
+        }
+        chem = new LargeCarbonate();
+        activity_model_name = ActivityModelFactory::debye_huckel;
+        break;
       }
-      chem = new LargeCarbonate();
-      activity_model_name = ActivityModelFactory::debye_huckel;
-      break;
-    default:
-      std::cout << "Invalid test number specified on command line. try using the \'-h\' option." << std::endl;
-      break;
+      default: {
+        std::cout << "Invalid test number specified on command line. try using the \'-h\' option." << std::endl;
+        break;
+      }
     }
   }
 
@@ -86,7 +91,7 @@ int main(int argc, char **argv) {
 }  // end main()
 
 
-int CommandLineOptions(int argc, char **argv, VERBOSITY& verbosity, int& test)
+int CommandLineOptions(int argc, char **argv, Verbosity& verbosity, int& test)
 {
   int error = -2;
   int option;
@@ -94,38 +99,42 @@ int CommandLineOptions(int argc, char **argv, VERBOSITY& verbosity, int& test)
 
   while ((option = getopt(argc, argv, "ht:v:?")) != EOF) {
     switch (option) {
-    case 't':
-      /* specify the test that should be run */
-      test = std::atoi(optarg);
-      error = EXIT_SUCCESS;
-      break;
-    case 'v':
-      verbosity = static_cast<VERBOSITY>(std::atoi(optarg));
-      break;
-    case '?': case 'h':  /* help mode */
-      /* print some help stuff and exit without doing anything */
-      std::cout << argv[0] << " command line options:" << std::endl;
-      std::cout << "    -t integer " << std::endl
-                << "         run a test case. valid test numbers are: " << std::endl
-                << "             1: simple carbonate speciation, unit activity coeff" << std::endl
-                << "             2: simple carbonate speciation, debye-huckel" << std::endl
-                << "             3: larger carbonate speciation, unit activity coeff" << std::endl
-                << "             4: larger carbonate speciation, debye-huckel" << std::endl;
-      std::cout << std::endl;
-      std::cout << std::endl;
-      std::cout << "    -v integer" << std::endl;
-      std::cout << "         verbosity output:" << std::endl;
-      std::cout << "             0: silent" << std::endl;
-      std::cout << "             1: terse" << std::endl;
-      std::cout << "             2: verbose" << std::endl;
-      std::cout << "             3: debug" << std::endl;
-      std::cout << "             4: debug beaker" << std::endl;
-      std::cout << "             5: debug mineral kinetics" << std::endl;
-      error = -1;
-      break;
-    default:
-      /* no options */
-      break;
+      case 't': {
+        /* specify the test that should be run */
+        test = std::atoi(optarg);
+        error = EXIT_SUCCESS;
+        break;
+      }
+      case 'v': {
+        verbosity = static_cast<Verbosity>(std::atoi(optarg));
+        break;
+      }
+      case '?': case 'h': { /* help mode */
+        /* print some help stuff and exit without doing anything */
+        std::cout << argv[0] << " command line options:" << std::endl;
+        std::cout << "    -t integer " << std::endl
+                  << "         run a test case. valid test numbers are: " << std::endl
+                  << "             1: simple carbonate speciation, unit activity coeff" << std::endl
+                  << "             2: simple carbonate speciation, debye-huckel" << std::endl
+                  << "             3: larger carbonate speciation, unit activity coeff" << std::endl
+                  << "             4: larger carbonate speciation, debye-huckel" << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
+        std::cout << "    -v integer" << std::endl;
+        std::cout << "         verbosity output:" << std::endl;
+        std::cout << "             0: silent" << std::endl;
+        std::cout << "             1: terse" << std::endl;
+        std::cout << "             2: verbose" << std::endl;
+        std::cout << "             3: debug" << std::endl;
+        std::cout << "             4: debug beaker" << std::endl;
+        std::cout << "             5: debug mineral kinetics" << std::endl;
+        error = -1;
+        break;
+      }
+      default: {
+        /* no options */
+        break;
+      }
     }
   }
 
