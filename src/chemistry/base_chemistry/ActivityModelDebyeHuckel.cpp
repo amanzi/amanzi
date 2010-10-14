5716fcdf15cfc8bfcdf33e23ad5e3c51b8a3b6bc
Ben Andre <bandre@lbl.gov>
2010-10-13 18:27:57 -0700

    geochemistry: add a comments about temperature of debye-huckel constants. refactor Evaluate function so there is a single return statement.

--- src/chemistry/base_chemistry/ActivityModelDebyeHuckel.cpp
+++ src/chemistry/base_chemistry/ActivityModelDebyeHuckel.cpp
@@ -5,9 +5,9 @@
 
 #include "ActivityModelDebyeHuckel.hpp"
 
-const double ActivityModelDebyeHuckel::debyeA = 0.5114;
-const double ActivityModelDebyeHuckel::debyeB = 0.3288;
-const double ActivityModelDebyeHuckel::debyeBdot = 0.0410;
+const double ActivityModelDebyeHuckel::debyeA = 0.5114;  // 25C
+const double ActivityModelDebyeHuckel::debyeB = 0.3288;  // 25C  
+const double ActivityModelDebyeHuckel::debyeBdot = 0.0410;  // 25C
 
 ActivityModelDebyeHuckel::ActivityModelDebyeHuckel()
     : ActivityModel()
@@ -22,17 +22,20 @@ ActivityModelDebyeHuckel::~ActivityModelDebyeHuckel()
 double ActivityModelDebyeHuckel::Evaluate(const Species& species)
 {
   // log(gamma_i) = - A * z_i^2 * sqrt(I) / (1 + a0 * B * sqrt(I)) + Bdot * I
+  double gamma(0.0);
+  if (fabs(species.charge()) < 1.e-10) {
+    // for now, neutral species activity = 1.
+    gamma = 1.0;
+  } else {
+    double sqrt_I = std::sqrt(I_);
 
-  // for now, neutral species activity = 1.
-  if (fabs(species.charge()) < 1.e-10) return 1.;
+    double log_gamma = -debyeA * species.charge() * species.charge() * sqrt_I /
+        (1.0 + species.ion_size_parameter() * debyeB * sqrt_I) +
+        debyeBdot * I_;
 
-  double sqrt_I = std::sqrt(I_);
-
-  double log_gamma = -debyeA * species.charge() * species.charge() * sqrt_I /
-      (1.0 + species.ion_size_parameter() * debyeB * sqrt_I) +
-      debyeBdot * I_;
-
-  return std::exp(log_to_ln(log_gamma));
+    gamma = std::exp(log_to_ln(log_gamma));  // why not just std::pow(10.0, log_gamma)? 
+  }
+  return gamma;
 }  // end Evaluate()
 
 void ActivityModelDebyeHuckel::Display(void) const
