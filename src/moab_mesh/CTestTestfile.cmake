40565371808963e1c7da6a0c3533c5d3d912d394
Markus Berndt <berndt@lanl.gov>
2010-10-26 15:59:17 -0600

    removing some spurious files that should not be under revision control, refs #190

--- src/moab_mesh/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /nh/u/lipnikov/Soft/ASCEM/amanzi/src/moab_mesh
-# Build directory: /nh/u/lipnikov/Soft/ASCEM/amanzi/src/moab_mesh
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(moab_mesh "test_moab_mesh")
