0985bb27aaa1445b92c021805e017bbb579f0317
William A. Perkins <william.perkins@pnl.gov>
2010-11-18 06:10:43 -0800

    Backed out changeset 4552e1b683b9

--- src/stk_mesh/Mesh_maps_stk.hh
+++ src/stk_mesh/Mesh_maps_stk.hh
@@ -1,6 +1,7 @@
 #ifndef _MESH_MAPS_STK_H_
 #define _MESH_MAPS_STK_H_
 
+#include "Mesh_maps_base.hh"
 #include "Mesh.hh"
 #include "Entity_map.hh"
 #include "Data_structures.hh"
@@ -15,10 +16,10 @@
 namespace STK_mesh
 {
 
-  class Mesh_maps_stk
-{
+  class Mesh_maps_stk : public Mesh_maps_base
+  {
 
-private:
+  private:
 
     Mesh_p mesh_;
     const Entity_map& entity_map_;
@@ -38,7 +39,7 @@ private:
     void build_tables_ ();
 
     stk::mesh::EntityRank kind_to_rank_ (Mesh_data::Entity_kind kind) const {
-        return entity_map_.kind_to_rank (kind);
+      return entity_map_.kind_to_rank (kind);
     }
 
     bool valid_entity_kind_ (Mesh_data::Entity_kind kind) const;
@@ -63,15 +64,6 @@ private:
     template <typename F, typename D, typename M>
     void add_global_ids_ (F from, F to, D destination, M& inverse);
 
-public:
-
-    Mesh_maps_stk (Mesh_p mesh);
-
-    void update ();
-
-    // Local id interfaces
-    // --------------------
-
     template <typename IT>
     void cell_to_faces (unsigned int cell, IT begin, IT end) const;
 
@@ -90,6 +82,82 @@ public:
     template <typename IT>
     void cell_to_coordinates (unsigned int cell, IT begin, IT end) const;
 
+    template <typename IT>
+    void get_set_ids (Mesh_data::Entity_kind kind, IT begin, IT end) const;
+
+    template <typename IT>
+    void get_set (unsigned int set_id, Mesh_data::Entity_kind kind, Element_Category category,
+                  IT begin, IT end) const;
+
+  public:
+
+    explicit Mesh_maps_stk (Mesh_p mesh);
+
+    void update ();
+
+    // Local id interfaces
+    // --------------------
+
+
+    void cell_to_faces (unsigned int cell, 
+                        std::vector<unsigned int>::iterator begin, 
+                        std::vector<unsigned int>::iterator end);
+
+    void cell_to_faces (unsigned int cell, 
+                        unsigned int* begin, unsigned int *end);
+    
+
+    void cell_to_face_dirs (unsigned int cell, 
+                            std::vector<int>::iterator begin, 
+                            std::vector<int>::iterator end);
+
+    void cell_to_face_dirs (unsigned int cell, 
+                            int * begin, int * end);
+  
+
+    void cell_to_nodes (unsigned int cell, 
+                        std::vector<unsigned int>::iterator begin, 
+                        std::vector<unsigned int>::iterator end);
+
+    void cell_to_nodes (unsigned int cell, 
+                        unsigned int * begin, unsigned int * end);
+
+
+    void face_to_nodes (unsigned int face, 
+                        std::vector<unsigned int>::iterator begin, 
+                        std::vector<unsigned int>::iterator end);
+
+    void face_to_nodes (unsigned int face, 
+                        unsigned int * begin, unsigned int * end);
+
+    void node_to_coordinates (unsigned int node, 
+                              std::vector<double>::iterator begin, 
+                              std::vector<double>::iterator end);
+
+    void node_to_coordinates (unsigned int node, 
+                              double * begin, 
+                              double * end);
+
+    void face_to_coordinates (unsigned int face, 
+                              std::vector<double>::iterator begin, 
+                              std::vector<double>::iterator end);
+
+    void face_to_coordinates (unsigned int face, 
+                              double * begin, 
+                              double * end);
+   
+
+    void cell_to_coordinates (unsigned int cell, 
+                              std::vector<double>::iterator begin,
+                              std::vector<double>::iterator end);
+
+    void cell_to_coordinates (unsigned int cell, 
+                              double * begin,
+                              double * end);
+
+
+    
+
     inline const Epetra_Map& cell_map (bool include_ghost) const;
     inline const Epetra_Map& face_map (bool include_ghost) const;
     inline const Epetra_Map& node_map (bool include_ghost) const;
@@ -100,6 +168,7 @@ public:
     // ------------------------------
 
     // Number and sizes
+
     unsigned int num_sets () const;
     unsigned int num_sets (Mesh_data::Entity_kind kind) const;
 
@@ -112,145 +181,62 @@ public:
                                Element_Category category) const;
 
     // Id numbers
-    template <typename IT>
-    void get_set_ids (Mesh_data::Entity_kind kind, IT begin, IT end) const;
+    void get_set_ids (Mesh_data::Entity_kind kind, 
+                      std::vector<unsigned int>::iterator begin, 
+                      std::vector<unsigned int>::iterator end) const;
+
+    void get_set_ids (Mesh_data::Entity_kind kind, 
+                      unsigned int * begin, 
+                      unsigned int * end) const;
 
     bool valid_set_id (unsigned int id, Mesh_data::Entity_kind kind) const;
 
 
-    template <typename IT>
-    void get_set (unsigned int set_id, Mesh_data::Entity_kind kind, Element_Category category,
-                  IT begin, IT end) const;
+    void get_set (unsigned int set_id, Mesh_data::Entity_kind kind, 
+                  Element_Category category, 
+                  std::vector<unsigned int>::iterator begin, 
+                  std::vector<unsigned int>::iterator end) const;
+
+    void get_set (unsigned int set_id, Mesh_data::Entity_kind kind, 
+                  Element_Category category, 
+                  unsigned int * begin, 
+                  unsigned int * end) const;
 
     template <typename IT>
     void get_set (const char* name, Mesh_data::Entity_kind kind, Element_Category category,
                   IT begin, IT end) const;
 
 
-};
+  };
 
-// -------------------------
-// Template & inline members
-// ------------------------
+  // -------------------------
+  // Template & inline members
+  // ------------------------
 
-// Inlined
+  // Inlined
 
-const Epetra_Map& Mesh_maps_stk::cell_map (bool include_ghost) const
-{
+  const Epetra_Map& Mesh_maps_stk::cell_map (bool include_ghost) const
+  {
     return map_ (Mesh_data::CELL, include_ghost);
-}
+  }
 
-const Epetra_Map& Mesh_maps_stk::face_map (bool include_ghost) const
-{
+  const Epetra_Map& Mesh_maps_stk::face_map (bool include_ghost) const
+  {
     return map_ (Mesh_data::FACE, include_ghost);
-}
+  }
 
-const Epetra_Map& Mesh_maps_stk::node_map (bool include_ghost) const
-{
+  const Epetra_Map& Mesh_maps_stk::node_map (bool include_ghost) const
+  {
     return map_ (Mesh_data::NODE, include_ghost);
-}
+  }
 
-// Connectivity accessors
-// ----------------------
+  // Connectivity accessors
+  // ----------------------
 
-
-template <typename IT>
-void Mesh_maps_stk::cell_to_faces (unsigned int cell, IT destination_begin, IT destination_end) const
-{
-    ASSERT ((unsigned int) (destination_end - destination_begin) == 6);
-    const unsigned int index = 6*cell;
-    std::vector<unsigned int>::const_iterator begin = cell_to_face_.begin () + index;
-    std::vector<unsigned int>::const_iterator end = begin + 6;
-    std::copy (begin, end, destination_begin);
-}
-
-template <typename IT>
-void Mesh_maps_stk::cell_to_nodes (unsigned int cell, IT destination_begin, IT destination_end) const
-{
-    ASSERT ((unsigned int) (destination_end - destination_begin) == 8);
-    const unsigned int index = 8*cell;
-    std::vector<unsigned int>::const_iterator begin = cell_to_node_.begin () + index;
-    std::vector<unsigned int>::const_iterator end   = begin + 8;
-    std::copy (begin, end, destination_begin);
-}
-
-template <typename IT>
-void Mesh_maps_stk::face_to_nodes (unsigned int face, IT destination_begin, IT destination_end) const
-{
-    ASSERT ((unsigned int) (destination_end - destination_begin) == 4);
-    const unsigned int index = 4*face;
-    std::vector<unsigned int>::const_iterator begin = face_to_node_.begin () + index;
-    std::vector<unsigned int>::const_iterator end   = begin + 4;
-    std::copy (begin, end, destination_begin);
-}
-
-
-// Cooordinate Getters
-// -------------------
-
-template <typename IT>
-void Mesh_maps_stk::node_to_coordinates (unsigned int local_node_id, IT begin, IT end) const
-{
-    ASSERT ((unsigned int) (end-begin) == 3);
-
-    // Convert local node to global node Id.
-    stk::mesh::EntityId global_node_id = map_ (Mesh_data::NODE, true).GID (local_node_id);
-
-    const double * coordinates = mesh_->coordinates (global_node_id);
-    std::copy (coordinates, coordinates+3, begin);
-}
-
-template <typename IT>
-void Mesh_maps_stk::face_to_coordinates (unsigned int local_face_id, IT begin, IT end) const
-{
-    ASSERT ((unsigned int) (end-begin) == 12);
-
-    unsigned int node_indices [4];
-    face_to_nodes (local_face_id, node_indices, node_indices+4);
-    for (int i = 0; i < 4; ++i)
-    {
-        node_to_coordinates (node_indices [i], begin, begin+4);
-        begin+=4;
-    }
-
-
-}
-
-template <typename IT>
-void Mesh_maps_stk::cell_to_coordinates (unsigned int local_cell_id, IT begin, IT end) const
-{
-    ASSERT ((unsigned int) (end-begin) == 24);
-
-    unsigned int node_indices [8];
-    cell_to_nodes (local_cell_id, node_indices, node_indices+8);
-    for (int i = 0; i < 8; ++i)
-    {
-        node_to_coordinates (node_indices [i], begin, begin+3);
-        begin+=3;
-    }
-
-}
-
-// Set getters
-// -----------
-
-template <typename IT>
-void Mesh_maps_stk::get_set_ids (Mesh_data::Entity_kind kind, IT begin, IT end) const
-{
-    std::vector<unsigned int> ids;
-    mesh_->get_set_ids (kind_to_rank_ (kind), ids);
-    ASSERT (ids.size () == num_sets (kind));
-
-    IT last = std::copy (ids.begin (), ids.end (), begin);
-
-    ASSERT (last == end);
-
-}
-
-template <typename IT>
-void Mesh_maps_stk::get_set (unsigned int set_id, Mesh_data::Entity_kind kind, Element_Category category,
-                         IT begin, IT end) const
-{
+  template <typename IT>
+  void Mesh_maps_stk::get_set (unsigned int set_id, Mesh_data::Entity_kind kind, Element_Category category,
+                               IT begin, IT end) const
+  {
     Entity_vector entities;
     stk::mesh::Part* set_part = mesh_->get_set (set_id, kind_to_rank_ (kind));
     mesh_->get_entities (*set_part, category, entities);
@@ -260,18 +246,19 @@ void Mesh_maps_stk::get_set (unsigned int set_id, Mesh_data::Entity_kind kind, E
     for (Entity_vector::const_iterator it = entities.begin ();
          it != entities.end ();
          ++it)
-    {
+      {
         *begin = global_to_local_ ( (*it)->identifier (), kind);
         begin++;
-    }
+      }
 
     ASSERT (begin == end);
-}
+  }
 
-template <typename IT>
-void Mesh_maps_stk::get_set (const char* name, Mesh_data::Entity_kind kind, Element_Category category,
-                         IT begin, IT end) const
-{
+  template <typename IT>
+  void Mesh_maps_stk::get_set (const char* name, 
+                               Mesh_data::Entity_kind kind, Element_Category category,
+                               IT begin, IT end) const
+  {
 
     Entity_vector entities;
     stk::mesh::Part* set_part = mesh_->get_set (name, kind_to_rank_ (kind));
@@ -281,35 +268,35 @@ void Mesh_maps_stk::get_set (const char* name, Mesh_data::Entity_kind kind, Elem
     for (Entity_vector::const_iterator it = entities.begin ();
          it != entities.end ();
          ++it)
-    {
+      {
         *begin = global_to_local_ ( (*it)->identifier (), kind);
         begin++;
-    }
+      }
 
     ASSERT (begin == end);
     
 
-}
+  }
 
 
-// Internal template functions
-// ---------------------------
+  // Internal template functions
+  // ---------------------------
 
 
-template <typename F, typename D, typename M>
-void Mesh_maps_stk::add_global_ids_ (F from, F to, D destination, M& global_to_local_map)
-{
+  template <typename F, typename D, typename M>
+  void Mesh_maps_stk::add_global_ids_ (F from, F to, D destination, M& global_to_local_map)
+  {
     int local_id = 0;
     for (F it = from; it != to;  ++it)
-    {
+      {
         const unsigned int global_id = (*it)->identifier ();
         *destination = global_id;
         global_to_local_map.insert (std::make_pair (global_id, local_id));
 
         destination++;
         local_id++;
-    }
-}
+      }
+  }
 
 }
 
