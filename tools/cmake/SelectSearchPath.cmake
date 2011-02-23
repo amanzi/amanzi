699745ab90f3413759fe51c996f4cfcd9038ddb4
Lori A. Pritchett-Sheats <lpritch@lanl.gov>
2011-02-23 14:12:07 -0700

    refs #322
    Changed this from a function to a macro
    A macro shares scope and function creates a new scope.

--- tools/cmake/SelectSearchPath.cmake
+++ tools/cmake/SelectSearchPath.cmake
@@ -5,7 +5,7 @@
 #
 include(PrintVariable)
 
-function(select_search_path PACKNAME OUT_PATH OUT_PATH_FOUND)
+macro(select_search_path PACKNAME OUT_PATH OUT_PATH_FOUND)
 
     set(pack_dir          "${${PACKNAME}_DIR}")
     set(env_pack_root     "${PACKNAME}_ROOT")
@@ -33,17 +33,27 @@ function(select_search_path PACKNAME OUT_PATH OUT_PATH_FOUND)
     #PRINT_VARIABLE(path_len)
     if ( ${path_len} GREATER "0" )
         if ( EXISTS "${test_path}" )
-            set(${OUT_PATH_FOUND} TRUE PARENT_SCOPE)
-            set(${OUT_PATH} "${test_path}" PARENT_SCOPE)
+            set(${OUT_PATH_FOUND} TRUE)
+            set(${OUT_PATH} "${test_path}")
         else()
             message(SEND_ERROR "The directory '${test_path}' does not exist. "
                                "Can not use this directory as a search path for ${PACKNAME}")
-            set(${OUT_PATH_FOUND} FALSE PARENT_SCOPE)
+            set(${OUT_PATH_FOUND} FALSE)
         endif()    
     else()
-        set(${OUT_PATH_FOUND} FALSE PARENT_SCOPE)
+        set(${OUT_PATH_FOUND} FALSE )
     endif()
 
+   # Print an error message
+   message(" ${OUT_PATH_FOUND}=${${OUT_PATH_FOUND}}")
+   if ( NOT ${OUT_PATH_FOUND} )
+       message(SEND_ERROR "An explicit installation path to ${PACKNAME} must be defined\n"
+                          "Define this path either at the command line with\n"
+                          " -D ${PACKNAME}_DIR:FILEPATH=<install_prefix>\n"
+                          "    or through environment variable ${PACKNAME}_ROOT=<install_prefix>\n")
+   endif()    
 
-endfunction()    
+
+
+endmacro()    
 
