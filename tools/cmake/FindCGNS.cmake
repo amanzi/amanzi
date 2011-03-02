68699879a58991a31c19819bd94a96952ecbeb76
Lori A. Pritchett-Sheats <lpritch@lanl.gov>
2011-03-02 12:04:36 -0700

    refs #345
    Removed select_search_path and added logic to search
    CGNS_LIBRARY_DIR and CGNS_INCLUDE_DIR for libraries and
    include files.

--- tools/cmake/FindCGNS.cmake
+++ tools/cmake/FindCGNS.cmake
@@ -11,16 +11,15 @@
 #
 #    Following variables are set:
 #    CGNS_FOUND            (BOOL)       Flag indicating if CGNS was found
-#    CGNS_INCLUDE_DIR      (PATH)       Path to the cgns include files
-#    CGNS_INCLUDE_DIRS     (PATH)       Path to the cgns include files
-#    CGNS_LIBRARY_DIR      (PATH)       Path to the cgns libraries
-#    CGNS_LIBRARY          (FILE)       CGNS libraries
-#    CGNS_LIBRARIES        (FILE)       CGNS libraries
+#    CGNS_INCLUDE_DIR      (PATH)       Path to the CGNS include file
+#    CGNS_INCLUDE_DIRS     (LIST)       List of all required include files
+#    CGNS_LIBRARY_DIR      (PATH)       Path to the CGNS library
+#    CGNS_LIBRARY          (FILE)       CGNS library
+#    CGNS_LIBRARIES        (LIST)       List of all required CGNS libraries
 #
-#    Additional variables:
-#    CGNS_VERSION          (STRING)     CGNS VERSION STRING
-#    
-# 
+#    Additional variables
+#    CGNS_VERSION          (STRING)     CGNS Version string
+#
 # #############################################################################
 
 # Standard CMake modules see CMAKE_ROOT/Modules
@@ -28,69 +27,176 @@ include(FindPackageHandleStandardArgs)
 
 # Amanzi CMake functions see <root>/tools/cmake for source
 include(PrintVariable)
-include(SelectSearchPath)
 
-# Define the search path 
-select_search_path(CGNS search_path search_path_found)
-#PRINT_VARIABLE(search_path)
-#PRINT_VARIABLE(search_path_found)
+if ( CGNS_LIBRARIES AND CGNS_INCLUDE_DIRS )
 
-if ( search_path_found )
-    message(STATUS "Searching for CGNS in ${search_path}")
+    # Do nothing. Variables are set. No need to search again
 
-    set(CGNS_DIR "${search_path}" CACHE PATH "Path to search for CGNS include and library files")
+else(CGNS_LIBRARIES AND CGNS_INCLUDE_DIRS)
 
+    # Cache variables
+    if(CGNS_DIR)
+        set(CGNS_DIR "${CGNS_DIR}" CACHE PATH "Path to search for CGNS include and library files")
+    endif()
+
+    if(CGNS_INCLUDE_DIR)
+        set(CGNS_INCLUDE_DIR "${CGNS_INCLUDE_DIR}" CACHE PATH "Path to search for CGNS include files")
+    endif()
+
+    if(CGNS_LIBRARY_DIR)
+        set(CGNS_LIBRARY_DIR "${CGNS_LIBRARY_DIR}" CACHE PATH "Path to search for CGNS library files")
+    endif()
+
+    
     # Search for include files
-    find_path(CGNS_INCLUDE_DIR
-                     NAMES cgnslib.h
-                     HINTS ${search_path}
-                     PATH_SUFFIXES include
-                     DOC "Path to CGNS include files"
-                     NO_DEFAULT_PATH)
+    # Search order preference:
+    #  (1) CGNS_INCLUDE_DIR - check existence of path AND if the include files exist
+    #  (2) CGNS_DIR/<include,include/CGNS++,include/cgns++>
+    #  (3) Default CMake paths See cmake --html-help=out.html file for more information.
+    #
+    set(cgns_inc_names "cgnslib.h")
+    if (CGNS_INCLUDE_DIR)
 
-    # Define the version
-    if ( CGNS_INCLUDE_DIR )
-        set(cgns_h "${CGNS_INCLUDE_DIR}/cgnslib.h")
-        file(STRINGS "${cgns_h}" cgns_version_string REGEX "^#define CGNS_VERSION ")
-        string(REGEX REPLACE "^#define CGNS_VERSION ([0-9]+).*$" "\\1" cgns_version "${cgns_version_string}")
+        if (EXISTS "${CGNS_INCLUDE_DIR}")
 
-        #PRINT_VARIABLE(cgns_version_string)
-        #PRINT_VARIABLE(cgns_version)
+            find_path(test_include_path
+                      NAMES ${cgns_inc_names}
+                      HINTS ${CGNS_INCLUDE_DIR}
+                      NO_DEFAULT_PATH)
+            if(NOT test_include_path)
+                message(SEND_ERROR "Can not locate ${cgns_inc_names} in ${CGNS_INCLUDE_DIR}")
+            endif()
+            set(CGNS_INCLUDE_DIR "${test_include_path}")
 
-        set(CGNS_VERSION "${cgns_version}")
+        else()
+            message(SEND_ERROR "CGNS_INCLUDE_DIR=${CGNS_INCLUDE_DIR} does not exist")
+            set(CGNS_INCLUDE_DIR "CGNS_INCLUDE_DIR-NOTFOUND")
+        endif()
 
+   else() 
+
+        set(cgns_inc_suffixes "include")
+        if(CGNS_DIR)
+
+            if (EXISTS "${CGNS_DIR}" )
+
+                find_path(CGNS_INCLUDE_DIR
+                          NAMES ${cgns_inc_names}
+                          HINTS ${CGNS_DIR}
+                          PATH_SUFFIXES ${cgns_inc_suffixes}
+                          NO_DEFAULT_PATH)
+
+            else()
+                 message(SEND_ERROR "CGNS_DIR=${CGNS_DIR} does not exist")
+                 set(CGNS_INCLUDE_DIR "CGNS_INCLUDE_DIR-NOTFOUND")
+            endif()    
+
+
+        else()
+
+            find_path(CGNS_INCLUDE_DIR
+                      NAMES ${cgns_inc_names}
+                      PATH_SUFFIXES ${cgns_inc_suffixes})
+
+        endif()
+
+    endif()
+
+    if ( NOT CGNS_INCLUDE_DIR )
+        message(SEND_ERROR "Can not locate CGNS include directory")
+    endif()
+
+    # Search for libraries 
+    # Search order preference:
+    #  (1) CGNS_LIBRARY_DIR - check existence of path AND if the include files exist
+    #  (2) CGNS_DIR/<lib,lib/CGNS++,lib/cgns++>
+    #  (3) Default CMake paths See cmake --html-help=out.html file for more information.
+    #
+    set(cgns_lib_names "cgns")
+    if (CGNS_LIBRARY_DIR)
+
+        if (EXISTS "${CGNS_LIBRARY_DIR}")
+
+            find_library(CGNS_LIBRARY
+                         NAMES ${cgns_lib_names}
+                         HINTS ${CGNS_LIBRARY_DIR}
+                         NO_DEFAULT_PATH)
+        else()
+            message(SEND_ERROR "CGNS_LIBRARY_DIR=${CGNS_LIBRARY_DIR} does not exist")
+            set(CGNS_LIBRARY "CGNS_LIBRARY-NOTFOUND")
+        endif()
+
+    else() 
+
+        list(APPEND cgns_lib_suffixes "lib" "Lib")
+        if(CGNS_DIR)
+
+            if (EXISTS "${CGNS_DIR}" )
+
+                find_library(CGNS_LIBRARY
+                             NAMES ${cgns_lib_names}
+                             HINTS ${CGNS_DIR}
+                             PATH_SUFFIXES ${cgns_lib_suffixes}
+                             NO_DEFAULT_PATH)
+
+            else()
+                 message(SEND_ERROR "CGNS_DIR=${CGNS_DIR} does not exist")
+                 set(CGNSLIBRARY "CGNS_LIBRARY-NOTFOUND")
+            endif()    
+
+
+        else()
+
+            find_library(CGNS_LIBRARY
+                         NAMES ${cgns_lib_names}
+                         PATH_SUFFIXES ${cgns_lib_suffixes})
+
+        endif()
+
+    endif()
+
+    if ( NOT CGNS_LIBRARY )
+        message(SEND_ERROR "Can not locate CGNS library")
     endif()    
 
-    # Search for library
-    find_library( CGNS_LIBRARY
-                  NAMES cgns
-                  HINTS ${search_path}
-                  PATH_SUFFIXES lib 
-                  DOC "The CGNS library"
-                  NO_DEFAULT_PATH)
+   
+    # CGNS does not have any prerequisite libraries
+    set(CGNS_INCLUDE_DIRS ${CGNS_INCLUDE_DIR})
+    set(CGNS_LIBRARIES    ${CGNS_LIBRARY})
 
-     if ( CGNS_LIBRARY ) 
-         get_filename_component(CGNS_LIBRARY_DIR "${CGNS_LIBRARY}" PATH)
-     else()    
-         message(SEND_ERROR "Can not locate CGNS library in ${CGNS_DIR}")
-     endif()
+   
+endif(CGNS_LIBRARIES AND CGNS_INCLUDE_DIRS )    
 
-endif()      
-
+# Send useful message if everything is found
 find_package_handle_standard_args(CGNS DEFAULT_MSG
-                                  CGNS_LIBRARY
-                                  CGNS_INCLUDE_DIR)
-if (CGNS_FOUND )
-   set(CGNS_INCLUDE_DIRS ${CGNS_INCLUDE_DIR})
-   set(CGNS_LIBRARIES    ${CGNS_LIBRARY})
+                                           CGNS_LIBRARIES
+                                           CGNS_INCLUDE_DIRS)
+
+# find_package)handle)standard_args should set CGNS_FOUND but it does not!
+if ( CGNS_LIBRARIES AND CGNS_INCLUDE_DIRS)
+    set(CGNS_FOUND TRUE)
+else()
+    set(CGNS_FOUND FALSE)
 endif()
 
+# Define the version
+if ( CGNS_INCLUDE_DIR )
+    set(cgns_h "${CGNS_INCLUDE_DIR}/cgnslib.h")
+    file(STRINGS "${cgns_h}" cgns_version_string REGEX "^#define CGNS_VERSION ")
+    string(REGEX REPLACE "^#define CGNS_VERSION ([0-9]+).*$" "\\1" cgns_version "${cgns_version_string}")
+
+    #PRINT_VARIABLE(cgns_version_string)
+    #PRINT_VARIABLE(cgns_version)
+
+    set(CGNS_VERSION "${cgns_version}")
+
+endif()    
+
 mark_as_advanced(
+  CGNS_VERSION
   CGNS_INCLUDE_DIR
   CGNS_INCLUDE_DIRS
   CGNS_LIBRARY
-  CGNS_LIBRARY_DIR
   CGNS_LIBRARIES
-  CGNS_VERSION
+  CGNS_LIBRARY_DIR
 )
-
