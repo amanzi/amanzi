4429b83d1b77330fff954f522f179ebd831a993f
Michael Buksas <mwbuksas@lanl.gov>
2011-04-15 12:04:52 -0600

    Adding functions for integration tests and unit tests with labels. Splitting out core functionality of tests into utitlity functions in TestManager.cmake
    Updating CMakeLists.txt files to use new functions.

--- tools/cmake/TestManager.cmake
+++ tools/cmake/TestManager.cmake
@@ -8,51 +8,101 @@ include(AddParallelTest)
 include(CMakeParseArguments)
 include(PrintVariable)
 
+
+
+function(_ADD_TEST_LABELS test_name)
+
+  get_test_property(${test_name} LABELS labels)
+  if ("${labels}" STREQUAL "NOTFOUND")
+    unset(labels)
+  endif()
+
+  list(APPEND labels "${ARGN}")
+  list(REMOVE_DUPLICATES labels)
+  set_tests_properties(${test_name} PROPERTIES LABELS "${labels}")
+
+endfunction(_ADD_TEST_LABELS)
+
+function (_REGISTER_TEST test_name test_exec nprocs labels mpi_args)
+
+  foreach(nproc ${nprocs})
+    if (${nproc} GREATER 1)
+      ADD_PARALLEL_TEST(${test_name} ${test_exec} NPROCS ${nproc} MPI_EXEC_ARGS ${mpi_args})
+      _add_test_labels(${test_name} "Parallel")
+    else()
+      add_test(${test_name} ${test_exec})
+      _add_test_labels(${test_name} "Serial")
+    endif()
+  endforeach()
+
+endfunction(_REGISTER_TEST)
+
+
+
+
 function(ADD_UNIT_TEST test_name test_exec)
+
 #
 # Usage:
 #
-# REGISTER_UNIT_TEST(<test_name> <test_executable>
-#                    [NPROCS <list of processor counts>]
-#                    [LABELS <list of labels>]
-#                    [MPI_EXEC_ARGS arg1 ... ]
+# ADD_UNIT_TEST(<test_name> <test_executable>
+#                PARALLEL
+#                [NPROCS procs1 ... ]
+#                [MPI_EXEC_ARGS arg1 ... ]
+#
+# Option PARALLEL signifies that this is a parallel job. This is also
+# implied by an NPROCS value > 1
 #
 # Optional NPROCS keyword starts a list of the number of processors to
 # run the test on. Defaults to 1.
 #
-# Optional LABELS assign string labels to the test.
+# Optional MPI_EXEC_ARGS keyword denotes extra arguments to give to
+# mpi. It is ignored for serial tests.
 #
-# Optional MPI_EXEC_ARGS keyword denotes extra arguments to give to mpi.
-#
 
-set(options "")
-set(oneValueArgs "")
-set(multiValueArgs NPROCS LABELS)
+  set(options "PARALLEL")
+  set(oneValueArgs "")
+  set(multiValueArgs NPROCS)
 
-# Will define values:
-#  REGISTER_UNIT_TEST_NPROCS
-#  REGISTER_UNIT_TEST_LABELS
-# And possibly...
-# REGISTER_UNIT_TEST_UNPARSED_ARGUMENTS
-cmake_parse_arguments(ADD_UNIT_TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  cmake_parse_arguments(ADD_UNIT_TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-set(nprocs "${ADD_UNIT_TEST_NPROCS}" )
-if (nprocs EQUAL "")
-  set(nprocs 1)
-endif()
-
-set(labels ${ADD_UNIT_TEST_LABELS} unit)
-
-message("Name: ${test_name}  Exec: ${test_exec}  NProcs: ${nprocs}  Labels: ${labels}")
-
-foreach(nproc ${nprocs})
-  if (${nproc} GREATER 1)
-    ADD_PARALLEL_TEST(${test_name} ${test_exec} NPROCS ${nproc} MPI_EXEC_ARGS ${ADD_UNIT_TEST_MPI_EXEC_ARGS})
-  else()
-    add_test(${test_name} ${test_exec})
+  set(nprocs "${ADD_UNIT_TEST_NPROCS}" )
+  if ("${nprocs}" STREQUAL "")
+    set(nprocs 1)
   endif()
-endforeach()
 
+  set(labels ${ADD_UNIT_TEST_LABELS})
+  set(mpi_args ${ADD_UNIT_TEST_MPI_EXEC_ARGS})
+
+  _register_test("${test_name}" "${test_exec}" "${nprocs}" "${labels}" "${mpiargs}")
+  _add_test_labels("${test_name}" "Unit")
 
 endfunction(ADD_UNIT_TEST)
+
+
+
+
+function(ADD_INTEGRATION_TEST test_name test_exec)
+
+  set(options "PARALLEL")
+  set(oneValueArgs "")
+  set(multiValueArgs NPROCS FILES)
+
+  cmake_parse_arguments(ADD_INTEGRATION_TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  set(nprocs "${ADD_INTEGRATION_TEST_NPROCS}" )
+  if ("${nprocs}" STREQUAL "")
+    set(nprocs 1)
+  endif()
+
+  set(labels ${ADD_UNIT_TEST_LABELS})
+  set(mpi_args ${ADD_UNIT_TEST_MPI_EXEC_ARGS})
+
+  _register_test("${test_name}" "${test_exec}" "${nprocs}" "${labels}" "${mpiargs}")
+  _add_test_labels(${test_name} "Integration")
+
+  # TODO: Add dependencies on files
+
+endfunction(ADD_INTEGRATION_TEST)
+
 
