41d53ca9875453475128f24439d31d7ee4029da6
Michael Buksas <mwbuksas@lanl.gov>
2011-04-20 12:49:08 -0600

    Fixes #397.
    Adding better string handling to _register_test so that users can use a string when specifying MPI_EXEC_ARGS

--- tools/cmake/AddParallelTest.cmake
+++ tools/cmake/AddParallelTest.cmake
@@ -19,36 +19,4 @@
 # CMake Modules
 include(CMakeParseArguments)
 
-macro(ADD_PARALLEL_TEST test_name test_exec mpi_args)
-  set(_options  "")
-  set(_oneValue "NPROCS")
-  set(_multiValue "")
-  cmake_parse_arguments(ADD_PARALLEL_TEST "${_options}" "${_oneValue}" "${_multiValue}" ${ARGN})
-
-  # Default number of procs is 1
-  set(_local_nprocs  1)
-  if (ADD_PARALLEL_TEST_NPROCS )
-    set(_local_nprocs ${ADD_PARALLEL_TEST_NPROCS})
-  endif()
-
-  # Do not allow mpi_exec to run more than the MAX_NPROCS
-  if ( MPI_EXEC_MAX_NUMPROCS )
-    if (${MPI_EXEC_MAX_NUMPROCS} LESS ${_local_nprocs}  )
-      set(_local_nprocs ${MPI_EXEC_MAX_NUMPROCS})
-      message(WARNING "Test ${test_name} requested too many procs. "
-        "Will run ${test_name} with ${_local_nprocs} ranks")
-    endif()
-  endif()
-
-  set(test_exec_args "${MPI_EXEC_ARGS_FLAG}" "${mpi_args}")
-
-  # Build MPI_EXEC cpmmand
-  # This will have to change if POE is used as MPI_EXEC!
-  set(_mpi_cmd ${MPI_EXEC} ${MPI_EXEC_NUMPROCS_FLAG} ${_local_nprocs})
-  list(APPEND _mpi_cmd ${test_exec_args} ${test_exec})
-
-  # Finally add the test
-  add_test(${test_name} ${_mpi_cmd})
-
-endmacro(ADD_PARALLEL_TEST)
 
--- tools/cmake/TestManager.cmake
+++ tools/cmake/TestManager.cmake
@@ -9,6 +9,53 @@ include(CMakeParseArguments)
 include(PrintVariable)
 
 
+function (_REGISTER_TEST test_name test_exec test_args nprocs is_parallel mpi_args)
+
+  if ("${nprocs}" STREQUAL "")
+    set(nprocs 1)
+  endif()
+
+  foreach(nproc ${nprocs})
+    if ((${nproc} GREATER 1) OR "${is_parallel}")
+      _add_parallel_test(${test_name} ${test_exec} "${test_args}" ${nproc} "${mpi_args}")
+      _add_test_labels(${test_name} "Parallel")
+    else()
+      add_test(${test_name} ${test_exec} ${test_args})
+      _add_test_labels(${test_name} "Serial")
+    endif()
+  endforeach()
+
+endfunction(_REGISTER_TEST)
+
+
+
+function(_ADD_PARALLEL_TEST test_name test_exec test_args nproc mpi_args)
+
+  set(_options  "")
+  set(_oneValue "")
+  set(_multiValue "MPI_EXEC_ARGS")
+  cmake_parse_arguments(ADD_PARALLEL_TEST "${_options}" "${_oneValue}" "${_multiValue}" ${ARGN}) 
+
+  # Do not allow mpi_exec to run more than the MAX_NPROCS
+  if ( MPI_EXEC_MAX_NUMPROCS )
+    if (${MPI_EXEC_MAX_NUMPROCS} LESS ${nproc}  )
+      set(nproc ${MPI_EXEC_MAX_NUMPROCS})
+      message(WARNING "Test ${test_name} requested too many procs. "
+        "Will run ${test_name} with ${nproc} ranks")
+    endif()
+  endif()
+
+  # Build MPI_EXEC cpmmand
+  # This will have to change if POE is used as MPI_EXEC!
+  set(_mpi_cmd "${MPI_EXEC}" "${MPI_EXEC_NUMPROCS_FLAG}" "${nproc}" "${MPI_EXEC_ARGS_FLAG}" "${mpi_args}" "${test_exec}" "${test_args}")
+
+  # Register the test
+  add_test(${test_name} ${_mpi_cmd})
+
+endfunction(_ADD_PARALLEL_TEST)
+
+
+
 
 function(_ADD_TEST_LABELS test_name)
 
@@ -27,24 +74,6 @@ endfunction(_ADD_TEST_LABELS)
 
 
 
-function (_REGISTER_TEST test_name test_exec nprocs is_parallel mpi_args)
-
-  if ("${nprocs}" STREQUAL "")
-    set(nprocs 1)
-  endif()
-
-  foreach(nproc ${nprocs})
-    if ((${nproc} GREATER 1) OR "${is_parallel}")
-      ADD_PARALLEL_TEST(${test_name} ${test_exec} "${mpi_args}" NPROCS ${nproc} )
-      _add_test_labels(${test_name} "Parallel")
-    else()
-      add_test(${test_name} ${test_exec})
-      _add_test_labels(${test_name} "Serial")
-    endif()
-  endforeach()
-
-endfunction(_REGISTER_TEST)
-
 
 
 
@@ -52,9 +81,15 @@ endfunction(_REGISTER_TEST)
 # Usage:
 #
 # ADD_UNIT_TEST(<test_name> <test_executable>
+#                [arg1 ...]
 #                PARALLEL
 #                [NPROCS procs1 ... ]
 #                [MPI_EXEC_ARGS arg1 ... ]
+#
+# Arguments:
+#  test_name: the name given to the resulting test in test reports
+#  test_executable: The test executable which performs the test
+#  arg1 ...: Additional arguments for the test executable
 #
 # Option PARALLEL signifies that this is a parallel job. This is also
 # implied by an NPROCS value > 1
@@ -74,16 +109,18 @@ function(ADD_UNIT_TEST test_name test_exec)
 
   cmake_parse_arguments(ADD_UNIT_TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-  set(is_parallel "${ADD_UNIT_TEST_PARALLEL}")
-  set(mpi_args    "${ADD_UNIT_TEST_MPI_EXEC_ARGS}")
-  set(nprocs      "${ADD_UNIT_TEST_NPROCS}")
+  set(test_args    "${ADD_UNIT_TEST_UNPARSED_ARGUMENTS}")
+  set(is_parallel  "${ADD_UNIT_TEST_PARALLEL}")
+  set(mpi_args     "${ADD_UNIT_TEST_MPI_EXEC_ARGS}")
+  set(nprocs       "${ADD_UNIT_TEST_NPROCS}")
 
-  _register_test("${test_name}" "${test_exec}" "${nprocs}" "${is_parallel}" "${mpiargs}")
+  separate_arguments(global_mpi_args UNIX_COMMAND "${MPI_EXEC_ARGS}")
+  list(APPEND mpi_args ${global_mpi_args})
+
+  _register_test("${test_name}" "${test_exec}" "${test_args}" "${nprocs}" "${is_parallel}" "${mpi_args}")
   _add_test_labels("${test_name}" "Unit")
 
 endfunction(ADD_UNIT_TEST)
-
-
 
 
 function(ADD_INTEGRATION_TEST test_name test_exec)
@@ -94,11 +131,12 @@ function(ADD_INTEGRATION_TEST test_name test_exec)
 
   cmake_parse_arguments(ADD_INTEGRATION_TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-  set(is_parallel "${ADD_INTEGRATION_TEST_PARALLEL}")
-  set(mpi_args    "${ADD_INTEGRATION_TEST_MPI_EXEC_ARGS}")
-  set(nprocs      "${ADD_INTEGRATION_TEST_NPROCS}")
+  set(test_args    "${ADD_INTEGRATION_TEST_UNPARSED_ARGUMENTS}")
+  set(is_parallel  "${ADD_INTEGRATION_TEST_PARALLEL}")
+  set(mpi_args     "${ADD_INTEGRATION_TEST_MPI_EXEC_ARGS}")
+  set(nprocs       "${ADD_INTEGRATION_TEST_NPROCS}")
 
-  _register_test("${test_name}" "${test_exec}" "${nprocs}" "${is_parallel}" "${mpiargs}")
+  _register_test("${test_name}" "${test_exec}" "${test_args}" "${nprocs}" "${is_parallel}" "${mpi_args}")
   _add_test_labels(${test_name} "Integration")
 
   # TODO: Add dependencies on files
