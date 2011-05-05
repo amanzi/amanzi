29ed369622a2b2fb8130a71d78aeeb096c65ecd5
Markus Berndt <berndt@lanl.gov>
2011-05-05 16:25:11 -0600

    starting new implementation for water retention model, refs #386

---
+++ src/flow/vanGenuchtenModel.cpp
@@ -0,0 +1,57 @@
+#include "vanGenuchtenModel.hpp"
+#include "math.h"
+
+vanGenuchtenModel::vanGenuchtenModel(double m_, double alpha_, 
+				     double sr_, double p_atm_) :
+  m(m_), alpha(alpha_), sr(sr_), p_atm(p_atm_)
+{
+  n = 1.0/(1.0-m);
+}
+
+ 
+double vanGenuchtenModel::k_relative(double p)
+{
+  double pc = p - p_atm; // capillary pressure
+  if (pc < 0.0)
+    {
+      double se = pow(1.0 + pow(-alpha*pc,n),-m);
+      return sqrt(se)*pow(1.0-pow(1.0-pow(se,1.0/m),m),2.0);
+    }
+  else
+    {
+      return 1.0;
+    }
+  
+}
+
+double vanGenuchtenModel::saturation(double p)
+{
+  double pc = p - p_atm; // capillary pressure
+  if (pc < 0.0) 
+    {
+      return pow(1.0 + pow(-alpha*pc,n),-m) * (1.0 - sr) + sr;
+    }
+  else
+    {
+      return 1.0;
+    }
+}
+
+double vanGenuchtenModel::d_saturation(double p)
+{
+  double pc = p - p_atm; // capillary pressure
+  if (pc < 0.0) 
+    {
+      return -m * pow(1.0 + pow(-alpha*pc,n),-m-1.0) 
+	* n * pow(-alpha*pc,n - 1.0) * (-alpha) * (1.0 - sr);
+    }
+  else
+    {
+      return 0.0;
+    }
+}  
+
+void vanGenuchtenModel::update_p_atm(double new_p_atm)
+{
+  p_atm = new_p_atm;
+}
