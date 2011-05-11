9d144bfd4c1fdc822977edbd4efdb59f5080b678
Rao Garimella <rao@lanl.gov>
2011-05-11 10:13:53 -0600

    Added back the HexFaceVert for backward compatibility. Once the rest of the modules switch to the new API and new way of getting the standard cell topology, we can get rid of it

--- src/mesh/Cell_topology.hh
+++ src/mesh/Cell_topology.hh
@@ -76,6 +76,9 @@ namespace Amanzi {
 
     // LEFT THESE IN HERE BUT WE SHOULD LOOK CAREFULLY IF WE NEED IT
 
+    // Outward oriented faces of the reference hexahedron.
+    static int HexFaceVert[6][4] = {{0,1,5,4}, {1,2,6,5}, {2,3,7,6}, {3,0,4,7}, {0,3,2,1}, {4,5,6,7}};
+
     // Indices of the three faces adjacent to each corner of the reference hexahedron.
     static int HexCornerFace[8][3] = {{0,3,4}, {0,1,4}, {1,2,4}, {2,3,4}, {0,3,5}, {0,1,5}, {1,2,5}, {2,3,5}};  
   
