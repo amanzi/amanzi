41e05ecf0add31ad34c38a9904c84c2172f19a32
Rao Garimella <rao@lanl.gov>
2011-05-11 10:12:31 -0600

    Mesh audit routines based on the old-style API. These files will go away once we switch the rest of the code to the new API

---
+++ src/mesh/MeshAuditOld.hh
@@ -0,0 +1,123 @@
+#ifndef __MESHAUDITOLD__
+#define __MESHAUDITOLD__
+
+#include "Teuchos_RCP.hpp"
+#include "Epetra_Comm.h"
+
+#include "Mesh.hh"
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/visitors.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+
+class MeshAuditOld {
+public:
+
+  MeshAuditOld(Teuchos::RCP<Amanzi::AmanziMesh::Mesh> &mesh_, std::ostream& os=std::cout);
+
+  // This is the main method.
+  int Verify() const;
+  
+  // The individual tests are also available.  While the tests are all formally
+  // independent, there is an implicit order dependence of the tests in that a
+  // test may assume certain mesh data has been verified, and that verification
+  // is done by other tests.
+
+  bool check_entity_counts() const;
+  bool check_cell_to_nodes() const;
+  bool check_cell_to_faces() const;
+  bool check_face_to_nodes() const;
+  bool check_cell_to_face_dirs() const;
+  bool check_cell_to_nodes_consistency() const;
+  bool check_cell_to_faces_consistency() const;
+  bool check_face_to_nodes_consistency() const;
+  bool check_cell_to_face_dirs_consistency() const;
+  bool check_node_refs_by_cells() const;
+  bool check_face_refs_by_cells() const;
+  bool check_node_refs_by_faces() const;
+  bool check_cell_degeneracy() const;
+  bool check_cell_to_faces_to_nodes() const;
+  bool check_node_to_coordinates() const;
+  bool check_cell_to_coordinates() const;
+  bool check_face_to_coordinates() const;
+  bool check_node_to_coordinates_alt() const;
+  bool check_cell_to_coordinates_alt() const;
+  bool check_face_to_coordinates_alt() const;
+  bool check_cell_topology() const;
+  bool check_node_maps() const;
+  bool check_face_maps() const;
+  bool check_cell_maps() const;
+  bool check_node_to_coordinates_ghost_data() const;
+  bool check_face_to_nodes_ghost_data() const;
+  bool check_cell_to_nodes_ghost_data() const;
+  bool check_cell_to_faces_ghost_data() const;
+  
+  bool check_node_set_ids() const;
+  bool check_face_set_ids() const;
+  bool check_cell_set_ids() const;
+  
+  bool check_valid_node_set_id() const;
+  bool check_valid_face_set_id() const;
+  bool check_valid_cell_set_id() const;
+  
+  bool check_node_sets() const;
+  bool check_face_sets() const;
+  bool check_cell_sets() const;
+  
+  bool check_node_sets_alt() const;
+  bool check_face_sets_alt() const;
+  bool check_cell_sets_alt() const;
+  
+  bool check_node_partition() const;
+  bool check_face_partition() const;
+
+private:
+
+  Teuchos::RCP<Amanzi::AmanziMesh::Mesh> mesh;
+
+  const Epetra_Comm &comm;
+  const int MyPID;
+  const int nnode;
+  const int nface;
+  const int ncell;
+
+  std::ostream& os;
+  unsigned int MAX_OUT;
+
+  bool distinct_values(const std::vector<unsigned int> &list) const;
+  void write_list(const std::vector<unsigned int>&, unsigned int) const;
+  bool global_any(bool) const;
+  int same_face(const std::vector<unsigned int>, const std::vector<unsigned int>) const;
+  
+  bool check_maps(const Epetra_Map&, const Epetra_Map&) const;
+  bool check_get_set_ids(Amanzi::AmanziMesh::Entity_kind) const;
+  bool check_valid_set_id(Amanzi::AmanziMesh::Entity_kind) const;
+  bool check_sets(Amanzi::AmanziMesh::Entity_kind, const Epetra_Map&, const Epetra_Map&) const;
+  bool check_get_set(unsigned int, Amanzi::AmanziMesh::Entity_kind, Amanzi::AmanziMesh::Parallel_type, const Epetra_Map&) const;
+  bool check_used_set(unsigned int, Amanzi::AmanziMesh::Entity_kind, const Epetra_Map&, const Epetra_Map&) const;
+  bool check_sets_alt(Amanzi::AmanziMesh::Entity_kind) const;
+  bool check_get_set_alt(unsigned int, Amanzi::AmanziMesh::Entity_kind, Amanzi::AmanziMesh::Parallel_type) const;
+  
+  // This is the vertex type for the test dependency graph.
+  typedef bool (MeshAuditOld::* Test)() const;
+  struct Vertex
+  {
+    Vertex() : run(true) {}
+    std::string name;
+    mutable bool run;
+    Test test;
+  };
+
+  typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, Vertex> Graph;
+  Graph g;
+  
+  struct mark_do_not_run : public boost::bfs_visitor<>
+  {
+    template <class Vertex, class Graph>
+    void discover_vertex(Vertex v, Graph &g) { g[v].run = false; }
+  };
+  
+  void create_test_dependencies();
+};
+
+#endif
