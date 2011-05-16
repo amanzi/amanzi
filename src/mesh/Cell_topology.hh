9270a5278d30f66cebabab8bc251eda8479c4479
Rao Garimella <rao@lanl.gov>
2011-05-16 05:37:54 -0600

    Made all the variables constant

--- src/mesh/Cell_topology.hh
+++ src/mesh/Cell_topology.hh
@@ -19,12 +19,12 @@ namespace Amanzi {
     // expected number of faces for standard cells
     // 0 indicates that it is not possible to specify the number for this cell type
     //
-    static int nface_std[9] = {0,3,4,0,4,5,5,6,0}; 
+    static const int nface_std[9] = {0,3,4,0,4,5,5,6,0}; 
 
     // Expected number of nodes for each face of standard cells
     // 0 indicates that it is not possible to specify the number for this cell type
     //
-    static int nfnodes_std[9][6] = { 
+    static const int nfnodes_std[9][6] = { 
       {0,0,0,0,0,0},         // UNKNOWN
       {2,2,2,0,0,0},         // TRI
       {2,2,2,2,0,0},         // QUAD
@@ -39,7 +39,7 @@ namespace Amanzi {
     // -1 indicates that it is not possible to specify the number for this cell type
     // or that it is a filler for extra nodes in the array
     //
-    static int fnodes_std[9][6][4] = { 
+    static const int fnodes_std[9][6][4] = { 
       { // UNKNOWN
 	{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1}
       },
@@ -77,15 +77,16 @@ namespace Amanzi {
     // LEFT THESE IN HERE BUT WE SHOULD LOOK CAREFULLY IF WE NEED IT
 
     // Outward oriented faces of the reference hexahedron.
-    static int HexFaceVert[6][4] = {{0,1,5,4}, {1,2,6,5}, {2,3,7,6}, {3,0,4,7}, {0,3,2,1}, {4,5,6,7}};
+    static const int HexFaceVert[6][4] = {{0,1,5,4}, {1,2,6,5}, {2,3,7,6}, {3,0,4,7}, {0,3,2,1}, {4,5,6,7}};
 
     // Indices of the three faces adjacent to each corner of the reference hexahedron.
-    static int HexCornerFace[8][3] = {{0,3,4}, {0,1,4}, {1,2,4}, {2,3,4}, {0,3,5}, {0,1,5}, {1,2,5}, {2,3,5}};  
+    static const int HexCornerFace[8][3] = {{0,3,4}, {0,1,4}, {1,2,4}, {2,3,4}, {0,3,5}, {0,1,5}, {1,2,5}, {2,3,5}};  
   
     // Corner tetrahedra (first 8) followed by the internal tetrahedra (last 2).
-    static int HexTetVert[10][4] = {{0,1,3,4}, {1,2,0,5}, {2,3,1,6}, {3,0,2,7},
-				    {4,7,5,0}, {5,4,6,1}, {6,5,7,2}, {7,6,4,3},
-				    {0,2,7,5}, {1,3,4,6}};
+    static const int HexTetVert[10][4] = {{0,1,3,4}, {1,2,0,5}, {2,3,1,6}, 
+					  {3,0,2,7}, {4,7,5,0}, {5,4,6,1}, 
+					  {6,5,7,2}, {7,6,4,3}, {0,2,7,5}, 
+					  {1,3,4,6}};
   
   } // close namespace Amanzi
 
