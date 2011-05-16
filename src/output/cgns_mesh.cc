8af8f1b4d6de7c2dfe2443eadb00382862e3e41a
Rao Garimella <rao@lanl.gov>
2011-05-16 05:17:37 -0600

    NODE from the AmanziMesh namespace was clashing with a symbol from another namespace. Removed 'using namespace Amanzi' and 'using namespace AmanziMesh' and directly referenced NODE, CELL and OWNED symbols

--- src/output/cgns_mesh.cc
+++ src/output/cgns_mesh.cc
@@ -16,10 +16,6 @@ int num_soln = 0;   // number of solution nodes
 std::vector<double> timeList;
 std::vector<string> nameList;
 
-using namespace Amanzi;
-using namespace AmanziMesh;
-using namespace AmanziGeometry;
-
 namespace CGNS {
 
     
@@ -39,8 +35,8 @@ void create_mesh_file(Mesh &mesh_maps, std::string filename)
 	cg_base_write(file_idx, "Base", icelldim, iphysdim, &base_idx);
 			
 	// get num_nodes, num_cells
-	unsigned int num_nodes = mesh_maps.num_entities(NODE,OWNED);
-	unsigned int num_cells = mesh_maps.num_entities(CELL,OWNED);
+	unsigned int num_nodes = mesh_maps.num_entities(Amanzi::AmanziMesh::NODE,Amanzi::AmanziMesh::OWNED);
+	unsigned int num_cells = mesh_maps.num_entities(Amanzi::AmanziMesh::CELL,Amanzi::AmanziMesh::OWNED);
 	
 	// create zone
 	isize[0][0] = num_nodes;
@@ -139,7 +135,7 @@ void open_data_file(const std::string soln_filename)
 	
     }
     
-void create_timestep(const double time, const int iter, Entity_kind kind)
+  void create_timestep(const double time, const int iter, Amanzi::AmanziMesh::Entity_kind kind)
     {
 	int size[2];
 	std::stringstream solname;
@@ -178,9 +174,9 @@ void create_timestep(const double time, const int iter, Entity_kind kind)
 	cg_array_write("SolutionPointers",Character,2,size,solutionList);
 	
 	// create solution node under zone
-	if (kind == CELL) {
+	if (kind == Amanzi::AmanziMesh::CELL) {
 	    cg_sol_write(file_idx, base_idx,zone_idx, nameList.back().c_str(), CellCenter, &soln_idx);
-	} else if (kind == NODE) {
+	} else if (kind == Amanzi::AmanziMesh::NODE) {
 	    cg_sol_write(file_idx, base_idx,zone_idx, nameList.back().c_str(), Vertex, &soln_idx);
 	} else {
 	    //throw an error
