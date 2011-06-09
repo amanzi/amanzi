4ee29e989ca0f710fbc4150613525ab3e181b2f4
Ben Andre <bandre@lbl.gov>
2011-06-08 18:36:36 -0700

    chemistry: rename test directories

--- src/chemistry/tests_unit/test_chemistry_pk.cc
+++ src/chemistry/tests_unit/test_chemistry_pk.cc
@@ -56,7 +56,7 @@ SUITE(GeochemistryTestsChemistryPK) {
     // mesh/state related code....
 
     // get the parameter list from the input file
-    std::string xml_input_filename("test/test_chemistry_pk.xml");
+    std::string xml_input_filename("test_chemistry_pk.xml");
     Teuchos::ParameterList parameter_list;
     Teuchos::updateParametersFromXmlFile(xml_input_filename, &parameter_list);
 
