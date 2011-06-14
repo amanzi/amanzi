b044cdbdd42f18e4323a998b6be6fb31c9a3ab50
Lori A. Pritchett-Sheats <lpritch@lanl.gov>
2011-06-14 10:30:59 -0600

    refs #405
    Initial commit of the InstallManager CMake file
    Added AMNZI_INSTALL* variables in teh main CMakeLists.txt file
    Added new variables to the config report log

---
+++ tools/cmake/InstallManager.cmake
@@ -0,0 +1,44 @@
+# # -*- mode: cmake -*-
+
+#
+# Functions for managing the install targets
+#
+
+
+include(CMakeParseArguments)
+include(PrintVariable)
+
+#
+# Usage: ADD_INSTALL_INCLUDE_FILE( file1 file2 file3 ... )
+#
+# Arguments:
+#  A list of files that will be installed in the AMANZI_INSTALL_INCLUDE_DIR
+#
+# 
+function ( ADD_INSTALL_INCLUDE_FILE )
+
+end function( ADD_INSTALL_INCLUDE_FILE )
+
+#
+# Usage: ADD_INSTALL_LIBRARY( lib1 lib2 lib3 ... )
+#
+# Arguments:
+#  A list of libraries that will be installed in the AMANZI_INSTALL_LIB_DIR
+#
+# 
+function ( ADD_INSTALL_LIBRARY )
+
+end function( ADD_INSTALL_LIBRARY )
+
+#
+# Usage: ADD_INSTALL_BINARY( exe1 exe2 ... )
+#
+# Arguments:
+#  A list of executables that will be installed in the AMANZI_INSTALL_BIN_DIR
+#
+# 
+function ( ADD_INSTALL_BINARY )
+
+end function( ADD_INSTALL_BINARY )
+
+
