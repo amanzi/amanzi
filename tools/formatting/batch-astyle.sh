8bbc230daa03b35e191f9428c76c5534b6438a83
Michael Buksas <mwbuksas@lanl.gov>
2011-06-21 15:46:27 -0600

    Adding some alpha-specific style specifications for astyle and cpplint.

--- tools/formatting/batch-astyle.sh
+++ tools/formatting/batch-astyle.sh
@@ -1,13 +1,22 @@
+#!/usr/bin/env bash
+
 if [ -z "$1" ]; then
     echo  usage: $0 file-to-process file2 file3 ...
     exit 1
 fi
 
-astyle_file=`dirname $0`/astylerc
+astyle_file=`dirname $0`/astylerc.alpha
+cpplint_options_file=`dirname $0`/cpplint_options.alpha
+
+cpplint=`dirname $0`/../py_lib/cpplint/cpplint.py
+cpplint_command="${cpplint} --filter=`cat ${cpplint_options_file}`"
+
+echo $cpplint_command
 
 for file in $@; do
     echo Processing $file
     astyle --options=${astyle_file} $file
+    `${cpplint_command} $file`
 done
 
 
