8532b86f19fa851960876034f163c47b69c228d9
Michael Buksas <mwbuksas@lanl.gov>
2011-06-21 15:51:28 -0600

    Fixed help display. Removed echo.

--- tools/formatting/batch-astyle.sh
+++ tools/formatting/batch-astyle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-    echo  usage: $0 file-to-process file2 file3 ...
+    echo  "usage: `basename $0` <file> [file2] [file3] ..."
     exit 1
 fi
 
@@ -11,12 +11,11 @@ cpplint_options_file=`dirname $0`/cpplint_options.alpha
 cpplint=`dirname $0`/../py_lib/cpplint/cpplint.py
 cpplint_command="${cpplint} --filter=`cat ${cpplint_options_file}`"
 
-echo $cpplint_command
-
 for file in $@; do
     echo Processing $file
     astyle --options=${astyle_file} $file
     `${cpplint_command} $file`
+    echo
 done
 
 
