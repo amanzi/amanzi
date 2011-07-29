b90315859266537a343c7d09cbbffae75fa9705a
William A. Perkins <william.perkins@pnl.gov>
2011-07-29 09:58:56 -0700

    refs #475: give Mesh_STK a constructor that generates a mesh from a Teuchos::ParameterList

--- src/mesh/mesh_stk/Mesh_STK_hex.cc
+++ src/mesh/mesh_stk/Mesh_STK_hex.cc
@@ -1,10 +1,11 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
 // -------------------------------------------------------------
 // file: Mesh_STK.cc
 // -------------------------------------------------------------
 /**
  * @file   Mesh_STK.cc
  * @author William A. Perkins
- * @date Mon Jun 20 13:16:37 2011
+ * @date Fri Jul 29 09:45:54 2011
  * 
  * @brief  
  * 
@@ -12,7 +13,7 @@
  */
 // -------------------------------------------------------------
 // Created May  2, 2011 by William A. Perkins
-// Last Change: Mon Jun 20 13:16:37 2011 by William A. Perkins <d3g096@PE10900.pnl.gov>
+// Last Change: Fri Jul 29 09:45:54 2011 by William A. Perkins <d3g096@PE10900.pnl.gov>
 // -------------------------------------------------------------
 
 #include "Mesh_STK.hh"
@@ -28,8 +29,23 @@ namespace AmanziMesh {
 // -------------------------------------------------------------
 
 // -------------------------------------------------------------
-// Mesh_maps_stk::generate_hex_
+// Mesh_maps_stk::generate_
 // -------------------------------------------------------------
+
+void
+Mesh_STK::generate_(Data::HexMeshGenerator& g)
+{
+  Teuchos::RCP<Data::Data> meshdata(g.generate());
+  Teuchos::RCP<Epetra_Map> cmap(g.cellmap(true));
+  Teuchos::RCP<Epetra_Map> vmap(g.vertexmap(true));
+  stk::ParallelMachine pm(communicator_->GetMpiComm());
+  STK::Mesh_STK_factory mf(pm, 1000);
+  Data::Fields nofields;
+  mesh_.reset(mf.build_mesh(*meshdata, *cmap, *vmap, nofields));
+  build_maps_();
+  redistribute();
+}
+
 void
 Mesh_STK::generate_(const unsigned int& ni, const unsigned int& nj, const unsigned int& nk,
                     const double& xorigin, 
@@ -43,15 +59,60 @@ Mesh_STK::generate_(const unsigned int& ni, const unsigned int& nj, const unsign
                            ni, nj, nk,
                            xorigin, yorigin, zorigin,
                            xdelta, ydelta, zdelta);
-  Teuchos::RCP<Data::Data> meshdata(g.generate());
-  Teuchos::RCP<Epetra_Map> cmap(g.cellmap(true));
-  Teuchos::RCP<Epetra_Map> vmap(g.vertexmap(true));
-  stk::ParallelMachine pm(communicator_->GetMpiComm());
-  STK::Mesh_STK_factory mf(pm, 1000);
-  Data::Fields nofields;
-  mesh_.reset(mf.build_mesh(*meshdata, *cmap, *vmap, nofields));
-  build_maps_();
-  redistribute();
+  generate_(g);
+}
+
+void
+Mesh_STK::generate_(Teuchos::ParameterList &parameter_list)
+{
+  int nx, ny, nz;
+  double x0, y0, z0;
+  double x1, y1, z1;
+
+  // read the parameters from the parameter list
+
+  nx = parameter_list.get<int>("Numer of Cells in X");
+  ny = parameter_list.get<int>("Numer of Cells in Y");
+  nz = parameter_list.get<int>("Numer of Cells in Z");
+  
+  x0 = parameter_list.get<double>("X_Min", 0);
+  x1 = parameter_list.get<double>("X_Max", 1);
+
+  y0 = parameter_list.get<double>("Y_Min", 0);
+  y1 = parameter_list.get<double>("Y_Max", 1);
+
+  z0 = parameter_list.get<double>("Z_Min", 0);
+  z1 = parameter_list.get<double>("Z_Max", 1);
+
+  double xdelta((x1 - x0)/static_cast<double>(nx));
+  double ydelta((y1 - y0)/static_cast<double>(ny));
+  double zdelta((z1 - z0)/static_cast<double>(nz));
+
+
+  Data::HexMeshGenerator g(communicator_.get(), 
+                           nx, ny, nz,
+                           x0, y0, z0,
+                           xdelta, ydelta, zdelta);
+
+  int nblk;
+
+  nblk = parameter_list.get<int>("Number of mesh blocks",0);
+  
+  if (nblk > 0) {
+    for (int nb = 1; nb <= nblk; nb++) {
+      std::stringstream s; 
+      s << "Mesh block " << nb;
+
+      Teuchos::ParameterList sublist = parameter_list.sublist(s.str());
+      
+      double bz0 = sublist.get<double>("Z0");
+      double bz1 = sublist.get<double>("Z1");
+
+      // tell the generator about the zone
+    }
+  }
+  
+  generate_(g);
 }
 
 // -------------------------------------------------------------
@@ -93,6 +154,17 @@ Mesh_STK::Mesh_STK(const double x0, const double y0, const double z0,
             static_cast<unsigned int>(ny), 
             static_cast<unsigned int>(nz), 
             x0, y0, z0, xdelta, ydelta, zdelta);
+}
+
+Mesh_STK::Mesh_STK(Teuchos::ParameterList &parameter_list,
+                   Epetra_MpiComm *communicator)
+  : communicator_(new Epetra_MpiComm(*communicator)),
+    mesh_(), 
+    entity_map_ (3), 
+    map_owned_(), map_used_()
+{
+  Mesh::set_comm(communicator_->GetMpiComm());
+  generate_(parameter_list);
 }
 
 } // namespace AmanziMesh
