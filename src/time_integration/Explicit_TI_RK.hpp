a5dba2c2ff5b447d77fb57aa964b4ae3212c47ce
Markus Berndt <berndt@lanl.gov>
2011-09-02 14:03:38 -0600

    small correction to the documentation of the RK methods, refs #489

--- src/time_integration/Explicit_TI_RK.hpp
+++ src/time_integration/Explicit_TI_RK.hpp
@@ -30,9 +30,9 @@ namespace Explicit_TI {
     //  .     | a(2,0)    a(2,1)
     //  .     |   .         .        
     //  .     |   .         . 
-    // c[s-1 ]| a(s-1,0)  a(s-1,1)  . . .  a(s-1,s-1) 
-    // -----------------------------------------------
-    //        | b[0]      b[1]      . . .  b[s-1]
+    // c[s-1 ]| a(s-1,0)  a(s-1,1)  . . .  a(s-1,s-2) 
+    // ---------------------------------------------------------
+    //        |   b[0]      b[1]    . . .    b[s-2]      b[s-1] 
     // 
     // note that c[0] should always equal zero, and that the entries in the matrix
     // a that are not listed in this tableau are not used
@@ -49,7 +49,7 @@ namespace Explicit_TI {
     //      .
     //      .
     //      .
-    //     k_{s-1} = h * f(t_n + c[s-1]*h, y_n + a(s-1,0)*k_0 + ... + a(s-1,s-1)*k_{s-1})
+    //     k_{s-1} = h * f(t_n + c[s-1]*h, y_n + a(s-1,0)*k_0 + ... + a(s-1,s-2)*k_{s-2})
     //
 
   public:
