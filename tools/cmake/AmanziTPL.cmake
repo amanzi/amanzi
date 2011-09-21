7e7e3d784fc681700b20ce9f04966f210c61e9fd
Erin Barker <erin.barker@pnnl.gov>
2011-09-21 16:48:44 -0700

    Added HDF5 version check to build. refs #503.
    There were major changes to the HDF5 interface between version 1.6 and 1.8.  The output library takes advantage of the new changes and therefore must be build with 1.8 or newer.  FindHDF5.cmake now compares the version number of the HDF5 found to the minimum value passed from AmanziTPL.cmake of 1.8.0.

--- tools/cmake/AmanziTPL.cmake
+++ tools/cmake/AmanziTPL.cmake
@@ -36,7 +36,7 @@ if (${ADJUST_POLICY})
   cmake_policy(SET CMP0017 OLD)
 endif()
 
-find_package(HDF5 REQUIRED)
+find_package(HDF5 1.8.0 REQUIRED)
 if ( NOT HDF5_IS_PARALLEL ) 
     message(WARNING     "The HDF5 installation found in ${HDF5_DIR} is not "
                         "a parallel build. At this time, this installation "
