5953dbc6570067aa911b69cf5a2520c91151bd66
Konstantin Lipnikov <lipnikov@lanl.gov>
2011-10-06 12:17:34 -0600

    Verified second-order 2D transport. Upgraded the gmv output to support MultiVectors. Ipgraded the transport test. refs #532

--- src/transport/test/test_transport_2D.cpp
+++ src/transport/test/test_transport_2D.cpp
@@ -18,6 +18,7 @@ Author: Konstantin Lipnikov (lipnikov@lanl.gov)
 #include "MSTK_types.h"
 #include "Mesh_MSTK.hh"
 #include "MeshAudit.hh"
+#include "gmv_mesh.hh"
 
 #include "State.hpp"
 #include "Transport_PK.hpp"
@@ -26,11 +27,12 @@ Author: Konstantin Lipnikov (lipnikov@lanl.gov)
 /* **************************************************************** */
 TEST(ADVANCE_WITH_2D_MESH) {
   using namespace Teuchos;
+  using namespace Amanzi;
   using namespace Amanzi::AmanziMesh;
   using namespace Amanzi::AmanziTransport;
   using namespace Amanzi::AmanziGeometry;
 
-  std::cout << "=== TEST ADVANCE ===" << endl;
+  std::cout << "=== TEST ADVANCE in 2D ===" << endl;
 #ifdef HAVE_MPI
   Epetra_MpiComm  *comm = new Epetra_MpiComm(MPI_COMM_WORLD);
 #else
@@ -45,7 +47,7 @@ TEST(ADVANCE_WITH_2D_MESH) {
 
   /* create a transport state from the MPC state and populate it */
   RCP<Transport_State> TS = rcp(new Transport_State(mpc_state));
-  Point u(1.0, 0.0);
+  Point u(1.0, 1.0);
 
   TS->analytic_darcy_flux(u);
   TS->analytic_porosity();
@@ -66,7 +68,7 @@ TEST(ADVANCE_WITH_2D_MESH) {
   double T = 0.0;
   RCP<Transport_State> TS_next = TPK.get_transport_state_next();
 
-  RCP<Epetra_MultiVector> tcc      = TS->get_total_component_concentration();
+  RCP<Epetra_MultiVector> tcc = TS->get_total_component_concentration();
   RCP<Epetra_MultiVector> tcc_next = TS_next->get_total_component_concentration();
 
   iter = 0;
@@ -76,17 +78,23 @@ TEST(ADVANCE_WITH_2D_MESH) {
     T += dT;
     iter++;
 
-    if (iter < 10) {
+    if (iter < 15) {
       printf( "T=%6.2f  C_0(x):", T );
       for( int k=0; k<9; k++ ) {
-        int k1 = 9 - k;  // reflects cell numbering
+        int k1 = 9 - k;  // reflects cell numbering in the exodus file
         printf("%7.4f", (*tcc_next)[0][k1]); 
       }
       printf("\n");
     }
 
-    for( int k=0; k<8; k++ ) {
-      CHECK( ((*tcc_next)[0][k+1] - (*tcc_next)[0][k]) > -1e-15 );
+    //for( int k=0; k<8; k++ )
+    //  CHECK( ((*tcc_next)[0][k+1] - (*tcc_next)[0][k]) > -1e-15 );
+    if (iter == 15) {
+      GMV::open_data_file(*mesh, (std::string)"transport.gmv");
+      GMV::start_data();
+      GMV::write_cell_data(*tcc_next, 0, "component0");
+      GMV::write_cell_data(*tcc_next, 1, "component1");
+      GMV::close_data_file();
     }
 
     *tcc = *tcc_next;
@@ -94,7 +102,7 @@ TEST(ADVANCE_WITH_2D_MESH) {
 
   /* check that the final state is constant */
   for (int k=0; k<10; k++) 
-    CHECK_CLOSE((*tcc_next)[0][k], 1.0, 1e-6);
+    CHECK_CLOSE(1.0, (*tcc_next)[0][k], 1e-6);
 
   delete comm;
 }
