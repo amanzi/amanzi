d5e7827cd36003f04f18477ecd4fcb751947787c
Ethan Coon <ecoon@lanl.gov>
2012-02-09 12:05:11 -0700

    compiling fields for types scalar, constant vector, and vector (i.e. composite vector).  Must decide how we want to do i/o, as this changes Vis and Restart a bit (and removes subfield names from the generic Field)  refs #11

--- src/state/Field_ConstantVector.cc
+++ src/state/Field_ConstantVector.cc
@@ -14,71 +14,74 @@ Field also stores some basic metadata for Vis, checkpointing, etc.
 ------------------------------------------------------------------------- */
 
 #include "errors.hh"
-#include "CompositeVector.hh"
+#include "Epetra_Vector.h"
 #include "Field.hh"
-#include "Field_CV.hh"
+#include "Field_ConstantVector.hh"
 
 namespace Amanzi {
 
-Field_CV::Field_CV(std::string fieldname) :
+Field_ConstantVector::Field_ConstantVector(std::string fieldname) :
     Field::Field(fieldname, "state"), data_() {
-  type_ = VECTOR_FIELD;
+  type_ = CONSTANT_VECTOR;
 };
 
-Field_CV::Field_CV(std::string fieldname, std::string owner) :
+Field_ConstantVector::Field_ConstantVector(std::string fieldname, std::string owner) :
     Field::Field(fieldname, owner), data_() {
-  type_ = VECTOR_FIELD;
+  type_ = CONSTANT_VECTOR;
 };
 
-Field_CV::Field_CV(std::string fieldname, Teuchos::RCP<CompositeVector>& data) :
+Field_ConstantVector::Field_ConstantVector(std::string fieldname,
+        Teuchos::RCP<Epetra_Vector>& data) :
     Field::Field(fieldname, "state"), data_(data) {
-  type_ = VECTOR_FIELD;
+  type_ = CONSTANT_VECTOR;
 };
 
-Field_CV::Field_CV(std::string fieldname, std::string owner,
-                           Teuchos::RCP<CompositeVector>& data) :
+Field_ConstantVector::Field_ConstantVector(std::string fieldname, std::string owner,
+                           Teuchos::RCP<Epetra_Vector>& data) :
     Field::Field(fieldname, owner), data_(data) {
-  type_ = VECTOR_FIELD;
+  type_ = CONSTANT_VECTOR;
 };
 
 // copy constructor:
-Field_CV::Field_CV(const Field_CV& other) :
+Field_ConstantVector::Field_ConstantVector(const Field_ConstantVector& other) :
     Field::Field(other.fieldname_, other.owner_) {
   io_restart_ = other.io_restart_;
   io_vis_ = other.io_vis_;
   initialized_ = other.initialized_;
   type_ = other.type_;
-  data_ = Teuchos::rcp(new CompositeVector(*other.data_));
+  data_ = Teuchos::rcp(new Epetra_Vector(*other.data_));
 };
 
 // Virtual copy constructor
-Teuchos::RCP<Field> Field_CV::Clone() const {
-  return Teuchos::rcp(new Field_CV(*this));
+Teuchos::RCP<Field> Field_ConstantVector::Clone() const {
+  return Teuchos::rcp(new Field_ConstantVector(*this));
 }
 
 // Virtual copy constructor with non-empty name
-Teuchos::RCP<Field> Field_CV::Clone(std::string fieldname) const {
-  Teuchos::RCP<Field_CV> other = Teuchos::rcp(new Field_CV(*this));
+Teuchos::RCP<Field> Field_ConstantVector::Clone(std::string fieldname) const {
+  Teuchos::RCP<Field_ConstantVector> other = Teuchos::rcp(new Field_ConstantVector(*this));
   other->fieldname_ = fieldname;
   return other;
 };
 
-// Virtual copy constructor with non-empty name
-Teuchos::RCP<Field> Field_CV::Clone(std::string fieldname, std::string owner) const {
-  Teuchos::RCP<Field_CV> other = Teuchos::rcp(new Field_CV(*this));
+// Virtual copy constructor with non-empty name and new owner
+Teuchos::RCP<Field> Field_ConstantVector::Clone(std::string fieldname,
+        std::string owner)  const {
+  Teuchos::RCP<Field_ConstantVector> other = Teuchos::rcp(new Field_ConstantVector(*this));
   other->fieldname_ = fieldname;
   other->owner_ = owner;
   return other;
 };
 
 // write-access to the data
-Teuchos::RCP<CompositeVector> Field_CV::vector_data(std::string pk_name) {
+Teuchos::RCP<Epetra_Vector> Field_ConstantVector::constant_vector_data(std::string pk_name) {
   assert_owner_or_die_(pk_name);
   return data_;
 };
 
 // Overwrite data by pointer, not copy
-void Field_CV::set_data(std::string pk_name, Teuchos::RCP<CompositeVector>& data) {
+void Field_ConstantVector::set_data(std::string pk_name,
+        Teuchos::RCP<Epetra_Vector>& data) {
   assert_owner_or_die_(pk_name);
   data_ = data;
 };
