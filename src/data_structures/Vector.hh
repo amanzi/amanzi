6123db664bbb09b2edb051916ff3fd5ed23d8651
Ethan Coon <ecoon@lanl.gov>
2012-02-13 18:02:59 -0700

    TreeVector now uses a single CompositeVector instead of a datum_ vector of Epetra vectors for its leaf nodes

--- src/data_structures/Vector.hh
+++ src/data_structures/Vector.hh
@@ -8,7 +8,7 @@ Author: Ethan Coon
 Purely virtual interface for an ATS Vector.  Provides basic vector
 functionality including scalar operations and an inner product.
 Implementations of this interface may be used within ATS time integrators and
-nonlinear solvers (eventually).
+nonlinear solvers (eventually through templating?).
 ------------------------------------------------------------------------- */
 
 #ifndef DATA_STRUCTURES_VECTOR_HH_
@@ -22,35 +22,28 @@ class Vector{
   // base class inherited by all Vectors used in Amanzi
 public:
 
-  // set data
-  //  - guaranteed to not error
-  virtual Vector& operator=(double value) = 0;
+  // set values
+  virtual int PutScalar(double scalar) = 0;
 
-  //  - not guaranteed not to error
-  virtual Vector& operator=(const Epetra_Vector &value) = 0;
-  virtual Vector& operator=(const Epetra_MultiVector &value) = 0;
-
-  // metadata
-  virtual void SetName(std::string name) = 0;
-  virtual std::string Name() = 0;
+  // norms
+  virtual int NormInf(double* result) const;
+  //virtual int Norm1(double* result) const;
+  //virtual int Norm2(double* result) const;
 
   // scalar operations
-  virtual void Scale(double value) = 0;
-  virtual void Shift(double value) = 0;
+  virtual int Scale(double value) = 0;
+  virtual int Shift(double value) = 0;
 
-  // this <- alpha*a + beta*b + gamma*this
-  virtual Vector& Update(double scalarA, const Vector& A, double scalarThis) = 0;
-  virtual Vector& Update(double scalarA, const Vector& A,
-                         double scalarB, const Vector& B, double scalarThis) = 0;
+  // The following operations must exist, but need not support other vectors
+  // of different types.
 
-  virtual int Multiply (double scalarA, const Vector& A, const Vector& B, double scalarThis) =0;
-  virtual int PutScalar(double scalar) = 0;
-  virtual int NormInf(double* ninf) const = 0;
+  // virtual int Dot(const Vector& other, double* result) const = 0;
+  // virtual Vector& Update(double scalarA, const Vector& A, double scalarThis) = 0;
+  // virtual Vector& Update(double scalarA, const Vector& A,
+  //                        double scalarB, const Vector& B, double scalarThis) = 0;
+  // virtual int Multiply(double scalarA, const Vector& A, const Vector& B,
+  //                      double scalarThis) = 0;
 
-  // note that a Vector must also provide an innerProduct method, but it need
-  // not work if the other Vector is not of the same type.  Implementations
-  // likely use dynamic_cast.
-  virtual int Dot(const Vector& other, double* result) const = 0;
 };
 } // namespace
 
