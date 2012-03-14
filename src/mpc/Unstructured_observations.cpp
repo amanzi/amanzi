d1b5cd7ac24806f26cc6c34dac3c63b40129c337
Markus Berndt <berndt@lanl.gov>
2012-03-14 11:34:27 -0600

    make unstructured observations spec compliant

--- src/mpc/Unstructured_observations.cpp
+++ src/mpc/Unstructured_observations.cpp
@@ -31,10 +31,10 @@ namespace Amanzi {
 	    Teuchos::Array<double> times = observable_plist.get<Teuchos::Array<double> >("Values", empty);
 	    
 	    // loop over all variables listed and create an observable for each
-	    Teuchos::Array<std::string> vars = observable_plist.get<Teuchos::Array<string> >("Variables");
+	    std::string var = observable_plist.get<string>("Variable");
 	    observations.insert(std::pair
 				<std::string, Observable>(observations_plist_.name(i), 
-							  Observable(vars,
+							  Observable(var,
 								     observable_plist.get<string>("Region"),
 								     observable_plist.get<string>("Functional"),
 								     times, sps)));
@@ -51,90 +51,72 @@ namespace Amanzi {
     // loop over all observables
     for (std::map<std::string, Observable>::iterator i = observations.begin();
 	 i != observations.end();
-	 i++)
-
-      {
-
-	// for now we can only observe Integrals and Values
-
-	if ( (i->second).functional != "Observation Data: Integral"  &&  
-	     (i->second).functional != "Observation Data: Point" )
-	  
-	  {
-	    Errors::Message m("Unstructured_observations: can only handle Functional == Observation Data: Integral, or Functional == Observation Data: Point");
-	    Exceptions::amanzi_throw(m); 
-	  }
-	
-	
-	std::string label = i->first;
-
-	// make sure that we need to make an observation now
-	if (observation_requested(state.get_time(), state.get_last_time(), (i->second).times, (i->second).sps))
-	  {
-	    // we need to make an observation for each variable in the observable
-	    for (int ii=0; ii<(i->second).variable.size(); ii++)
-	      {
-		std::string var = (i->second).variable[ii];
-
-		// data structure to store the observation
-		Amanzi::ObservationData::DataTriple data_triplet;
-		
-		// build the name of the observation
-		std::stringstream ss;
-		ss << label << ", " << var;
-
-		std::vector<Amanzi::ObservationData::DataTriple> &od = observation_data[ label ]; //ss.str() ];
-
-		if ((i->second).functional == "Observation Data: Integral")
-		  {
-		    if (var == "Water")
-		      {
-			data_triplet.value   = state.water_mass();
-		      }
-		  }
-
-		else if ((i->second).functional == "Observation Data: Point")
-		  {
-		    data_triplet.value   = state.point_value((i->second).region, var);
-		  }
-		
-		data_triplet.is_valid = true;
-		data_triplet.time = state.get_time();
-		
-		od.push_back(data_triplet);
-	      }
-	  }
+	 i++) {
+      
+      // for now we can only observe Integrals and Values
+      
+      if ( (i->second).functional != "Observation Data: Integral"  &&  
+	   (i->second).functional != "Observation Data: Point" )  {
+	Errors::Message m("Unstructured_observations: can only handle Functional == Observation Data: Integral, or Functional == Observation Data: Point");
+	Exceptions::amanzi_throw(m); 
       }
+      
+      
+      std::string label = i->first;
+      
+      // make sure that we need to make an observation now
+      if (observation_requested(state.get_time(), state.get_last_time(), (i->second).times, (i->second).sps)) {
+	// we need to make an observation for each variable in the observable
+	std::string var = (i->second).variable;
+	
+	// data structure to store the observation
+	Amanzi::ObservationData::DataTriple data_triplet;
+	
+	// build the name of the observation
+	std::stringstream ss;
+	ss << label << ", " << var;
+	
+	std::vector<Amanzi::ObservationData::DataTriple> &od = observation_data[ label ]; //ss.str() ];
+	
+	if ((i->second).functional == "Observation Data: Integral")  {
+	  if (var == "Water") {
+	    data_triplet.value   = state.water_mass();
+	  }
+	} else if ((i->second).functional == "Observation Data: Point") {
+	  data_triplet.value   = state.point_value((i->second).region, var);
+	}
+	
+	data_triplet.is_valid = true;
+	data_triplet.time = state.get_time();
+	
+	od.push_back(data_triplet);
+      }
+      
+    }
   } 
-
-  bool  Unstructured_observations::observation_requested(double time, double last_time, Teuchos::Array<double>& T, Teuchos::Array<double>& SPS)
-  {
+  
+  bool  Unstructured_observations::observation_requested(double time, double last_time, Teuchos::Array<double>& T, Teuchos::Array<double>& SPS) {
     
-    for (int i=0; i<T.size(); i++) 
-      {
-	if ( last_time < T[i] && T[i] <= time )
-	  {
-	    return true;
-	  }
+    for (int i=0; i<T.size(); i++) {
+      if ( last_time < T[i] && T[i] <= time ) {
+	return true;
       }
+    }
     
-    if (SPS.size() > 0)
-      {
-	if ( time >= SPS[0] && (SPS[2]==-1.0 || time <=SPS[2]) )
-	  {
-	    if (time == SPS[0]) return true;
-
-	    int n0 =  floor( (last_time - SPS[0])/SPS[1] );
-	    int n1 =  floor( (time - SPS[0])/SPS[1] );
-	    
-	    if (n0+1 == n1) return true;
-	  }
+    if (SPS.size() > 0) {
+      if ( time >= SPS[0] && (SPS[2]==-1.0 || time <=SPS[2]) ) {
+	if (time == SPS[0]) return true;
+	
+	int n0 =  floor( (last_time - SPS[0])/SPS[1] );
+	int n1 =  floor( (time - SPS[0])/SPS[1] );
+	
+	if (n0+1 == n1) return true;
       }
+    }
     
-    return false;
-
+    return false;  
   }
-
+  
 
 
 }
