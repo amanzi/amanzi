0ee6b3579b010f0ea4980f79d83888f80b870839
Rao Garimella <rao@lanl.gov>
2012-04-30 10:04:04 -0600

    Fixed bug in face_normal where code was returning centroid instead of the normal (applicable to dynamically changing meshes only)

--- src/mesh/Mesh.cc
+++ src/mesh/Mesh.cc
@@ -256,7 +256,7 @@ AmanziGeometry::Point Mesh::face_normal (const Entity_ID faceid, const bool reco
       double area;
       AmanziGeometry::Point centroid(spacedim), normal(spacedim);
       compute_face_geometry(faceid, &area, &centroid, &normal);
-      return centroid;
+      return normal;
     }
     else
       return face_normals[faceid];
