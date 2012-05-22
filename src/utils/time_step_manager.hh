35fa49105d76651fc0c3faf5c4a238037cb9335d
Markus Berndt <berndt@lanl.gov>
2012-05-22 11:55:51 -0600

    complete time step manager, refs #731

--- src/utils/time_step_manager.hh
+++ src/utils/time_step_manager.hh
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <list>
+#include <ostream>
 
 namespace Amanzi {
 
@@ -13,19 +14,26 @@ class TimeStepManager {
     TimeEvent() {}
     TimeEvent(double start, double period, double stop)
         : start_(start), period_(period), stop_(stop), type_(SPS) {}
-    TimeEvent(const std::vector<double>& cycles) 
-        : cycles_(cycles), type_(TIMES) {}
-    TimeType Type() const { return type_; }
+    TimeEvent(const std::vector<double>& times)
+        : times_(times), type_(TIMES) {}
+    explicit TimeEvent(double time) : type_(TIMES) {
+      times_.push_back(time);
+    }
+    TimeType Type() const {
+      return type_;
+    }
 
     double start_, period_, stop_;
-    std::vector<double> cycles_;
+    std::vector<double> times_;
     TimeType type_;
-  };  
+  };
 
  public:
   void RegisterTimeEvent(double start, double period, double stop);
   void RegisterTimeEvent(const std::vector<double>& cycles);
-  double TimeStep(const double T, const double dT);
+  void RegisterTimeEvent(double time);
+  double TimeStep(const double T, const double dT) const;
+  void print(std::ostream& os, double start, double end) const;
 
  private:
   std::list<TimeEvent> timeEvents_;
