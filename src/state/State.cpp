52349f41e1b729a056828fb6b66c4d1128b14e69
Rao Garimella <rao@lanl.gov>
2012-05-23 10:55:57 -0600

    Changed all 'unsigned int' references to entities and sets to Entity_ID and Set_ID respectively

--- src/state/State.cpp
+++ src/state/State.cpp
@@ -263,7 +263,6 @@ void State::initialize_from_parameter_list()
       }
       set_total_component_concentration(tcc_const, region);
       set_free_ion_concentrations(free_ion_guess, region);
-      // activity coefficients...
       SetRegionMaterialChemistry(region, &sublist);
     }
   }
@@ -394,15 +393,12 @@ void State::update_darcy_flux(const Epetra_Vector &new_darcy_flux)
   *darcy_flux = new_darcy_flux;
 }
 
+
 void State::update_pressure(const Epetra_Vector &new_pressure)
 {
   *pressure = new_pressure;
 }
 
-void State::update_lambda(const Epetra_Vector &new_lambda)
-{
-  *lambda = new_lambda;
-}
 
 /* *******************************************************************/
 void State::advance_time(double dT)
@@ -424,7 +420,7 @@ void State::set_cell_value_in_region(const double& value, Epetra_Vector& v,
                                                          Amanzi::AmanziMesh::CELL,
                                                          Amanzi::AmanziMesh::OWNED);
 
-  std::vector<unsigned int> cell_ids(mesh_block_size);
+  Amanzi::AmanziMesh::Entity_ID_List cell_ids(mesh_block_size);
 
   //mesh_maps->get_set(mesh_block_id, Amanzi::AmanziMesh::CELL, Amanzi::AmanziMesh::OWNED,
   //cell_ids.begin(),cell_ids.end());
@@ -432,7 +428,7 @@ void State::set_cell_value_in_region(const double& value, Epetra_Vector& v,
   mesh_maps->get_set_entities(region, Amanzi::AmanziMesh::CELL, Amanzi::AmanziMesh::OWNED,
                               &cell_ids);
 
-  for (std::vector<unsigned int>::iterator c = cell_ids.begin(); c != cell_ids.end(); c++) {
+  for (Amanzi::AmanziMesh::Entity_ID_List::iterator c = cell_ids.begin(); c != cell_ids.end(); c++) {
     v[*c] = value;
   }
 }
@@ -451,12 +447,12 @@ void State::set_cell_value_in_region(const Epetra_Vector& x, Epetra_Vector& v,
                                                          Amanzi::AmanziMesh::CELL,
                                                          Amanzi::AmanziMesh::OWNED);
 
-  std::vector<unsigned int> cell_ids(mesh_block_size);
+  Amanzi::AmanziMesh::Entity_ID_List cell_ids(mesh_block_size);
 
   mesh_maps->get_set_entities(region, Amanzi::AmanziMesh::CELL, Amanzi::AmanziMesh::OWNED,
                               &cell_ids);
 
-  for(std::vector<unsigned int>::iterator c = cell_ids.begin(); c != cell_ids.end(); c++) {
+  for(Amanzi::AmanziMesh::Entity_ID_List::iterator c = cell_ids.begin(); c != cell_ids.end(); c++) {
     v[*c] = x[*c];
   }
 }
@@ -474,11 +470,11 @@ void State::set_cell_value_in_region(const Amanzi::Function& fun, Epetra_Vector&
   unsigned int mesh_block_size = mesh_maps->get_set_size(region,
                                                          Amanzi::AmanziMesh::CELL,
                                                          Amanzi::AmanziMesh::OWNED);
-  std::vector<unsigned int> cell_ids(mesh_block_size);
+  Amanzi::AmanziMesh::Entity_ID_List cell_ids(mesh_block_size);
   mesh_maps->get_set_entities(region, Amanzi::AmanziMesh::CELL, Amanzi::AmanziMesh::OWNED,
                               &cell_ids);
 
-  for( std::vector<unsigned int>::iterator c = cell_ids.begin(); c != cell_ids.end();  c++) {
+  for( Amanzi::AmanziMesh::Entity_ID_List::iterator c = cell_ids.begin(); c != cell_ids.end();  c++) {
     const Amanzi::AmanziGeometry::Point& p = mesh_maps->cell_centroid(*c);
     v[*c] = fun(&p[0]);
   }
@@ -497,12 +493,12 @@ void State::set_cell_value_in_mesh_block(double value, Epetra_Vector &v,
                                                          Amanzi::AmanziMesh::CELL,
                                                          Amanzi::AmanziMesh::OWNED);
 
-  std::vector<unsigned int> cell_ids(mesh_block_size);
+  Amanzi::AmanziMesh::Entity_ID_List cell_ids(mesh_block_size);
 
   mesh_maps->get_set(mesh_block_id, Amanzi::AmanziMesh::CELL, Amanzi::AmanziMesh::OWNED,
                      cell_ids.begin(),cell_ids.end());
 
-  for( std::vector<unsigned int>::iterator c = cell_ids.begin();
+  for( Amanzi::AmanziMesh::Entity_ID_List::iterator c = cell_ids.begin();
        c != cell_ids.end();  c++) {
     v[*c] = value;
   }
@@ -524,12 +520,12 @@ void State::set_darcy_flux(const double* u, const int mesh_block_id)
   int dim = mesh_maps->space_dimension();
   Amanzi::AmanziMesh::Entity_ID_List faces;
   std::vector<int> dirs;
-  std::vector<unsigned int> cell_ids(mesh_block_size);
+  Amanzi::AmanziMesh::Entity_ID_List cell_ids(mesh_block_size);
 
   mesh_maps->get_set(mesh_block_id, Amanzi::AmanziMesh::CELL, Amanzi::AmanziMesh::OWNED,
                      cell_ids.begin(),cell_ids.end());
 
-  for (std::vector<unsigned int>::iterator c = cell_ids.begin(); c != cell_ids.end(); c++) {
+  for (Amanzi::AmanziMesh::Entity_ID_List::iterator c = cell_ids.begin(); c != cell_ids.end(); c++) {
     mesh_maps->cell_get_faces_and_dirs(*c, &faces, &dirs);
     int nfaces = faces.size();
 
@@ -560,12 +556,12 @@ void State::set_darcy_flux(const double* u, const std::string region)
   int dim = mesh_maps->space_dimension();
   Amanzi::AmanziMesh::Entity_ID_List faces;
   std::vector<int> dirs;
-  std::vector<unsigned int> cell_ids(mesh_block_size);
+  Amanzi::AmanziMesh::Entity_ID_List cell_ids(mesh_block_size);
 
   mesh_maps->get_set_entities(region, Amanzi::AmanziMesh::CELL, Amanzi::AmanziMesh::OWNED,
                               &cell_ids);
 
-  for( std::vector<unsigned int>::iterator c = cell_ids.begin(); c != cell_ids.end(); c++) {
+  for( Amanzi::AmanziMesh::Entity_ID_List::iterator c = cell_ids.begin(); c != cell_ids.end(); c++) {
     mesh_maps->cell_get_faces_and_dirs(*c, &faces, &dirs);
     int nfaces = faces.size();
 
@@ -766,7 +762,7 @@ double State::point_value(const std::string& point_region, const std::string& na
   double value(0.0);
   double volume(0.0);
 
-  std::vector<unsigned int> cell_ids(mesh_block_size);
+  Amanzi::AmanziMesh::Entity_ID_List cell_ids(mesh_block_size);
 
   mesh_maps->get_set_entities(point_region, Amanzi::AmanziMesh::CELL, Amanzi::AmanziMesh::OWNED,
                               &cell_ids);
@@ -839,10 +835,6 @@ void State::set_water_saturation(const Epetra_Vector& water_saturation_)
   *water_saturation = water_saturation_;
 };
 
-void State::set_prev_water_saturation(const Epetra_Vector& prev_water_saturation_)
-{
-  *prev_water_saturation = prev_water_saturation_;
-};
 
 void State::set_porosity(const Epetra_Vector& porosity_)
 {
