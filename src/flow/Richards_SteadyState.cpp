d0639f5f3ad09eb6aec4df8a563bd8ae7e3f7af9
Konstantin Lipnikov <lipnikov@lanl.gov>
2012-05-29 22:49:32 -0600

    Ooopss. Reverted changes made to Picard time integrator.

--- src/flow/Richards_SteadyState.cpp
+++ src/flow/Richards_SteadyState.cpp
@@ -145,21 +145,22 @@ int Richards_PK::AdvanceSteadyState_Picard()
   solver->SetAztecOption(AZ_output, AZ_none);
   solver->SetAztecOption(AZ_conv, AZ_rhs);
 
-  // update boundary conditions
+  // update steady state boundary conditions
   double time = 0.0;
   bc_pressure->Compute(time);
   bc_flux->Compute(time);
   bc_head->Compute(time);
-  bc_seepage->Compute(time);
-  ProcessBoundaryConditions(
-      bc_pressure, bc_head, bc_flux, bc_seepage,
-      *solution_faces, atm_pressure,
-      bc_markers, bc_values);
 
   int itrs = 0;
   double L2norm, L2error = 1.0;
 
   while (L2error > residual_tol_sss && itrs < max_itrs_sss) {
+    // update dynamic boundary conditions
+    bc_seepage->Compute(time);
+    ProcessBoundaryConditions(
+        bc_pressure, bc_head, bc_flux, bc_seepage,
+        *solution_faces, atm_pressure,
+        bc_markers, bc_values);
 
     if (!is_matrix_symmetric) {
       CalculateRelativePermeabilityFace(*solution_cells);
