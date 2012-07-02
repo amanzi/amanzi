e01d23c3da08b3844d44d472d3cdf553b52eab19
Ethan Coon <ecoon@lanl.gov>
2012-07-02 13:03:33 -0600

    adding elevation and slope based upon the Amanzi Mesh class of the volume/3D surface mesh

--- src/pks/flow/overland/overland_physics.cc
+++ src/pks/flow/overland/overland_physics.cc
@@ -1,13 +1,14 @@
 /* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
 
-/*
-  A base two-phase, thermal Richard's equation with water vapor.
+/* -----------------------------------------------------------------------------
+This is the overland flow component of ATS.
+License: BSD
+Authors: Gianmarco Manzini
+         Ethan Coon (ecoon@lanl.gov)
+----------------------------------------------------------------------------- */
 
-  License: BSD
-  Authors: Neil Carlson (version 1)
-  Konstantin Lipnikov (version 2) (lipnikov@lanl.gov)
-  Ethan Coon (ATS version) (ecoon@lanl.gov)
-*/
+#include "Mesh.hh"
+#include "Mesh_MSTK.hh"
 
 #include "overland.hh"
 
@@ -158,6 +159,125 @@ void OverlandFlow::Conductivity_(const Teuchos::RCP<State>& S,
     (*conductivity)("cell",c) = std::pow(pressure("cell",c), exponent) / scaling;
   }
 };
+
+
+// -----------------------------------------------------------------------------
+// Update the elevation of the surface mesh and its slope.
+// -----------------------------------------------------------------------------
+void OverlandFlow::UpdateElevationAndSlope_(const Teuchos::RCP<State>& S) {
+  Teuchos::RCP<CompositeVector> elev = S->GetFieldData("elevation", "overland_flow");
+  Teuchos::RCP<CompositeVector> slope = S->GetFieldData("slope_magnitude", "overland_flow");
+
+  if (standalone_mode_) {
+    // No domain mesh, so get the elevation and slope values from functions.
+    elevation_function_->Compute(S->time(), elev.ptr());
+    slope_function_->Compute(S->time(), slope.ptr());
+
+  } else {
+    // Get the elevation and slope values from the domain mesh.
+    Teuchos::RCP<const AmanziMesh::Mesh_MSTK> domain_mesh =
+      Teuchos::rcp_static_cast<const AmanziMesh::Mesh_MSTK>(S->Mesh());
+                    // Note that static cast is safe here because we have
+                    // already ensured it was MSTK.
+
+    Teuchos::RCP<const AmanziMesh::Mesh_MSTK> surface_mesh =
+      Teuchos::rcp_static_cast<const AmanziMesh::Mesh_MSTK>(S->Mesh("surface"));
+                    // Note that static cast is safe here because we have
+                    // already ensured it was MSTK.
+
+    if (domain_mesh->cell_dimension() == 3) {
+      // Set the elevation on cells by getting the corresponding face and its
+      // centroid.
+      int ncells = elev->size("cell");
+      for (int c=0; c!=ncells; ++c) {
+        // Note that a surface cell is a volume mesh's face
+        AmanziMesh::Entity_ID domain_face =
+          surface_mesh->entity_get_parent(AmanziMesh::CELL, c);
+
+        // First elevation.
+        AmanziGeometry::Point x = domain_mesh->face_centroid(domain_face);
+        (*elev)("cell",c) = x[2];
+
+        // Now slope.
+        AmanziGeometry::Point n = domain_mesh->face_normal(domain_face);
+        // -- S = || n - (n dot z) z || / | n dot z |
+        (*slope)("cell",c) = std::sqrt( std::pow(n[0],2) + std::pow(n[1],2))
+                                          / std::abs(n[2]);
+      }
+
+      // Set the elevation on faces by getting the corresponding nodes and
+      // averaging.
+      AmanziMesh::Entity_ID_List surface_nodes(2);
+      AmanziMesh::Entity_ID node0, node1;
+      AmanziGeometry::Point coord0(3);
+      AmanziGeometry::Point coord1(3);
+
+      int nfaces = elev->size("face");
+      for (int f=0; f!=nfaces; ++f) {
+        surface_mesh->face_get_nodes(f, &surface_nodes);
+        node0 = surface_mesh->entity_get_parent(AmanziMesh::NODE, surface_nodes[0]);
+        node1 = surface_mesh->entity_get_parent(AmanziMesh::NODE, surface_nodes[1]);
+
+        domain_mesh->node_get_coordinates(node0, &coord0);
+        domain_mesh->node_get_coordinates(node1, &coord1);
+        (*elev)("face",f) = (coord0[2] + coord1[2])/2.0;
+      }
+    } else if (domain_mesh->cell_dimension() == 2) {
+      // Set the elevation on cells by getting the corresponding cell and its
+      // centroid.
+      int ncells = elev->size("cell");
+      for (int c=0; c!=ncells; ++c) {
+        // Note that a surface cell is a surface mesh's cell.
+        AmanziMesh::Entity_ID domain_cell =
+          surface_mesh->entity_get_parent(AmanziMesh::CELL, c);
+
+        // First elevation.
+        AmanziGeometry::Point x = domain_mesh->cell_centroid(domain_cell);
+        (*elev)("cell",c) = x[2];
+
+        // Now slope.
+        AmanziMesh::Entity_ID_List faces;
+        domain_mesh->cell_get_faces(domain_cell, &faces);
+
+        // -- Get the normals of all faces of the surface cell.
+        int count = faces.size();
+        std::vector<AmanziGeometry::Point> normals(count);
+        for (int lcv=0; lcv!=count; ++lcv) {
+          normals[lcv] = domain_mesh->face_normal(faces[lcv], false, domain_cell);
+        }
+
+        // -- Average the cross product of successive faces to get a cell normal.
+        AmanziGeometry::Point cross(0.0, 0.0, 0.0);
+        for (int lcv=0; lcv!=(count-1); ++lcv) {
+          cross[0] += normals[lcv][1]*normals[lcv+1][2] - normals[lcv][2]*normals[lcv+1][1];
+          cross[1] += normals[lcv][2]*normals[lcv+1][0] - normals[lcv][0]*normals[lcv+1][2];
+          cross[2] += normals[lcv][0]*normals[lcv+1][1] - normals[lcv][1]*normals[lcv+1][0];
+        }
+        cross[0] += normals[count][1]*normals[0][2] - normals[count][2]*normals[0][1];
+        cross[1] += normals[count][2]*normals[0][0] - normals[count][0]*normals[0][2];
+        cross[2] += normals[count][0]*normals[0][1] - normals[count][1]*normals[0][0];
+        cross /= count;
+
+        // -- S = || n - (n dot z) z || / | n dot z |
+        (*slope)("cell",c) = std::sqrt( std::pow(cross[0],2) + std::pow(cross[1],2))
+                                          / std::abs(cross[2]);
+      }
+
+      // Set the elevation on faces by getting the corresponding face and its
+      // centroid.
+      int nfaces = elev->size("face");
+      for (int f=0; f!=nfaces; ++f) {
+        // Note that a surface face is a surface mesh's face.
+        AmanziMesh::Entity_ID domain_face =
+          surface_mesh->entity_get_parent(AmanziMesh::FACE, f);
+        AmanziGeometry::Point x = domain_mesh->face_centroid(domain_face);
+
+        (*elev)("face",f) = x[2];
+      }
+    }
+  }
+};
+
 
 } //namespace
 } //namespace
