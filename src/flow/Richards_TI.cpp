51b175dd6ed2799eb9cd042f3835231ca7a46eca
Konstantin Lipnikov <lipnikov@lanl.gov>
2012-07-30 23:06:41 -0600

    Fixed violation of Codying Standards.

--- src/flow/Richards_TI.cpp
+++ src/flow/Richards_TI.cpp
@@ -26,9 +26,8 @@ namespace AmanziFlow {
 ****************************************************************** */
 void Richards_PK::fun(
     double Tp, const Epetra_Vector& u, const Epetra_Vector& udot, Epetra_Vector& f, double dTp)
-{
-        
-   ComputePreconditionerMFD(u, matrix_, Tp, 0.0, false);  // Calculate only stiffness matrix.
+{      
+  ComputePreconditionerMFD(u, matrix_, Tp, 0.0, false);  // Calculate only stiffness matrix.
   matrix_->ComputeNegativeResidual(u, f);  // compute A*u - g
 
   int ncells = mesh_->num_entities(AmanziMesh::CELL, AmanziMesh::OWNED);
