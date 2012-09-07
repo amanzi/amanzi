6f69bbea75a303a972d9c0d870f15b34c6766e22
Ethan Coon <ecoon@lanl.gov>
2012-09-07 17:59:15 -0600

    adding files for bleeding edge coupling

---
+++ src/pks/flow/overland/source_from_subsurface_evaluator.hh
@@ -0,0 +1,52 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+
+/*
+  An evaluator for pulling the darcy flux, at the surface, from the
+  subsurface field and putting it into a surface field.
+
+  Authors: Ethan Coon (ecoon@lanl.gov)
+*/
+
+#ifndef AMANZI_FLOWRELATIONS_SOURCE_FROM_SUBSURFACE_EVALUATOR_HH_
+#define AMANZI_FLOWRELATIONS_SOURCE_FROM_SUBSURFACE_EVALUATOR_HH_
+
+#include "secondary_variable_field_evaluator.hh"
+
+namespace Amanzi {
+namespace Flow {
+namespace FlowRelations {
+
+class SourceFromSubsurfaceEvaluator :
+    public SecondaryVariableFieldEvaluator {
+
+ public:
+  explicit
+  SourceFromSubsurfaceEvaluator(Teuchos::ParameterList& plist);
+
+  SourceFromSubsurfaceEvaluator(const SourceFromSubsurfaceEvaluator& other);
+
+  Teuchos::RCP<FieldEvaluator> Clone() const;
+
+protected:
+  // Required methods from SecondaryVariableFieldEvaluator
+  virtual void EvaluateField_(const Teuchos::Ptr<State>& S,
+          const Teuchos::Ptr<CompositeVector>& result);
+  virtual void EvaluateFieldPartialDerivative_(const Teuchos::Ptr<State>& S,
+          Key wrt_key, const Teuchos::Ptr<CompositeVector>& result);
+
+  void IdentifyFaceAndDirection_(const Teuchos::Ptr<State>& S);
+
+  typedef std::pair<int, int> FaceDir;
+  Teuchos::RCP<std::vector<FaceDir> > face_and_dirs_;
+  Key flux_key_;
+  Key density_key_;
+
+  Key surface_mesh_key_;
+  Key subsurface_mesh_key_;
+};
+
+} //namespace
+} //namespace
+} //namespace
+
+#endif
