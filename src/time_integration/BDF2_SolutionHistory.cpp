7f7eb9b450c075cd64aa70a58906582e298ba33f
Markus Berndt <berndt@lanl.gov>
2012-09-11 13:11:24 -0600

    protect against division by zero, refs #629

--- src/time_integration/BDF2_SolutionHistory.cpp
+++ src/time_integration/BDF2_SolutionHistory.cpp
@@ -1,14 +1,15 @@
 #include "BDF2_SolutionHistory.hpp"
 #include "dbc.hh"
+#include "errors.hh"
 
 namespace BDF2 {
-  
+
 SolutionHistory::SolutionHistory(int mvec, const Epetra_BlockMap& map)
 {
   ASSERT(mvec>0);
   initialize(mvec, map);
 }
-  
+
 
 SolutionHistory::SolutionHistory(int mvec, double t, const Epetra_Vector& x)
 {
@@ -17,10 +18,10 @@ SolutionHistory::SolutionHistory(int mvec, double t, const Epetra_Vector& x)
   record_solution(t,x);
 }
 
-  
+
 SolutionHistory::SolutionHistory(int mvec, double t, const Epetra_Vector& x, const Epetra_Vector& xdot)
 {
-  ASSERT(mvec>0);                     
+  ASSERT(mvec>0);
   // ASSERT(x.Map().SameAs(xdot.Map()));  // x and xdot have the same structure
 
   initialize(mvec, x.Map() );
@@ -35,12 +36,12 @@ void SolutionHistory::initialize(int mvec, const Epetra_BlockMap& map)
   nvec = 0;
   d.resize(mvec);
   times.resize(mvec);
-    
-  for (int j=0; j<mvec; j++) 
-    d[j] = Teuchos::rcp(new Epetra_Vector(map));     
+
+  for (int j=0; j<mvec; j++)
+    d[j] = Teuchos::rcp(new Epetra_Vector(map));
 }
 
-  
+
 void SolutionHistory::flush_history(double t, const Epetra_Vector& x)
 {
   nvec = 0;
@@ -63,14 +64,14 @@ void SolutionHistory::record_solution(double t, const Epetra_Vector& x)
   nvec++;
   if (nvec > d.size()) nvec = d.size();
 
-  // shift the times and vectors, 
+  // shift the times and vectors,
   // while storing the pointer to the last one
   Teuchos::RCP<Epetra_Vector> tmp = d[nvec-1];
   for (int j=nvec-1; j>=1; j--) {
     times[j] = times[j-1];
     d[j] = d[j-1];
   }
-    
+
   // insert the new vector
   times[0] = t;
   d[0] = tmp;
@@ -78,7 +79,11 @@ void SolutionHistory::record_solution(double t, const Epetra_Vector& x)
 
   // update the divided differences
   for (int j=1; j<=nvec-1; j++) {
-    double div = 1.0/(times[0] - times[j]); 
+    if (times[0] - times[j] == 0.0) {
+      Errors::Message message("SolutionHistory: Time step was too small.");
+      Exceptions::amanzi_throw(message);
+    }
+    double div = 1.0/(times[0] - times[j]);
     d[j]->Update(div,*d[j-1],-div);
   }
 }
@@ -89,26 +94,26 @@ void SolutionHistory::record_solution(double t, const Epetra_Vector& x, const Ep
   //ASSERT(x.Map().SameAs( xdot.Map() ) );
   //ASSERT(x.Map().SameAs( (*d[0]).Map() ) );
   //ASSERT(xdot.Map().SameAs( (*d[0]).Map() ) );
-    
+
   record_solution(t,x);
 
   // update the number of vectors
   nvec++;
   if (nvec > d.size()) nvec = d.size();
-    
+
   // shift the divided differences, except the first; the new vector and
   // time index are the same as the most recent.
   Teuchos::RCP<Epetra_Vector> tmp = d[nvec-1];
   for (int j=nvec-1; j>=2; j--) {
     times[j] = times[j-1];
     d[j] = d[j-1];
-  }    
+  }
 
   // the first divided difference (same time index) is the specified derivative.
   times[1] = times[0];
   d[1] = tmp;
   *d[1] = xdot;
-   
+
   // update the rest of the divided differences
   for (int j=2; j<=nvec-1; j++) {
     double div = 1.0/(times[0]-times[j]);
@@ -118,8 +123,8 @@ void SolutionHistory::record_solution(double t, const Epetra_Vector& x, const Ep
 
 
 void SolutionHistory::interpolate_solution(double t, Epetra_Vector& x)
-{  
-  //ASSERT(x.Map().SameAs( (*d[0]).Map() ) );    
+{
+  //ASSERT(x.Map().SameAs( (*d[0]).Map() ) );
 
   int order = nvec-1;
   interpolate_solution(t, x, order);
@@ -128,10 +133,10 @@ void SolutionHistory::interpolate_solution(double t, Epetra_Vector& x)
 
 void SolutionHistory::interpolate_solution(double t, Epetra_Vector& x, int order)
 {
-  //ASSERT(x.Map().SameAs( (*d[0]).Map() ) );    
+  //ASSERT(x.Map().SameAs( (*d[0]).Map() ) );
   ASSERT(order<nvec);
   ASSERT(order>0);
-    
+
   x = *d[order];
   for (int k=order-1; k>=0; k--) {
     x.Update(1.0,*d[k],t-times[k]);
@@ -144,7 +149,7 @@ void SolutionHistory::most_recent_solution(Epetra_Vector& x)
   //ASSERT(x.Map().SameAs( (*d[0]).Map() ) );
   x = *d[0];
 }
-  
+
 
 double SolutionHistory::most_recent_time()
 {
@@ -159,11 +164,11 @@ void SolutionHistory::time_deltas(std::vector<double>& h)
   for (int j=0; j<=nvec-2; j++)
     h[j] = times[0] - times[j+1];
 }
-  
+
 
 int SolutionHistory::history_size()
 {
   return nvec;
 }
-  
-}  // namespace BDF2 
+
+}  // namespace BDF2
