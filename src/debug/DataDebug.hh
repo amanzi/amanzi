d49be753a5aa327a9276468de0acc392dc44e0ad
Markus Berndt <berndt@lanl.gov>
2012-09-27 16:37:41 -0600

    add rudimentary data debug class, refs #894

---
+++ src/debug/DataDebug.hh
@@ -0,0 +1,26 @@
+#ifndef DATADEBUG_HH__
+#define DATADEBUG_HH__
+
+#include "Epetra_Vector.h"
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_VerboseObject.hpp"
+#include "Mesh.hh"
+
+namespace Amanzi {
+
+class DataDebug {
+
+ public:
+  
+  explicit DataDebug(Teuchos::RCP<AmanziMesh::Mesh> mesh);
+  ~DataDebug() {}
+
+  void write_region_data(std::string region_name, const Epetra_Vector& data, std::string description);
+  void write_region_statistics(std::string region_name, const Epetra_Vector& data, std::string description);  
+ private:
+  Teuchos::RCP<AmanziMesh::Mesh> mesh_;
+};
+
+}
+
+#endif
