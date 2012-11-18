6844e4e22caf63c2b56a4c41a1c8ed4b46a218d1
Rao Garimella <rao@lanl.gov>
2012-11-18 09:48:20 -0700

    Tried to incorporate mpicc --showme:compile options into the configuration. Commented out

--- config/examples/conejo-intel-acml-int32-config.cmake
+++ config/examples/conejo-intel-acml-int32-config.cmake
@@ -9,8 +9,8 @@
 # MPI: OpenMPI 1.4.3
 #
 # Usage:
-#   (1) Load the default Intel, OpenMPI and ACML modules
-#        module load intel/12.1.5 openmpi-intel acml-intel
+#   (1) Load the default Intel, OpenMPI module and ACML (not default) modules
+#        module load intel openmpi acml-intel
 #   (2) Configure 
 #       cmake -C <root path>/conejo-acml-int32-config.cmake <root directory amanzi>/amanzi/config/SuperBuild
 # 
@@ -26,8 +26,19 @@ set(CMAKE_BUILD_TYPE "Release" CACHE STRING "CMake build type")
 
 # --- Set the compilers
 set(CMAKE_C_COMPILER "${MPI_ROOT}/bin/mpicc" CACHE FILEPATH "MPI C compiler wrapper" FORCE)
+execute_process(COMMAND ${MPI_ROOT}/bin/mpicc --showme:compile OUTPUT_VARIABLE CFLAGS)
+#set(CMAKE_C_FLAGS ${CFLAGS} CACHE STRING "MPI C compile flags" FORCE)
+
 set(CMAKE_CXX_COMPILER "${MPI_ROOT}/bin/mpicxx" CACHE FILEPATH "MPI C++ compiler wrapper" FORCE)
+execute_process(COMMAND ${MPI_ROOT}/bin/mpicxx --showme:compile OUTPUT_VARIABLE CXXFLAGS)
+#set(CMAKE_CXX_FLAGS ${CXXFLAGS} CACHE STRING "MPI CXX compile flags" FORCE)
+
 set(CMAKE_Fortran_COMPILER "${MPI_ROOT}/bin/mpif90" CACHE FILEPATH "MPI Fortran compiler wrapper" FORCE)
+execute_process(COMMAND ${MPI_ROOT}/bin/mpif90 --showme:compile OUTPUT_VARIABLE F90FLAGS)
+#set(CMAKE_F90_FLAGS ${F90FLAGS} CACHE STRING "MPI F90 compile flags" FORCE)
+
+#execute_process(COMMAND ${MPI_ROOT}/bin/mpicxx --showme:link OUTPUT_VARIABLE LINKFLAGS)
+#set(CMAKE_EXE_LINKER_FLAGS ${LINKFLAGS} CACHE STRING "link flags" FORCE)
 
 # --- Set test suite behavior
 # Like many clusters, parallel runs are not allowed on login/compiler nodes.
