a12038b968c7908ee905117f7b0fc6a34228c16e
Rao Garimella <rao@lanl.gov>
2012-11-20 15:10:25 -0700

    force intel compile on conejo to use -O1

--- config/examples/conejo-intel-acml-int32-config.cmake
+++ config/examples/conejo-intel-acml-int32-config.cmake
@@ -26,19 +26,20 @@ set(CMAKE_BUILD_TYPE "Release" CACHE STRING "CMake build type")
 
 # --- Set the compilers
 set(CMAKE_C_COMPILER "${MPI_ROOT}/bin/mpicc" CACHE FILEPATH "MPI C compiler wrapper" FORCE)
-execute_process(COMMAND ${MPI_ROOT}/bin/mpicc --showme:compile OUTPUT_VARIABLE CFLAGS)
-#set(CMAKE_C_FLAGS ${CFLAGS} CACHE STRING "MPI C compile flags" FORCE)
+set(CMAKE_C_FLAGS_RELEASE "-O1" CACHE STRING "Intel C compile flags for Release Build" FORCE)
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O1 -g" CACHE STRING "Intel C compile flags for RelWithDebInfo build" FORCE)
+set(CMAKE_C_FLAGS_MINSIZEREL "-O1" CACHE STRING "Intel C compile flags for MinSizeRel" FORCE)
 
 set(CMAKE_CXX_COMPILER "${MPI_ROOT}/bin/mpicxx" CACHE FILEPATH "MPI C++ compiler wrapper" FORCE)
-execute_process(COMMAND ${MPI_ROOT}/bin/mpicxx --showme:compile OUTPUT_VARIABLE CXXFLAGS)
-#set(CMAKE_CXX_FLAGS ${CXXFLAGS} CACHE STRING "MPI CXX compile flags" FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE "-O1" CACHE STRING "Intel CXX compile flags for Release Build" FORCE)
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O1 -g" CACHE STRING "Intel CXX compile flags for RelWithDebInfo build" FORCE)
+set(CMAKE_CXX_FLAGS_MINSIZEREL "-O1" CACHE STRING "Intel CXX compile flags for MinSizeRel" FORCE)
 
 set(CMAKE_Fortran_COMPILER "${MPI_ROOT}/bin/mpif90" CACHE FILEPATH "MPI Fortran compiler wrapper" FORCE)
-execute_process(COMMAND ${MPI_ROOT}/bin/mpif90 --showme:compile OUTPUT_VARIABLE F90FLAGS)
-#set(CMAKE_F90_FLAGS ${F90FLAGS} CACHE STRING "MPI F90 compile flags" FORCE)
+set(CMAKE_Fortran_FLAGS_RELEASE "-O1" CACHE STRING "Intel Fortran compile flags for Release Build" FORCE)
+set(CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-O1 -g" CACHE STRING "Intel Fortran compile flags for RelWithDebInfo build" FORCE)
+set(CMAKE_Fortran_FLAGS_MINSIZEREL "-O1" CACHE STRING "Intel Fortran compile flags for MinSizeRel" FORCE)
 
-#execute_process(COMMAND ${MPI_ROOT}/bin/mpicxx --showme:link OUTPUT_VARIABLE LINKFLAGS)
-#set(CMAKE_EXE_LINKER_FLAGS ${LINKFLAGS} CACHE STRING "link flags" FORCE)
 
 # --- Set test suite behavior
 # Like many clusters, parallel runs are not allowed on login/compiler nodes.
