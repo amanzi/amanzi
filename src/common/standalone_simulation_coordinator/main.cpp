d1ef16e28c81c595ba1de0acbe4d8a994220cbc4
Ethan Coon <ecoon@lanl.gov>
2012-12-19 11:43:04 -0700

    added --version option to standalone executable.  version # picked up from amanzi_version.hh, which gets generated at CMake configure-time

--- src/common/standalone_simulation_coordinator/main.cpp
+++ src/common/standalone_simulation_coordinator/main.cpp
@@ -1,8 +1,8 @@
 #ifdef ENABLE_Unstructured
-  #include "amanzi_unstructured_grid_simulation_driver.hpp"
+#include "amanzi_unstructured_grid_simulation_driver.hpp"
 #endif
 #ifdef ENABLE_Structured
-  #include "amanzi_structured_grid_simulation_driver.H"
+#include "amanzi_structured_grid_simulation_driver.H"
 #endif
 
 #include <iostream>
@@ -18,6 +18,7 @@
 #include "Teuchos_CommandLineProcessor.hpp"
 #include "Teuchos_StandardParameterEntryValidators.hpp"
 
+#include "amanzi_version.hh"
 #include "dbc.hh"
 #include "errors.hh"
 #include "exceptions.hh"
@@ -26,21 +27,21 @@
 // include fenv if it exists
 #include "boost/version.hpp"
 #if (BOOST_VERSION / 100 % 1000 >= 46)
- #include "boost/config.hpp"
- #ifndef BOOST_NO_FENV_H
-  #ifdef _GNU_SOURCE
-   #define AMANZI_USE_FENV
-   #include "boost/detail/fenv.hpp"
-  #endif
- #endif
+#include "boost/config.hpp"
+#ifndef BOOST_NO_FENV_H
+#ifdef _GNU_SOURCE
+#define AMANZI_USE_FENV
+#include "boost/detail/fenv.hpp"
+#endif
+#endif
 #endif
 
 struct RunLog
     : public std::ostream
 {
-    RunLog(std::ostream& _os);
-protected:
-    int rank;
+  RunLog(std::ostream& _os);
+ protected:
+  int rank;
 };
 
 int main(int argc, char *argv[]) {
@@ -50,27 +51,38 @@ int main(int argc, char *argv[]) {
 #endif
 
   Teuchos::GlobalMPISession mpiSession(&argc,&argv,0);
-  
+
   // make sure only PE0 can write to std::cout  --  NO...do not do this!!
   int rank;
   rank = mpiSession.getRank();
   //if (rank != 0) cout.rdbuf(0);
-  
+
   try {
 
     Teuchos::CommandLineProcessor CLP;
-    
+
     CLP.setDocString("\nThe Amanzi driver reads an XML input file and\n"
-		     "runs a reactive flow and transport simulation.\n");
-    
+                     "runs a reactive flow and transport simulation.\n");
+
     std::string xmlInFileName = "options.xml";
     CLP.setOption("xml_file", &xmlInFileName, "XML options file");
-    
+
+    bool print_version(false);
+    CLP.setOption("version", "no_version", &print_version, "Print version number and exit.");
+
     CLP.throwExceptions(true);
-    
+
     Teuchos::CommandLineProcessor::EParseCommandLineReturn
-      parseReturn = CLP.parse(argc, argv);
- 
+        parseReturn = CLP.parse(argc, argv);
+
+    // check for verbose option
+    if (print_version) {
+      if (rank == 0) {
+        std::cout << "Amanzi Version " << AMANZI_VERSION << std::endl;
+      }
+      exit(0);
+    }
+
     // read the main parameter list
     Teuchos::ParameterList driver_parameter_list;
     // DEPRECATED    Teuchos::updateParametersFromXmlFile(xmlInFileName,&driver_parameter_list);
@@ -79,8 +91,8 @@ int main(int argc, char *argv[]) {
 
     const Teuchos::ParameterList& mesh_parameter_list = driver_parameter_list.sublist("Mesh");
     driver_parameter_list.set<string>("input file name", xmlInFileName);
-    
-    // The Mesh list contains a "Structured" sublist or a "Unstructured" sublist, and will 
+
+    // The Mesh list contains a "Structured" sublist or a "Unstructured" sublist, and will
     // determine which simulation driver to call
     std::string framework;
     if (mesh_parameter_list.isSublist("Structured")) {
@@ -90,12 +102,12 @@ int main(int argc, char *argv[]) {
     } else {
       amanzi_throw(Errors::Message("The Mesh parameter list must contain one sublist: \"Structured\" or \"Unstructured\""));
     }
-    
+
     Amanzi::Simulator* simulator = NULL;
-    
+
     Amanzi::timer_manager.add("Full Simulation", Amanzi::Timer::ONCE);
     Amanzi::timer_manager.start("Full Simulation");
-    
+
     if (framework=="Structured") {
 #ifdef ENABLE_Structured
       simulator = new AmanziStructuredGridSimulationDriver();
@@ -109,22 +121,22 @@ int main(int argc, char *argv[]) {
       amanzi_throw(Errors::Message("Unstructured not supported in current build"));
 #endif
     }
-    
+
     MPI_Comm mpi_comm(MPI_COMM_WORLD);
-    Amanzi::ObservationData output_observations;  
+    Amanzi::ObservationData output_observations;
     Amanzi::Simulator::ReturnType ret = simulator->Run(mpi_comm, driver_parameter_list, output_observations);
 
     if (ret == Amanzi::Simulator::FAIL) {
       amanzi_throw(Errors::Message("The amanzi simulator returned an error code, this is most likely due to an error in the mesh creation."));
     }
 
-    // print out observation file in ASCII format 
+    // print out observation file in ASCII format
     Teuchos::ParameterList obs_list;
 
     bool is_native_unstructured = false;
     std::string nui_str = "Native Unstructured Input";
     if (driver_parameter_list.isParameter(nui_str)) {
-        is_native_unstructured = driver_parameter_list.get<bool>(nui_str);
+      is_native_unstructured = driver_parameter_list.get<bool>(nui_str);
     }
 
     if (is_native_unstructured) {
@@ -137,37 +149,38 @@ int main(int argc, char *argv[]) {
       std::string obs_file = obs_list.get<std::string>("Observation Output Filename");
 
       if (rank == 0) {
-        std::ofstream out; out.open(obs_file.c_str(),std::ios::out);
+        std::ofstream out;
+        out.open(obs_file.c_str(),std::ios::out);
         if (!out.good()) {
-            std::cout << "OPEN PROBLEM" << endl;
+          std::cout << "OPEN PROBLEM" << endl;
         }
-      
+
         out.precision(16);
         out.setf(std::ios::scientific);
-	
+
         out << "Observation Name, Region, Functional, Variable, Time, Value\n";
         out << "===========================================================\n";
 
-	for (Teuchos::ParameterList::ConstIterator i=obs_list.begin(); i!=obs_list.end(); ++i) {
-	  std::string label  = obs_list.name(i);
-	  const Teuchos::ParameterEntry& entry = obs_list.getEntry(label);
-	  if (entry.isList()) {
+        for (Teuchos::ParameterList::ConstIterator i=obs_list.begin(); i!=obs_list.end(); ++i) {
+          std::string label  = obs_list.name(i);
+          const Teuchos::ParameterEntry& entry = obs_list.getEntry(label);
+          if (entry.isList()) {
             const Teuchos::ParameterList& ind_obs_list = obs_list.sublist(label);
             for (int j = 0; j < output_observations[label].size(); j++) {
 
               if (output_observations[label][j].is_valid) {
-                  if (!out.good()) {
-                      std::cout << "PROBLEM BEFORE" << endl;
-                  }
-                  out << label << ", " 
+                if (!out.good()) {
+                  std::cout << "PROBLEM BEFORE" << endl;
+                }
+                out << label << ", "
                     << ind_obs_list.get<std::string>("Region") << ", "
                     << ind_obs_list.get<std::string>("Functional") << ", "
                     << ind_obs_list.get<std::string>("Variable") << ", "
                     << output_observations[label][j].time << ", "
-                      << output_observations[label][j].value << '\n';
-                  if (!out.good()) {
-                      std::cout << "PROBLEM AFTER" << endl;
-                  }
+                    << output_observations[label][j].value << '\n';
+                if (!out.good()) {
+                  std::cout << "PROBLEM AFTER" << endl;
+                }
               }
             }
           }
@@ -175,30 +188,29 @@ int main(int argc, char *argv[]) {
         out.close();
       }
     }
- 
+
     Amanzi::timer_manager.stop( "Full Simulation" );
     Amanzi::timer_manager.parSync(mpi_comm);
     if (rank == 0) {
-        std::cout << "Amanzi::SIMULATION_SUCCESSFUL\n\n";
-        
-        std::cout << Amanzi::timer_manager << std::endl;
+      std::cout << "Amanzi::SIMULATION_SUCCESSFUL\n\n";
+
+      std::cout << Amanzi::timer_manager << std::endl;
     }
     delete simulator;
   }
 
   catch (std::exception& e) {
     if (rank == 0) {
-        std::cout << e.what() << std::endl;
-        std::cout << "Amanzi::SIMULATION_FAILED\n";
+      std::cout << e.what() << std::endl;
+      std::cout << "Amanzi::SIMULATION_FAILED\n";
     }
   }
 
   // catch all
   catch (...) {
     if (rank == 0) {
-        std::cout << "Amanzi::SIMULATION_FAILED\n";    
+      std::cout << "Amanzi::SIMULATION_FAILED\n";
     }
   }
 
 }
-
