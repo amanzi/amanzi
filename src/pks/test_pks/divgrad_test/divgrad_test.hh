1ae3a857483064e3ef43180b70a4cd15bc525db7
ecoon <ecoon@lanl.gov>
2012-12-19 12:48:07 -0700

    new test PK for testing matrix implementations and their capabilities in system-level tests

---
+++ src/pks/test_pks/divgrad_test/divgrad_test.hh
@@ -0,0 +1,79 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+
+/*
+  A high level test class for the MatrixMFD operator.
+
+  License: BSD
+  Authors: Ethan Coon (ATS version) (ecoon@lanl.gov)
+*/
+
+#ifndef PK_TESTS_DIVGRAD_TEST_HH_
+#define PK_TESTS_DIVGRAD_TEST_HH_
+
+#include "boundary_function.hh"
+
+#include "pk_factory.hh"
+#include "pk_physical_base.hh"
+
+namespace Amanzi {
+
+// forward declarations
+namespace Operators { class MatrixMFD; }
+
+namespace TestPKs {
+
+class DivGradTest : public PKPhysicalBase {
+
+public:
+  DivGradTest(Teuchos::ParameterList& plist,
+           const Teuchos::RCP<TreeVector>& solution) :
+      PKDefaultBase(plist,solution),
+      PKPhysicalBase(plist, solution) {
+    // set a few parameters before setup
+    plist_.set("solution key", "solution");
+  }
+
+  // Virtual destructor
+  virtual ~DivGradTest() {}
+
+  // main methods
+  // -- Setup data.
+  virtual void setup(const Teuchos::Ptr<State>& S);
+
+  // -- Initialize owned (dependent) variables.
+  virtual void initialize(const Teuchos::Ptr<State>& S);
+
+  // -- Commit any secondary (dependent) variables.
+  virtual void commit_state(double dt, const Teuchos::RCP<State>& S) {}
+
+  // -- Update diagnostics for vis.
+  virtual void calculate_diagnostics(const Teuchos::RCP<State>& S) {}
+
+  virtual bool advance(double dt) {}
+
+  virtual double get_dt() { return 1.e99; }
+
+protected:
+  // boundary condition members
+  virtual void UpdateBoundaryConditions_();
+  virtual void ApplyBoundaryConditions_(const Teuchos::RCP<CompositeVector>& pres);
+
+protected:
+  // mathematical operators
+  Teuchos::RCP<Operators::MatrixMFD> matrix_;
+
+  // boundary condition data
+  Teuchos::RCP<Functions::BoundaryFunction> bc_dirichlet_;
+  Teuchos::RCP<Functions::BoundaryFunction> bc_neumann_;
+  std::vector<Operators::Matrix_bc> bc_markers_;
+  std::vector<double> bc_values_;
+
+ private:
+  // factory registration
+  static RegisteredPKFactory<DivGradTest> reg_;
+};
+
+}  // namespace
+}  // namespace Amanzi
+
+#endif
