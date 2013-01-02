b145a4ad03d3b78a8732d8af002336d7755f5d25
Markus Berndt <berndt@lanl.gov>
2013-01-02 11:55:52 -0700

    more prescribed deformation fixes

--- src/pks/deform/prescribed_deformation/prescribed_deformation.cc
+++ src/pks/deform/prescribed_deformation/prescribed_deformation.cc
@@ -19,12 +19,14 @@
 namespace Amanzi {
 namespace Deform {
 
+using namespace Amanzi::AmanziMesh;
+
 RegisteredPKFactory<PrescribedDeformation> PrescribedDeformation::reg_("prescribed deformation");
 
 
 // -- Setup data
 void PrescribedDeformation::setup(const Teuchos::Ptr<State>& S) {
-
+  S->RequireFieldEvaluator("cell_volume");
 }
 
 // -- Initialize owned (dependent) variables.
@@ -34,7 +36,69 @@ void PrescribedDeformation::initialize(const Teuchos::Ptr<State>& S) {
   
 // -- advance via one of a few methods
 bool PrescribedDeformation::advance(double dt) {
+  std::cout << "ADVANCING prescribed deformation" << std:: endl;
 
+  AmanziMesh::Mesh * write_access_mesh_ =  const_cast<AmanziMesh::Mesh*>(&*S_next_->GetMesh());
+  
+  
+  Teuchos::RCP<CompositeVector> cell_volume = S_next_->GetFieldData("cell_volume", "cell_volume");  
+  Teuchos::RCP<FieldEvaluator> cveval = S_next_->GetFieldEvaluator("cell_volume");
+
+  double ss = S_next_->time();
+  double ss0 = S_->time();
+
+  double thickness = 0.95 + 0.05*cos( ss / (24*60*60) * 2.0*M_PI );   
+  double thickness0 = 0.95 + 0.05*cos( ss0 / (24*60*60) * 2.0*M_PI );
+
+  double factor = thickness/thickness0;
+
+  std::cout << "factor = " << factor << std::endl;
+
+
+  // get space dimensions
+  int dim = write_access_mesh_->space_dimension();
+
+    // set the list of the new position coords
+  AmanziGeometry::Point_List newpos, finpos;
+  
+  // move the mid point on the mesh top
+  Entity_ID_List nodeids;
+
+  // number of vertices
+  int nV = write_access_mesh_->num_entities(Amanzi::AmanziMesh::NODE, 
+                               Amanzi::AmanziMesh::OWNED);
+
+  std::cout << "number of nodes = " << nV << std::endl;
+
+  Amanzi::AmanziGeometry::Point coords, new_coords;
+  coords.init(dim);
+  new_coords.init(dim);
+  
+  // search the id of the mid point on the top
+  for (int iV=0; iV<nV; iV++) {
+    
+    // get the coords of the node
+    write_access_mesh_->node_get_coordinates(iV,&coords);
+
+    for ( int s=0; s<dim-1; ++s ) {
+      new_coords[s] = coords[s];
+    }
+    // new_coords[dim-1] = coords[dim-1] * fac;
+    new_coords[dim-1] = coords[dim-1] * factor;
+    
+    // puch back for deform method
+    nodeids.push_back(iV);
+    newpos.push_back( new_coords );
+  }
+  
+  std::cout << "now modifying the mesh..." << std::endl;
+  // compute the deformed mesh
+  write_access_mesh_->deform( nodeids, newpos, true, &finpos);
+
+  // now update cell volumes
+  cell_volume->Scale(factor);
+
+  std::cout << "DONE" << std::endl;
 }
 
 } // namespace
