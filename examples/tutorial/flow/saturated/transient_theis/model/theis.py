11201c438909e7f76c7f4b8a7ee7ad220e640a3f
Alec Thomas <thomasan@lanl.gov>
2013-07-09 14:41:13 -0600

    theis example, nearly working

--- examples/tutorial/flow/saturated/transient_theis/model/theis.py
+++ examples/tutorial/flow/saturated/transient_theis/model/theis.py
@@ -31,13 +31,10 @@ class TransientTheis(object):
     def __init__(self, params=None):
         if params is None:
             params = dict()
-        
-        params.setdefault("x_low",-1)
-        params.setdefault("x_high",1)
-        params.setdefault("z_low",0)
-        params.setdefault("z_high",1)
-        params.setdefault("y_low",-1)
-        params.setdefault("y_high",1)
+        params.setdefault("t",[1,2,3])
+        params.setdefault("r", [20,40,50])
+        params.setdefault("High",[1,1,1])
+        params.setdefault("Cells",[1,1,1])
         params.setdefault("S_s",0.00001)
         params.setdefault("pi",math.pi)
         params.setdefault("g",9.80665)
@@ -45,20 +42,29 @@ class TransientTheis(object):
         params.setdefault("rho",1000)
         params.setdefault("mu",1.e-3)
         params.setdefault("h_0", 10)
-        params.setdefault("Q",-1.e-3)
+        params.setdefault("Q",-4.e-3)
        
         self.__dict__.update(params)
+
+        self.r
+        
+        size = []
+        for i,cell in zip(self.High,self.Cells):
+            size.append(i / cell)
+        print size
+            
+        self.Vol_cell = size[0]*size[1]*size[2]
+        
+        self.Q_vol = -self.Q * self.Vol_cell / self.rho
         
         self.K_h = self.K*self.g*self.rho / self.mu
         
         self.T =self. K_h*self.h_0
         
-        self.var = -self.Q / 4 / self.pi /self.T
+        self.var = self.Q_vol / 4 / self.pi /self.T
         
         self.S = self.S_s*self.h_0
         
-        
-
     def runForFixedTime(self, radi, time):
         #This method evaluates Theis solution for a given time at multiple radial distances from the well 
         drawdown_r = []
@@ -79,39 +85,33 @@ class TransientTheis(object):
             drawdown_t.append(s)
         return drawdown_t
  
-def paramsFromXML(filename):
+def createFromXML(filename):
         
-        #grab parameters from input XML file.
-    import amanzi_xml.utils.io
-    xml = amanzi_xml.utils.io.fromFile(filename)
+    #-- grab parameters from input XML file.
+    import amanzi_xml.observations.ObservationXML as Obs_xml
+    observations = Obs_xml.ObservationXML(filename)
+    xml = observations.xml
+    coords = observations.getAllCoordinates()
     import amanzi_xml.utils.search as search
-
     params = dict()
-    obs_lists = []
-    obs_list = search.getElementByPath(xml, "/Main/Regions/Region: Point")
-    for el in obs_list.getchildren():
-        if el.tag == "Parameter":
-            obs_lists.append(el)
-    print obs_list
-
+   
+    params["r"] = []
+    for coord in coords:
+        params["r"].append(coord[0]) 
+    
     params.setdefault("g",9.80665)
     params.setdefault("pi",math.pi)
+    params["High"]= search.getElementByPath(xml, "/Main/Mesh/Unstructured/Generate Mesh/Uniform Structured/Domain High Corner").value
+    params["Cells"]= search.getElementByPath(xml, "/Main/Mesh/Unstructured/Generate Mesh/Uniform Structured/Number of Cells").value
     params["K"] = search.getElementByPath(xml, "/Main/Material Properties/Soil/Intrinsic Permeability: Uniform/Value").value
     params["mu"]= search.getElementByPath(xml, "/Main/Phase Definitions/Aqueous/Phase Properties/Viscosity: Uniform/Viscosity").value
     params["rho"]= search.getElementByPath(xml, "/Main/Phase Definitions/Aqueous/Phase Properties/Density: Uniform/Density").value
     params["h_0"]= search.getElementByPath(xml, "/Main/Boundary Conditions/Far Field Head/BC: Hydrostatic/Water Table Height").value[0]
     params["Q"]= search.getElementByPath(xml, "/Main/Sources/Pumping Well/Source: Volume Weighted/Values").value[0]
     params["S_s"]= search.getElementByPath(xml, "/Main/Material Properties/Soil/Specific Storage: Uniform/Value").value
-    params["n"]= search.getElementByPath(xml, "/Main/Material Properties/Soil/Porosity: Uniform/Value").value
-    params["x_low"]=search.getElementByPath(xml, "/Main/Regions/Entire Domain/Region: Box/Low Coordinate").value[0] 
-    params["y_low"]=search.getElementByPath(xml, "/Main/Regions/Entire Domain/Region: Box/Low Coordinate").value[1]    
-    params["z_low"]=search.getElementByPath(xml, "/Main/Regions/Entire Domain/Region: Box/Low Coordinate").value[2]
-    params["x_high"]=search.getElementByPath(xml, "/Main/Regions/Entire Domain/Region: Box/High Coordinate").value[0]  
-    params["y_high"]=search.getElementByPath(xml, "/Main/Regions/Entire Domain/Region: Box/High Coordinate").value[1] 
-    params["z_high"]=search.getElementByPath(xml, "/Main/Regions/Entire Domain/Region: Box/High Coordinate").value[2]
-
+    params["t"]= search.getElementByPath(xml, "/Main/Output/Time Macros/Observation Times/Values").value
+    
     return TransientTheis(params)
-
 
 def getWellFunction(U):
     u = float(U)
@@ -141,10 +141,6 @@ def getWellFunction(U):
         W = (math.exp(-u) / u)*((u**4 +b_1*(u**3) + b_2*(u**2) + b_3*u + b_4)/(u**4 + c_1*(u**3) + c_2*(u**2) + c_3*u + c_4))
         return W
 
-     
-         
-         
-
 if __name__ == "__main__":
     #instaniate the class
     Tt = TransientTheis()
@@ -164,9 +160,6 @@ if __name__ == "__main__":
 
     radius = [30,50,60]
 
-#s_fixed_time = Tt.runForFixedTime(radi, time)
-# s_fixed_radius = Tt.runForFixedRadius(times, radius)
-
     fig1 = plt.figure()
     fig2 = plt.figure()
     ax1 = fig1.add_axes([.1,.1,.8,.8]) 
@@ -182,12 +175,10 @@ if __name__ == "__main__":
         s_fixed_radius = Tt.runForFixedRadius(times, r)
         ax2.plot(times , s_fixed_radius, label ='$r=%0.1f m$'%r )
     
-
     for t in time: 
         s_fixed_time = Tt.runForFixedTime(radi, t)
         ax1.plot(radi , s_fixed_time, label = '$t=%0.1f s$'%t )
     
-
     ax1.legend(title='Theis Solution',loc = 'upper right', fancybox=True, shadow = True)
     ax2.legend(title = 'Theis Solution',loc = 'lower right', fancybox=True, shadow = True)
 
