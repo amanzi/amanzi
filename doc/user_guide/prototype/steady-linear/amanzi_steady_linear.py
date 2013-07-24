6d94ec11f4d177f872bfca1044bb84da9feb11af
Ethan Coon <ecoon@lanl.gov>
2013-07-24 16:04:13 -0600

    user guild docs: makes theis and steady_linear use the same format for import

--- doc/user_guide/prototype/steady-linear/amanzi_steady_linear.py
+++ doc/user_guide/prototype/steady-linear/amanzi_steady_linear.py
@@ -2,16 +2,15 @@ import os
 import matplotlib.pyplot as plt
 import numpy
 
-import sys
-if "./model" not in sys.path:
+try:
+    import steady_linear
+except ImportError:
     sys.path.append("./model")
-import steady_linear
+    import steady_linear
 
 from amanzi_xml.observations.ObservationXML import ObservationXML as ObsXML
 from amanzi_xml.observations.ObservationData import ObservationData as ObsDATA
 import amanzi_xml.utils.search as search
-
-
 
 
 # load input xml file
@@ -44,7 +43,7 @@ def plotExampleObservations(Obs_xml, Obs_data, axes1):
     z_vals = set(z_vals)
     colors = ['r','b','g']
     cmap = dict((zval,color) for (zval,color) in zip(z_vals, colors))
-        
+
     # -- grab the right coordinate and value
     # -- call ObservationPlotter to plot with format="bo"
     coords_list=[]
@@ -52,7 +51,7 @@ def plotExampleObservations(Obs_xml, Obs_data, axes1):
     for obs in Obs_data.observations.itervalues(): 
         pressure_value.append(obs.data)
         coords_list.append(obs.coordinate)
-        
+
     axes1.scatter([coords_list[0][0],coords_list[2][0],coords_list[3][0]],[pressure_value[0],pressure_value[2],pressure_value[3]],c=cmap[coords_list[0][2]],marker='s',s=50, label = '$Amanzi$')
     axes1.scatter([coords_list[1][0]],[pressure_value[1]],c = cmap[coords_list[1][2]],marker = 's',s = 50,label = '$Amanzi$')
     axes1.scatter([coords_list[4][0]],[pressure_value[4]],c = cmap[coords_list[4][2]],marker = 's',s = 50,label = '$Amanzi$')
@@ -60,7 +59,7 @@ def plotExampleObservations(Obs_xml, Obs_data, axes1):
     axes1.set_xlabel('Distance in x-direction [meter]')
     axes1.set_ylabel('Pressure [Pa]')
     axes1.set_title('Aqueous Pressure vs Distance')
-    
+
     return cmap
 
 def plotExampleModel(filename, cmap, axes1,Obs_xml, Obs_data):
@@ -73,26 +72,26 @@ def plotExampleModel(filename, cmap, axes1,Obs_xml, Obs_data):
 
     for (z_val, color) in cmap.iteritems():
         coords[:,1] = z_val
-        
+
         pres = mymodel.run(coords)
         axes1.plot(x,pres,color,label='$z = %0.2f $'%z_val)
         axes1.legend(loc="upper right" , fancybox = True , shadow = True)
-          
+
     all_coordinates = Obs_xml.coordinates
     coordinates = []
     pressure_value = [] 
-   
+
     for obs in Obs_data.observations.itervalues():
         coordinates.append([obs.coordinate[0], obs.coordinate[2]])
         pressure_value.append(obs.data)
-    
+
     coordinates = numpy.array(coordinates)
     pres_analytic = mymodel.run(coordinates)
     pressure_analytic = list(pres_analytic)
-    
+
     for coord,press_amanzi, press_analytic in zip(coordinates,pressure_value,pressure_analytic):
         table_values.append([coord[0],coord[1],press_amanzi,press_analytic])
-        
+
     col_labels = ['x [m]', 'z [m]' ,'Pressure(Amanzi) [Pa]','Pressure (analytic)[Pa]']
     ax = plt.subplot(111, frame_on = False)
     ax.xaxis.set_visible(False)
@@ -102,7 +101,7 @@ def plotExampleModel(filename, cmap, axes1,Obs_xml, Obs_data):
     cells = properties['child_artists']
     for cell in cells: 
         cell.set_height(.06)
-    
+
     the_table.auto_set_font_size(False)
     the_table.set_fontsize(14)
 
