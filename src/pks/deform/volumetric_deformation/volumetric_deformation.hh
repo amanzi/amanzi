faf74662cf52c650db2e2b07a73ac56a5d26c804
ecoon <ecoon@lanl.gov>
2013-08-23 10:54:09 -0600

    temperary commit, this is what was used for deformation movies.  This PK is being re-organized

--- src/pks/deform/volumetric_deformation/volumetric_deformation.hh
+++ src/pks/deform/volumetric_deformation/volumetric_deformation.hh
@@ -19,6 +19,7 @@
 
 #include "composite_matrix.hh"
 #include "composite_vector_function.hh"
+#include "Function.hh"
 
 #include "pk_factory.hh"
 #include "pk_default_base.hh"
@@ -62,6 +63,14 @@ class VolumetricDeformation : public PKPhysicalBase {
   Key poro_key_;
 
   // function describing d(cv)/dT
+  enum DeformMode {
+    DEFORM_MODE_DVDT,
+    DEFORM_MODE_THAW_FRONT,
+  };
+  DeformMode mode_;
+  std::string deform_region_;
+  Teuchos::RCP<Function> thaw_front_func_;
+  double min_vol_frac_;
   Teuchos::RCP<Functions::CompositeVectorFunction> deform_func_;
 
   // meshes
