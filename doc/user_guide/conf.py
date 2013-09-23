f186477df9b538a129479ca6a7ad5c67a7074c1c
David Moulton <moulton@lanl.gov>
2013-09-23 08:44:04 -0600

    Switched html themes and removed conflicting extension (closes #1100, #1101).
    The default.css from matplotlib appears to have motivated a more complete
    implementation in the sphinxdoc theme.  For now we'll use this as it does
    center figures properly.
    
    The matplotlib extension to render mathematics seems have conflicted
    as opposed to augmented MathJax.  The motivation for it appears to be to
    help systems without a TeX installation, but we have TeX so no problems
    here.

--- doc/user_guide/conf.py
+++ doc/user_guide/conf.py
@@ -30,7 +30,6 @@ extensions = ['sphinx.ext.todo',
               'sphinx.ext.ifconfig',
               'sphinx.ext.autodoc',
               'sphinx.ext.doctest',
-              'matplotlib.sphinxext.mathmpl',
               'matplotlib.sphinxext.only_directives',
               'matplotlib.sphinxext.plot_directive',
               'matplotlib.sphinxext.ipython_directive',
@@ -49,7 +48,6 @@ source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
-
 
 # General information about the project.
 project = u'Amanzi'
@@ -103,7 +101,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
