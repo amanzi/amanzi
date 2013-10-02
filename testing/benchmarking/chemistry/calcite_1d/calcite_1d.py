2382b281f0f0918a225cd2db7b9a9dc7690389b1
Sergi Molins <smolins@lbl.gov>
2013-10-02 05:10:06 -0600

    fixed bug in calcite_1d input file for native chemistry

--- testing/benchmarking/chemistry/calcite_1d/calcite_1d.py
+++ testing/benchmarking/chemistry/calcite_1d/calcite_1d.py
@@ -68,11 +68,26 @@ if __name__ == "__main__":
     # path_to_pflotran = "/home/scratch/smolins/amanzi/examples/phase2/chemistry/1d-calcite/pflotran-run/"
 
      # hardwired for 1d-calcite: time and comp
-    time = 'Time:  5.00000E+01 y'
-    comp = 'Total_Ca++ [M]'
+    times = ['Time:  0.00000E+00 y','Time:  1.00000E+01 y','Time:  2.00000E+01 y','Time:  3.00000E+01 y','Time:  4.00000E+01 y','Time:  5.00000E+01 y']
 
-    x_pflotran, c_pflotran = GetXY_PFloTran(path_to_pflotran,root,time,comp)    
-    
+    comp = 'Total_Ca++ [M]'
+    Ca_pflotran = []
+    for i, time in enumerate(times):
+       x_pflotran, c_pflotran = GetXY_PFloTran(path_to_pflotran,root,time,comp)
+       Ca_pflotran = Ca_pflotran + [c_pflotran]
+
+    comp = 'pH'
+    pH_pflotran = []
+    for i, time in enumerate(times):
+       x_pflotran, c_pflotran = GetXY_PFloTran(path_to_pflotran,root,time,comp)
+       pH_pflotran = pH_pflotran + [c_pflotran]
+
+    comp = 'Calcite_VF'
+    VF_pflotran = []
+    for i, time in enumerate(times):
+       x_pflotran, c_pflotran = GetXY_PFloTran(path_to_pflotran,root,time,comp)
+       VF_pflotran = VF_pflotran + [c_pflotran]
+
     CWD = os.getcwd()
     local_path = "" 
 
@@ -81,28 +96,63 @@ if __name__ == "__main__":
         
     try:
         # hardwired for 1d-calcite: Ca = component 2, last time = '71'
-        time = '71'
-        comp = 'total_component_concentration.cell.Component 2 conc'
+        times = ['0','31','41','51','61','71']
 
         # Amanzi native chemistry
         input_filename = os.path.join("amanzi-u-1d-calcite.xml")
         path_to_amanzi = "amanzi-native-output"
         run_amanzi_chem.run_amanzi_chem("../"+input_filename,run_path=path_to_amanzi,chemfiles=["calcite.bgd"])
-        x_amanzi_native, c_amanzi_native = GetXY_Amanzi(path_to_amanzi,root,time,comp)
+        
+        comp = 'total_component_concentration.cell.Component 2 conc'
+        Ca_amanzi_native = []
+        for i, time in enumerate(times):
+           x_amanzi_native, c_amanzi_native = GetXY_Amanzi(path_to_amanzi,root,time,comp)
+           Ca_amanzi_native = Ca_amanzi_native +[c_amanzi_native]
+
+        comp = 'free_ion_species.cell.0'
+        pH_amanzi_native = []
+        for i, time in enumerate(times):
+           x_amanzi_native, c_amanzi_native = GetXY_Amanzi(path_to_amanzi,root,time,comp)
+           pH_amanzi_native = pH_amanzi_native +[-np.log10(c_amanzi_native)]
+
+        comp = 'mineral_volume_fractions.cell.Calcite vol frac'
+        VF_amanzi_native = []
+        for i, time in enumerate(times):
+           x_amanzi_native, c_amanzi_native = GetXY_Amanzi(path_to_amanzi,root,time,comp)
+           VF_amanzi_native = VF_amanzi_native +[c_amanzi_native]
 
         # Amanzi-Alquimia
         input_filename = os.path.join("amanzi-u-1d-calcite-alq.xml")
         path_to_amanzi = "amanzi-alquimia-output"
         run_amanzi_chem.run_amanzi_chem("../"+input_filename,run_path=path_to_amanzi,chemfiles=["1d-calcite.in","calcite.dat"])
-        x_amanzi_alquimia, c_amanzi_alquimia = GetXY_Amanzi(path_to_amanzi,root,time,comp)
+
+        comp = 'total_component_concentration.cell.Component 2 conc'
+        Ca_amanzi_alquimia = []
+        for i, time in enumerate(times):
+           x_amanzi_alquimia, c_amanzi_alquimia = GetXY_Amanzi(path_to_amanzi,root,time,comp)
+           Ca_amanzi_alquimia = Ca_amanzi_alquimia +[c_amanzi_alquimia]
+
+        comp = 'free_ion_species.cell.0'
+        pH_amanzi_alquimia = []
+        for i, time in enumerate(times):
+           x_amanzi_alquimia, c_amanzi_alquimia = GetXY_Amanzi(path_to_amanzi,root,time,comp)
+           pH_amanzi_alquimia = pH_amanzi_alquimia +[-np.log10(c_amanzi_alquimia)]
+
+        comp = 'mineral_volume_fractions.cell.Calcite vol frac'
+        VF_amanzi_alquimia = []
+        for i, time in enumerate(times):
+           x_amanzi_alquimia, c_amanzi_alquimia = GetXY_Amanzi(path_to_amanzi,root,time,comp)
+           VF_amanzi_alquimia = VF_amanzi_alquimia +[c_amanzi_alquimia]
 
         # subplots
         fig, ax = plt.subplots()
 
-        # lines on axes
-        alq = ax.plot(x_amanzi_alquimia, c_amanzi_alquimia,'r-',label='Amanzi+Alquimia(PFloTran)',linewidth=2)
-        ama = ax.plot(x_amanzi_native, c_amanzi_native,'ro',label='Amanzi Native Chemistry')
-        pfl = ax.plot(x_pflotran, c_pflotran,'b-',label='PFloTran',linewidth=2)
+        for i, time in enumerate(times)
+
+          # lines on axes
+          alq[i] = ax.plot(x_amanzi_alquimia, Ca_amanzi_alquimia[i],'r-',label='Amanzi+Alquimia(PFloTran)',linewidth=2)
+          ama[i] = ax.plot(x_amanzi_native, Ca_amanzi_native[i],'ro',label='Amanzi Native Chemistry')
+          pfl[i] = ax.plot(x_pflotran, Ca_pflotran[i],'b-',label='PFloTran',linewidth=2)
 
         # axes
         ax.set_xlabel("Distance (m)",fontsize=20)
@@ -111,7 +161,7 @@ if __name__ == "__main__":
         # plot adjustments
         plt.subplots_adjust(left=0.20,bottom=0.15,right=0.95,top=0.90)
         plt.legend(loc='upper left',fontsize=13)
-        plt.suptitle("Amanzi 1D Calcite Benchmark at 50 years",x=0.57,fontsize=20)
+        plt.suptitle("Amanzi 1D Calcite Benchmark",x=0.57,fontsize=20)
         plt.tick_params(axis='both', which='major', labelsize=20)
 
         #pyplot.show()
