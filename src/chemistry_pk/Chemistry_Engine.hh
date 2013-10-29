7240aa4d52bac0415c11538ddac514bd42e7c562
Jeffrey Johnson <jnjohnson@lbl.gov>
2013-10-29 18:08:53 +0000

    More work on Chemistry_Engine (point of access for Alquimia).

--- src/chemistry_pk/Chemistry_Engine.hh
+++ src/chemistry_pk/Chemistry_Engine.hh
@@ -37,20 +37,26 @@ class Chemistry_Engine {
 
   // Constructs a chemistry engine using the Chemistry parameter list in the input file and 
   // Amanzi's chemistry state.
-  Chemistry_Entine(const Teuchos::ParameterList& param_list,
-                   Teuchos::RCP<Chemistry_State> chem_state);
+  explicit Chemistry_Engine(const Teuchos::ParameterList& param_list);
 
   // Destructor.
   ~Chemistry_Engine();
 
-  // Initializes the chemistry engine, preparing it to answer queries from other entities.
-  void Initialize();
+  // Returns the number of primary chemical species in the present simulation.
 
-  // Returns the number of chemical species in the present simulation.
-  int NumSpecies() const;
-
-  // Returns an array of names for the chemical species in the present simulation.
-  void GetSpeciæsNames(std::vector<std::string>& speciesNames) const;
+  // These query methods return metadata for the chemical configuration represented 
+  // within the chemistry engine.
+  int NumPrimarySpecies() const;
+  int NumSorbedSpecies() const;
+  void GetPrimarySpeciesNames(std::vector<std::string>& speciesNames) const;
+  int NumMinerals() const;
+  void GetMineralNames(std::vector<std::string>& mineralNames) const;
+  int NumSurfaceSites() const;
+  void GetSurfaceSiteNames(std::vector<std::string>& siteNames) const;
+  int NumIonExchangeSites() const;
+  void GetIonExchangeNames(std::vector<std::string>& ionExchangeNames) const;
+  int NumIsothermSpecies() const;
+  void GetIsothermSpeciesNames(std::vector<std::string>& speciesNames) const;
 
   // Enforces the geochemical condition with the given name on the chemical configuration 
   // represented by the given array of concentrations at the given time. The order of the 
@@ -68,13 +74,49 @@ class Chemistry_Engine {
   // FIXME: How is auxiliary output handled?
  private:
 
-  // parameter lists
-  Teuchos::ParameterList main_param_list_, chem_param_list_;
+  // Reads parameters from the XML input file to set up chemistry.
+  void ReadXMLParameters();
+
+  // Initializes the chemistry engine, preparing it to answer queries from other entities.
+  void Initialize();
 
   // This helper parses geochemical conditions from the given parameter list, placing them into a
   // data structure that will be used to retrieve them by name.
   void ParseChemicalConditions(const Teuchos::ParameterList& param_list,
                                std::map<std::string, AlquimiaGeochemicalCondition*>& conditions);
+
+  // Copies data from given buffers to Alquimia in preparation for chemistry.
+  void CopyFromBuffersToAlquimia(const double* component_concentrations,
+                                 const double* sorbed_components,
+                                 const double* mineral_volume_fractions,
+                                 const double* mineral_specific_surface_areas,
+                                 const double* cation_exchange_capacity,
+                                 const double* sorption_sites,
+                                 double water_density,
+                                 double porosity,
+                                 double volume,
+                                 double saturation);
+
+  // Copies data from Alquimia to the given buffers to Alquimia after computation.
+  void CopyFromAlquimiaToBuffers(double* component_concentrations,
+                                 double* sorbed_components,
+                                 double* mineral_volume_fractions,
+                                 double* mineral_specific_surface_areas,
+                                 double* cation_exchange_capacity,
+                                 double* sorption_sites,
+                                 double& water_density,
+                                 double& porosity,
+                                 double& volume,
+                                 double& saturation,
+                                 double* free_ion_species,
+                                 double* primary_activity_coeffs,
+                                 double* secondary_activity_coeffs,
+                                 double* ion_exchange_ref_cation_concs,
+                                 double* surface_complex_free_site_concs,
+                                 double& pH) const;
+
+  // parameter lists
+  Teuchos::ParameterList main_param_list_, chem_param_list_;
 
   // Alquimia data structures.
   bool chem_initialized_;
@@ -87,8 +129,7 @@ class Chemistry_Engine {
   // conditions in the corresponding condition vector applied to them. NOTE
   // that these maps do not own the geochemical conditions--they only hold 
   // pointers to the objects.
-  std::map<std::string, AlquimiaGeochemicalCondition*> chem_initial_conditions_;
-  std::map<std::string, AlquimiaGeochemicalCondition*> chem_boundary_conditions_;
+  std::map<std::string, AlquimiaGeochemicalCondition*> chem_conditions_;
   
   // Vector that takes responsibility for ownership of geochemical conditions.
   std::vector<AlquimiaGeochemicalCondition*> all_chem_conditions_;
