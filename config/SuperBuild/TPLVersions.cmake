7dab97ba415e5103506158b92dd92b2dee01fc65
David Moulton <moulton@lanl.gov>
2013-11-14 14:25:07 -0700

    Update version number to reflect update in comments (refs #1040).

--- config/SuperBuild/TPLVersions.cmake
+++ config/SuperBuild/TPLVersions.cmake
@@ -81,7 +81,7 @@ endmacro(amanzi_tpl_version_write)
 
 set (AMANZI_TPLS_VERSION_MAJOR 0)
 set (AMANZI_TPLS_VERSION_MINOR 91)
-set (AMANZI_TPLS_VERSION_PATCH 8)
+set (AMANZI_TPLS_VERSION_PATCH 9)
 set (AMANZI_TPLS_VERSION ${AMANZI_TPLS_VERSION}.${AMANZI_TPLS_VERSION_MINOR}.${AMANZI_TPLS_VERSION_PATCH})
 #   Not sure how to create a meaningful hash key for the collection
 
