2c837bb6ad2645e01f8e031c7a5df3851ef0a355
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-01-07 10:30:47 -0800

    Addressing a C++ bug in our older version of Trilinos (Teuchos specifically).

--- config/SuperBuild/include/Build_Trilinos.cmake
+++ config/SuperBuild/include/Build_Trilinos.cmake
@@ -45,6 +45,10 @@ foreach(package ${Trilinos_PACKAGE_LIST})
   list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DTrilinos_ENABLE_${package}:STRING=ON")
 endforeach()
 
+# Trilinos 11.0.3 has some C++ compile errors in it that we can sidestep by 
+# defining HAVE_TEUCHOS_ARRAY_BOUNDSCHECK.
+list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DTeuchos_ENABLE_ABC:BOOL=ON")
+
 # Remove SEACAS from the build and force STK to use external Exodus
 if ( ENABLE_STK_Mesh )
   list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DTrilinos_ENABLE_SEACAS:STRING=OFF")
