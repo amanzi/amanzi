ec2aa30f855f419f60ab764c2e44c44ae25e64f5
ecoon <ecoon@lanl.gov>
2014-02-13 11:51:48 -0700

    parallelize GID info in enorm print, now all enorm writeouts are GIDs, and Debugger accepts GIDs not LIDs.

--- src/pks/energy/base/energy_base_ti.cc
+++ src/pks/energy/base/energy_base_ti.cc
@@ -269,20 +269,25 @@ double EnergyBase::enorm(Teuchos::RCP<const TreeVector> u,
     res_c.NormInf(&infnorm_c);
     res_f.NormInf(&infnorm_f);
 
+    ENorm_t err_f, err_c;
 #ifdef HAVE_MPI
-    double buf_c(enorm_cell), buf_f(enorm_face);
-    MPI_Allreduce(&buf_c, &enorm_cell, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    MPI_Allreduce(&buf_f, &enorm_face, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    ENorm_t l_err_f, l_err_c;
+    l_err_f.value = enorm_face;
+    l_err_f.gid = res_f.Map().GID(bad_face);
+    l_err_c.value = enorm_cell;
+    l_err_c.gid = res_c.Map().GID(bad_cell);
+
+    MPI_Allreduce(&l_err_c, &err_c, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+    MPI_Allreduce(&l_err_f, &err_f, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+#else
+    err_f.value = enorm_face;
+    err_f.gid = bad_face;
+    err_c.value = enorm_cell;
+    err_c.gid = bad_cell;
 #endif
 
-    AmanziMesh::Entity_ID_List cells;
-    mesh_->face_get_cells(bad_face, AmanziMesh::USED, &cells);
-    *vo_->os() << "ENorm (cells) = " << enorm_cell << "[" << bad_cell << "] (" << infnorm_c << ")" << std::endl;
-    if (cells.size() == 1) {
-      *vo_->os() << "ENorm (faces) = " << enorm_face << "[" << cells[0] << "] (" << infnorm_f << ")" << std::endl;
-    } else {
-      *vo_->os() << "ENorm (faces) = " << enorm_face << "[" << cells[0] << "," << cells[1] << "] (" << infnorm_f << ")" << std::endl;
-    }
+    *vo_->os() << "ENorm (cells) = " << err_c.value << "[" << err_c.gid << "] (" << infnorm_c << ")" << std::endl;
+    *vo_->os() << "ENorm (faces) = " << err_f.value << "[" << err_f.gid << "] (" << infnorm_f << ")" << std::endl;
   }
 
   // Communicate and take the max.
