dc90405bc1637ac97756a148123e4f7e0019c714
Ethan Coon <ecoon@lanl.gov>
2014-03-11 17:29:55 -0600

    continued work to ensure same solutions for subsurf, surf without surf

--- src/operators/divgrad/MatrixMFD_Coupled_Surf.cc
+++ src/operators/divgrad/MatrixMFD_Coupled_Surf.cc
@@ -23,6 +23,28 @@ MatrixMFD_Coupled_Surf::MatrixMFD_Coupled_Surf(const MatrixMFD_Coupled_Surf& oth
 {}
 
 
+void
+MatrixMFD_Coupled_Surf::SetSurfaceOperators(const Teuchos::RCP<MatrixMFD_TPFA>& surface_A,
+                         const Teuchos::RCP<MatrixMFD_TPFA>& surface_B) {
+  surface_A_ = surface_A;
+  surface_B_ = surface_B;
+  ASSERT(surface_A_->Mesh() == surface_B_->Mesh());
+  surface_mesh_ = surface_A_->Mesh();
+
+  // Create the surface->subsurface importer and map
+  int nsurf_cells = surface_mesh_->num_entities(AmanziMesh::CELL, AmanziMesh::OWNED);
+  std::vector<int> surf_gids(nsurf_cells, -1);
+
+  const Epetra_Map& face_map = blockA_->Mesh()->face_epetra_map(false);
+  for (unsigned int sc=0; sc!=nsurf_cells; ++sc) {
+    surf_gids[sc] = face_map.GID(surface_mesh_->entity_get_parent(AmanziMesh::CELL, sc));
+  }
+
+  surf_map_in_subsurf_ = Teuchos::rcp(new Epetra_Map(-1, nsurf_cells, &surf_gids[0], 0, *blockA_->Mesh()->get_comm()));
+  surf_importer_ = Teuchos::rcp(new Epetra_Import(*surf_map_in_subsurf_,face_map));
+}
+
+
 void MatrixMFD_Coupled_Surf::ComputeSchurComplement() {
   // Base ComputeSchurComplement() gets the standard face parts
   MatrixMFD_Coupled::ComputeSchurComplement();
@@ -92,7 +114,8 @@ void MatrixMFD_Coupled_Surf::ComputeSchurComplement() {
 
 
     for (int m=0; m!=entriesA; ++m) {
-      block(0,0) = std::max(valuesA[m], 1.e-10);
+      block(0,0) = valuesA[m];
+      //      block(0,0) = std::max(valuesA[m], 1.e-10);
       block(1,1) = valuesB[m];
 
       if (frow_global == indicesA[m]) {
@@ -124,6 +147,97 @@ void MatrixMFD_Coupled_Surf::ComputeSchurComplement() {
   delete[] valuesB;
   delete[] gsubsurfindices;
   delete[] gsubsurfvalues;
+}
+
+
+int
+MatrixMFD_Coupled_Surf::Apply(const TreeVector& X,
+        TreeVector& Y) const {
+  // Apply the coupled Matrix, which gets the subsurface.
+  int ierr = MatrixMFD_Coupled::Apply(X,Y);
+
+  // Add in the Surface components
+  Teuchos::RCP<const CompositeVector> XA = X.SubVector(0)->Data();
+  Teuchos::RCP<const CompositeVector> XB = X.SubVector(1)->Data();
+  Teuchos::RCP<CompositeVector> YA = Y.SubVector(0)->Data();
+  Teuchos::RCP<CompositeVector> YB = Y.SubVector(1)->Data();
+
+  // -- import X from subsurface faces into surface cells
+
+  // NOTE --etc
+  // This confuses the hell out of me.  Is it not possible to pull out an
+  // arbitrary set of global indices into a vector from a previously created
+  // map?  The obvious form of this errors because the target vector's map
+  // (the surface cell map) is not the same as the importer's target map (the
+  // subset of face GIDs).  To me that should be ok anyway... the nth face GID
+  // corresponds to the nth surface cell.
+  //
+  // Note that the target MUST have the surface cell map, as it is used with
+  // Matrices that use that map.
+  //
+  // Two potential options forward:
+  // -- two imports are required (the send taking the face GID referenced data
+  //    and copying it to the cell Vector), and the second one is a simple
+  //    copy with no re-mapping.
+  // -- fool Trilinos by making a new Epetra_Vector for the surface cell
+  //    vector that shares data with the old one but has a new map.
+  // We try the latter as it removes the copy...
+  //
+  Epetra_MultiVector surf_XA(surface_mesh_->cell_epetra_map(false),1);
+  Epetra_MultiVector surf_XB(surface_mesh_->cell_epetra_map(false),1);
+
+  double **surf_XA_data, **surf_XB_data;
+  ierr |= surf_XA.ExtractView(&surf_XA_data);
+  ierr |= surf_XB.ExtractView(&surf_XB_data);
+  ASSERT(!ierr);
+
+  Epetra_MultiVector surf_XA_face_GIDs(View, *surf_map_in_subsurf_, surf_XA_data, 1);
+  Epetra_MultiVector surf_XB_face_GIDs(View, *surf_map_in_subsurf_, surf_XB_data, 1);
+
+  // Now, surf_XA_face_GIDs and surf_XA have the same values, but different maps.
+  // Importing into the face_GIDs version results in the values in surf_XA
+  ierr |= surf_XA_face_GIDs.Import(*XA->ViewComponent("face",false), *surf_importer_, Insert);
+  ierr |= surf_XB_face_GIDs.Import(*XB->ViewComponent("face",false), *surf_importer_, Insert);
+  ASSERT(!ierr);
+
+  // -- apply the surface-only operators, blockwise, repeating the "multiple views" trick
+  Epetra_MultiVector surf_YA(surface_mesh_->cell_epetra_map(false),1);
+  Epetra_MultiVector surf_YB(surface_mesh_->cell_epetra_map(false),1);
+
+  double **surf_YA_data, **surf_YB_data;
+  ierr |= surf_YA.ExtractView(&surf_YA_data);
+  ierr |= surf_YB.ExtractView(&surf_YB_data);
+  ASSERT(!ierr);
+
+  Epetra_MultiVector surf_YA_face_GIDs(View, *surf_map_in_subsurf_, surf_YA_data, 1);
+  Epetra_MultiVector surf_YB_face_GIDs(View, *surf_map_in_subsurf_, surf_YB_data, 1);
+
+  // -- A_surf * lambda_p
+  ierr |= surface_A_->Apply(surf_XA, surf_YA);
+
+  // -- Ccc_surf * lambda_T
+  ierr |= surf_YA.Multiply(scaling_, *Ccc_surf_, surf_XB, 1.0);
+
+  // -- B_surf * lambda_T
+  ierr |= surface_B_->Apply(surf_XB, surf_YB);
+
+  // -- Dcc_surf * lambda_p
+  ierr |= surf_YB.Multiply(scaling_, *Dcc_surf_, surf_XA, 1.0);
+  ASSERT(!ierr);
+  
+  // -- export back into subsurface face vectors
+  ierr |= YA->ViewComponent("face",false)->Export(surf_YA_face_GIDs, *surf_importer_, Add);
+  ierr |= YB->ViewComponent("face",false)->Export(surf_YB_face_GIDs, *surf_importer_, Add);
+  ASSERT(!ierr);
+
+  return ierr;
+}
+
+
+void MatrixMFD_Coupled_Surf::UpdateConsistentFaceCorrection(const TreeVector& u,
+        const Teuchos::Ptr<TreeVector>& Pu) {
+  std::cout << "WARNING: This is not correctly implemented for MatrixMFD_Coupled_Surf!" << std::endl;
+  MatrixMFD_Coupled::UpdateConsistentFaceCorrection(u,Pu);
 }
 
 
