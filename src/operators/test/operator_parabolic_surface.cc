be3eda247d50303bc006b3d440cbafec5e92271a
Konstantin Lipnikov <lipnikov@lanl.gov>
2014-03-13 11:45:01 -0600

    Added aggregation option to CMake. Tests will be aggregated by default to reduce the disk space occupied by statically linked executables, 30M per a non-optimized test on average. Developers can set either parameter ENABLE_INDIVIDUAL_TESTS or ENABLE_INDIVUAL_TEST_<CLASS> to true to activate creation of small tests. At the moment this option is tested for <CLASS>=OPERATORS. refs #1211

--- src/operators/test/operator_parabolic_surface.cc
+++ src/operators/test/operator_parabolic_surface.cc
@@ -35,7 +35,11 @@
 #include "OperatorSource.hh"
 
 
-TEST(LAPLACE_BELTRAMI_FLAT) {
+/* *****************************************************************
+* This test replaves tensor and boundary conditions by continuous
+* functions. This is a prototype for future solvers.
+* **************************************************************** */
+TEST(LAPLACE_BELTRAMI_CLOSED) {
   using namespace Teuchos;
   using namespace Amanzi;
   using namespace Amanzi::AmanziMesh;
@@ -45,7 +49,7 @@ TEST(LAPLACE_BELTRAMI_FLAT) {
   Epetra_MpiComm comm(MPI_COMM_WORLD);
   int MyPID = comm.MyPID();
 
-  if (MyPID == 0) std::cout << "\nTest: Laplace Beltrami solver" << std::endl;
+  if (MyPID == 0) std::cout << "\nTest: Singular-perturbed Laplace Beltrami solver" << std::endl;
 
   // read parameter list
   std::string xmlFileName = "test/operator_laplace_beltrami.xml";
@@ -53,7 +57,7 @@ TEST(LAPLACE_BELTRAMI_FLAT) {
   ParameterList plist = xmlreader.getParameters();
 
   // create an SIMPLE mesh framework
-  ParameterList region_list = plist.get<Teuchos::ParameterList>("Regions Flat");
+  ParameterList region_list = plist.get<Teuchos::ParameterList>("Regions Closed");
   GeometricModelPtr gm = new GeometricModel(2, region_list, &comm);
 
   FrameworkPreference pref;
@@ -62,7 +66,7 @@ TEST(LAPLACE_BELTRAMI_FLAT) {
 
   MeshFactory meshfactory(&comm);
   meshfactory.preference(pref);
-  RCP<const Mesh> mesh = meshfactory(0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 10, 10, 5, gm);
+  RCP<const Mesh> mesh = meshfactory("test/sphere.exo", gm);
   RCP<const Mesh_MSTK> mesh_mstk = rcp_static_cast<const Mesh_MSTK>(mesh);
 
   // extract surface mesh
@@ -86,15 +90,6 @@ TEST(LAPLACE_BELTRAMI_FLAT) {
   std::vector<int> bc_model(nfaces_wghost, OPERATOR_BC_NONE);
   std::vector<double> bc_values(nfaces_wghost);
 
-  for (int f = 0; f < nfaces_wghost; f++) {
-    const Point& xf = surfmesh->face_centroid(f);
-    if (fabs(xf[0]) < 1e-6 || fabs(xf[0] - 1.0) < 1e-6 ||
-        fabs(xf[1]) < 1e-6 || fabs(xf[1] - 1.0) < 1e-6) {
-      bc_model[f] = OPERATOR_BC_FACE_DIRICHLET;
-      bc_values[f] = xf[1] * xf[1];
-    }
-  }
-
   // create diffusion operator 
   Teuchos::RCP<CompositeVectorSpace> cvs = Teuchos::rcp(new CompositeVectorSpace());
   cvs->SetMesh(surfmesh);
@@ -103,32 +98,53 @@ TEST(LAPLACE_BELTRAMI_FLAT) {
   cvs->SetOwned(false);
   cvs->AddComponent("face", AmanziMesh::FACE, 1);
 
-  Teuchos::RCP<OperatorDiffusionSurface> op = Teuchos::rcp(new OperatorDiffusionSurface(cvs, 0));
+  // create source and add it to the operator
+  CompositeVector source(*cvs);
+  source.PutScalar(0.0);
+  
+  Epetra_MultiVector& src = *source.ViewComponent("cell");
+  for (int c = 0; c < 20; c++) {
+    src[0][c] = 1.0;
+  }
 
-  // populate the diffusion operator
+  Teuchos::RCP<OperatorSource> op1 = Teuchos::rcp(new OperatorSource(cvs, 0));
+  op1->Init();
+  op1->UpdateMatrices(source);
+
+  // create accumulation operator
+  Teuchos::RCP<OperatorAccumulation> op2 = Teuchos::rcp(new OperatorAccumulation(*op1));
+
+  CompositeVector solution(*cvs);
+  solution.PutScalar(0.0);  // solution at time T=0
+
+  CompositeVector phi(*cvs);
+  phi.PutScalar(0.2);
+
+  double dT = 10.0;
+  op2->UpdateMatrices(solution, phi, dT);
+
+  // add the diffusion operator
+  Teuchos::RCP<OperatorDiffusionSurface> op3 = Teuchos::rcp(new OperatorDiffusionSurface(*op2));
+
   Teuchos::ParameterList olist;
   int schema = Operators::OPERATOR_SCHEMA_DOFS_FACE + Operators::OPERATOR_SCHEMA_DOFS_CELL;
-  op->Init();
-  op->InitOperator(K, Teuchos::null, olist);
-  op->UpdateMatrices();
-  op->ApplyBCs(bc_model, bc_values);
-  op->SymbolicAssembleMatrix(Operators::OPERATOR_SCHEMA_DOFS_FACE);
-  op->AssembleMatrix(schema);
+  op3->InitOperator(K, Teuchos::null, olist);
+  op3->UpdateMatrices();
+  op3->ApplyBCs(bc_model, bc_values);
+  op3->SymbolicAssembleMatrix(Operators::OPERATOR_SCHEMA_DOFS_FACE);
+  op3->AssembleMatrix(schema);
 
   // create preconditoner
   ParameterList slist = plist.get<Teuchos::ParameterList>("Preconditioners");
-  op->InitPreconditioner("Hypre AMG", slist, bc_model, bc_values);
+  op3->InitPreconditioner("Hypre AMG", slist, bc_model, bc_values);
 
   // solve the problem
   ParameterList lop_list = plist.get<Teuchos::ParameterList>("Solvers");
   AmanziSolvers::LinearOperatorFactory<OperatorDiffusionSurface, CompositeVector, CompositeVectorSpace> factory;
   Teuchos::RCP<AmanziSolvers::LinearOperator<OperatorDiffusionSurface, CompositeVector, CompositeVectorSpace> >
-     solver = factory.Create("AztecOO CG", lop_list, op);
+     solver = factory.Create("AztecOO CG", lop_list, op3);
 
-  CompositeVector rhs = *op->rhs();
-  CompositeVector solution(rhs);
-  solution.PutScalar(0.0);
-
+  CompositeVector rhs = *op3->rhs();
   int ierr = solver->ApplyInverse(rhs, solution);
 
   std::cout << "pressure solver (" << solver->name() 
@@ -137,8 +153,10 @@ TEST(LAPLACE_BELTRAMI_FLAT) {
 
   // visualization
   const Epetra_MultiVector& p = *solution.ViewComponent("cell");
-  GMV::open_data_file(*surfmesh, (std::string)"surface_flat.gmv");
+  GMV::open_data_file(*surfmesh, (std::string)"surface_closed.gmv");
   GMV::start_data();
   GMV::write_cell_data(p, 0, "solution");
   GMV::close_data_file();
 }
+
+
