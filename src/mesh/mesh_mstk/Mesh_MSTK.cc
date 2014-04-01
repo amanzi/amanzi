682567095c826fdb2f60e5ed2a8bc77fd3a127b5
Rao Garimella <rao@lanl.gov>
2014-04-01 14:37:41 -0600

    parentheses around booleans to make check work correctly

--- src/mesh/mesh_mstk/Mesh_MSTK.cc
+++ src/mesh/mesh_mstk/Mesh_MSTK.cc
@@ -559,7 +559,7 @@ void Mesh_MSTK::extract_mstk_mesh(const Mesh_MSTK& inmesh,
   }
 
 
-  if (inmesh.cell_dimension() == 2 && setkind == FACE && flatten) {
+  if ((inmesh.cell_dimension() == 2) && (setkind == FACE) && flatten) {
     Errors::Message mesg("Requested mesh constructor produces 1D mesh which is not supported by Amanzi");
     amanzi_throw(mesg);
   }
