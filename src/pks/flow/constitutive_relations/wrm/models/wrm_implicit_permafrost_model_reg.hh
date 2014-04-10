f42e23749f98c7fd32d60fa4447f705f9636b1f4
ecoon <ecoon@lanl.gov>
2014-04-10 11:25:49 -0600

    fixes dKrel_dp terms for TPFA Jacobian in the case of abs perm = 1

---
+++ src/pks/flow/constitutive_relations/wrm/models/wrm_implicit_permafrost_model_reg.hh
@@ -0,0 +1,15 @@
+
+#include "wrm.hh"
+#include "wrm_implicit_permafrost_model.hh"
+
+
+namespace Amanzi {
+namespace Flow {
+namespace FlowRelations {
+
+// registry of method
+Utils::RegisteredFactory<WRMPermafrostModel,WRMImplicitPermafrostModel> WRMImplicitPermafrostModel::factory_("permafrost model");
+
+} // namespace
+} // namespace
+} // namespace
