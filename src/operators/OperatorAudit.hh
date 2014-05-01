10d8cb744e1c69d60dc3e3946d1a4d4259c0fafd
Konstantin Lipnikov <lipnikov@lanl.gov>
2014-05-01 09:53:30 -0600

    Moved audit from matrix to operators. refs #1211

---
+++ src/operators/OperatorAudit.hh
@@ -0,0 +1,39 @@
+/*
+  This is the operators component of the Amanzi code. 
+
+  Copyright 2010-2012 held jointly by LANS/LANL, LBNL, and PNNL. 
+  Amanzi is released under the three-clause BSD License. 
+  The terms of use and "as is" disclaimer for this license are 
+  provided in the top-level COPYRIGHT file.
+
+  Author: Konstantin Lipnikov (lipnikov@lanl.gov)
+*/
+
+#ifndef AMANZI_OPERATOR_AUDIT_HH_
+#define AMANZI_OPERATOR_AUDIT_HH_
+
+#include "Operator.hh"
+
+
+namespace Amanzi {
+namespace Operators {
+
+class OperatorAudit : public Operator {
+ public:
+  OperatorAudit() {};
+  OperatorAudit(const Operator& op) : Operator(op) {};
+  ~OperatorAudit();
+
+  // main members
+  int CheckSpectralBounds(int schema);
+  int CheckMatrixSymmetry();
+  int CheckMatrixCoercivity();
+
+ private:
+  void OrderByIncrease(int n, double* mem);
+};
+
+}  // namespace Operators
+}  // namespace Amanzi
+
+#endif
