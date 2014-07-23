7546c2fd47bae74698e726495909a89cd2b82f3f
ecoon <ecoon@lanl.gov>
2014-07-23 14:30:43 -0600

    better error reporting for surface_balance_implicit to match other PKs' reporting

---
+++ src/pks/biogeochemistry/bgc_simple/SoilCarbonParameters.cc
@@ -0,0 +1,87 @@
+/*
+
+Soil carbon parameters data structures class
+
+Author: Ethan Coon (ecoon@lanl.gov)
+        Chonggang Xu (cxu@lanl.gov)
+
+Licencse: BSD
+*/
+
+#include "dbc.hh"
+#include "SoilCarbonParameters.hh"
+
+namespace Amanzi {
+namespace BGC {
+
+SoilCarbonParameters::SoilCarbonParameters(int nPools_, double percent_sand) :
+    nPools(nPools_),
+    RespF(nPools_),
+    TurnoverRates(nPools_),
+    Tij(nPools_,nPools_)
+{
+  InitCentury_(percent_sand);
+}
+
+
+SoilCarbonParameters::SoilCarbonParameters(int nPools_, Teuchos::ParameterList& plist) :
+    nPools(nPools_),
+    RespF(nPools_),
+    TurnoverRates(nPools_),
+    Tij(nPools_,nPools_)
+{
+  Init_(plist);
+}
+
+
+void SoilCarbonParameters::InitCentury_(double percent_sand)
+{
+  double tt = 0.85 - 0.68 * 0.01 * (100 - percent_sand);
+
+  // initialize the respiration fraction
+  RespF[0] = 0.0;
+  RespF[1] = 0.55;
+  RespF[2] = 0.5;
+  RespF[3] = 0.5;
+  RespF[4] = tt;
+  RespF[5] = 0.55;
+  RespF[6] = 0.55;
+
+  // initialize baseline turnover rates, years
+  TurnoverRates[0] = 4.1;
+  TurnoverRates[1] = 0.06;
+  TurnoverRates[2] = 0.25;
+  TurnoverRates[3] = 0.25;
+  TurnoverRates[4] = 0.17;
+  TurnoverRates[5] = 6.1;
+  TurnoverRates[6] = 270.0;
+
+  // initialize conversion factors
+  Tij[0][2] = 0.76;
+  Tij[0][3] = 0.24;
+  Tij[1][4] = 1.0;
+  Tij[2][4] = 1.0;
+  Tij[3][5] = 1.0;
+  Tij[4][5] = 1.0 - 0.004 / (1.0 - tt);
+  Tij[4][6] = 1.0 - Tij[4][5];
+  Tij[5][4] = 0.93;
+  Tij[5][6] = 0.07;
+  Tij[6][4] = 1.0;
+
+}
+
+void SoilCarbonParameters::Init_(Teuchos::ParameterList& plist)
+{
+  std::string model = plist.get<std::string>("soil carbon model", "century");
+  if (model == "century") {
+    double p_sand = plist.get<double>("percent sand");
+    InitCentury_(p_sand);
+  } else {
+    ASSERT(0);
+  }
+
+  // special case init from plist here...
+}
+
+} // namespace
+} // namespace
---
+++ src/pks/flow/overland_head/height_model.hh
@@ -0,0 +1,44 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+
+/*
+  Evaluates height(pressure)
+
+  Authors: Ethan Coon (ecoon@lanl.gov)
+*/
+
+#ifndef AMANZI_FLOWRELATIONS_HEIGHT_MODEL_
+#define AMANZI_FLOWRELATIONS_HEIGHT_MODEL_
+
+#include "Teuchos_ParameterList.hpp"
+
+namespace Amanzi {
+namespace Flow {
+namespace FlowRelations {
+
+class HeightModel {
+public:
+  explicit
+  HeightModel(Teuchos::ParameterList& plist) : plist_(plist) {}
+
+  double Height(double pres, double rho, double p_atm, double g_z) {
+    return (pres - p_atm) / (rho * g_z);
+  }
+
+  double DHeightDPressure(double pres, double rho, double p_atm, double g_z) {
+    return 1. / (rho * g_z);
+  }
+
+  double DHeightDRho(double pres, double rho, double p_atm, double g_z) {
+    return -(pres - p_atm) / (rho * rho * g_z);
+  }
+
+protected:
+  Teuchos::ParameterList plist_;
+
+};
+
+} // namespace
+} // namespace
+} // namespace
+
+#endif
