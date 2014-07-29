a1490a98e2165a51eccd507d64e9740b5fc43d56
ecoon <ecoon@lanl.gov>
2014-07-29 08:19:01 -0600

    adds time monitors for showing wallclock time of setup and cycle

---
+++ src/operators/deformation/Matrix_PreconditionerDelegate.cc
@@ -0,0 +1,204 @@
+/*
+  License:
+  Authors: Ethan Coon (ecoon@lanl.gov) (ATS version)
+
+  Delegate for setting up and solving a preconditioned system.
+*/
+
+#include "errors.hh"
+#include "dbc.hh"
+
+#include "Matrix_PreconditionerDelegate.hh"
+
+namespace Amanzi {
+namespace Operators {
+
+Matrix_PreconditionerDelegate::Matrix_PreconditionerDelegate(
+        Teuchos::ParameterList& plist) :
+    plist_(plist) {
+  InitializeFromPlist_();
+}
+
+
+void Matrix_PreconditionerDelegate::set_matrix(
+    const Teuchos::RCP<Epetra_RowMatrix>& op) {
+  op_ = op;
+
+  // do this once per op set, as it does some setup work that can be saved?
+  if (prec_method_ == TRILINOS_ML) {
+    ml_prec_ = Teuchos::rcp(new ML_Epetra::MultiLevelPreconditioner(*op_,
+            solver_plist_, false));
+  }
+}
+
+
+void Matrix_PreconditionerDelegate::InitializePreconditioner() {
+
+  if (prec_method_ == TRILINOS_ML) {
+    if (ml_prec_->IsPreconditionerComputed()) ml_prec_->DestroyPreconditioner();
+    ml_prec_->SetParameterList(solver_plist_);
+    ml_prec_->ComputePreconditioner();
+
+  } else if (prec_method_ == TRILINOS_ILU) {
+    ilu_prec_ = Teuchos::rcp(new Ifpack_ILU(&*op_));
+    ilu_prec_->SetParameters(solver_plist_);
+    ilu_prec_->Initialize();
+    ilu_prec_->Compute();
+
+  } else if (prec_method_ == TRILINOS_BLOCK_ILU) {
+    Ifpack factory;
+    std::string prectype("ILU");
+    int ovl = solver_plist_.get<int>("overlap",0);
+    solver_plist_.set<std::string>("schwarz: combine mode","Add");
+    ifp_prec_ = Teuchos::rcp(factory.Create(prectype, &*op_, ovl));
+    ifp_prec_->SetParameters(solver_plist_);
+    ifp_prec_->Initialize();
+    ifp_prec_->Compute();
+
+#ifdef HAVE_HYPRE
+  } else if (prec_method_ == HYPRE_AMG) {
+    hypre_prec_ = Teuchos::rcp(new Ifpack_Hypre(&*op_));
+    Teuchos::RCP<FunctionParameter> functs[8];
+
+    int hypre_coarsen_type = solver_plist_.get<int>("coarsen type",0);
+    functs[0] = Teuchos::rcp(new FunctionParameter(Preconditioner,
+            &HYPRE_BoomerAMGSetCoarsenType, hypre_coarsen_type));
+
+    int hypre_verbose = solver_plist_.get<int>("verbosity level",0);
+    functs[1] = Teuchos::rcp(new FunctionParameter(Preconditioner,
+            &HYPRE_BoomerAMGSetPrintLevel, hypre_verbose));
+
+    int hypre_nsmooth = solver_plist_.get<int>("number of smoothing iterations",2);
+    functs[2] = Teuchos::rcp(new FunctionParameter(Preconditioner,
+            &HYPRE_BoomerAMGSetNumSweeps, hypre_nsmooth));
+
+    int hypre_ncycles = solver_plist_.get<int>("number of cycles",2);
+    functs[3] = Teuchos::rcp(new FunctionParameter(Preconditioner,
+            &HYPRE_BoomerAMGSetMaxIter, hypre_ncycles));
+
+    int hypre_relax_type = solver_plist_.get<int>("relax type",6);
+    functs[4] = Teuchos::rcp(new FunctionParameter(Preconditioner,
+            &HYPRE_BoomerAMGSetRelaxType, hypre_relax_type));
+
+    double hypre_strong_threshold = solver_plist_.get<double>("strong threshold", 0.5);
+    functs[5] = Teuchos::rcp(new FunctionParameter(Preconditioner,
+            &HYPRE_BoomerAMGSetStrongThreshold, hypre_strong_threshold));
+
+    double hypre_tol = solver_plist_.get<double>("tolerance", 1e-12);
+    functs[6] = Teuchos::rcp(new FunctionParameter(Preconditioner,
+            &HYPRE_BoomerAMGSetTol, hypre_tol));
+
+    int hypre_cycle_type = solver_plist_.get<int>("cycle type",1);
+    functs[7] = Teuchos::rcp(new FunctionParameter(Preconditioner,
+            &HYPRE_BoomerAMGSetCycleType, hypre_cycle_type));
+
+    Teuchos::ParameterList hypre_list;
+    hypre_list.set("Preconditioner", BoomerAMG);
+    hypre_list.set("SolveOrPrecondition", Preconditioner);
+    hypre_list.set("SetPreconditioner", true);
+    hypre_list.set("NumFunctions", 8);
+    hypre_list.set<Teuchos::RCP<FunctionParameter>*>("Functions", functs);
+
+    hypre_prec_->SetParameters(hypre_list);
+    hypre_prec_->Initialize();
+    hypre_prec_->Compute();
+
+  } else if (prec_method_ == HYPRE_EUCLID) {
+    hypre_prec_ = Teuchos::rcp(new Ifpack_Hypre(&*op_));
+
+    Teuchos::ParameterList hypre_list;
+    hypre_list.set("Preconditioner", Euclid);
+    hypre_list.set("SolveOrPrecondition", Preconditioner);
+    hypre_list.set("SetPreconditioner", true);
+    hypre_list.set("NumFunctions", 0);
+
+    hypre_prec_->SetParameters(hypre_list);
+    hypre_prec_->Initialize();
+    hypre_prec_->Compute();
+
+  } else if (prec_method_ == HYPRE_PARASAILS) {
+    hypre_prec_ = Teuchos::rcp(new Ifpack_Hypre(&*op_));
+
+    Teuchos::ParameterList hypre_list;
+    hypre_list.set("Preconditioner", ParaSails);
+    hypre_list.set("SolveOrPrecondition", Preconditioner);
+    hypre_list.set("SetPreconditioner", true);
+    hypre_list.set("NumFunctions", 0);
+
+    hypre_prec_->SetParameters(hypre_list);
+    hypre_prec_->Initialize();
+    hypre_prec_->Compute();
+#endif
+  }
+
+}
+
+
+int Matrix_PreconditionerDelegate::ApplyInverse(const Epetra_MultiVector& b,
+        Epetra_MultiVector& x) const {
+  int ierr;
+  if (prec_method_ == TRILINOS_ML) {
+    ierr = ml_prec_->ApplyInverse(b, x);
+  } else if (prec_method_ == TRILINOS_ILU) {
+    ierr = ilu_prec_->ApplyInverse(b,x);
+  } else if (prec_method_ == TRILINOS_BLOCK_ILU) {
+    ierr = ifp_prec_->ApplyInverse(b,x);
+#ifdef HAVE_HYPRE
+  } else if (prec_method_ == HYPRE_AMG || prec_method_ == HYPRE_EUCLID
+             || prec_method_ == HYPRE_PARASAILS) {
+    ierr = hypre_prec_->ApplyInverse(b,x);
+#endif
+  } else {
+    ASSERT(0); // this error would have been caught already
+  }
+
+  return ierr;
+}
+
+void Matrix_PreconditionerDelegate::InitializeFromPlist_() {
+  // which preconditioner to use
+  prec_method_ = PREC_METHOD_NULL;
+
+  std::string precmethodstring = plist_.get<std::string>("preconditioner");
+
+  if (precmethodstring == "ML") {
+    prec_method_ = TRILINOS_ML;
+    solver_plist_ =  plist_.sublist("ML Parameters");
+
+  } else if (precmethodstring == "ILU" ) {
+    prec_method_ = TRILINOS_ILU;
+    solver_plist_ = plist_.sublist("ILU Parameters");
+
+  } else if (precmethodstring == "Block ILU" ) {
+    prec_method_ = TRILINOS_BLOCK_ILU;
+    solver_plist_ = plist_.sublist("Block ILU Parameters");
+
+#ifdef HAVE_HYPRE
+  } else if (precmethodstring == "HYPRE AMG") {
+    prec_method_ = HYPRE_AMG;
+    solver_plist_ = plist_.sublist("HYPRE AMG Parameters");
+
+  } else if (precmethodstring == "HYPRE Euclid") {
+    prec_method_ = HYPRE_EUCLID;
+    solver_plist_ = plist_.sublist("HYPRE Euclid Parameters");
+
+  } else if (precmethodstring == "HYPRE ParaSails") {
+    prec_method_ = HYPRE_EUCLID;
+    solver_plist_ = plist_.sublist("HYPRE ParaSails Parameters");
+#endif
+
+  } else {
+    // throw an error
+#ifdef HAVE_HYPRE
+    Errors::Message msg("Matrix_MFD: The specified preconditioner "+precmethodstring+" is not supported, we only support ML, ILU, HYPRE AMG, HYPRE Euclid, and HYPRE ParaSails");
+#else
+    Errors::Message msg("Matrix_MFD: The specified preconditioner "+precmethodstring+" is not supported, we only support ML, and ILU");
+#endif
+    Exceptions::amanzi_throw(msg);
+  }
+}
+
+
+} // namespace
+} // namespace
+
---
+++ src/operators/divgrad/MatrixMFD_Coupled_Surf.cc
@@ -0,0 +1,284 @@
+#include "EpetraExt_RowMatrixOut.h"
+#include "Epetra_FECrsGraph.h"
+
+#include "MatrixMFD_Coupled_Surf.hh"
+#include "MatrixMFD_Surf.hh"
+
+namespace Amanzi {
+namespace Operators {
+
+MatrixMFD_Coupled_Surf::MatrixMFD_Coupled_Surf(Teuchos::ParameterList& plist,
+        const Teuchos::RCP<const AmanziMesh::Mesh> mesh) :
+    MatrixMFD_Coupled(plist,mesh)
+{
+  dump_schur_ = MatrixMFD_Coupled::dump_schur_;
+  MatrixMFD_Coupled::dump_schur_ = false;
+}
+
+MatrixMFD_Coupled_Surf::MatrixMFD_Coupled_Surf(const MatrixMFD_Coupled_Surf& other) :
+    MatrixMFD_Coupled(other),
+    surface_mesh_(other.surface_mesh_),
+    surface_A_(other.surface_A_),
+    surface_B_(other.surface_B_)
+{
+  dump_schur_ = MatrixMFD_Coupled::dump_schur_;
+  MatrixMFD_Coupled::dump_schur_ = false;
+}
+
+
+void
+MatrixMFD_Coupled_Surf::SetSurfaceOperators(const Teuchos::RCP<MatrixMFD_TPFA>& surface_A,
+                         const Teuchos::RCP<MatrixMFD_TPFA>& surface_B) {
+  surface_A_ = surface_A;
+  surface_B_ = surface_B;
+  ASSERT(surface_A_->Mesh() == surface_B_->Mesh());
+  surface_mesh_ = surface_A_->Mesh();
+
+  MarkLocalMatricesAsChanged_();
+}
+
+
+void
+MatrixMFD_Coupled_Surf::SetOffDiagonals(const Teuchos::RCP<const Epetra_MultiVector>& Ccc,
+					const Teuchos::RCP<const Epetra_MultiVector>& Dcc,
+					const Teuchos::RCP<const Epetra_MultiVector>& Ccc_surf,
+					const Teuchos::RCP<const Epetra_MultiVector>& Dcc_surf,
+					double scaling) {
+  MatrixMFD_Coupled::SetOffDiagonals(Ccc,Dcc,scaling);
+
+  if (Ccc_surf == Teuchos::null) {
+    Teuchos::RCP<Epetra_MultiVector> Ccc_s = 
+      Teuchos::rcp(new Epetra_MultiVector(surface_mesh_->cell_map(false),1));
+    Ccc_s->PutScalar(0.);
+    Ccc_surf_ = Ccc_s;
+  } else {
+    Ccc_surf_ = Ccc_surf;
+  }
+
+  if (Dcc_surf == Teuchos::null) {
+    Teuchos::RCP<Epetra_MultiVector> Dcc_s = 
+      Teuchos::rcp(new Epetra_MultiVector(surface_mesh_->cell_map(false),1));
+    Dcc_s->PutScalar(0.);
+    Dcc_surf_ = Dcc_s;
+  } else {
+    Dcc_surf_ = Dcc_surf;
+  }
+}
+  
+
+void MatrixMFD_Coupled_Surf::SymbolicAssembleGlobalMatrices() {
+  // Identical to MatrixMFD_Coupled, but does the FillMatrixGraph call
+  // with a _Surf matrix, which must first be created.
+  int ierr(0);
+  const Epetra_BlockMap& cmap = mesh_->cell_map(false);
+  const Epetra_BlockMap& fmap = mesh_->face_map(false);
+  const Epetra_BlockMap& fmap_wghost = mesh_->face_map(true);
+
+  // Make the double maps
+  double_fmap_ = Teuchos::rcp(new Epetra_BlockMap(fmap.NumGlobalPoints(),
+          fmap.NumMyPoints(), fmap.MyGlobalElements(), 2,
+          fmap.IndexBase(), fmap.Comm() ));
+
+  double_cmap_ = Teuchos::rcp(new Epetra_BlockMap(cmap.NumGlobalPoints(),
+          cmap.NumMyPoints(), cmap.MyGlobalElements(), 2,
+          cmap.IndexBase(), cmap.Comm() ));
+
+  double_fmap_wghost_ = Teuchos::rcp(
+      new Epetra_BlockMap(fmap_wghost.NumGlobalPoints(),
+                          fmap_wghost.NumMyPoints(),
+                          fmap_wghost.MyGlobalElements(), 2,
+                          fmap_wghost.IndexBase(), fmap_wghost.Comm() ));
+
+  // Make the matrix graphs
+  int avg_entries_row = 6;
+  Teuchos::RCP<Epetra_CrsGraph> cf_graph =
+      Teuchos::rcp(new Epetra_CrsGraph(Copy, *double_cmap_, *double_fmap_wghost_,
+              avg_entries_row, false));
+  Teuchos::RCP<Epetra_FECrsGraph> ff_graph =
+      Teuchos::rcp(new Epetra_FECrsGraph(Copy, *double_fmap_, 2*avg_entries_row - 1,
+              false));
+
+  // Fill the graphs from the symbolic patterns in matrix_mfd from one of the
+  // sub-block matrices.
+
+  // IT IS ASSUMED that the two sub-block matrices fill in identical ways!
+  // Create a MatrixMFD_Surf operator for filling.
+  MatrixMFD_Surf blockA_surf(blockA_->plist_, blockA_->mesh_);
+  blockA_surf.SetSurfaceOperator(surface_A_);
+  blockA_surf.FillMatrixGraphs_(cf_graph.ptr(), ff_graph.ptr());
+
+  // Assemble the graphs
+  ierr = cf_graph->FillComplete(*double_fmap_, *double_cmap_);
+  ASSERT(!ierr);
+  ierr = ff_graph->GlobalAssemble();  // Symbolic graph is complete.
+  ASSERT(!ierr);
+
+  // Create the matrices
+  P2f2f_ = Teuchos::rcp(new Epetra_FEVbrMatrix(Copy, *ff_graph, false));
+  ierr = P2f2f_->GlobalAssemble();
+  ASSERT(!ierr);
+}
+
+void MatrixMFD_Coupled_Surf::AssembleSchur_() const {
+  // Base ComputeSchurComplement() gets the standard face parts
+  MatrixMFD_Coupled::AssembleSchur_();
+
+  // Add the TPFA on the surface parts from surface_A.
+  const Epetra_Map& surf_cmap_wghost = surface_mesh_->cell_map(true);
+  const Epetra_Map& fmap_wghost = mesh_->face_map(true);
+
+  // Grab the surface operators' TPFA cell-cell matrices
+  const Epetra_FECrsMatrix& App = *surface_A_->TPFA();
+  const Epetra_FECrsMatrix& Bpp = *surface_B_->TPFA();
+
+  int entriesA = 0;
+  int entriesB = 0;
+
+  double *valuesA, *valuesB;
+  valuesA = new double[9];
+  valuesB = new double[9];
+
+  int *indicesA, *indicesB;
+  indicesA = new int[9];
+  indicesB = new int[9];
+
+  int subsurf_entries = 0;
+  int *gsubsurfindices;
+  gsubsurfindices = new int[9];
+  double *gsubsurfvalues;
+  gsubsurfvalues = new double[9];  
+
+  Epetra_SerialDenseMatrix block(2,2);
+
+  int ierr(0);
+
+  // Now, add in the contributions from App
+  // Loop over surface cells (subsurface faces)
+  int ncells_surf = surface_mesh_->num_entities(AmanziMesh::CELL, AmanziMesh::OWNED);
+  for (AmanziMesh::Entity_ID sc=0; sc!=ncells_surf; ++sc) {
+    // Access the row from the surfaces
+    AmanziMesh::Entity_ID sc_global = surf_cmap_wghost.GID(sc);
+
+    ierr = App.ExtractGlobalRowCopy(sc_global, 9, entriesA, valuesA, indicesA);
+    ASSERT(!ierr);
+    ierr = Bpp.ExtractGlobalRowCopy(sc_global, 9, entriesB, valuesB, indicesB);
+    ASSERT(!ierr);
+
+    // ensure consistency of the sparsity structure, this can likely be
+    // removed eventually.
+    ASSERT(entriesA == entriesB);
+    for (int m=0; m!=entriesA; ++m) {
+      ASSERT(indicesA[m] == indicesB[m]);
+    }
+
+    // Convert local cell numbers to domain's local face numbers
+    AmanziMesh::Entity_ID frow = surface_mesh_->entity_get_parent(AmanziMesh::CELL,sc);
+    AmanziMesh::Entity_ID frow_global = fmap_wghost.GID(frow);
+    int diag = -1;
+    for (int m=0; m!=entriesA; ++m) {
+      indicesA[m] = surf_cmap_wghost.LID(indicesA[m]);
+      if (sc == indicesA[m]) diag = m; // save the diagonal index
+      indicesA[m] = surface_mesh_->entity_get_parent(AmanziMesh::CELL,indicesA[m]);
+      indicesA[m] = fmap_wghost.GID(indicesA[m]);
+    }
+
+    // Add the entries
+    ierr = P2f2f_->BeginSumIntoGlobalValues(frow_global, entriesA, indicesA);
+    ASSERT(!ierr);
+
+
+    for (int m=0; m!=entriesA; ++m) {
+      block(0,0) = valuesA[m];
+      //      block(0,0) = std::max(valuesA[m], 1.e-10);
+      block(1,1) = valuesB[m];
+
+      if (frow_global == indicesA[m]) {
+        block(0,1) = (*Ccc_surf_)[0][sc] * scaling_;
+        block(1,0) = (*Dcc_surf_)[0][sc] * scaling_;
+      } else {
+        block(0,1) = 0.;
+        block(1,0) = 0.;
+      }
+      ierr = P2f2f_->SubmitBlockEntry(block.A(), block.LDA(), block.M(), block.N());
+      ASSERT(!ierr);
+    }
+
+    ierr = P2f2f_->EndSubmitEntries();
+    ASSERT(!ierr);
+  }
+
+  ierr = P2f2f_->GlobalAssemble();
+  ASSERT(!ierr);
+
+  // DEBUG dump
+  if (dump_schur_) {
+    std::stringstream filename_s;
+    filename_s << "schur_MatrixMFD_Coupled_Surf_" << 0 << ".txt";
+    EpetraExt::RowMatrixToMatlabFile(filename_s.str().c_str(), *P2f2f_);
+  }
+
+  delete[] indicesA;
+  delete[] indicesB;
+  delete[] valuesA;
+  delete[] valuesB;
+  delete[] gsubsurfindices;
+  delete[] gsubsurfvalues;
+}
+
+
+int
+MatrixMFD_Coupled_Surf::Apply(const TreeVector& X,
+        TreeVector& Y) const {
+  // Apply the coupled Matrix, which gets the subsurface.
+  int ierr = MatrixMFD_Coupled::Apply(X,Y);
+  ASSERT(!ierr);
+
+  // Add in the Surface components
+  Teuchos::RCP<const CompositeVector> XA = X.SubVector(0)->Data();
+  Teuchos::RCP<const CompositeVector> XB = X.SubVector(1)->Data();
+  Teuchos::RCP<CompositeVector> YA = Y.SubVector(0)->Data();
+  Teuchos::RCP<CompositeVector> YB = Y.SubVector(1)->Data();
+
+  // Manually copy data -- TRILINOS FAIL
+  const Epetra_MultiVector& XA_f = *XA->ViewComponent("face",false);
+  const Epetra_MultiVector& XB_f = *XB->ViewComponent("face",false);
+  Epetra_MultiVector surf_XA(surface_mesh_->cell_map(false),1);
+  Epetra_MultiVector surf_XB(surface_mesh_->cell_map(false),1);
+
+  for (int sc=0; sc!=surf_XA.MyLength(); ++sc) {
+    AmanziMesh::Entity_ID f = surface_mesh_->entity_get_parent(AmanziMesh::CELL, sc);
+    surf_XA[0][sc] = XA_f[0][f];
+    surf_XB[0][sc] = XB_f[0][f]; 
+ }
+
+  // Apply the surface-only operators, blockwise
+  Epetra_MultiVector surf_YA(surface_mesh_->cell_map(false),1);
+  Epetra_MultiVector surf_YB(surface_mesh_->cell_map(false),1);
+
+  // -- A_surf * lambda_p ...
+  ierr |= surface_A_->Apply(surf_XA, surf_YA);
+
+  // -- + Ccc_surf * lambda_T
+  ierr |= surf_YA.Multiply(scaling_, *Ccc_surf_, surf_XB, 1.0);
+
+  // -- B_surf * lambda_T ...
+  ierr |= surface_B_->Apply(surf_XB, surf_YB);
+
+  // -- + Dcc_surf * lambda_p
+  ierr |= surf_YB.Multiply(scaling_, *Dcc_surf_, surf_XA, 1.0);
+  ASSERT(!ierr);
+  
+  // Add back into Y
+  Epetra_MultiVector& YA_f = *YA->ViewComponent("face",false);
+  Epetra_MultiVector& YB_f = *YB->ViewComponent("face",false);
+  for (int sc=0; sc!=surf_YA.MyLength(); ++sc) {
+    AmanziMesh::Entity_ID f = surface_mesh_->entity_get_parent(AmanziMesh::CELL, sc);
+    YA_f[0][f] += surf_YA[0][sc];
+    YB_f[0][f] += surf_YB[0][sc];
+  }
+  return ierr;
+}
+
+
+} // namespace
+} // namespace
