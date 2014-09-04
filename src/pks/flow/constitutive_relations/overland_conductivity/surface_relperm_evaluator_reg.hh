36cb12a56ce9eb4584b787633c752881ac073aa1
shelef <shelef@lanl.gov>
2014-09-04 15:57:05 -0600

    C debug - results now match Xus at day1 and <10% diff at day 360

---
+++ src/pks/flow/constitutive_relations/overland_conductivity/surface_relperm_evaluator_reg.hh
@@ -0,0 +1,22 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+
+/*
+  Evaluates the conductivity of surface flow.
+
+  Authors: Ethan Coon (ecoon@lanl.gov)
+*/
+
+#include "surface_relperm_evaluator.hh"
+
+namespace Amanzi {
+namespace Flow {
+namespace FlowRelations {
+
+// registry of method
+Utils::RegisteredFactory<FieldEvaluator,SurfaceRelPermEvaluator>
+SurfaceRelPermEvaluator::fac_("surface rel perm");
+
+} //namespace
+} //namespace
+} //namespace
+
---
+++ src/pks/flow/constitutive_relations/wrm/models/wrm_permafrost_factory_reg.hh
@@ -0,0 +1,16 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+/* -------------------------------------------------------------------------
+
+   ATS
+   Author: Ethan Coon
+
+   Self-registering factory for WRM implementations.
+   ------------------------------------------------------------------------- */
+
+#include "wrm_permafrost_factory.hh"
+
+// explicity instantitate the static data of Factory<WRM>
+template<> 
+Amanzi::Utils::Factory<Amanzi::Flow::FlowRelations::WRMPermafrostModel>::map_type* 
+Amanzi::Utils::Factory<Amanzi::Flow::FlowRelations::WRMPermafrostModel>::map_;
+
