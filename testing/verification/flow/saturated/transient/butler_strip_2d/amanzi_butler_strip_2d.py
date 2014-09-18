6327b36b3876485876a9c2f7bdd1542d1ec8bd40
Zhiming Lu <zhiming@lanl.gov>
2014-09-18 10:12:25 -0600

    Add documentation for linear strip, pod, and bounded domain cases

---
+++ testing/verification/flow/saturated/transient/butler_strip_2d/amanzi_butler_strip_2d.py
@@ -0,0 +1,70 @@
+import sys
+#sys.path.append('.')
+sys.path.append('../../../../../../tools/amanzi_xml')
+sys.path.append('../../../../../../tools/prettytable')
+import matplotlib.pyplot as plt
+import numpy
+import math
+from amanzi_xml.observations.ObservationXML import ObservationXML as ObsXML
+from amanzi_xml.observations.ObservationData import ObservationData as ObsDATA
+import amanzi_xml.utils.search as search
+import prettytable
+
+# load the data from output file, which is in amanzi-output
+def load_amanzi_obs():
+    obs_data_file = open('amanzi-output/observations.out', 'r')
+    obs_data_file.readline()
+    obs_data_file.readline()
+    obs_data = [[float(line.split(',')[4]),float(line.split(',')[5])] for line in obs_data_file]
+    return obs_data
+
+# load data from analytic solution
+def load_ana_solution():
+    ana_data =numpy.genfromtxt('analytic/drdn.res')
+    return ana_data
+
+def plottest(axes1, obs_data, ana_data):
+
+    axes1.set_ylabel('Drawdown [m]')
+    axes1.set_xlabel('Time after pumping [days]')
+    axes1.set_title('Drawdown at Two Observation Wells with Distance r1  = 24m and r2 = 100m')
+    
+    ntime1 = len(ana_data[:,0])/2
+    ntime2= len(ana_data[:,0])
+
+    ntime3 = len(obs_data[:,0])/2
+    ntime4 = len(obs_data[:,0])
+    axes1.plot(numpy.log10(ana_data[0:ntime1,0]), ana_data[0:ntime1,1], '-r', label='Butler Strip Solution: r=24m')
+    axes1.plot(numpy.log10(obs_data[0:ntime3,0]), obs_data[0:ntime3,1], 'ro', label='Amanzi: r=24m')
+    axes1.plot(numpy.log10(ana_data[ntime1+1:ntime2,0]), ana_data[ntime1+1:ntime2,1], '-b', label='Butler Strip Solution: r=100m')
+    axes1.plot(numpy.log10(obs_data[ntime3+1:ntime4,0]), obs_data[ntime3+1:ntime4,1], 'bo', label='Amanzi: r=100m')
+
+    axes1.legend(loc='upper left')
+
+if __name__ == "__main__":
+
+    import os
+    import run_amanzi
+
+    input_filename =os.path.join("amanzi_butler_strip_2d.xml")
+
+    CWD = os.getcwd()
+    try: 
+        run_amanzi.run_amanzi('../'+input_filename)
+        obs_data = load_amanzi_obs()
+        obs_data = numpy.array(obs_data) 
+        obs_data[:,0] =  obs_data[:,0]/24./3600.
+        ana_data=load_ana_solution()
+        ana_data[:,0] =  ana_data[:,0]/24./3600.
+      
+
+        fig1= plt.figure()
+        axes1=fig1.add_axes([.1,.1,.8,.8])
+       
+        plottest(axes1,obs_data, ana_data)
+#        plt.show()
+
+    finally:
+        os.chdir(CWD)
+
+
