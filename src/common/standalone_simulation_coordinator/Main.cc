03aea07db5a6f422563a045dd9380c023c6f0932
Erin Barker <erin.barker@pnnl.gov>
2014-09-18 09:19:57 -0700

    Branch 0.81.5 clean up.

--- src/common/standalone_simulation_coordinator/Main.cc
+++ src/common/standalone_simulation_coordinator/Main.cc
@@ -310,7 +310,6 @@ int main(int argc, char *argv[]) {
       // if amanzi_input  - validate, convert to old
 
       xercesc::XMLString::release(&temp2) ;
-      doc->release();
       delete parser;
       xercesc::XMLPlatformUtils::Terminate();
     }
