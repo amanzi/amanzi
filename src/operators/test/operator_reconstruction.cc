55b4a5122a6e3c41077ec611c499eaa02ccdec7a
Konstantin Lipnikov <lipnikov@lanl.gov>
2015-01-29 17:58:09 -0700

    Added polygonal mesh to the test on limiters.

--- src/operators/test/operator_reconstruction.cc
+++ src/operators/test/operator_reconstruction.cc
@@ -651,14 +651,144 @@ TEST(RECONSTRUCTION_SMOOTH_FIELD_3D) {
       error[0] /= norm;
       error[1] /= norm;
       error[2] /= norm;
-      // CHECK(error[0] < 1.0 / n);
-      // CHECK(error[1] < 1.0 / n);
-      // CHECK(error[2] < 1.0 / n);
+      CHECK(error[0] < 1.0 / n);
+      CHECK(error[1] < 1.0 / n);
+      CHECK(error[2] < 1.0 / n);
   
       if (MyPID == 0)
           printf("%9s: rel errors: %10.6f %10.6f %10.6f\n", LIMITERS[i].c_str(), error[0], error[1], error[2]);
     }
   }
 }
+
+
+/* *****************************************************************
+* Convergece of limited functions in two dimensions.
+***************************************************************** */
+TEST(RECONSTRUCTION_SMOOTH_FIELD_2D_POLYMESH) {
+  using namespace Amanzi;
+  using namespace Amanzi::AmanziMesh;
+  using namespace Amanzi::AmanziGeometry;
+  using namespace Amanzi::Operators;
+
+  Epetra_MpiComm comm(MPI_COMM_WORLD);
+  int MyPID = comm.MyPID();
+  if (MyPID == 0) std::cout << "\nTest: Accuracy on a smooth field on polygonal mesh." << std::endl;
+
+  // load polygonal mesh
+  Teuchos::ParameterList region_list;
+  GeometricModelPtr gm = new GeometricModel(2, region_list, &comm);
+
+  FrameworkPreference pref;
+  pref.clear();
+  pref.push_back(MSTK);
+  pref.push_back(STKMESH);
+
+  MeshFactory meshfactory(&comm);
+  meshfactory.preference(pref);
+
+  Teuchos::RCP<const Mesh> mesh = meshfactory("test/median32x33.exo", gm);
+
+  // create and initialize cell-based field ussing f(x,y) = x^2 y + 2 x y^3
+  Teuchos::RCP<Epetra_MultiVector> field = Teuchos::rcp(new Epetra_MultiVector(mesh->cell_map(true), 1));
+  Epetra_MultiVector grad_exact(mesh->cell_map(false), 2);
+
+  int ncells_owned = mesh->num_entities(AmanziMesh::CELL, AmanziMesh::OWNED);
+  int ncells_wghost = mesh->num_entities(AmanziMesh::CELL, AmanziMesh::USED);
+
+  for (int c = 0; c < ncells_wghost; c++) {
+    const AmanziGeometry::Point& xc = mesh->cell_centroid(c);
+    double x = xc[0], y = xc[1];
+    (*field)[0][c] = x*x*y + 2*x*y*y*y;
+    if (c < ncells_owned) {
+      grad_exact[0][c] = 2*x*y + 2*y*y*y;
+      grad_exact[1][c] = x*x + 6*x*y*y;
+    }
+  }
+
+  // Create and initialize flux
+  // Since limiters do not allow maximum on the outflow bounadry, 
+  // we use this trick: re-entering flow everywhere.
+  const Epetra_Map& fmap = mesh->face_map(true);
+  Teuchos::RCP<Epetra_MultiVector> flux = Teuchos::rcp(new Epetra_MultiVector(fmap, 1));
+
+  int nfaces_wghost = mesh->num_entities(AmanziMesh::FACE, AmanziMesh::USED);
+  int nnodes_wghost = mesh->num_entities(AmanziMesh::NODE, AmanziMesh::USED);
+  AmanziGeometry::Point velocity(1.0, 2.0), center(0.5, 0.5);
+
+  for (int f = 0; f < nfaces_wghost; f++) {
+    const AmanziGeometry::Point& xf = mesh->face_centroid(f);
+    velocity = center - xf;
+    const AmanziGeometry::Point& normal = mesh->face_normal(f);
+    (*flux)[0][f] = (velocity * normal) / mesh->face_area(f);
+  }
+
+  for (int i = 0; i < 3; i++) {
+    std::vector<int> bc_model;
+    std::vector<double> bc_value;
+    Teuchos::ParameterList plist;
+    if (i == 0) {
+      plist.set<std::string>("limiter", "Barth-Jespersen");
+    } else if (i == 1) {
+      plist.set<std::string>("limiter", "tensorial");
+    } else if (i == 2) {
+      plist.set<std::string>("limiter", "Kuzmin");
+    }
+
+    if (i < 2) {
+      bc_model.assign(nfaces_wghost, 0);
+      bc_value.assign(nfaces_wghost, 0.0);
+
+      for (int f = 0; f < nfaces_wghost; f++) {
+        const AmanziGeometry::Point& xf = mesh->face_centroid(f);
+        double x = xf[0], y = xf[1];
+        if (fabs(xf[0]) < 1e-6 || fabs(1.0 - xf[0]) < 1e-6 ||
+            fabs(xf[1]) < 1e-6 || fabs(1.0 - xf[1]) < 1e-6) {
+          bc_model[f] = OPERATOR_BC_DIRICHLET;
+          bc_value[f] = x*x*y + 2*x*y*y*y;
+        }
+      }
+    } else {
+      bc_model.assign(nnodes_wghost, 0);
+      bc_value.assign(nnodes_wghost, 0.0);
+      AmanziGeometry::Point xv(2);
+
+      for (int v = 0; v < nnodes_wghost; v++) {
+        mesh->node_get_coordinates(v, &xv);
+        double x = xv[0], y = xv[1];
+        if (fabs(xv[0]) < 1e-6 || fabs(1.0 - xv[0]) < 1e-6 ||
+            fabs(xv[1]) < 1e-6 || fabs(1.0 - xv[1]) < 1e-6) {
+          bc_model[v] = OPERATOR_BC_DIRICHLET;
+          bc_value[v] = x*x*y + 2*x*y*y*y;
+        }
+      }
+    } 
+
+    // Compute reconstruction
+    ReconstructionCell lifting(mesh);
+    lifting.Init(field, plist);
+    lifting.Compute(); 
+
+    // Apply limiter
+    lifting.InitLimiter(flux);
+    lifting.ApplyLimiter(bc_model, bc_value);
+
+    // calculate gradient error
+    Epetra_MultiVector grad_computed(*lifting.gradient()->ViewComponent("cell"));
+    grad_computed.Update(-1.0, grad_exact, 1.0);
+
+    double error[2], norms[2];
+    grad_computed.Norm2(error);
+    grad_exact.Norm2(norms);
+
+    error[0] /= norms[0];
+    error[1] /= norms[1];
+  
+    if (MyPID == 0)
+        printf("%9s: rel errors: %10.6f %10.6f\n", LIMITERS[i].c_str(), error[0], error[1]);
+  }
+}
+
+
 
 
