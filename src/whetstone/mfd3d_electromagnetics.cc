4b9d588f2b37dc79db52234ab6303da68f3a9d01
Konstantin Lipnikov <lipnikov@lanl.gov>
2015-03-20 11:17:39 -0600

    Added more member functons to diffsuion and electromagnetics. Improved code documentation of underlying algorithms. closes #1489

--- src/whetstone/mfd3d_electromagnetics.cc
+++ src/whetstone/mfd3d_electromagnetics.cc
@@ -26,7 +26,8 @@ namespace WhetStone {
 
 /* ******************************************************************
 * Consistency condition for the mass matrix in electromagnetics.
-* Only the upper triangular part of Ac is calculated.
+* Only the upper triangular part of Mc = R (R^T N)^{-1} R^T is 
+* calculated. Here R^T N = |c| T.
 ****************************************************************** */
 int MFD3D_Electromagnetics::L2consistency(int c, const Tensor& T,
                                           DenseMatrix& N, DenseMatrix& Mc)
@@ -101,12 +102,98 @@ int MFD3D_Electromagnetics::L2consistency(int c, const Tensor& T,
     }
   }
 
-  // Matrix N are simply tangents
+  // Rows of matrix N are simply tangents. Since N goes to the
+  // Gramm-Schmidt orthogonalizetion procedure, we can skip scaling
+  // tensorial factor T.
   for (int i = 0; i < nedges; i++) {
     int e = edges[i];
     const AmanziGeometry::Point& tau = mesh_->edge_vector(e);
     double len = mesh_->edge_length(e);
     for (int k = 0; k < d; ++k) N(i, k) = tau[k] / len;
+  }
+
+  return WHETSTONE_ELEMENTAL_MATRIX_OK;
+}
+
+
+/* ******************************************************************
+* Consistency condition for inverse of the mass matrix in 
+* electromagnetics. Only the upper triangular part of matrix 
+* Wc = N (N^T R)^{-1} N^T is calculated. Here N^T R = |c| T.
+****************************************************************** */
+int MFD3D_Electromagnetics::L2consistencyInverse(
+    int c, const Tensor& T, DenseMatrix& R, DenseMatrix& Wc)
+{
+  Entity_ID_List edges, faces;
+  std::vector<int> fdirs, edirs, map;
+
+  mesh_->cell_get_faces_and_dirs(c, &faces, &fdirs);
+  int nfaces = faces.size();
+
+  int n1, n2, d = mesh_->space_dimension();
+  ASSERT(d == 3);
+  AmanziGeometry::Point v1(d), v2(d), v3(d), tau(d), p1(d), p2(d);
+  AmanziGeometry::Point vv[3];
+
+  // Since the matrix N is the matrix of scaled tangent vextors,
+  // N = N0 T, we use the tensor T.
+  const AmanziGeometry::Point& xc = mesh_->cell_centroid(c);
+  double volume = mesh_->cell_volume(c);
+
+  mesh_->cell_get_edges(c, &edges);
+  int nedges = edges.size();
+
+  for (int i = 0; i < nedges; i++) {
+    int e1 = edges[i];
+    const AmanziGeometry::Point& v1 = mesh_->edge_vector(e1);
+    double a1 = mesh_->edge_length(e1);
+    v3 = T * v1;
+
+    for (int j = i; j < nedges; j++) {
+      int e2 = edges[j];
+      const AmanziGeometry::Point& v2 = mesh_->edge_vector(e2);
+      double a2 = mesh_->edge_length(e2);
+      Wc(i, j) = (v2 * v3) / (a1 * a2 * volume);
+    }
+  }
+
+  // Calculate matrix R
+  R.PutScalar(0.0);
+
+  for (int i = 0; i < nfaces; ++i) {
+    int f = faces[i];
+    const AmanziGeometry::Point& xf = mesh_->face_centroid(f);
+    const AmanziGeometry::Point& normal = mesh_->face_normal(f);
+    double area = mesh_->face_area(f);
+
+    v1 = xc - xf; 
+ 
+    double a1 = normal * v1;
+    for (int k = 0; k < d; ++k) {
+      vv[k].set(normal[k] * v1);
+      vv[k][k] -= a1;
+    }
+
+    mesh_->face_get_edges_and_dirs(f, &edges, &edirs);
+    int nedges = edges.size();
+
+    mesh_->face_to_cell_edge_map(f, c, &map);
+
+    for (int m = 0; m < nedges; ++m) {
+      int e = edges[m];
+      mesh_->edge_get_nodes(e, &n1, &n2);
+      mesh_->node_get_coordinates(n1, &p1);
+      mesh_->node_get_coordinates(n2, &p2);
+ 
+      v3 = ((p1 + p2) / 2) - xf;
+
+      double len = mesh_->edge_length(e);
+      len /= 2.0 * area * area * fdirs[i] * edirs[m];
+
+      for (int k = 0; k < d; ++k) {
+        R(map[m], k) -= len * ((vv[k]^v3) * normal);
+      }
+    }
   }
 
   return WHETSTONE_ELEMENTAL_MATRIX_OK;
@@ -238,6 +325,46 @@ int MFD3D_Electromagnetics::MassMatrixOptimized(int c, const Tensor& T, DenseMat
 
 
 /* ******************************************************************
+* Inverse of the mass matrix for edge-based discretization.
+****************************************************************** */
+int MFD3D_Electromagnetics::MassMatrixInverse(int c, const Tensor& T, DenseMatrix& W)
+{
+  int d = mesh_->space_dimension();
+  int nrows = W.NumRows();
+
+  DenseMatrix R(nrows, d);
+  DenseMatrix Wc(nrows, nrows);
+
+  int ok = L2consistencyInverse(c, T, R, Wc);
+  if (ok) return WHETSTONE_ELEMENTAL_MATRIX_WRONG;
+
+  StabilityScalar(c, R, Wc, W);
+  return WHETSTONE_ELEMENTAL_MATRIX_OK;
+}
+
+
+/* ******************************************************************
+* Inverse of matrix matrix for edge-based discretization optimized
+* for starcity.
+****************************************************************** */
+int MFD3D_Electromagnetics::MassMatrixInverseOptimized(
+    int c, const Tensor& T, DenseMatrix& W)
+{
+  int d = mesh_->space_dimension();
+  int nrows = W.NumRows();
+
+  DenseMatrix R(nrows, d);
+  DenseMatrix Wc(nrows, nrows);
+
+  int ok = L2consistencyInverse(c, T, R, Wc);
+  if (ok) return WHETSTONE_ELEMENTAL_MATRIX_WRONG;
+
+  ok = StabilityOptimized(T, R, Wc, W);
+  return ok;
+}
+
+
+/* ******************************************************************
 * Stiffness matrix: the standard algorithm.
 ****************************************************************** */
 int MFD3D_Electromagnetics::StiffnessMatrix(int c, const Tensor& T, DenseMatrix& A)
@@ -256,6 +383,26 @@ int MFD3D_Electromagnetics::StiffnessMatrix(int c, const Tensor& T, DenseMatrix&
   return WHETSTONE_ELEMENTAL_MATRIX_OK;
 }
 
+
+/* ******************************************************************
+* Stiffness matrix optimized for sparsity.
+****************************************************************** */
+int MFD3D_Electromagnetics::StiffnessMatrixOptimized(
+    int c, const Tensor& T, DenseMatrix& A)
+{
+  int d = mesh_->space_dimension();
+  int nedges = A.NumRows();
+
+  int nd = d * (d + 1) / 2;
+  DenseMatrix N(nedges, nd);
+  DenseMatrix Ac(nedges, nedges);
+
+  int ok = H1consistency(c, T, N, Ac);
+  if (ok) return WHETSTONE_ELEMENTAL_MATRIX_WRONG;
+
+  ok = StabilityOptimized(T, N, Ac, A);
+  return ok;
+}
 
 }  // namespace WhetStone
 }  // namespace Amanzi
