703469ddcd106af7e7322688d65ac699b5fa507c
Ethan Coon <ecoon@lanl.gov>
2015-05-04 22:47:14 -0600

    adds Additive evaluator, which simply adds (with a potential constant scaling) multiple other variables

--- src/constitutive_relations/generic_evaluators/MultiplicativeEvaluator.cc
+++ src/constitutive_relations/generic_evaluators/MultiplicativeEvaluator.cc
@@ -1,5 +1,3 @@
-/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
-
 /*
   MultiplicativeEvaluator is the generic evaluator for multipying two vectors.
 
@@ -14,15 +12,62 @@ namespace Relations {
 MultiplicativeEvaluator::MultiplicativeEvaluator(Teuchos::ParameterList& plist) :
     SecondaryVariableFieldEvaluator(plist) {}
 
-MultiplicativeEvaluator(const MultiplicativeEvaluator& other) :
-  virtual Teuchos::RCP<FieldEvaluator> Clone() const;
+MultiplicativeEvaluator::MultiplicativeEvaluator(const MultiplicativeEvaluator& other) :
+    SecondaryVariableFieldEvaluator(other) {}
 
-  // Required methods from SecondaryVariableFieldEvaluator
-  virtual void EvaluateField_(const Teuchos::Ptr<State>& S,
-          const Teuchos::Ptr<CompositeVector>& result);
-  virtual void EvaluateFieldPartialDerivative_(const Teuchos::Ptr<State>& S,
-          Key wrt_key, const Teuchos::Ptr<CompositeVector>& result);
+Teuchos::RCP<FieldEvaluator>
+MultiplicativeEvaluator::Clone() const
+{
+  return Teuchos::rcp(new MultiplicativeEvaluator(*this));
+}
+
+
+// Required methods from SecondaryVariableFieldEvaluator
+void
+MultiplicativeEvaluator::EvaluateField_(const Teuchos::Ptr<State>& S,
+        const Teuchos::Ptr<CompositeVector>& result)
+{
+  ASSERT(dependencies_.size() > 1);
+  KeySet::const_iterator key = dependencies_.begin();
+  *result = *S->GetFieldData(*key);
+  key++;
+
+  for (; key!=dependencies_.end(); ++key) {
+    Teuchos::RCP<const CompositeVector> dep = S->GetFieldData(*key);
+    for (CompositeVector::name_iterator lcv=result->begin(); lcv!=result->end(); ++lcv) {
+      Epetra_MultiVector& res_c = *result->ViewComponent(*lcv, false);
+      const Epetra_MultiVector& dep_c = *dep->ViewComponent(*lcv, false);
+
+      for (int c=0; c!=res_c.MyLength(); ++c) res_c[0][c] *= dep_c[0][c];
+    }
+  }
+}
+
+void
+MultiplicativeEvaluator::EvaluateFieldPartialDerivative_(const Teuchos::Ptr<State>& S,
+        Key wrt_key, const Teuchos::Ptr<CompositeVector>& result)
+{
+  ASSERT(dependencies_.size() > 1);
+
+  KeySet::const_iterator key = dependencies_.begin();
+  while (*key == wrt_key) key++;
+  *result = *S->GetFieldData(*key);
+  key++;
+
+  for (; key!=dependencies_.end(); ++key) {
+    if (*key != wrt_key) {
+      Teuchos::RCP<const CompositeVector> dep = S->GetFieldData(*key);
+      for (CompositeVector::name_iterator lcv=result->begin(); lcv!=result->end(); ++lcv) {
+        Epetra_MultiVector& res_c = *result->ViewComponent(*lcv, false);
+        const Epetra_MultiVector& dep_c = *dep->ViewComponent(*lcv, false);
+
+        for (int c=0; c!=res_c.MyLength(); ++c) res_c[0][c] *= dep_c[0][c];
+      }
+    }
+  }
+}
 
 
 } // namespace
 } // namespace
+
