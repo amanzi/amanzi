88d47798bdaf932fa21b933934407d1d158be3d4
Jeffrey Johnson <jnjohnson@lbl.gov>
2015-06-02 16:42:16 -0700

    First cut at an apples-to-apples comparison.

--- tools/testing/compare_field_results.py
+++ tools/testing/compare_field_results.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, os.path, sys
 import h5py
 import numpy as np
 
@@ -57,34 +56,97 @@ def GetXY_AmanziS(path,root,comp):
     
     return (y, v)
 
-def find_output_data_path(input_xml):
+def parse_input_xml(input_xml):
+    """Parses the Amanzi input XML file and returns a tuple (type, output_file), 
+where type is 'structured' or 'unstructured', and output_file is the full path 
+of the most recent output file to be used in the comparison."""
     import xml.etree.ElementTree as ET
     tree = ET.parse(input_xml)
     root = tree.getroot()
     if root.tag != 'amanzi_input':
-        raise RuntimeError, 'The given XML file is not an Amanzi input.'
-    output = root.find('output')
-    observations = output.find('observations')
-    obs_file = observations.find('filename').text
-    return obs_file
+        raise RuntimeError, 'The given XML file is not a valid Amanzi input.'
+    if 'type' not in root.attrib.keys():
+        raise RuntimeError, 'Could not find a type (structured/unstructured) in the given Amanzi input.'
 
-def compute_error_norms(x_output, c_output, x_ref, c_ref):
-    errors = {}
-    for region in x_ref.keys():
-        diff = c_ref[region] - c_output[region]
-        errors[region] = np.linalg.norm(diff)
-    return errors
+    # Find the simulation type.
+    file_type = root.attrib['type'].lower()
+    if file_type not in ['structured', 'unstructured']:
+        raise RuntimeError, 'Invalid simulation type in given Amanzi input: %s'%file_type
+
+    # Now find the output prefix.
+    output = root.find('output')
+    vis = output.find('vis')
+    vis_prefix = vis.find('base_filename').text
+
+    # Search on the disk for the most recent vis file.
+    class Walker:
+        pass
+    walker = Walker()
+    walker.prefix = vis_prefix
+    walker.vis_files = []
+
+    def append_vis_file(walker, dir_name, files):
+        walker.vis_files.extend([os.path.join(dir_name, f) for f in files if (walker.prefix in f and '_mesh' not in f)])
+
+    os.path.walk(os.path.dirname(input_xml), append_vis_file, walker)
+    walker.vis_files.sort()
+
+    return file_type, walker.vis_files[-1]
+
+def compute_error_norm(x_output, c_output, x_ref, c_ref):
+    diff = c_ref - c_output
+    return np.linalg.norm(diff)
+
+def compare_structured_data(output_file, ref_file, field):
+    out_dir = os.path.dirname(output_file)
+    out_prefix = os.path.basename(output_file).replace('_data.h5', '')
+
+    ref_dir = os.path.dirname(ref_file)
+    ref_prefix = os.path.basename(ref_file).replace('_data.h5', '')
+
+    # Now fetch the data from the output and reference files.
+    x_output, c_output = GetXY_AmanziS(out_dir, out_prefix, field)
+    x_ref, c_ref = GetXY_AmanziS(ref_dir, ref_prefix, field)
+
+    # Compute the error norm for the field.
+    error = compute_error_norm(x_output, c_output, x_ref, c_ref)
+
+    # Report.
+    if (error > tolerance):
+        sys.exit('error %g > tolerance %g for field %s'%(error, tolerance, field))
+    else:
+        sys.exit('Comparison Passed')
+
+def compare_unstructured_data(output_file, ref_file, field):
+    out_dir = os.path.dirname(output_file)
+    out_prefix = os.path.basename(output_file).replace('_data.h5', '')
+
+    ref_dir = os.path.dirname(ref_file)
+    ref_prefix = os.path.basename(ref_file).replace('_data.h5', '')
+
+    # Now fetch the data from the output and reference files.
+    x_output, c_output = GetXY_AmanziU(out_dir, out_prefix, field)
+    x_ref, c_ref = GetXY_AmanziU(ref_dir, ref_prefix, field)
+
+    # Compute the error norm for the field.
+    error = compute_error_norm(x_output, c_output, x_ref, c_ref)
+
+    # Report.
+    if (error > tolerance):
+        sys.exit('error %g > tolerance %g for field %s'%(error, tolerance, field))
+    else:
+        sys.exit('Comparison Passed')
 
 if __name__ == "__main__":
 
     # Check argument number.
     if len(sys.argv) != 5:
         print('%s: usage:'%sys.argv[0])
-        print('%s observation input.xml reference.obs tolerance'%sys.argv[0])
+        print('%s field input.xml reference.h5 tolerance'%sys.argv[0])
         exit(0)
 
     # Get arguments.
-    observation = sys.argv[1]
+    field = sys.argv[1]
     input_xml = sys.argv[2]
     ref_file = sys.argv[3]
     tolerance = float(sys.argv[4])
@@ -101,27 +163,12 @@ if __name__ == "__main__":
         exit(0)
 
     # Root through the input file and find out where the output lives.
-    obs_file = find_output_observation_path(input_xml)
-    if not os.path.exists(obs_file):
-        print('%s: output file %s does not exist.'%(sys.argv[0], obs_file))
+    sim_type, output_file = parse_input_xml(input_xml)
+    if not os.path.exists(output_file):
+        print('%s: output file %s does not exist.'%(sys.argv[0], output_file))
 
-    # Now fetch the data from the output and reference files.
-    x_output, c_output = get_observation_data(obs_file, observation)
-    x_ref, c_ref = get_observation_data(ref_file, observation)
-
-    # Compute the error norms for each of the observations in their respective regions.
-    errors = compute_error_norms(x_output, c_output, x_ref, c_ref)
-    msg = ""
-
-    # Report.
-    total_errors = 0
-    error_lines = []
-    for region, error in errors.items():
-        if (error > tolerance):
-            total_errors += 1
-            error_lines.append('  error %g > tolerance %g in region %s'%(error, tolerance, region))
-
-    if total_errors == 0:
-        sys.exit('Comparison Passed')
+    if sim_type == 'structured':
+        compare_structured_data(output_file, ref_file, field)
     else:
-        sys.exit('\n'.join(['Comparison Failed for observation %s:'%observation] + error_lines))
+        compare_unstructured_data(output_file, ref_file, field)
+
