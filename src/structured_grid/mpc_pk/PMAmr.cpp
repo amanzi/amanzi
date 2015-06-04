51d462d366ed97ead13571bef880535d0ebb7731
Marc Day <MSDay@lbl.gov>
2015-06-03 20:11:23 -0700

    Fix minor error in out stream for structured observations

--- src/structured_grid/mpc_pk/PMAmr.cpp
+++ src/structured_grid/mpc_pk/PMAmr.cpp
@@ -1404,7 +1404,7 @@ void PMAmr::FlushObservations(std::ostream& out)
   // print out observations
   if (observations.size() && ParallelDescriptor::IOProcessor()) {
 
-    out.precision(16);
+    const int old_prec = out.precision(16);
     out.setf(std::ios::scientific);
 
     out << "Observation Name, Region, Functional, Variable, Time, Value\n";
@@ -1422,7 +1422,8 @@ void PMAmr::FlushObservations(std::ostream& out)
             << ", " << it->second << std::endl;
       }
     }
-    std::cout << "\n";
+    out << "\n";
+    out.precision(old_prec);
   }
 }
 
