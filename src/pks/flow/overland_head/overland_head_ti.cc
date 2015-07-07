77eec7dbe3c195332fed855f4901c0b1effeb741
Eugene Kikinzon <kikinzon@lanl.gov>
2015-07-07 15:31:20 -0600

    Updates for scaled constraint implementation in overland and overland_head

--- src/pks/flow/overland_head/overland_head_ti.cc
+++ src/pks/flow/overland_head/overland_head_ti.cc
@@ -346,5 +346,98 @@ void OverlandHeadFlow::UpdatePreconditioner(double t, Teuchos::RCP<const TreeVec
   */
 };
 
+// -----------------------------------------------------------------------------
+// Default enorm that uses an abs and rel tolerance to monitor convergence.
+// -----------------------------------------------------------------------------
+double OverlandHeadFlow::ErrorNorm(Teuchos::RCP<const TreeVector> u,
+                               Teuchos::RCP<const TreeVector> du) {
+  S_next_->GetFieldEvaluator(conserved_key_)->HasFieldChanged(S_next_.ptr(), name_);
+  const Epetra_MultiVector& conserved = *S_next_->GetFieldData(conserved_key_)
+  ->ViewComponent("cell",true);
+  const Epetra_MultiVector& cv = *S_next_->GetFieldData(cell_vol_key_)
+  ->ViewComponent("cell",true);
+  
+  // VerboseObject stuff.
+  Teuchos::OSTab tab = vo_->getOSTab();
+  if (vo_->os_OK(Teuchos::VERB_MEDIUM))
+    *vo_->os() << "ENorm (Infnorm) of: " << conserved_key_ << ": " << std::endl;
+  
+  Teuchos::RCP<const CompositeVector> dvec = du->Data();
+  double h = S_next_->time() - S_inter_->time();
+  
+  double enorm_val = 0.0;
+  for (CompositeVector::name_iterator comp=dvec->begin();
+       comp!=dvec->end(); ++comp) {
+    double enorm_comp = 0.0;
+    int enorm_loc = -1;
+    const Epetra_MultiVector& dvec_v = *dvec->ViewComponent(*comp, false);
+    
+    if (*comp == std::string("cell")) {
+      // error done relative to extensive, conserved quantity
+      int ncells = dvec->size(*comp,false);
+      for (unsigned int c=0; c!=ncells; ++c) {
+        double enorm_c = std::abs(h * dvec_v[0][c])
+        / (atol_*cv[0][c] + rtol_*std::abs(conserved[0][c]));
+        
+        if (enorm_c > enorm_comp) {
+          enorm_comp = enorm_c;
+          enorm_loc = c;
+        }
+      }
+      
+    } else if (*comp == std::string("face")) {
+      // error in flux -- relative to cell's extensive conserved quantity
+      int nfaces = dvec->size(*comp, false);
+      bool scaled_constraint = plist_->sublist("Diffusion").get<bool>("scaled constraint equation", false);
+      const Epetra_MultiVector& kr_f = *S_next_->GetFieldData("upwind_overland_conductivity")
+      ->ViewComponent("face",false);
+      
+      for (unsigned int f=0; f!=nfaces; ++f) {
+        AmanziMesh::Entity_ID_List cells;
+        mesh_->face_get_cells(f, AmanziMesh::OWNED, &cells);
+        double cv_min = cells.size() == 1 ? cv[0][cells[0]]
+        : std::min(cv[0][cells[0]],cv[0][cells[1]]);
+        double conserved_min = cells.size() == 1 ? conserved[0][cells[0]]
+        : std::min(conserved[0][cells[0]],conserved[0][cells[1]]);
+        
+        double enorm_f = fluxtol_ * h * std::abs(dvec_v[0][f])
+        / (atol_*cv_min + rtol_*std::abs(conserved_min));
+        if (scaled_constraint && (kr_f[0][f] < 1.0)) enorm_f *= kr_f[0][f];
+        if (enorm_f > enorm_comp) {
+          enorm_comp = enorm_f;
+          enorm_loc = f;
+        }
+      }
+      
+    } else {
+      Errors::Message msg;
+      msg << "Unused error component \"" << *comp << "\" in conserved quantity error norm.";
+      Exceptions::amanzi_throw(msg);
+    }
+    
+    // Write out Inf norms too.
+    if (vo_->os_OK(Teuchos::VERB_MEDIUM)) {
+      double infnorm(0.);
+      dvec_v.NormInf(&infnorm);
+      
+      ENorm_t err;
+      ENorm_t l_err;
+      l_err.value = enorm_comp;
+      l_err.gid = dvec_v.Map().GID(enorm_loc);
+      
+      int ierr = MPI_Allreduce(&l_err, &err, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+      ASSERT(!ierr);
+      *vo_->os() << "  ENorm (" << *comp << ") = " << err.value << "[" << err.gid << "] (" << infnorm << ")" << std::endl;
+    }
+    
+    enorm_val = std::max(enorm_val, enorm_comp);
+  }
+  
+  double enorm_val_l = enorm_val;
+  int ierr = MPI_Allreduce(&enorm_val_l, &enorm_val, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+  ASSERT(!ierr);
+  return enorm_val;
+}
+  
 }  // namespace Flow
 }  // namespace Amanzi
