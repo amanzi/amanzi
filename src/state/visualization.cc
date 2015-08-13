e8e2ea52b33241db35950e894dc657104d742c8e
Konstantin Lipnikov <lipnikov@lanl.gov>
2015-08-13 11:14:14 -0600

    Implemented new translation of "write_regions". Updated code and native documentation. refs# 1618 closes #1622

--- src/state/visualization.cc
+++ src/state/visualization.cc
@@ -55,8 +55,13 @@ void Visualization::ReadParameters_() {
   filebasename_ = plist_.get<std::string>("file name base","amanzi_vis");
   dynamic_mesh_ = plist_.get<bool>("dynamic mesh",false);
 
-  Teuchos::Array<std::string> default_no_regions(0);
-  regions_ = plist_.get<Teuchos::Array<std::string> >("Write Regions", default_no_regions);
+  Teuchos::Array<std::string> no_regions(0); 
+  Teuchos::ParameterList& tmp = plist_.sublist("Write Regions");
+  
+  regions_.clear();
+  for (Teuchos::ParameterList::ConstIterator it = tmp.begin(); it != tmp.end(); ++it) {
+    regions_[it->first] = tmp.get<Teuchos::Array<std::string> >(it->first, no_regions);
+  }
   write_partition_ = plist_.get<bool>("Write Partitions", false);
 }
 
@@ -97,25 +102,30 @@ void Visualization::WriteMesh(const double time, const int iteration) const {
 // -----------------------------------------------------------------------------
 void Visualization::WriteRegions() {
   if (regions_.size() > 0) {
-    // first make an Epetra_Vector to hold the region information
-    Epetra_MultiVector reg(mesh_->cell_map(false),1,true);
-    // loop over the regions and initialize the reg array
-    double reg_index = 1.0;
-    for (Teuchos::Array<std::string>::const_iterator reg_it = regions_.begin();
-         reg_it != regions_.end(); ++reg_it, reg_index += 1.0) {
-      // only do something if the user provided a valid region name
-      // for a region that consists of cells
-      if (mesh_->valid_set_name(*reg_it, AmanziMesh::CELL)) {
-        AmanziMesh::Entity_ID_List ids;
-        mesh_->get_set_entities(*reg_it, AmanziMesh::CELL, AmanziMesh::OWNED, &ids);
-        for (AmanziMesh::Entity_ID_List::const_iterator it = ids.begin(); it != ids.end(); ++it) {
-          reg[0][*it] = reg_index;
+    for (std::map<std::string, Teuchos::Array<std::string> >::const_iterator it = regions_.begin();
+         it != regions_.end(); ++it) {
+      // first make an Epetra_Vector to hold the region information
+      Epetra_MultiVector reg(mesh_->cell_map(false), 1, true);
+
+      // loop over the regions and initialize the reg array
+      double reg_index = 1.0;
+      for (Teuchos::Array<std::string>::const_iterator reg_it = (it->second).begin();
+         reg_it != (it->second).end(); ++reg_it, reg_index += 1.0) {
+        // only do something if the user provided a valid region name
+        // for a region that consists of cells
+        if (mesh_->valid_set_name(*reg_it, AmanziMesh::CELL)) {
+          AmanziMesh::Entity_ID_List ids;
+          mesh_->get_set_entities(*reg_it, AmanziMesh::CELL, AmanziMesh::OWNED, &ids);
+
+          for (AmanziMesh::Entity_ID_List::const_iterator it = ids.begin(); it != ids.end(); ++it) {
+            reg[0][*it] = reg_index;
+          }
         }
       }
+      std::vector<std::string> name; 
+      name.push_back(it->first);
+      WriteVector(reg, name);
     }
-    std::vector<std::string> name; 
-    name.push_back("regions");
-    WriteVector(reg, name);
   }
 }
 
