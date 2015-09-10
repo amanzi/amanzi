090205c4398f5a59d6ab8cc4feb57bd8e98d8cc9
Konstantin Lipnikov <lipnikov@lanl.gov>
2015-09-10 10:34:22 -0600

    Updated validation files. refs #1643

--- testing/verification/transport/saturated/steady-state/dispersion_45_point_2d/run_amanzi.py
+++ testing/verification/transport/saturated/steady-state/dispersion_45_point_2d/run_amanzi.py
@@ -1,7 +1,7 @@
 #### Running Amanzi to generate an output file ####
 import os, sys, shutil, subprocess, re
 
-def run_amanzi(input_file, directory=None, parameters=None):
+def run_amanzi(input_file, directory=None, parameters=None, mesh_file=None, overwrite=False):
 
     if directory is None:
         directory = "amanzi-output"
@@ -9,31 +9,44 @@ def run_amanzi(input_file, directory=None, parameters=None):
     CWD =os.getcwd()
     run_directory= os.path.join(CWD,directory)
 
-    if os.path.isdir(run_directory):
-        if os.listdir(run_directory):
-            return
-    else:
+    if not os.path.isdir(run_directory):
         os.mkdir(run_directory) 
-
 
     # Change to run directory
     os.chdir(run_directory)
 
+    # Obersvation file - should really come from the input file.
+    obs_file="obs5_2_1_r5.out"
+
+    # Return if there is already output and we aren't overwriting
+    if ( os.path.isfile(input_file) and os.path.isfile(obs_file) and not overwrite ):
+        os.chdir(CWD)
+        return
 
     # copy file
-    if parameters is None:
+    if parameters is None and mesh_file is None:
         shutil.copy(os.path.join("..",input_file), run_directory)
         parameters={}
     else:
         orig_file=open(os.path.join("..",input_file),'r')
         new_file=open(input_file,"w")
+        InMesh=False
         for line in orig_file:
-            for name in parameters:
-                if ( name in line ): 
-                    new_file.write(re.sub(r'value=.*"','value="'+parameters[name]+'"', line))
-                else:
+            if ( not InMesh ):
+                if ( "Read Mesh File" in line ):
+                    InMesh=True
                     new_file.write(line)
-
+                else:
+                    for name in parameters:
+                        if ( name in line ): 
+                            new_file.write(re.sub(r'value=.*"','value="'+parameters[name]+'"', line))
+                        else:
+                            new_file.write(line)
+            elif ( "File" in line ):
+                new_file.write(re.sub(r'value=.*"','value="'+mesh_file+'"', line))
+                InMesh = False
+            else:
+                new_file.write(line)
 
         orig_file.close()
         new_file.close()
@@ -45,6 +58,7 @@ def run_amanzi(input_file, directory=None, parameters=None):
     except KeyError:
         raise RuntimeError("Missing Amanzi installation, please set the AMANZI_INSTALL_DIR environmental variable.")
     executable = os.path.join(path, "amanzi")
+    print executable
 
     if not os.path.isfile(executable):
         raise RuntimeError("Missing Amanzi installation, please build and install Amanzi.")
