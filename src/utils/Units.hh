b349e8f5197701e08f971d0ba75e03ae7b58c1b0
Konstantin Lipnikov <lipnikov@lanl.gov>
2015-12-02 12:07:24 -0700

    Reverted changes due to 2D conversion problem. refs #1669.

--- src/utils/Units.hh
+++ src/utils/Units.hh
@@ -13,13 +13,19 @@
 #include <boost/units/make_scaled_unit.hpp>
 #include <boost/units/make_system.hpp>
 #include <boost/units/systems/si.hpp>
+#include <boost/units/unit.hpp>
+#include <boost/units/derived_dimension.hpp>
 
 #include "Teuchos_ParameterList.hpp"
 
 namespace Utils {
 
+// derived unit for concentration density functions
 typedef boost::units::make_scaled_unit<
     boost::units::si::volume, boost::units::scale<10, boost::units::static_rational<-3> > >::type liter;
+
+typedef boost::units::derived_dimension<boost::units::amount_base_dimension, 1,
+                                        boost::units::length_base_dimension, -3>::type concentration_si;
 
 class Units {
  public:
@@ -28,9 +34,12 @@ class Units {
   ~Units() {};
 
   double concentration_factor() {
+    return 1.0;
     return conversion_factor(boost::units::si::amount() / liter(),
                              boost::units::si::amount() / boost::units::si::volume());
   } 
+
+ private:
 };
 
 }  // namespace Utils
