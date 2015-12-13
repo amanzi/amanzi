9d7d2f3dd2a79c982f1948e8d23edb686817859f
ahmadjan <ahmadjan@localhost>
2015-12-12 20:34:26 -0500

    modified weak mpc semi coupled

--- src/pks/mpc/weak_mpc_semi_coupled.cc
+++ src/pks/mpc/weak_mpc_semi_coupled.cc
@@ -18,18 +18,19 @@ bool WeakMPCSemiCoupled::advance(double dt) {
 
   // advance surface_star-pressure from t_n to t_(n+1)
   fail = (*pk)->advance(dt);
-
+/*
   // copy surface_star-pressure at t_(n+1) to surface-pressure at t_n
-  *S_inter_->GetFieldData("surface-pressure",sub_pks_[1]->name()) =
+  *S_inter_->GetFieldData("surface-pressure",S_inter_->GetField("surface-pressure")->owner()) =
     *S_next_->GetFieldData("surface_star-pressure");
   CopySurfaceToSubsurface(*S_inter_->GetFieldData("surface-pressure"),
-			  S_inter_->GetFieldData("pressure", sub_pks_[1]->name()).ptr());
-  // NOTE: later do it in the setup or constructor --aj
+			  S_inter_->GetFieldData("pressure", S_inter_->GetField("pressure")->owner()).ptr());
+  // NOTE: later do it in the setup --aj
   Teuchos::RCP<PKBDFBase> pk_domain =
     Teuchos::rcp_dynamic_cast<PKBDFBase>(sub_pks_[1]);
-  ASSERT(pk_domain.get());
+  ASSERT(pk_domain.get()); // make sure the pk_domain is not empty
   pk_domain->ChangedSolution(S_inter_.ptr());
-  
+*/
+if(fail) return fail;  
   // advance surface-pressure from t_n to t_(n+1)
   ++pk;
   fail += (*pk)->advance(dt);
@@ -38,14 +39,14 @@ bool WeakMPCSemiCoupled::advance(double dt) {
   // copy surface-pressure at t_(n+1) to surface_star-pressure at t_n+1
   *S_next_->GetFieldData("surface_star-pressure",sub_pks_[0]->name()) =
     *S_next_->GetFieldData("surface-pressure");
-
+  
   // Mark surface_star-pressure evaluator as changed.
-  // NOTE: later do it in the setup or constructor --aj
+  // NOTE: later do it in the setup --aj
   Teuchos::RCP<PKBDFBase> pk_surf =
     Teuchos::rcp_dynamic_cast<PKBDFBase>(sub_pks_[0]);
   ASSERT(pk_surf.get());
   pk_surf->ChangedSolution();
-
+  
   return fail;
 };
  
