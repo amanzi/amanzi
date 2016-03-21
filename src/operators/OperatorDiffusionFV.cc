d7ecc61c2453b6efea4edd4e2dc47f9d9d8f1ef2
Ethan Coon <ecoon@lanl.gov>
2016-03-21 16:10:48 -0600

    we need an OperatorAudit with more tests for Operators -- fixes sign error when the mesh boundary face is oriented so that the face points inward

--- src/operators/OperatorDiffusionFV.cc
+++ src/operators/OperatorDiffusionFV.cc
@@ -291,7 +291,7 @@ void OperatorDiffusionFV::UpdateFlux(
       } else if (bc_model[f] == OPERATOR_BC_NEUMANN) {
         double value = bc_value[f];
         double area = mesh_->face_area(f);
-        flux[0][f] = value * area;
+        flux[0][f] = dirs[n] * value * area;
         
       } else {
         if (f < nfaces_owned && !flag[f]) {
