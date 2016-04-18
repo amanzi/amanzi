2f5b664b3d011384ec2f055bac9a3ca8f79d4804
Konstantin Lipnikov <lipnikov@lanl.gov>
2016-04-18 10:17:07 -0600

    Updated intersection of convex polytopes code. #refs 1688 General cleaning.

--- src/mpc/test/mpc_driver_impes.cc
+++ src/mpc/test/mpc_driver_impes.cc
@@ -42,87 +42,25 @@ using namespace std;
   driver_parameter_list = xmlreader.getParameters();
   
   // For now create one geometric model from all the regions in the spec
-  Teuchos::ParameterList reg_params = driver_parameter_list.sublist("regions");
-   
-  int spdim = 2;
-  Teuchos::RCP<Amanzi::AmanziGeometry::GeometricModel> geom_model_ptr =
-      Teuchos::rcp(new Amanzi::AmanziGeometry::GeometricModel(spdim, reg_params, comm));
+  Teuchos::ParameterList region_list = plist.get<Teuchos::ParameterList>("regions");
+  Teuchos::RCP<Amanzi::AmanziGeometry::GeometricModel> gm =
+      Teuchos::rcp(new Amanzi::AmanziGeometry::GeometricModel(2, region_list, &comm));
 
-  Amanzi::AmanziGeometry::Domain *simdomain_ptr = new Amanzi::AmanziGeometry::Domain(spdim);
+  // create mesh
+  FrameworkPreference pref;
+  pref.clear();
+  pref.push_back(MSTK);
 
-  simdomain_ptr->Add_Geometric_Model(geom_model_ptr);
-
-  // ---------------- MESH -----------------------------------------------
-  int rank, ierr, aerr, size;
-
-  // get the Mesh sublist
-  Teuchos::ParameterList mesh_parameter_list = driver_parameter_list.sublist("mesh");
-
-  Amanzi::VerboseObject *meshverbobj = 
-    new Amanzi::VerboseObject("mesh", driver_parameter_list);
-
-  // Create a mesh factory for this geometric model
-  Amanzi::AmanziMesh::MeshFactory factory(comm,meshverbobj) ;
-
-  // get the Mesh sublist
-  ierr = 0;
-  Teuchos::ParameterList mesh_params = driver_parameter_list.sublist("mesh");
-  
-  Teuchos::ParameterList unstr_mesh_params = mesh_params.sublist("unstructured");
-
-  // Decide on which mesh framework to use
-  Teuchos::RCP<Amanzi::AmanziMesh::Mesh> mesh;
-
-  if (unstr_mesh_params.isSublist("generate mesh")) {  // If Read parameters are specified
-    Teuchos::ParameterList gen_params = unstr_mesh_params.sublist("generate mesh");
-    ierr = 0;
-    
-    try {
-      // create the mesh by internal generation
-      mesh = factory.create(gen_params, geom_model_ptr);
-
-    } catch (const std::exception& e) {
-      std::cerr << rank << ": error: " << e.what() << std::endl;
-      ierr++;
-    }
-  
-    comm->SumAll(&ierr, &aerr, 1);
-    if (aerr > 0) {
-      exit(-aerr);
-    }
-
-  } else {  // If Generate parameters are specified
-    std::cerr << rank << ": error: " << "Neither Read nor Generate options specified for mesh" << std::endl;
-    throw std::exception();
-  }
-
+  MeshFactory meshfactory(&comm);
+  meshfactory.preference(pref);
+  Teuchos::RCP<Amanzi::AmanziMesh::Mesh> mesh = meshfactory(0.0, 0.0, 1.0, 1.0, 8, 8, gm);
   ASSERT(!mesh.is_null());
-
-  bool mpc_new = true;
-  
-  // if (input_parameter_list.isParameter("New multi-process coordinator")) {
-  //   mpc_new = input_parameter_list.get<bool>("New multi-process coordinator",false);
-  //   //mpc_new = true;
-  // }
 
   // create dummy observation data object
   Amanzi::ObservationData obs_data;    
-
-
-  if (mpc_new) {
-    if (driver_parameter_list.isSublist("state")) {
-      // Create the state.    
-      Teuchos::ParameterList state_plist = driver_parameter_list.sublist("state");
-      Teuchos::RCP<Amanzi::State> S = Teuchos::rcp(new Amanzi::State(state_plist));
-      S->RegisterMesh("domain",mesh);      
-
-      // -------------- MULTI-PROCESS COORDINATOR------- --------------------
-      Amanzi::CycleDriver cycle_driver(driver_parameter_list, S, comm, obs_data);
-      //--------------- DO THE SIMULATION -----------------------------------
-      cycle_driver.go();
-      //-----------------------------------------------------
-    }
-  }
+  Teuchos::RCP<Teuchos::ParameterList> glist_rcp = Teuchos::rcp(new Teuchos::ParameterList(plist));
+  Amanzi::CycleDriver cycle_driver(glist_rcp, mesh, &comm, obs_data);
+  cycle_driver.Go();
 
   delete comm;
 }
