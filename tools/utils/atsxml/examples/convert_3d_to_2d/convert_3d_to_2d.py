ac9b60af6512571aeea8c49fe06d13ac9ca9ac28
Dylan Harp <dharp73@gmail.com>
2016-04-18 13:43:07 -0600

    atsxml now uses xml_functions from amanzi_xml and functional style

--- tools/utils/atsxml/examples/convert_3d_to_2d/convert_3d_to_2d.py
+++ tools/utils/atsxml/examples/convert_3d_to_2d/convert_3d_to_2d.py
@@ -1,16 +1,16 @@
 import sys,os
 try:
-    from atsxml import ATSXML
+    from atsxml import *
 except:
     try:
         sys.path.append(os.sep.join(['..','..','..']))
-        from atsxml import ATSXML
+        from atsxml import *
     except:
         print "Unable to locate atsxml module"
         sys.exit()
 
-x3d = ATSXML('3d_run_from_Ethan.xml')
-x2d = ATSXML('geoinv7.xml')
+x3d = get_root('3d_run_from_Ethan.xml')
+x2d = get_root('geoinv7.xml')
 # Replace mesh regions
 mapping = {'computational domain':'computational domain',
            'surface domain':'surface domain',
@@ -20,33 +20,30 @@ mapping = {'computational domain':'computational domain',
            'bottom face':'bottom face',
            'south edge':'front,back'
           }
-x3d.replace_regions(x2d,mapping=mapping)
+# New functional replace regions
+replace_regions(x3d,x2d,mapping=mapping)
 
 # Replace BC files and names
-x3d.find_value('Snow').set('value','/Ps')
-x3d.find_value('Rain').set('value','/Pr')
-x3d.find_value('/Windspeed').set('value','/Us')
-x3d.find_value('/Relative_humidity').set('value','/RH')
-x3d.find_value('/Temperature').set('value','/Ta')
-x3d.find_value('/Shortwave').set('value','/QswIn')
-x3d.replace_file('/scratch/tundra/dharp/arctic/geophysics/run_newmesh02/10yr-WinterStartRAW-heavystart.h5')
-
-#x3d.replace_by_value('/scratch/tundra/dharp/arctic/geophysics/mesh/from_lucia/arctic_siteb_2d.exo','/scratch/tundra/dharp/arctic/geophysics/mesh/from_lucia/arctic_siteb_2d.par')
+find_value(x3d,'Snow').set('value','/Ps')
+find_value(x3d,'Rain').set('value','/Pr')
+find_value(x3d,'/Windspeed').set('value','/Us')
+find_value(x3d,'/Relative_humidity').set('value','/RH')
+find_value(x3d,'/Temperature').set('value','/Ta')
+find_value(x3d,'/Shortwave').set('value','/QswIn')
+replace_by_name(x3d,'../../Data/Barrow-Anna-2010-2013.h5','/scratch/tundra/dharp/arctic/geophysics/run_newmesh02/10yr-WinterStartRAW-heavystart.h5')
 
 # Remove longwave radiation BC
-x3d.remove(x3d.find_name('incoming_longwave_radiation'))
+remove(x3d,find_name(x3d,'incoming_longwave_radiation'))
 
 # Replace initialization with restart file
-e_ic2d = x2d.parent_map[x2d.find_name('restart file')]
-for e in x3d.findall_name('initialize from 1D column'):
-    x3d.replace_elem(x3d.parent_map[e],e_ic2d)    
-
-#x3d.replace_restart_file('/scratch/tundra/dharp/arctic/geophysics/run_newmesh01/checkpoint10644.h5')
+e_ic2d = create_parent_map(x2d)[findall_name(x2d,'restart file')[0]]
+for e in findall_name(x3d,'initialize from 1D column'):
+    replace_elem(x3d,create_parent_map(x3d)[e],e_ic2d)    
 
 # Change bottom temp bc
-x3d.replace_by_value('267.15','263.55')
+replace_by_value(x3d,'267.15','263.55')
 
 # Remove wallclock duration
-x3d.remove(x3d.find_name('wallclock duration [hrs]'))
+remove(x3d,find_name(x3d,'wallclock duration [hrs]'))
 
-x3d.write('geoinv7_serial.xml')
+write_xml(x3d,'geoinv7_serial.xml')
