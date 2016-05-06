e486cb077e77baa83e3705f073ce0e3218644fed
Ethan Coon <ecoon@lanl.gov>
2016-05-06 13:45:21 -0600

    increases max number of faces

--- src/operators/OperatorDefs.hh
+++ src/operators/OperatorDefs.hh
@@ -57,7 +57,7 @@ const int OPERATOR_QUAD_FACES = 4;  // Quadrilateral is the common element
 const int OPERATOR_QUAD_NODES = 4;
 const int OPERATOR_QUAD_EDGES = 4;
 
-const int OPERATOR_MAX_FACES = 14;  // Kelvin's tetrakaidecahedron
+const int OPERATOR_MAX_FACES = 21;  // Kelvin's tetrakaidecahedron isn't big enough
 const int OPERATOR_MAX_NODES = 47;  // More accurate bounds could be
 const int OPERATOR_MAX_EDGES = 60;  // calculated during initialization.
 
