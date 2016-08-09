d68759db92494d5bc891f7d75b3af7505fdd0c9a
J David Moulton <moulton@lanl.gov>
2016-08-09 10:18:04 -0600

    Assume default language standard is used for _NOEXCEPT_OP(x) (refs #27).

--- config/SuperBuild/templates/unittest-TestTestMacros.cpp
+++ config/SuperBuild/templates/unittest-TestTestMacros.cpp
@@ -22,9 +22,9 @@ using namespace std;
    #endif
 
    #ifndef __clang__
-      /* Test for GCC >= 4.8.0 */
+      /* Test for GCC >= 6 --assuming default language standard, which changed in 6.x */
       #ifdef __GNUC__
-         #if (__GNUC__ > 4) || (__GNUC__ == 4 && (__GNUC_MINOR__ > 7 ))
+         #if (__GNUC__ >= 6) 
             #define _NOEXCEPT_OP(x) noexcept(x)
          #else
             #define _NOEXCEPT_OP(x)
