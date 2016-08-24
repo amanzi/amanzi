1744710b237f8c50b250ffb5022929a01d3c15b5
Ethan Coon <ecoon@lanl.gov>
2016-08-23 23:28:49 -0600

    bug fix in overland pressure newton on FV

--- src/pks/flow/overland_pressure/overland_pressure_ti.cc
+++ src/pks/flow/overland_pressure/overland_pressure_ti.cc
@@ -211,10 +211,12 @@ void OverlandPressureFlow::UpdatePreconditioner(double t, Teuchos::RCP<const Tre
   preconditioner_diff_->SetScalarCoefficient(cond, dcond);
   Teuchos::RCP<const CompositeVector> pres_elev = S_next_->GetFieldData("pres_elev");
   preconditioner_diff_->UpdateMatrices(Teuchos::null, pres_elev.ptr());
-  if (jacobian_ && preconditioner_->RangeMap().HasComponent("face")) {
+  //  if (jacobian_ && preconditioner_->RangeMap().HasComponent("face")) {
+  if (jacobian_) {
     Teuchos::RCP<CompositeVector> flux = S_next_->GetFieldData("surface-mass_flux", name_);
     preconditioner_diff_->UpdateFlux(*pres_elev, *flux);
-    preconditioner_diff_->UpdateMatricesNewtonCorrection(flux.ptr(), Teuchos::null);
+    //    preconditioner_diff_->UpdateMatricesNewtonCorrection(flux.ptr(), Teuchos::null);
+    preconditioner_diff_->UpdateMatricesNewtonCorrection(flux.ptr(), pres_elev.ptr());
   }
 
   // 2. Accumulation shift
