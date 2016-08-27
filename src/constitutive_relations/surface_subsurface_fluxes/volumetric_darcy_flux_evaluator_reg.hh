9692598c6e44346a0ddfa2be596af29672e981cf
Daniil Svyatsky <dasvyat@lanl.gov>
2016-08-26 18:10:29 -0600

    add missing files

---
+++ src/constitutive_relations/surface_subsurface_fluxes/volumetric_darcy_flux_evaluator_reg.hh
@@ -0,0 +1,20 @@
+
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+
+/*
+  An evaluator for converting the darcy flux to volumetric flux
+
+  Authors: Daniil Svyatsky  (dasvyat@lanl.gov)
+*/
+
+
+#include "volumetric_darcy_flux_evaluator.hh"
+
+namespace Amanzi {
+namespace Relations {
+
+Utils::RegisteredFactory<FieldEvaluator,Volumetric_FluxEvaluator>
+Volumetric_FluxEvaluator::fac_("volumetric darcy flux");
+
+} // namespace
+} // namespace
