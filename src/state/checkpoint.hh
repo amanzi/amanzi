c0ff07e4a38c79f3631166eaeb4f326a341f3f13
Ethan Coon <ecoon@lanl.gov>
2016-11-02 12:20:53 -0600

    documentation on checkpoint usage

--- src/state/checkpoint.hh
+++ src/state/checkpoint.hh
@@ -1,12 +1,42 @@
-/* -------------------------------------------------------------------------
-Amanzi
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+//! Visualization: a class for controlling simulation output.
+/*
+  Copyright 2010-201x held jointly by LANS/LANL, LBNL, and PNNL. 
+  Amanzi is released under the three-clause BSD License. 
+  The terms of use and "as is" disclaimer for this license are 
+  provided in the top-level COPYRIGHT file.
 
-License:
-Author: Markus Berndt
-        Ethan Coon (ecoon@lanl.gov)
+  Authors: Markus Berndt
+           Ethan Coon (ecoon@lanl.gov)
+*/
 
-Checkpointing for state.
-------------------------------------------------------------------------- */
+/*!
+
+Each list contains all parameters as in a IOEvent_ spec, and also:
+
+* `"file name base`" ``[string]`` **"checkpoint"**
+
+* `"file name digits`" ``[int]`` **5**
+
+  Write mesh data for every visualization dump, this facilitates visualizing deforming meshes.
+
+Example:
+
+.. code-block:: xml
+
+  <ParameterList name="checkpoint">
+    <Parameter name="cycles start period stop" type="Array(int)" value="{{0, 100, -1}}" />
+    <Parameter name="cycles" type="Array(int)" value="{{999, 1001}}" />
+    <Parameter name="times start period stop 0" type="Array(double)" value="{{0.0, 10.0, 100.0}}"/>
+    <Parameter name="times start period stop 1" type="Array(double)" value="{{100.0, 25.0, -1.0}}"/>
+    <Parameter name="times" type="Array(double)" value="{{101.0, 303.0, 422.0}}"/>
+  </ParameterList>
+
+In this example, checkpoint files are written when the cycle number is
+a multiple of 100, every 10 seconds for the first 100 seconds, and
+every 25 seconds thereafter, along with times 101, 303, and 422.  Files will be written in the form: `"checkpoint00000.h5`".
+
+*/
 
 
 #ifndef AMANZI_STATE_CHECKPOINT_HH_
