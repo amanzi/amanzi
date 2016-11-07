c8f8bf2d1bb91c3981511d5c9a5b9b197b44bf0c
Ethan Coon <ecoon@lanl.gov>
2016-11-07 08:56:32 -0700

    adds compile-time option for checking that a CompositeVectorFunction fully initializes a vector.  Not doing so is useful, but also can result in input spec bugs that are hard to catch.  Needs discussion as to whether this should become standard or not

--- src/mesh_functions/CompositeVectorFunction.cc
+++ src/mesh_functions/CompositeVectorFunction.cc
@@ -42,12 +42,14 @@ void CompositeVectorFunction::Compute(double time,
   Teuchos::RCP<const AmanziMesh::Mesh> mesh = func_->mesh();
 
   cv->PutScalar(0.);
-  
+
+#ifdef ENSURE_INITIALIZED_CVFUNCS  
   // ensure all components are touched
   std::map<std::string,bool> done;
   for (auto compname : *cv) {
     done[compname] = false;
   }
+#endif
 
   // create the input tuple
   int dim = mesh->space_dimension();
@@ -58,7 +60,9 @@ void CompositeVectorFunction::Compute(double time,
   for (CompositeVectorSpecList::const_iterator cv_spec=cv_spec_list_.begin();
        cv_spec!=cv_spec_list_.end(); ++cv_spec) {
     std::string compname = (*cv_spec)->first;
+#ifdef ENSURE_INITIALIZED_CVFUNCS  
     done[compname] = true;
+#endif
     
     Epetra_MultiVector& compvec = *cv->ViewComponent(compname,false);
     Teuchos::RCP<MeshFunction::Spec> spec = (*cv_spec)->second;
@@ -197,6 +201,7 @@ void CompositeVectorFunction::Compute(double time,
     }
   }
 
+#ifdef ENSURE_INITIALIZED_CVFUNCS  
   for (auto compname : *cv) {
     if (!done[compname]) {
       Errors::Message message;
@@ -204,6 +209,7 @@ void CompositeVectorFunction::Compute(double time,
       Exceptions::amanzi_throw(message);
     }
   }
+#endif
 }
 
 } // namespace
