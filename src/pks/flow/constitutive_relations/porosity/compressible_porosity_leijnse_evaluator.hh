4fed61a938bb426305c651b0559990aee2fedf90
Ahmad Jan <ajkhattak@gmail.com>
2017-02-06 13:23:12 -0500

    Added new porosity compressibility (Leijnse) model

---
+++ src/pks/flow/constitutive_relations/porosity/compressible_porosity_leijnse_evaluator.hh
@@ -0,0 +1,57 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+
+/*
+  Evaluates the porosity, given a small compressibility of rock.
+
+  Compressible grains are both physically realistic (based on bulk modulus)
+  and a simple way to provide a non-elliptic, diagonal term for helping
+  solvers to converge.
+
+
+  Authors: Ethan Coon (ecoon@lanl.gov)
+*/
+
+#ifndef AMANZI_FLOWRELATIONS_COMPRESSIBLE_POROSITY_LEIJNSE_EVALUATOR_HH_
+#define AMANZI_FLOWRELATIONS_COMPRESSIBLE_POROSITY_LEIJNSE_EVALUATOR_HH_
+
+#include "factory.hh"
+#include "secondary_variable_field_evaluator.hh"
+#include "compressible_porosity_leijnse_model_partition.hh"
+
+namespace Amanzi {
+namespace Flow {
+namespace FlowRelations {
+
+class CompressiblePorosityLeijnseEvaluator : public SecondaryVariableFieldEvaluator {
+ public:
+  explicit
+  CompressiblePorosityLeijnseEvaluator(Teuchos::ParameterList& plist);
+  CompressiblePorosityLeijnseEvaluator(const CompressiblePorosityLeijnseEvaluator& other);
+  Teuchos::RCP<FieldEvaluator> Clone() const;
+
+  // Required methods from SecondaryVariableFieldEvaluator
+  virtual void EvaluateField_(const Teuchos::Ptr<State>& S,
+          const Teuchos::Ptr<CompositeVector>& result);
+  virtual void EvaluateFieldPartialDerivative_(const Teuchos::Ptr<State>& S,
+          Key wrt_key, const Teuchos::Ptr<CompositeVector>& result);
+
+  Teuchos::RCP<CompressiblePorosityLeijnseModelPartition> get_Models() { return models_; }
+
+protected:
+  Key poro_key_;
+  Key pres_key_;
+
+  Teuchos::RCP<CompressiblePorosityLeijnseModelPartition> models_;
+
+ private:
+  static Utils::RegisteredFactory<FieldEvaluator,CompressiblePorosityLeijnseEvaluator> fac_;
+
+
+
+};
+
+} // namespace
+} // namespace
+} // namespace
+
+#endif
---
+++ src/pks/flow/constitutive_relations/porosity/compressible_porosity_leijnse_model_partition.cc
@@ -0,0 +1,42 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+
+/*
+  A collection of comp poro models along with a Mesh Partition.
+  Authors: Ethan Coon (ecoon@lanl.gov)
+*/
+
+#include "dbc.hh"
+#include "compressible_porosity_leijnse_model_partition.hh"
+
+
+namespace Amanzi {
+namespace Flow {
+namespace FlowRelations {
+
+// Non-member factory
+Teuchos::RCP<CompressiblePorosityLeijnseModelPartition>
+createCompressiblePorosityLeijnseModelPartition(Teuchos::ParameterList& plist) {
+  CompressiblePorosityLeijnseModelList mlist;
+  std::vector<std::string> region_list;
+
+  for (Teuchos::ParameterList::ConstIterator lcv=plist.begin();
+       lcv!=plist.end(); ++lcv) {
+    std::string name = lcv->first;
+    if (plist.isSublist(name)) {
+      Teuchos::ParameterList sublist = plist.sublist(name);
+      region_list.push_back(sublist.get<std::string>("region"));
+      mlist.push_back(Teuchos::rcp(new CompressiblePorosityLeijnseModel(sublist)));
+    } else {
+      ASSERT(0);
+    }
+  }
+
+  Teuchos::RCP<Functions::MeshPartition> part =
+      Teuchos::rcp(new Functions::MeshPartition(AmanziMesh::CELL,region_list));
+
+  return Teuchos::rcp(new CompressiblePorosityLeijnseModelPartition(part, mlist));
+}
+
+} // namespace
+} // namespace
+} // namespace
