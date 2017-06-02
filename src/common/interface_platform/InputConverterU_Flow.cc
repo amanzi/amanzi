153ac526553b48402b30726cf8b9822866517faf
Konstantin Lipnikov <lipnikov@lanl.gov>
2017-06-02 17:42:37 -0600

    Picard initialization algorithm needs GMRES.

--- src/common/interface_platform/InputConverterU_Flow.cc
+++ src/common/interface_platform/InputConverterU_Flow.cc
@@ -263,7 +263,7 @@ Teuchos::ParameterList InputConverterU::TranslateWRM_()
           file_list.set<std::string>("file", name.str());
           file_list.set<int>("number of points", 1000);
 
-          *vo_->os() << "water retention curve file:" << name.str() << std::endl;
+          *vo_->os() << "water retention curve file: wrm_" << name.str() << std::endl;
         }
       }
     } else if (strcmp(model.c_str(), "brooks_corey")) {
