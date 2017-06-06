21b215670dff78a0efad2477c62f7a7afb1a8684
aperry <aperry@lanl.gov>
2017-06-06 16:15:04 -0600

    removed one too many things from Build_HDF5.cmake

--- config/SuperBuild/include/Build_HDF5.cmake
+++ config/SuperBuild/include/Build_HDF5.cmake
@@ -83,6 +83,7 @@ ExternalProject_Add(${HDF5_BUILD_TARGET}
                                                  --disable-cxx
                                                  --enable-production
                                                  --enable-largefile
+						 --enable-parallel
                                                  --with-zlib=${TPL_INSTALL_PREFIX}
                                                  CC=${CMAKE_C_COMPILER_USE}
                                                  CFLAGS=${hdf5_cflags}
