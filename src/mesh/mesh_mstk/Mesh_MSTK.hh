dd708a0bbf3d27234ed99d393c064c46f5051c45
Rao Garimella <rao@lanl.gov>
2017-06-15 11:44:15 -0600

    added method run_internal_mstk_checks

--- src/mesh/mesh_mstk/Mesh_MSTK.hh
+++ src/mesh/mesh_mstk/Mesh_MSTK.hh
@@ -349,6 +349,11 @@ class Mesh_MSTK : public Mesh {
   // Miscellaneous
   void write_to_exodus_file(const std::string filename) const;
 
+  // Run MSTK's internal checks - meant for debugging only
+  // Returns true if everything is ok, false otherwise
+
+  bool run_internal_mstk_checks() const;
+
  private:
   MPI_Comm mpicomm_;
   int myprocid, numprocs;
@@ -476,7 +481,7 @@ class Mesh_MSTK : public Mesh {
   void init_cells();
 
   void init_set_info();
-  void inherit_labeled_sets(MAttrib_ptr copyatt);
+  void inherit_labeled_sets(MAttrib_ptr copyatt, List_ptr src_entities);
   std::string internal_name_of_set(const Teuchos::RCP<const AmanziGeometry::Region>& region,
                                    const Entity_kind entity_kind) const;
   std::string other_internal_name_of_set(const Teuchos::RCP<const AmanziGeometry::Region>& r,
