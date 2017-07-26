amanzi/amanzi

commit 768a3e2de5d7d06c7d7ad1cc4926c9431eaf28c8

Added more checks to transport tests. refs #3

src/pks/transport/Transport_PK.cc
@@ -699,14 +699,20 @@ bool Transport_PK::AdvanceStep(double t_old, double t_new, bool reinit)
       swap = 1 - swap;
     }
 
-    if (mesh_->space_dimension() != mesh_->manifold_dimension()) {
-      AdvanceDonorUpwindNonManifold(dt_cycle);
-    } else if (spatial_disc_order == 1) {  // temporary solution (lipnikov@lanl.gov)
-      AdvanceDonorUpwind(dt_cycle);
-    } else if (spatial_disc_order == 2 && temporal_disc_order == 1) {
-      AdvanceSecondOrderUpwindRK1(dt_cycle);
-    } else if (spatial_disc_order == 2 && temporal_disc_order == 2) {
-      AdvanceSecondOrderUpwindRK2(dt_cycle);
+    if (mesh_->space_dimension() == mesh_->manifold_dimension()) {
+      if (spatial_disc_order == 1) {
+        AdvanceDonorUpwind(dt_cycle);
+      } else if (spatial_disc_order == 2 && temporal_disc_order == 1) {
+        AdvanceSecondOrderUpwindRK1(dt_cycle);
+      } else if (spatial_disc_order == 2 && temporal_disc_order == 2) {
+        AdvanceSecondOrderUpwindRK2(dt_cycle);
+      }
+    } else {  // transport on intersecting manifolds
+      if (spatial_disc_order == 1) {
+        AdvanceDonorUpwindNonManifold(dt_cycle);
+      } else {
+        AdvanceSecondOrderUpwindRK2(dt_cycle);
+      }
     }
 
     // add multiscale model
@@ -1508,8 +1514,8 @@ void Transport_PK::IdentifyUpwindCells()
       if (downwind_cells_[f].size() == 0) downwind_cells_[f].push_back(-1);
     }
   } else {
-    upwind_flux_.clear();
-    downwind_flux_.clear();
+    upwind_cells_.clear();
+    downwind_cells_.clear();
 
     upwind_flux_.resize(nfaces_wghost);
     downwind_flux_.resize(nfaces_wghost);

src/pks/transport/test/transport_fractures.cc
@@ -134,6 +134,9 @@ std::cout << "Test: Advance on a 2D square mesh" << std::endl;
 
     t_old = t_new;
     iter++;
+
+    // verify solution
+    for (int c = 0; c < ncells_owned; ++c) CHECK(tcc[0][c] >= 0.0);
   }
 
   // test the maximum principle

src/pks/transport/test/transport_parallel_mstk.cc
@@ -36,14 +36,10 @@ TEST(ADVANCE_WITH_MSTK_PARALLEL) {
   using namespace Amanzi::Transport;
   using namespace Amanzi::AmanziGeometry;
 
-  std::cout << "Test: advance with MSTK in parallel" << std::endl;
-#ifdef HAVE_MPI
+  std::cout << "Test: second-order transport with parallel MSTK framework" << std::endl;
   Epetra_MpiComm* comm = new Epetra_MpiComm(MPI_COMM_WORLD);
-#else
-  Epetra_SerialComm* comm = new Epetra_SerialComm();
-#endif
 
-  /* read parameter list */
+  // read parameter list
   std::string xmlFileName = "test/transport_parallel_mstk.xml";
   Teuchos::RCP<Teuchos::ParameterList> plist = Teuchos::getParametersFromXmlFile(xmlFileName);
 
@@ -52,12 +48,8 @@ TEST(ADVANCE_WITH_MSTK_PARALLEL) {
   Teuchos::RCP<Amanzi::AmanziGeometry::GeometricModel> gm =
       Teuchos::rcp(new Amanzi::AmanziGeometry::GeometricModel(3, region_list, comm));
 
-  FrameworkPreference pref;
-  pref.clear();
-  pref.push_back(MSTK);
-
   MeshFactory meshfactory(comm);
-  meshfactory.preference(pref);
+  meshfactory.preference(FrameworkPreference({MSTK}));
   RCP<const Mesh> mesh = meshfactory("test/hex_3x3x3_ss.exo", gm);
   
   /* create a simple state and populate it */
@@ -89,16 +81,15 @@ TEST(ADVANCE_WITH_MSTK_PARALLEL) {
     (*flux)[0][f] = velocity * normal;
   }
 
-  Teuchos::RCP<Epetra_MultiVector>
-      tcc = S->GetFieldData("total_component_concentration", passwd)->ViewComponent("cell", false);
+  Epetra_MultiVector& tcc = *S->GetFieldData("total_component_concentration", passwd)->ViewComponent("cell");
 
   int ncells_owned = mesh->num_entities(AmanziMesh::CELL, AmanziMesh::OWNED);
   for (int c = 0; c < ncells_owned; c++) {
     const AmanziGeometry::Point& xc = mesh->cell_centroid(c);
-    (*tcc)[0][c] = f_step(xc, 0.0);
+    tcc[0][c] = f_step(xc, 0.0);
   }
 
-  /* initialize a transport process kernel */
+  // initialize a transport process kernel
   TPK.Initialize(S.ptr());
 
   /* advance the state */
@@ -107,7 +98,7 @@ TEST(ADVANCE_WITH_MSTK_PARALLEL) {
   t_new = t_old + dt;
   TPK.AdvanceStep(t_old, t_new);
 
-  /* print cell concentrations */
+  // print cell concentrations
   int iter = 0;
   while(t_new < 1.0) {
     dt = TPK.StableTimeStep();
@@ -121,12 +112,17 @@ TEST(ADVANCE_WITH_MSTK_PARALLEL) {
 
     if (iter < 10 && TPK.MyPID == 3) {
       printf("T=%7.2f  C_0(x):", t_new);
-      for (int k = 0; k < 2; k++) printf("%7.4f", (*tcc)[0][k]); std::cout << std::endl;
+      for (int k = 0; k < 2; k++) printf("%7.4f", tcc[0][k]); std::cout << std::endl;
     }
+
+    for (int k = 0; k < ncells_owned; ++k) 
+      CHECK(tcc[0][k] >= 0.0 && tcc[0][k] <= 1.0);
   }
 
-  //for (int k=0; k<12; k++) 
-  //  CHECK_CLOSE((*tcc_next)[0][k], 1.0, 1e-6);
+  for (int k = 0; k < ncells_owned; ++k) 
+    CHECK_CLOSE(tcc[0][k], 1.0, 1e-6);
+
+  delete comm;
 }
  
  

commit e9525845248b325ad8c02aba29f02817d325b38b

Fixed error introduced in commit 768a3e2. Upgraded one transport test to capture it.

src/pks/transport/Transport_PK.cc
@@ -1507,8 +1507,8 @@ void Transport_PK::IdentifyUpwindCells()
       }
     }
   } else {
-    upwind_cells_.clear();
-    downwind_cells_.clear();
+    upwind_flux_.clear();
+    downwind_flux_.clear();
 
     upwind_flux_.resize(nfaces_wghost);
     downwind_flux_.resize(nfaces_wghost);

src/pks/transport/test/transport_fractures.cc
@@ -136,7 +136,8 @@ std::cout << "Test: Advance on a 2D square mesh" << std::endl;
     iter++;
 
     // verify solution
-    for (int c = 0; c < ncells_owned; ++c) CHECK(tcc[0][c] >= 0.0);
+    for (int c = 0; c < ncells_owned; ++c) 
+        CHECK(tcc[0][c] >= 0.0 && tcc[0][c] <= 1.0);
   }
 
   // test the maximum principle