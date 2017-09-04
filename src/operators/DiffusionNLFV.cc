377a3498f94c0fc1e3eef6f185bb86375bff8552
lipnikov <lipnikov@lanl.gov>
2017-09-03 18:19:12 -0600

    Post-merge clean up-3

--- src/operators/DiffusionNLFV.cc
+++ src/operators/DiffusionNLFV.cc
@@ -389,8 +389,6 @@ void DiffusionNLFV::UpdateMatrices(
 
   //stencil_data_->ScatterMasterToGhosted("flux_data");
   stencil_data_->GatherGhostedToMaster("flux_data");
-
-  stencil_data_->GatherGhostedToMaster("flux_data");
   matrix_cv.GatherGhostedToMaster();
 
   // populate local matrices
