114754f05de5e9ab4b25a92e3c441ef2dff09dea
Konstantin Lipnikov <lipnikov@gmail.com>
2017-10-15 21:37:29 -0600

    Improved sed command to work on both Linux and BSD systems

--- config/SuperBuild/templates/alquimia-patch-step.sh
+++ config/SuperBuild/templates/alquimia-patch-step.sh
@@ -3,5 +3,5 @@ cp CMakeLists.txt CMakeLists.txt.old
 sed s/\$ENV/$/g CMakeLists.txt.old > CMakeLists.txt
 
 cp alquimia/CMakeLists.txt alquimia/CMakeLists.txt.old
-sed 's/ALQUIMIA_HAVE_CRUNCHFLOW)/&\n  list\(APPEND ALQUIMIA_TPLS lapack\)/g' \
+sed 's/ALQUIMIA_HAVE_CRUNCHFLOW)/ALQUIMIA_HAVE_CRUNCHFLOW)\'$'\n  list\(APPEND ALQUIMIA_TPLS lapack\)/g' \
     alquimia/CMakeLists.txt.old > alquimia/CMakeLists.txt
