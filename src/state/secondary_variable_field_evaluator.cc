03137d98ef6f12c10631e901c3aca509a957adbd
Ethan Coon <etcoon@gmail.com>
2017-12-04 15:35:01 -0500

    fumble fingers

--- src/state/secondary_variable_field_evaluator.cc
+++ src/state/secondary_variable_field_evaluator.cc
@@ -304,7 +304,7 @@ void SecondaryVariableFieldEvaluator::EnsureCompatibility(const Teuchos::Ptr<Sta
   Teuchos::RCP<CompositeVectorSpace> my_fac = S->RequireField(my_key_, my_key_);
 
   // check plist for vis or checkpointing control
-  bool io_my_key = plist_.get<bool>"visualize", true);
+  bool io_my_key = plist_.get<bool>("visualize", true);
   S->GetField(my_key_, my_key_)->set_io_vis(io_my_key);
   bool checkpoint_my_key = plist_.get<bool>("checkpoint", false);
   S->GetField(my_key_, my_key_)->set_io_checkpoint(checkpoint_my_key);
