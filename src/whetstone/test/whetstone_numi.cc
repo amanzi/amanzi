ab411545dec69d0bdb5c8b3ce0b09b3d314f9ba7
Konstantin Lipnikov <lipnikov@lanl.gov>
2018-03-23 10:16:02 -0600

    Gave more transparent names to exodus meshes.

--- src/whetstone/test/whetstone_numi.cc
+++ src/whetstone/test/whetstone_numi.cc
@@ -26,12 +26,53 @@
 
 
 /* **************************************************************** */
-TEST(NUMI_CELL_2D) {
+TEST(NUMI_CELL_2D_EULER_FORMULA) {
   using namespace Amanzi;
   using namespace Amanzi::AmanziMesh;
   using namespace Amanzi::WhetStone;
 
-  std::cout << "Test: Numerical Integartion over 2D cells" << std::endl;
+  std::cout << "Test: Numerical Integration over 2D cells" << std::endl;
+  Epetra_MpiComm *comm = new Epetra_MpiComm(MPI_COMM_WORLD);
+
+  MeshFactory meshfactory(comm);
+  meshfactory.preference(FrameworkPreference({MSTK}));
+  Teuchos::RCP<const Amanzi::AmanziGeometry::GeometricModel> gm;
+  Teuchos::RCP<Mesh> mesh = meshfactory("test/one_pentagon.exo", gm, true, true); 
+ 
+  NumericalIntegration numi(mesh);
+
+  int cell(0);
+  double val;
+
+  // 0th-order polynomial
+  Polynomial poly(2, 0);
+  poly(0, 0) = 1.0;
+  val = numi.IntegratePolynomialCell(cell, poly);
+
+  printf("order=0  value=%10.6g\n", val);
+  CHECK_CLOSE(val, mesh->cell_volume(cell), 1e-10);
+ 
+  // 1st-order polynomial
+  poly.Reshape(2, 1);
+  poly(1, 0) = 2.0;
+  poly(1, 1) = 3.0;
+  poly.set_origin(mesh->cell_centroid(cell));
+  val = numi.IntegratePolynomialCell(cell, poly);
+
+  printf("order=1  value=%10.6g\n", val);
+  CHECK_CLOSE(val, mesh->cell_volume(cell), 1e-10);
+ 
+  delete comm;
+}
+
+
+/* **************************************************************** */
+TEST(NUMI_CELL_2D_QUADRATURE) {
+  using namespace Amanzi;
+  using namespace Amanzi::AmanziMesh;
+  using namespace Amanzi::WhetStone;
+
+  std::cout << "Test: Numerical Integration over 2D cells" << std::endl;
   Epetra_MpiComm *comm = new Epetra_MpiComm(MPI_COMM_WORLD);
 
   MeshFactory meshfactory(comm);
