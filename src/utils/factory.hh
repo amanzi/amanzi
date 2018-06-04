2000197101b75cd7755537d364413dff0fdd42ed
Ethan Coon <etcoon@gmail.com>
2018-06-04 14:50:08 -0400

    makes compiler warnings go away

--- src/utils/factory.hh
+++ src/utils/factory.hh
@@ -98,9 +98,9 @@ public:
     if (iter == GetMap()->end()) {
       std::cout << "Factory: cannot get item of type: " << s << std::endl;
 
-      for (typename map_type::iterator iter=GetMap()->begin();
-           iter!=GetMap()->end(); ++iter) {
-        std::cout << "  option: " << iter->first << std::endl;
+      for (typename map_type::iterator piter=GetMap()->begin();
+           piter!=GetMap()->end(); ++piter) {
+        std::cout << "  option: " << piter->first << std::endl;
       }
       return 0;
     }
