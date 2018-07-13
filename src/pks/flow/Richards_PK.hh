6d6bb6e803102f48eae01aa17ac0f8a5e3751d60
lipnikov <lipnikov@lanl.gov>
2018-07-12 20:54:59 -0600

    Fixed a warning from clang6.

--- src/pks/flow/Richards_PK.hh
+++ src/pks/flow/Richards_PK.hh
@@ -72,7 +72,7 @@ class Richards_PK : public Flow_PK {
       const double t_old, double t_new, 
       Teuchos::RCP<TreeVector> u_old, Teuchos::RCP<TreeVector> u_new, 
       Teuchos::RCP<TreeVector> f) override;
-  double ErrorNorm(Teuchos::RCP<const TreeVector> u, Teuchos::RCP<const TreeVector> du);
+  virtual double ErrorNorm(Teuchos::RCP<const TreeVector> u, Teuchos::RCP<const TreeVector> du) override;
 
   // -- management of the preconditioner
   virtual int ApplyPreconditioner(Teuchos::RCP<const TreeVector> u, Teuchos::RCP<TreeVector> pu) override;
