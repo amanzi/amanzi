49bbd077ed0d9312817f96d2db8981016a35280d
Ethan Coon <etcoon@gmail.com>
2018-09-25 10:44:18 -0400

    adds input spec translaters for dev and dev-seb branch

---
+++ tools/input_converters/xml-0.87-0.88.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+"""ATS input converter from 0.87 to dev"""
+
+import sys, os
+try:
+    amanzi_xml = os.path.join(os.environ["AMANZI_SRC_DIR"], "tools","amanzi_xml")
+except KeyError:
+    pass
+else:
+    if amanzi_xml not in sys.path:
+        sys.path.append(amanzi_xml)
+
+
+from amanzi_xml.utils import search as asearch
+from amanzi_xml.utils import io as aio
+from amanzi_xml.utils import errors as aerrors
+from amanzi_xml.common import parameter
+
+
+def linear_operator(xml):
+    """Changes any instances of "linear operator" to "linear solver",
+    which is now standard across all PKs."""
+
+    pks = asearch.childByName(xml, "PKs")
+    for pk in pks:
+        try:
+            lin_op = asearch.childByName(pk, "linear operator")
+        except aerrors.MissingXMLError:
+            pass
+        else:
+            lin_op.setName("linear solver")
+
+def max_valid_change(xml):
+    """Adds options for max valid change, which aren't required, but are strongly suggested."""
+    pks = asearch.childByName(xml, "PKs")
+    for pk in pks:
+        pk_type = asearch.childByName(pk, "PK type")
+        if pk_type.get('value') == 'permafrost flow':
+            try:
+                pk.getElement("max valid change in saturation in a time step [-]")
+            except aerrors.MissingXMLError:
+                pk.append(parameter.DoubleParameter("max valid change in saturation in a time step [-]", 0.1))
+
+            try:
+                pk.getElement("max valid change in ice saturation in a time step [-]")
+            except aerrors.MissingXMLError:
+                pk.append(parameter.DoubleParameter("max valid change in ice saturation in a time step [-]", 0.1))
+
+
+def bad_spinup_longwave(xml):
+    """One spinup file commonly used includes a longwave radiation 
+    value that is totally wrong.  Not many runs actually used it.  
+    Some runs even had a spec for it in their file, but didn't include 
+    the necessary flag to use it.  So this just removes it to avoid 
+    confusion."""
+    evals = asearch.childByNamePath(xml, "state/field evaluators")
+    try:
+        lw = asearch.childByName(evals, "surface-incoming_longwave_radiation")
+    except aerrors.MissingXMLError:
+        pass
+    else:
+        try:
+            filename = asearch.childByNamePath(lw, "function/domain/function/function-tabular/file")
+        except aerrors.MissingXMLError:
+            pass
+        else:
+            if "spinup-10yr.h5" in filename.get('value'):
+                evals.pop("surface-incoming_longwave_radiation")
+
+
+def sources(xml):
+    """Can turn off derivative of source terms"""
+    pks = asearch.childByName(xml, "PKs")
+    for pk in pks:
+        try:
+            source_term = asearch.childByName(pk, "mass source key")
+        except aerrors.MissingXMLError:
+            pass
+        else:
+            source_term.setName('source key')
+        try:
+            source_term = asearch.childByName(pk, "energy source key")
+        except aerrors.MissingXMLError:
+            pass
+        else:
+            source_term.setName('source key')
+            
+
+        try:
+            source_term = asearch.childByName(pk, "source term")
+        except aerrors.MissingXMLError:
+            pass
+        else:
+            if source_term.getValue():
+                try:
+                    source_is_diff = asearch.childByName(pk, "source term is differentiable")
+                except aerrors.MissingXMLError:
+                    pk.append(parameter.BoolParameter("source term is differentiable", True))
+    
+            
+def update(xml):
+    linear_operator(xml)
+    max_valid_change(xml)
+    bad_spinup_longwave(xml)
+    sources(xml)
+    
+    pks = asearch.childByName(xml, "PKs")
+    for pk in pks:
+        pk_type = asearch.childByName(pk, "PK type")
+        if pk_type.get('value') == 'surface balance implicit':
+            print('updating seb monolitic')
+            import seb_monolithic_to_evals
+            seb_monolithic_to_evals.update_seb(xml)
+
+
+    
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Fix a number of changes from ATS input spec 0.86 to 0.9x")
+    parser.add_argument("infile", help="input filename")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-i", "--inplace", action="store_true", help="fix file in place")
+    group.add_argument("-o", "--outfile", help="output filename")
+
+    args = parser.parse_args()
+
+    print "Converting file: %s"%args.infile
+    xml = aio.fromFile(args.infile, True)
+    update(xml)
+    if args.inplace:
+        aio.toFile(xml, args.infile)
+    else:
+        aio.toFile(xml, args.outfile)
+    sys.exit(0)
+    
+
+    
