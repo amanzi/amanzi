b2f19c900fd462d810d4fc4ccfc9fb1402dcc3d9
Ethan Coon <etcoon@gmail.com>
2018-10-04 16:33:08 -0400

    clm pk

---
+++ src/pks/surface_balance/CLM/surface_balance_CLM_reg.hh
@@ -0,0 +1,20 @@
+/* -*-  mode: c++; indent-tabs-mode: nil -*- */
+/* -------------------------------------------------------------------------
+ * ATS
+ *
+ * License: see $ATS_DIR/COPYRIGHT
+ * Author: Ethan Coon, Adam Atchley, Satish Karra
+ *
+ * ------------------------------------------------------------------------- */
+
+
+#include "surface_balance_CLM.hh"
+
+namespace Amanzi {
+namespace SurfaceBalance {
+
+RegisteredPKFactory<SurfaceBalanceCLM>
+SurfaceBalanceCLM::reg_("surface balance CLM");
+
+} // namespace
+} // namespace
