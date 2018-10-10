d4df28803ce3fad807e1df2b74335b4b4c338dc6
lipnikov <lipnikov@lanl.gov>
2018-10-09 21:18:04 -0600

    Simplified one operator's test.

--- src/operators/test/operator_laplace_beltrami.cc
+++ src/operators/test/operator_laplace_beltrami.cc
@@ -34,7 +34,8 @@
 #include "Verification.hh"
 
 
-TEST(LAPLACE_BELTRAMI_FLAT_SFF) {
+void LaplaceBeltramiFlat(const std::string diff_op)
+{
   using namespace Teuchos;
   using namespace Amanzi;
   using namespace Amanzi::AmanziMesh;
@@ -44,7 +45,7 @@ TEST(LAPLACE_BELTRAMI_FLAT_SFF) {
   Epetra_MpiComm comm(MPI_COMM_WORLD);
   int MyPID = comm.MyPID();
 
-  if (MyPID == 0) std::cout << "\nTest: Laplace Beltrami solver: preconditioner Sff" << std::endl;
+  if (MyPID == 0) std::cout << "\nTest: Laplace Beltrami solver: " << diff_op << std::endl;
 
   // read parameter list
   std::string xmlFileName = "test/operator_laplace_beltrami.xml";
@@ -95,7 +96,7 @@ TEST(LAPLACE_BELTRAMI_FLAT_SFF) {
   }
 
   // create diffusion operator 
-  Teuchos::ParameterList olist = plist.sublist("PK operator").sublist("diffusion operator Sff");
+  Teuchos::ParameterList olist = plist.sublist("PK operator").sublist(diff_op);
   Teuchos::RCP<PDE_Diffusion> op = Teuchos::rcp(new PDE_DiffusionMFD(olist, surfmesh));
   op->SetBCs(bc, bc);
   const CompositeVectorSpace& cvs = op->global_operator()->DomainMap();
@@ -130,6 +131,8 @@ TEST(LAPLACE_BELTRAMI_FLAT_SFF) {
   solution.PutScalar(0.0);
 
   int ierr = solver->ApplyInverse(rhs, solution);
+  if (diff_op == "diffusion operator") 
+      ver.CheckResidual(solution, 1.0e-12);
 
   int num_itrs = solver->num_itrs();
   CHECK(num_itrs < 10);
@@ -146,237 +149,11 @@ TEST(LAPLACE_BELTRAMI_FLAT_SFF) {
     GMV::close_data_file();
   }
 }
-
-
-TEST(LAPLACE_BELTRAMI_FLAT_SCC) {
-  using namespace Teuchos;
-  using namespace Amanzi;
-  using namespace Amanzi::AmanziMesh;
-  using namespace Amanzi::AmanziGeometry;
-  using namespace Amanzi::Operators;
-
-  Epetra_MpiComm comm(MPI_COMM_WORLD);
-  int MyPID = comm.MyPID();
-
-  if (MyPID == 0) std::cout << "\nTest: Laplace Beltrami solver: preconditioner Scc" << std::endl;
-
-  // read parameter list
-  std::string xmlFileName = "test/operator_laplace_beltrami.xml";
-  ParameterXMLFileReader xmlreader(xmlFileName);
-  ParameterList plist = xmlreader.getParameters();
-
-  // create an SIMPLE mesh framework
-  ParameterList region_list = plist.sublist("Regions Flat");
-  Teuchos::RCP<GeometricModel> gm = Teuchos::rcp(new GeometricModel(3, region_list, &comm));
-
-  MeshFactory meshfactory(&comm);
-  meshfactory.preference(FrameworkPreference({Framework::MSTK}));
-  RCP<const Mesh> mesh = meshfactory(0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 10, 10, 5, gm);
-  RCP<const Mesh_MSTK> mesh_mstk = rcp_static_cast<const Mesh_MSTK>(mesh);
-
-  // extract surface mesh
-  std::vector<std::string> setnames;
-  setnames.push_back(std::string("Top surface"));
-
-  RCP<Mesh> surfmesh = Teuchos::rcp(new Mesh_MSTK(*mesh_mstk, setnames, AmanziMesh::FACE));
-
-  // modify diffusion coefficient
-  // -- since rho=mu=1.0, we do not need to scale the diffusion coefficient.
-  Teuchos::RCP<std::vector<WhetStone::Tensor> > K = Teuchos::rcp(new std::vector<WhetStone::Tensor>());
-  int ncells_owned = surfmesh->num_entities(AmanziMesh::CELL, AmanziMesh::Parallel_type::OWNED);
-  int nfaces_wghost = surfmesh->num_entities(AmanziMesh::FACE, AmanziMesh::Parallel_type::ALL);
-
-  for (int c = 0; c < ncells_owned; c++) {
-    WhetStone::Tensor Kc(2, 1);
-    const Point& xc = mesh->cell_centroid(c);
-    Kc(0, 0) = 1.0 + xc[0] * xc[0];
-    K->push_back(Kc);
-  }
-  double rho(1.0), mu(1.0);
-
-  // create boundary data (no mixed bc)
-  Teuchos::RCP<BCs> bc = Teuchos::rcp(new BCs(surfmesh, AmanziMesh::FACE, DOF_Type::SCALAR));
-  std::vector<int>& bc_model = bc->bc_model();
-  std::vector<double>& bc_value = bc->bc_value();
-
-  for (int f = 0; f < nfaces_wghost; f++) {
-    const Point& xf = surfmesh->face_centroid(f);
-    if (fabs(xf[0]) < 1e-6 || fabs(xf[0] - 1.0) < 1e-6 ||
-        fabs(xf[1]) < 1e-6 || fabs(xf[1] - 1.0) < 1e-6) {
-      bc_model[f] = OPERATOR_BC_DIRICHLET;
-      bc_value[f] = xf[1] * xf[1];
-    }
-  }
-
-  // create diffusion operator 
-  Teuchos::ParameterList olist = plist.sublist("PK operator").sublist("diffusion operator Scc");
-  Teuchos::RCP<PDE_Diffusion> op = Teuchos::rcp(new PDE_DiffusionMFD(olist, surfmesh));
-  op->SetBCs(bc, bc);
-  const CompositeVectorSpace& cvs = op->global_operator()->DomainMap();
-
-  op->Setup(K, Teuchos::null, Teuchos::null);
-  op->UpdateMatrices(Teuchos::null, Teuchos::null);
-
-  // get and assmeble the global operator
-  Teuchos::RCP<Operator> global_op = op->global_operator();
-  op->ApplyBCs(true, true, true);
-  global_op->SymbolicAssembleMatrix();
-  global_op->AssembleMatrix();
-
-  // create preconditoner
-  ParameterList slist = plist.sublist("preconditioners").sublist("Hypre AMG");
-  global_op->InitializePreconditioner(slist);
-  global_op->UpdatePreconditioner();
-
-  // Test SPD properties of the matrix and preconditioner.
-  VerificationCV ver(global_op);
-  ver.CheckMatrixSPD();
-  ver.CheckPreconditionerSPD();
-
-  // solve the problem
-  ParameterList lop_list = plist.sublist("solvers").sublist("PCG").sublist("pcg parameters");
-  auto solver = Teuchos::rcp(new AmanziSolvers::LinearOperatorPCG<
-      Operator, CompositeVector, CompositeVectorSpace>(global_op, global_op));
-  solver->Init(lop_list);
-
-  CompositeVector rhs = *global_op->rhs();
-  CompositeVector solution(rhs);
-  solution.PutScalar(0.0);
-
-  int ierr = solver->ApplyInverse(rhs, solution);
-
-  ver.CheckResidual(solution, 1.0e-12);
-
-  int num_itrs = solver->num_itrs();
-  CHECK(num_itrs < 10);
-
-  if (MyPID == 0) {
-    std::cout << "pressure solver (pcg): ||r||=" << solver->residual() << " itr=" << num_itrs
-              << " code=" << solver->returned_code() << std::endl;
-
-    // visualization
-    const Epetra_MultiVector& p = *solution.ViewComponent("cell");
-    GMV::open_data_file(*surfmesh, (std::string)"operators.gmv");
-    GMV::start_data();
-    GMV::write_cell_data(p, 0, "solution");
-    GMV::close_data_file();
-  }
-}
-
 
 TEST(LAPLACE_BELTRAMI_FLAT) {
-  using namespace Teuchos;
-  using namespace Amanzi;
-  using namespace Amanzi::AmanziMesh;
-  using namespace Amanzi::AmanziGeometry;
-  using namespace Amanzi::Operators;
-
-  Epetra_MpiComm comm(MPI_COMM_WORLD);
-  int MyPID = comm.MyPID();
-
-  if (MyPID == 0) std::cout << "\nTest: Laplace Beltrami solver: preconditioner full" << std::endl;
-
-  // read parameter list
-  std::string xmlFileName = "test/operator_laplace_beltrami.xml";
-  ParameterXMLFileReader xmlreader(xmlFileName);
-  ParameterList plist = xmlreader.getParameters();
-
-  // create an SIMPLE mesh framework
-  ParameterList region_list = plist.sublist("Regions Flat");
-  Teuchos::RCP<GeometricModel> gm = Teuchos::rcp(new GeometricModel(3, region_list, &comm));
-
-  MeshFactory meshfactory(&comm);
-  meshfactory.preference(FrameworkPreference({Framework::MSTK}));
-  RCP<const Mesh> mesh = meshfactory(0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 10, 10, 5, gm);
-  RCP<const Mesh_MSTK> mesh_mstk = rcp_static_cast<const Mesh_MSTK>(mesh);
-
-  // extract surface mesh
-  std::vector<std::string> setnames;
-  setnames.push_back(std::string("Top surface"));
-
-  RCP<Mesh> surfmesh = Teuchos::rcp(new Mesh_MSTK(*mesh_mstk, setnames, AmanziMesh::FACE));
-
-  // modify diffusion coefficient
-  // -- since mu = 1.0, we do not need to scale the diffusion coefficient.
-  Teuchos::RCP<std::vector<WhetStone::Tensor> > K = Teuchos::rcp(new std::vector<WhetStone::Tensor>());
-  int ncells_owned = surfmesh->num_entities(AmanziMesh::CELL, AmanziMesh::Parallel_type::OWNED);
-  int nfaces_wghost = surfmesh->num_entities(AmanziMesh::FACE, AmanziMesh::Parallel_type::ALL);
-
-  for (int c = 0; c < ncells_owned; c++) {
-    WhetStone::Tensor Kc(2, 1);
-    const Point& xc = mesh->cell_centroid(c);
-    Kc(0, 0) = 1.0 + xc[0] * xc[0];
-    K->push_back(Kc);
-  }
-  double rho(1.0), mu(1.0);
-
-  // create boundary data (no mixed bc)
-  Teuchos::RCP<BCs> bc = Teuchos::rcp(new BCs(surfmesh, AmanziMesh::FACE, DOF_Type::SCALAR));
-  std::vector<int>& bc_model = bc->bc_model();
-  std::vector<double>& bc_value = bc->bc_value();
-
-  for (int f = 0; f < nfaces_wghost; f++) {
-    const Point& xf = surfmesh->face_centroid(f);
-    if (fabs(xf[0]) < 1e-6 || fabs(xf[0] - 1.0) < 1e-6 ||
-        fabs(xf[1]) < 1e-6 || fabs(xf[1] - 1.0) < 1e-6) {
-      bc_model[f] = OPERATOR_BC_DIRICHLET;
-      bc_value[f] = xf[1] * xf[1];
-    }
-  }
-
-  // create diffusion operator 
-  Teuchos::ParameterList olist = plist.sublist("PK operator").sublist("diffusion operator");
-  Teuchos::RCP<PDE_Diffusion> op = Teuchos::rcp(new PDE_DiffusionMFD(olist, surfmesh));
-  op->SetBCs(bc, bc);
-  const CompositeVectorSpace& cvs = op->global_operator()->DomainMap();
-
-  op->Setup(K, Teuchos::null, Teuchos::null);
-  op->UpdateMatrices(Teuchos::null, Teuchos::null);
-
-  // get and assmeble the global operator
-  Teuchos::RCP<Operator> global_op = op->global_operator();
-  op->ApplyBCs(true, true, true);
-  global_op->SymbolicAssembleMatrix();
-  global_op->AssembleMatrix();
-
-  // create preconditoner
-  ParameterList slist = plist.sublist("preconditioners").sublist("Hypre AMG");
-  global_op->InitializePreconditioner(slist);
-  global_op->UpdatePreconditioner();
-
-  // Test SPD properties of the matrix and preconditioner.
-  VerificationCV ver(global_op);
-  ver.CheckMatrixSPD();
-  ver.CheckPreconditionerSPD();
-
-  // solve the problem
-  ParameterList lop_list = plist.sublist("solvers").sublist("PCG").sublist("pcg parameters");
-  auto solver = Teuchos::rcp(new AmanziSolvers::LinearOperatorPCG<
-      Operator, CompositeVector, CompositeVectorSpace>(global_op, global_op));
-  solver->Init(lop_list);
-
-  CompositeVector rhs = *global_op->rhs();
-  CompositeVector solution(rhs);
-  solution.PutScalar(0.0);
-
-  int ierr = solver->ApplyInverse(rhs, solution);
-
-  ver.CheckResidual(solution, 1.0e-12);
-
-  int num_itrs = solver->num_itrs();
-  CHECK(num_itrs < 10);
-
-  if (MyPID == 0) {
-    std::cout << "pressure solver (pcg): ||r||=" << solver->residual() << " itr=" << num_itrs
-              << " code=" << solver->returned_code() << std::endl;
-
-    // visualization
-    const Epetra_MultiVector& p = *solution.ViewComponent("cell");
-    GMV::open_data_file(*surfmesh, (std::string)"operators.gmv");
-    GMV::start_data();
-    GMV::write_cell_data(p, 0, "solution");
-    GMV::close_data_file();
-  }
+  LaplaceBeltramiFlat("diffusion operator Sff");
+  LaplaceBeltramiFlat("diffusion operator Scc");
+  LaplaceBeltramiFlat("diffusion operator");
 }
 
 
