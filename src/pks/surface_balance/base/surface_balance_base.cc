673b36ccc5ee87d35e3ff2234b7a26847703d181
ecoon <etcoon@gmail.com>
2018-10-17 08:18:57 -0400

    removes debugging cruft

--- src/pks/surface_balance/base/surface_balance_base.cc
+++ src/pks/surface_balance/base/surface_balance_base.cc
@@ -104,10 +104,6 @@ SurfaceBalanceBase::FunctionalResidual(double t_old, double t_new, Teuchos::RCP<
                             Teuchos::RCP<TreeVector> u_new, Teuchos::RCP<TreeVector> g) {
   Teuchos::OSTab tab = vo_->getOSTab();
   double dt = t_new - t_old;
-  int cycle = S_next_->cycle();
-  if (cycle >= 175) {
-    std::cout << "we is here!" << std::endl;
-  }
 
   // pointer-copy temperature into state and update any auxilary data
   Solution_to_State(*u_new, S_next_);
