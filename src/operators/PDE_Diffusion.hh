9fc29c405e016bcad10d2c8a255c2483535fe95d
lipnikov <lipnikov@lanl.gov>
2019-01-19 12:55:34 -0700

    Removed incorrect comments.

--- src/operators/PDE_Diffusion.hh
+++ src/operators/PDE_Diffusion.hh
@@ -108,15 +108,6 @@ class PDE_Diffusion : public PDE_HelperDiscretization {
                                      const Teuchos::Ptr<CompositeVector>& flux) = 0;
 
   // -- matrix modification
-  //    primary=true indicates that the operator updates both matrix and right-hand
-  //      side using BC data. If primary=false, only matrix is changed.
-  //    eliminate=true indicates that we eliminate essential BCs for a trial 
-  //      function, i.e. zeros go in the corresponding matrix columns and 
-  //      right-hand side is modified using BC values. This is the optional 
-  //      parameter that enforces symmetry for a symmetric tree  operators.
-  //    essential_eqn=true indicates that the operator places a positive number on 
-  //      the main matrix diagonal for the case of essential BCs. This is the
-  //      implementtion trick.
   virtual void ModifyMatrices(const CompositeVector& u) = 0;
   virtual void ScaleMassMatrices(double s) = 0;
 
