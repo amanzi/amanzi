926fcdf5ff1a09c3495b8f6d19fd41d153cd9d84
Ethan Coon <etcoon@gmail.com>
2019-04-18 14:59:42 -0400

    enables turning off parts of the mpc_pk library that are incompatible with ATS and ENABLE_Physics=False

--- src/pks/mpc_pk/mpc_pks_registration.hh
+++ src/pks/mpc_pk/mpc_pks_registration.hh
@@ -9,18 +9,24 @@
  FLowReactiveTransport_PK registration
 */
 
+#ifndef DISABLE_PHYSICS
 #include "FlowEnergy_PK.hh"
 #include "FlowMatrixFracture_PK.hh"
-#include "TransportMatrixFracture_PK.hh"
 #include "FlowReactiveTransport_PK.hh"
+#endif
+
+#include "TransportMatrixFracture_PK.hh"
 #include "ReactiveTransport_PK.hh"
 
 namespace Amanzi {
 
-RegisteredPKFactory<TransportMatrixFracture_PK> TransportMatrixFracture_PK::reg_("transport matrix fracture");  
-RegisteredPKFactory<FlowMatrixFracture_PK> FlowMatrixFracture_PK::reg_("darcy matrix fracture");
+#ifndef DISABLE_PHYSICS
 RegisteredPKFactory<FlowEnergy_PK> FlowEnergy_PK::reg_("thermal richards");
 RegisteredPKFactory<FlowReactiveTransport_PK> FlowReactiveTransport_PK::reg_("flow reactive transport");
+RegisteredPKFactory<FlowMatrixFracture_PK> FlowMatrixFracture_PK::reg_("darcy matrix fracture");
+#endif
+
+RegisteredPKFactory<TransportMatrixFracture_PK> TransportMatrixFracture_PK::reg_("transport matrix fracture");  
 RegisteredPKFactory<ReactiveTransport_PK> ReactiveTransport_PK::reg_("reactive transport");
 
 }  // namespace Amanzi
