6a9c7d1f424c96846d2cac8198112c616dbe9fa8
lipnikov <lipnikov@lanl.gov>
2019-12-28 18:18:44 -0700

    Modified converter for clear support of hierachical PKs.

--- src/common/interface_platform/test/converter_u_base.cc
+++ src/common/interface_platform/test/converter_u_base.cc
@@ -108,7 +108,7 @@ TEST(CONVERTER_BASE) {
   if (MyPID == 0) std::cout << "Test: convert transport test" << std::endl;
 
   // read parameter list
-  for (int i = 1; i < 6; i++) {
+  for (int i = 1; i < 7; i++) {
     std::stringstream xmlFileName;
     xmlFileName << "test/converter_u_test" << i << ".xml";
 
