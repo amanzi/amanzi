49e7407515e80d18515702b59ec34c7718031674
Daniel Reece Livingston <livingston@lanl.gov>
2020-02-19 13:50:27 -0700

    Raise error if using Python 2

--- doc/web_front/conf.py
+++ doc/web_front/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os, subprocess
 
+assert sys.version_info.major >= 3, 'Python 3.x is required to build documentation'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
