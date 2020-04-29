a118251e52dc848896d0ae29030cdceb15474e9c
Ahmad Jan <ajkhattak@gmail.com>
2020-04-29 14:09:17 -0400

    corrected sources for ats hyporheic transport-chemisty to work.

--- src/pks/transport/TransportSourceFunction_Alquimia.hh
+++ src/pks/transport/TransportSourceFunction_Alquimia.hh
@@ -39,6 +39,9 @@ class TransportSourceFunction_Alquimia : public TransportDomainFunction {
   
   void Compute(double t_old, double t_new);
 
+  void set_mol_dens_data_( Teuchos::Ptr<const Epetra_MultiVector> data) {mol_dens_data_ = data;}
+  void set_liquid_src_data_( Teuchos::Ptr<const Epetra_MultiVector> data) {src_liq_data_ = data;}
+  
   // require by the case class
   virtual std::string name() const { return "volume"; } 
 
@@ -47,7 +50,9 @@ class TransportSourceFunction_Alquimia : public TransportDomainFunction {
 
  private:
   Teuchos::RCP<const AmanziMesh::Mesh> mesh_;
-
+  Teuchos::Ptr<const Epetra_MultiVector> mol_dens_data_;
+  Teuchos::Ptr<const Epetra_MultiVector> src_liq_data_;
+  
   // string function of geochemical conditions
   Teuchos::RCP<FunctionTabularString> f_;
 
