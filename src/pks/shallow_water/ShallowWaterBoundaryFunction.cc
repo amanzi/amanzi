2cc56a724bb1d45aec32935f65af0f027a090e4f
Svetlana <44826089+stokareva@users.noreply.github.com>
2020-06-09 14:48:34 -0600

    Update ShallowWaterBoundaryFunction.cc

--- src/pks/shallow_water/ShallowWaterBoundaryFunction.cc
+++ src/pks/shallow_water/ShallowWaterBoundaryFunction.cc
@@ -14,38 +14,38 @@
 #include "ShallowWaterBoundaryFunction.hh"
 
 namespace Amanzi {
-    namespace ShallowWater {
+namespace ShallowWater {
         
-        /* ****************************************************************
-         * Constructor: extract attributes to setup a submodel.
-         **************************************************************** */
-        ShallowWaterBoundaryFunction::ShallowWaterBoundaryFunction(const Teuchos::ParameterList& plist)
-        {
-        }
+  /* ****************************************************************
+   * Constructor: extract attributes to setup a submodel.
+   **************************************************************** */
+  ShallowWaterBoundaryFunction::ShallowWaterBoundaryFunction(const Teuchos::ParameterList& plist)
+  {
+  }
+
+
+  /* ****************************************************************
+   * Process additional parameters for BC submodels.
+   **************************************************************** */
+  void ShallowWaterBoundaryFunction::ComputeSubmodel(const Teuchos::RCP<const AmanziMesh::Mesh>& mesh)
+  {
+      int dim = mesh->space_dimension();
+
+      if (type_ == WhetStone::DOF_Type::NORMAL_COMPONENT) {
+          for (auto it = begin(); it != end(); ++it) {
+              int f = it->first;
+              const AmanziGeometry::Point& normal = mesh->face_normal(f);
+
+              double tmp(0.0);
+              for (int k = 0; k < dim; ++k) {
+                  tmp += it->second[k] * normal[k];
+              }
+              tmp /= norm(normal);
+              it->second = std::vector<double>(1, tmp);
+          }
+      }
+  }
         
-        
-        /* ****************************************************************
-         * Process additional parameters for BC submodels.
-         **************************************************************** */
-        void ShallowWaterBoundaryFunction::ComputeSubmodel(const Teuchos::RCP<const AmanziMesh::Mesh>& mesh)
-        {
-            int dim = mesh->space_dimension();
-            
-            if (type_ == WhetStone::DOF_Type::NORMAL_COMPONENT) {
-                for (auto it = begin(); it != end(); ++it) {
-                    int f = it->first;
-                    const AmanziGeometry::Point& normal = mesh->face_normal(f);
-                    
-                    double tmp(0.0);
-                    for (int k = 0; k < dim; ++k) {
-                        tmp += it->second[k] * normal[k];
-                    }
-                    tmp /= norm(normal);
-                    it->second = std::vector<double>(1, tmp);
-                }
-            }
-        }
-        
-    }  // namespace ShallowWater
+}  // namespace ShallowWater
 }  // namespace Amanzi
 
