31d9174b3ca7bc2881577007a997f1b0a32c5235
Svetlana Tokareva <s.a.tokareva@gmail.com>
2020-07-10 18:19:41 -0600

    Added mpc_lake_***reg

---
+++ src/pks/mpc/mpc_lake_1D_reg.hh
@@ -0,0 +1,15 @@
+/* -------------------------------------------------------------------------
+ATS
+
+License: see $ATS_DIR/COPYRIGHT
+
+MPC for the 1D lake model.
+
+------------------------------------------------------------------------- */
+#include "mpc_lake_1D.hh"
+
+namespace Amanzi {
+
+RegisteredPKFactory<MPCLake1D> MPCLake1D::reg_("lake model");
+
+} // namespace
