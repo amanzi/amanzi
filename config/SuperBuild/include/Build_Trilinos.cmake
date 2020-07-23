98cc173e923d5ec8f3c2ef2ad010525382e7cc42
Julien Loiseau <jloiseau@lanl.gov>
2020-07-23 09:37:05 -0600

    Correctiong for MueLu in SuperBuild

--- config/SuperBuild/include/Build_Trilinos.cmake
+++ config/SuperBuild/include/Build_Trilinos.cmake
@@ -46,7 +46,7 @@ if (ENABLE_EPETRA)
 endif()
 
 if (ENABLE_KOKKOS)
-  list(APPEND Trilinos_PACKAGE_LIST Kokkos KokkosKernels Tpetra Ifpack2 MueLu ShyLU ShyLU_Node Amesos2 Basker)
+  list(APPEND Trilinos_PACKAGE_LIST Belos Galeri Zoltan2 Kokkos KokkosKernels Tpetra Xpetra Ifpack2 MueLu ShyLU ShyLU_Node Amesos2 Basker) 
 endif()
 
 if (ENABLE_STK_Mesh)
@@ -78,7 +78,9 @@ if (ENABLE_KOKKOS)
   list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DTpetra_INST_INT_LONG_LONG:BOOL=OFF")
   list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DXpetra_Epetra_NO_64BIT_GLOBAL_INDICIES:BOOL=ON")
 
+  list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DMueLu_ENABLE_Epetra:BOOL=OFF")
   list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DAmesos2_ENABLE_ShyLU_NodeBasker:BOOL=ON")
+  list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DTpetra_INST_SERIAL:BOOL=ON") 
 endif()
 
 
