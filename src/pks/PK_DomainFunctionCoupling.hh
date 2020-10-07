2dac39c61af60a93a441a3fac9d9a13da836415f
Ethan Coon <coonet@ornl.gov>
2020-10-07 15:52:10 -0400

    code cleanup, remove debug messages, fixes bug in python amanzi_xml search

--- src/pks/PK_DomainFunctionCoupling.hh
+++ src/pks/PK_DomainFunctionCoupling.hh
@@ -1,9 +1,9 @@
 /*
-  Process Kernels 
+  Process Kernels
 
-  Copyright 2010-201x held jointly by LANS/LANL, LBNL, and PNNL. 
-  Amanzi is released under the three-clause BSD License. 
-  The terms of use and "as is" disclaimer for this license are 
+  Copyright 2010-201x held jointly by LANS/LANL, LBNL, and PNNL.
+  Amanzi is released under the three-clause BSD License.
+  The terms of use and "as is" disclaimer for this license are
   provided in the top-level COPYRIGHT file.
 
   Author: Daniil Svyatsky (dasvyat@lanl.gov)
@@ -11,7 +11,7 @@
   This provide coupling of consistent fields located on manifold and
   in space. For the space, the coupling creates a list of boundary
   conditions. For the manifold, the coupling creates a list of sources.
-  
+
   Typically the mesh provide the map manifold (cell) -> space (face).
   In space, we need the reverse map.
 */
@@ -62,7 +62,7 @@ class PK_DomainFunctionCoupling : public FunctionBase {
 
   Teuchos::RCP<const AmanziMesh::Mesh> mesh_;
   Teuchos::RCP<const State> S_;
-  
+
  private:
   std::string submodel_;
 
@@ -71,7 +71,7 @@ class PK_DomainFunctionCoupling : public FunctionBase {
   std::string field_out_key_, copy_field_out_key_;
   std::string field_in_key_, copy_field_in_key_;
 
-  Teuchos::RCP<MeshIDs> entity_ids_; 
+  Teuchos::RCP<MeshIDs> entity_ids_;
   std::map<AmanziMesh::Entity_ID, AmanziMesh::Entity_ID> reverse_map_;
 };
 
@@ -117,7 +117,7 @@ void PK_DomainFunctionCoupling<FunctionBase>::Init(
 
   flux_key_ = slist.get<std::string>("flux_key");
   copy_flux_key_ = slist.get<std::string>("copy_flux_key", "default");
-    
+
   // create a list of domain ids
   RegionList regions = plist.get<Teuchos::Array<std::string> >("regions").toVector();
   Teuchos::RCP<Domain> domain= Teuchos::rcp(new Domain(regions, region_kind));
@@ -132,7 +132,7 @@ void PK_DomainFunctionCoupling<FunctionBase>::Init(
       entity_ids_->insert(id_list.begin(), id_list.end());
     } else {
       std::stringstream m;
-      m << "Unknown region in processing coupling source: name=" << *region 
+      m << "Unknown region in processing coupling source: name=" << *region
         << ", kind=" << kind << "\n";
       Errors::Message message(m.str());
       Exceptions::amanzi_throw(message);
@@ -156,8 +156,8 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
     const auto& flux_map = S_->GetFieldData(flux_key_)->Map().Map("face", true);
     const auto& source_mesh = S_->GetMesh(Keys::getDomain(field_in_key_));
 
-    S_->GetFieldCopyData(field_out_key_, copy_field_out_key_)->ScatterMasterToGhosted("cell");    
-    S_->GetFieldCopyData(field_in_key_, copy_field_in_key_)->ScatterMasterToGhosted("cell"); 
+    S_->GetFieldCopyData(field_out_key_, copy_field_out_key_)->ScatterMasterToGhosted("cell");
+    S_->GetFieldCopyData(field_in_key_, copy_field_in_key_)->ScatterMasterToGhosted("cell");
 
     if (field_in.NumVectors() != field_out.NumVectors()) {
       std::stringstream m;
@@ -170,7 +170,7 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
     AmanziMesh::Entity_ID_List cells, faces;
     std::vector<int> dirs;
     auto mesh_out = S_->GetFieldData(field_out_key_)->Mesh();
-    
+
     // loop over cells on the manifold
     for (auto c : *entity_ids_) {
       AmanziMesh::Entity_ID f = mesh_->entity_get_parent(AmanziMesh::CELL, c);
@@ -179,7 +179,7 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
 
       if (cells.size() != flux_map->ElementSize(f)) {
         std::stringstream m;
-        m << "Number of flux DOFs doesn't equal to the number of cells sharing a face: " 
+        m << "Number of flux DOFs doesn't equal to the number of cells sharing a face: "
           << cells.size() << " != " << flux_map->ElementSize(f) << std::endl;
         Errors::Message message(m.str());
         Exceptions::amanzi_throw(message);
@@ -188,31 +188,29 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
       double linear(0.0);
       std::vector<double> val(num_vec+1, 0.0); // extra is for water
       int pos = Operators::UniqueIndexFaceToCells(*mesh_out, f, cells[0]);
-      
+
       for (int j = 0; j != cells.size(); ++j) {
         mesh_out->cell_get_faces_and_dirs(cells[j], &faces, &dirs);
 
         for (int i = 0; i < faces.size(); i++) {
           if (f == faces[i]) {
-            int g = flux_map->FirstPointInElement(f);            
+            int g = flux_map->FirstPointInElement(f);
             double fln = flux[0][g + (pos + j)%2] * dirs[i];
-            
-            if (fln >= 0) {        
+
+            if (fln >= 0) {
               linear += fln;
               for (int k = 0; k < num_vec; ++k) {
                 // flux is water flux * Conc
                 val[k] += field_out[k][cells[j]] * fln / source_mesh->cell_volume(c);
-                std::cout << "Transport DF from sub = " << val[k] << std::endl;
               }
             } else {
               val[num_vec] += -fln * (t1 - t0);
-              std::cout << "Transport DF water flux out to sub = " << val[num_vec] << std::endl;
             }
             break;
           }
         }
       }
-      value_[c] = val; 
+      value_[c] = val;
       linear_term_[c] = linear;
     }
 
@@ -255,10 +253,8 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
       for (int k = 0; k < num_vec; ++k) {
         if (field_cons[num_vec][sc] > 0.) {
           val[k] = field_cons[k][sc] / field_cons[num_vec][sc];
-          std::cout << "Transport DF to sub (dry): C = " << val[k] << std::endl;
         } else {
           val[k] = field_out[k][sc];
-          std::cout << "Transport DF to sub (water): C = " << val[k] << std::endl;
         }
       }
       value_[f] = val;
