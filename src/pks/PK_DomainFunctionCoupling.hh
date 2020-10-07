a79f95a4b419df5ea4d91b730e427cf272b2d02e
Coon, Ethan T <coonet@ornl.gov>
2020-10-07 11:02:16 -0400

    updates to domain function coupling to deal with integration of cases where sinks must take the implicit (new timestep) concentration in order to conserve mass of C

--- src/pks/PK_DomainFunctionCoupling.hh
+++ src/pks/PK_DomainFunctionCoupling.hh
@@ -67,6 +67,7 @@ class PK_DomainFunctionCoupling : public FunctionBase {
   std::string submodel_;
 
   std::string flux_key_, copy_flux_key_;
+  std::string field_cons_key_, copy_field_cons_key_;
   std::string field_out_key_, copy_field_out_key_;
   std::string field_in_key_, copy_field_in_key_;
 
@@ -109,10 +110,13 @@ void PK_DomainFunctionCoupling<FunctionBase>::Init(
   field_out_key_ = slist.get<std::string>("field_out_key");
   copy_field_out_key_ = slist.get<std::string>("copy_field_out_key", "default");
 
-  if (submodel_ == "rate") {
-    flux_key_ = slist.get<std::string>("flux_key", "none");
-    copy_flux_key_ = slist.get<std::string>("copy_flux_key", "default");
+  if (submodel_ == "field") {
+    field_cons_key_ = slist.get<std::string>("field_conserved_key");
+    copy_field_cons_key_ = slist.get<std::string>("copy_field_cons_key", "default");
   }
+
+  flux_key_ = slist.get<std::string>("flux_key");
+  copy_flux_key_ = slist.get<std::string>("copy_flux_key", "default");
     
   // create a list of domain ids
   RegionList regions = plist.get<Teuchos::Array<std::string> >("regions").toVector();
@@ -124,7 +128,7 @@ void PK_DomainFunctionCoupling<FunctionBase>::Init(
   for (auto region = domain->first.begin(); region != domain->first.end(); ++region) {
     if (mesh_->valid_set_name(*region, kind)) {
       AmanziMesh::Entity_ID_List id_list;
-      mesh_->get_set_entities(*region, kind, AmanziMesh::Parallel_type::ALL, &id_list);
+      mesh_->get_set_entities(*region, kind, AmanziMesh::Parallel_type::OWNED, &id_list);
       entity_ids_->insert(id_list.begin(), id_list.end());
     } else {
       std::stringstream m;
@@ -150,6 +154,7 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
     const auto& field_in = *S_->GetFieldCopyData(field_in_key_, copy_field_in_key_)->ViewComponent("cell", true);
 
     const auto& flux_map = S_->GetFieldData(flux_key_)->Map().Map("face", true);
+    const auto& source_mesh = S_->GetMesh(Keys::getDomain(field_in_key_));
 
     S_->GetFieldCopyData(field_out_key_, copy_field_out_key_)->ScatterMasterToGhosted("cell");    
     S_->GetFieldCopyData(field_in_key_, copy_field_in_key_)->ScatterMasterToGhosted("cell"); 
@@ -167,8 +172,8 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
     auto mesh_out = S_->GetFieldData(field_out_key_)->Mesh();
     
     // loop over cells on the manifold
-    for (auto c = entity_ids_->begin(); c != entity_ids_->end(); ++c) {
-      AmanziMesh::Entity_ID f = mesh_->entity_get_parent(AmanziMesh::CELL, *c);
+    for (auto c : *entity_ids_) {
+      AmanziMesh::Entity_ID f = mesh_->entity_get_parent(AmanziMesh::CELL, c);
 
       mesh_out->face_get_cells(f, AmanziMesh::Parallel_type::ALL, &cells);
 
@@ -181,7 +186,7 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
       }
 
       double linear(0.0);
-      std::vector<double> val(num_vec, 0.0);
+      std::vector<double> val(num_vec+1, 0.0); // extra is for water
       int pos = Operators::UniqueIndexFaceToCells(*mesh_out, f, cells[0]);
       
       for (int j = 0; j != cells.size(); ++j) {
@@ -195,26 +200,27 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
             if (fln >= 0) {        
               linear += fln;
               for (int k = 0; k < num_vec; ++k) {
-                val[k] += field_out[k][cells[j]] * fln;
+                // flux is water flux * Conc
+                val[k] += field_out[k][cells[j]] * fln / source_mesh->cell_volume(c);
+                std::cout << "Transport DF from sub = " << val[k] << std::endl;
               }
-            } else {       
-              for (int k = 0; k < num_vec; ++k) {
-                val[k] += field_in[k][*c] * fln;
-              }
+            } else {
+              val[num_vec] += -fln * (t1 - t0);
+              std::cout << "Transport DF water flux out to sub = " << val[num_vec] << std::endl;
             }
             break;
           }
         }
       }
-      value_[*c] = val; 
-      linear_term_[*c] = linear;
+      value_[c] = val; 
+      linear_term_[c] = linear;
     }
-  }
-  else if (submodel_ == "field") {
-    // create reserse map from space (face) onto manifold (cell)
+
+  } else if (submodel_ == "field") {
+    // create reverse map from space (face) onto manifold (cell)
     auto mesh_out = S_->GetFieldData(field_out_key_)->Mesh();
 
-    if (mesh_->space_dimension() == mesh_->manifold_dimension() && reverse_map_.size() == 0) {
+    if (reverse_map_.size() == 0 && mesh_->space_dimension() == mesh_->manifold_dimension()) {
       const Epetra_Map& cell_map = mesh_out->cell_map(true);
       for (int c = 0 ; c < cell_map.NumMyElements(); ++c) {
         AmanziMesh::Entity_ID f = mesh_out->entity_get_parent(AmanziMesh::CELL, c);
@@ -223,19 +229,39 @@ void PK_DomainFunctionCoupling<FunctionBase>::Compute(double t0, double t1)
     }
 
     const auto& field_out = *S_->GetFieldCopyData(field_out_key_, copy_field_out_key_)->ViewComponent("cell", true);
+    const auto& field_cons = *S_->GetFieldCopyData(field_cons_key_, copy_field_cons_key_)->ViewComponent("cell", true);
 
     int num_vec = field_out.NumVectors();
     std::vector<double> val(num_vec);
+    AMANZI_ASSERT(num_vec + 2 == field_cons.NumVectors());
 
     // Loop over faces (owned + ghosted) in the space restricted to the manifold.
     // The set of these faces could be bigger then the set of manifold cells (owned + ghosted)
-    for (auto f = entity_ids_->begin(); f != entity_ids_->end(); ++f) {
-      auto it = reverse_map_.find(*f);
+    for (auto f : *entity_ids_) {
+      auto it = reverse_map_.find(f);
       if (it == reverse_map_.end()) continue;
+      int sc = it->second;
 
-      int c = it->second;
-      for (int k = 0; k < num_vec; ++k) val[k] = field_out[k][c]; 
-      value_[*f] = val;     
+      // accept it all
+      AmanziMesh::Entity_ID_List cells, faces;
+      std::vector<int> dirs;
+      mesh_->face_get_cells(f, AmanziMesh::Parallel_type::OWNED, &cells);
+      AMANZI_ASSERT(cells.size() == 1);
+
+      mesh_->cell_get_faces_and_dirs(cells[0], &faces, &dirs);
+      auto i = std::find(faces.begin(), faces.end(), f) - faces.begin();
+      AMANZI_ASSERT(i < dirs.size());
+
+      for (int k = 0; k < num_vec; ++k) {
+        if (field_cons[num_vec][sc] > 0.) {
+          val[k] = field_cons[k][sc] / field_cons[num_vec][sc];
+          std::cout << "Transport DF to sub (dry): C = " << val[k] << std::endl;
+        } else {
+          val[k] = field_out[k][sc];
+          std::cout << "Transport DF to sub (water): C = " << val[k] << std::endl;
+        }
+      }
+      value_[f] = val;
     }
   }
 }
