b3e9be7d12b5b75b6ea9f545a90916d3318e9320
Daniil Svyatsky <dasvyat@lanl.gov>
2020-11-23 12:01:59 -0700

    added missing reg file

---
+++ src/state/time_average_field_evaluator_reg.hh
@@ -0,0 +1,7 @@
+#include "time_average_field_evaluator.hh"
+namespace Amanzi {
+
+Utils::RegisteredFactory<FieldEvaluator,TimeAverageFieldEvaluator> TimeAverageFieldEvaluator::fac_("time average");
+
+} // namespace
+
