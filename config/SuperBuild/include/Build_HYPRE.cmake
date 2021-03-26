227ed7c51c02d4e8927406bc2691f292c59d5880
J David Moulton <moulton@lanl.gov>
2021-03-26 15:35:17 -0600

    This update fixes a minor bug in bootstrap.sh, and the OpenMP build for Hypre.
    The --with-openmp option for Hypre doesn't configure the linking correctly
    so I added find(OpenMP) at the CMakeLists.txt level, and use the appropriate
    library to fix the linking problem.
    
    Having access to OpenMP flags throughout seems useful so I think I'd like to
    keep this there, but admittedly it seems to have some problems with intel.
    So needs more testing.
    
    With this update our common flags pick up the OpenMP command line options,
    so all libraries that don't override these flags are building with OpenMP
    when ENABLE_KOKKOS_OPENMP is set.

--- config/SuperBuild/include/Build_HYPRE.cmake
+++ config/SuperBuild/include/Build_HYPRE.cmake
@@ -22,38 +22,49 @@ amanzi_tpl_version_write(FILENAME ${TPL_VERSIONS_INCLUDE_FILE}
 # if (ENABLE_OpenMP)
 #   find_package(OpenMP)
 
-set(hypre_openmp_opt)
-if (KOKKOS_ENABLE_OPENMP)
-  set(hypre_openmp_opt "--with-openmp")
+if (ENABLE_KOKKOS_OPENMP)
+  set(hypre_openmp_opt "--with-openmp" "--with-LDFLAGS=${OpenMP_C_LIBRARIES}")
+else()
+  set(hypre_openmp_opt)
 endif()
 
 # Locate LAPACK and BLAS
-set(hypre_blas_opt)
-find_package(BLAS)
 if (BLAS_FOUND)
-  set(hypre_blas_opt "--with-blas"
-                     "--with-blas-lib=${BLAS_LIBRARIES}")
+  if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    set(hypre_blas_opt "--with-blas-lib=-framework Accelerate")
+  else()
+    set(hypre_blas_opt "--with-blas-lib=${BLAS_LIBRARIES}")
+  endif()
+else()
+  set(hypre_blas_opt)
 endif()
 
-set(hypre_lapack_opt)
-find_package(LAPACK)
 if (LAPACK_FOUND)
-  set(hypre_lapack_opt "--with-lapack"
-                       "--with-lapack-lib=${LAPACK_LIBRARIES}")
+  if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    set(hypre_lapack_opt "--with-lapack-lib=-framework Accelerate")
+  else()
+    set(hypre_lapack_opt "--with-lapack-lib=${LAPACK_LIBRARIES}")
+  endif()
+else()
+  set(hypre_lapack_opt)
 endif()
 
-set(hypre_kokkos_cuda)
-set(CUDA_HOME)
-set(Hypre_CUDA_SM)
 if(ENABLE_KOKKOS_CUDA)
   find_package(CUDA REQUIRED)
   set(CUDA_HOME ${CUDA_INCLUDE_DIRS}/..)
   message(STATUS "CUDA_HOME: ${CUDA_HOME}")
   set(Hypre_CUDA_SM 70)
   set(hypre_kokkos_cuda "--with-cuda" "--enable-cusparse" "--enable-unified-memory")
+else()
+  set(CUDA_HOME)
+  set(Hypre_CUDA_SM)
+  set(hypre_kokkos_cuda)
 endif()
 
 # set(hypre_fortran_opt -"--disable-fortran)
+
+set(Hypre_CC_FLAGS ${CMAKE_C_FLAGS})
+set(Hypre_CXX_FLAGS ${CMAKE_CXX_FLAGS})
 
 # Locate SuperLU and SuperLUDist
 set(hypre_superlu_opt "--with-superlu" 
@@ -64,9 +75,10 @@ set(hypre_superlu_opt "--with-superlu"
                       "--with-dsuperlu-lib=${SuperLUDist_LIBRARY}")
 
 # shared/static libraries (shared FEI is broken in HYPRE)
-set(hypre_shared_opt "no")
 if (BUILD_SHARED_LIBS)
-  set(hypre_shared_opt "yes")
+  set(hypre_shared_opt "--enable-shared")
+else()
+  set(hypre_shared_opt "--disable-shared")
 endif()
 
 # --- Set the name of the patch
@@ -101,27 +113,27 @@ ExternalProject_Add(${HYPRE_BUILD_TARGET}
                     SOURCE_DIR    ${HYPRE_source_dir}
                     SOURCE_SUBDIR src                          # cmake 3.7+ feature 
                     CONFIGURE_COMMAND
-                                 ${HYPRE_source_dir}/src/configure
-                                       --prefix=${TPL_INSTALL_PREFIX}
-                                       --with-MPI
-                                       --enable-shared=${hypre_shared_opt}
-                                       ${hypre_openmp_opt}
-                                       ${hypre_lapack_opt}
-                                       ${hypre_blas_opt}
-                                       ${hypre_superlu_opt}
-                                       ${hypre_kokkos_cuda}
-                                       CC=${CMAKE_C_COMPILER}
-                                       CFLAGS=${Hypre_CC_FLAGS}
-                                       CXX=${CMAKE_CXX_COMPILER}
-                                       CXXFLAGS=${Hypre_CXX_FLAGS}
-                                       CUDA_HOME=${CUDA_HOME}
-                                       HYPRE_CUDA_SM=${Hypre_CUDA_SM}               
+                       ${HYPRE_source_dir}/src/configure
+                         --prefix=${TPL_INSTALL_PREFIX}
+                         --with-MPI
+                         ${hypre_shared_opt}
+                         ${hypre_openmp_opt}
+                         ${hypre_lapack_opt}
+                         ${hypre_blas_opt}
+                         ${hypre_superlu_opt}
+                         ${hypre_kokkos_cuda}
+                         CC=${CMAKE_C_COMPILER}
+                         CFLAGS=${Hypre_CC_FLAGS}
+                         CXX=${CMAKE_CXX_COMPILER}
+                         CXXFLAGS=${Hypre_CXX_FLAGS}
+                         CUDA_HOME=${CUDA_HOME}
+                         HYPRE_CUDA_SM=${Hypre_CUDA_SM}               
                     # -- Build
                     BINARY_DIR       ${HYPRE_source_dir}/src        # Build directory 
                     BUILD_COMMAND    $(MAKE)  
                     # -- Install
                     INSTALL_DIR      ${TPL_INSTALL_PREFIX}     # Install directory
-      		    INSTALL_COMMAND  $(MAKE) install
+      		        INSTALL_COMMAND  $(MAKE) install
                     # -- Output control
                     ${HYPRE_logging_args})
 
