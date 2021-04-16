789a44333d17dfbb22a30371d602a09fc4adcaf1
Ethan Coon <coonet@ornl.gov>
2021-04-16 10:17:33 -0400

    documents and cleans up micropore-macropore flux evaluator

--- src/pks/surface_balance/constitutive_relations/litter/micropore_macropore_flux_evaluator.cc
+++ src/pks/surface_balance/constitutive_relations/litter/micropore_macropore_flux_evaluator.cc
@@ -1,21 +1,47 @@
 /*
-  The micropore-macropore flux evaluator is an algebraic evaluator of a given model.
+  ATS is released under the three-clause BSD License.
+  The terms of use and "as is" disclaimer for this license are
+  provided in the top-level COPYRIGHT file.
 
-  Generated via evaluator_generator with:
-    evalName = micropore_macropore_flux
-    modelMethodDeclaration =   double MicroporeMacroporeFlux(double pm, double pM, double krM, double krm, double K) const;
-    namespaceCaps = SURFACEBALANCE
-    namespace = SurfaceBalance
-    evalNameCaps = MICROPORE_MACROPORE_FLUX
-    myMethodArgs = pm_v[0][i], pM_v[0][i], krM_v[0][i], krm_v[0][i], K_v[0][i]
-    myKeyMethod = MicroporeMacroporeFlux
-    myKeyFirst = micropore
-    evalNameString = micropore-macropore flux
-    myMethodDeclarationArgs = double pm, double pM, double krM, double krm, double K
-    evalClassName = MicroporeMacroporeFlux
-    myKey = micropore_macropore_flux  
-  
   Authors: Ethan Coon (ecoon@lanl.gov)
+*/
+//! Exchange flux between multiple continua.
+
+/*!
+
+Evaluates the following exchange flux model:
+
+.. math::
+   q_{exchange} = k_r K \frac{\Gamma}{\delta} (p_M - p_m)
+
+where :math:`p` is the pressure of the Macro and micro porespaces,
+respectively, K is some measure of an absolute permeability, :math:`\Gamma [-]`
+is the exchange coefficient, :math:`\delta [m]` is a unit of distance
+characterizing the typical distance between pore, and :math:`k_r` is the
+relative permeability, which is upwinded based on the larger of the two
+pressures.
+
+Note that the expected domain for this is the micropore domain, but may be
+changed on the input line.
+
+.. _micropore-macropore-flux-evaluator-spec:
+.. admonition:: micropore-micropore-flux-evaluator
+
+   * `"micropore domain`" ``[string]`` **""** Defaults to the domain of the flux's
+     variable name.
+
+   * `"macropore domain`" ``[string]`` **macropore**
+
+   * `"micropore macropore flux model parameters`" ``[micropore-macropore-flux-model-spec]``
+
+   KEYS:
+
+   * `"micropore pressure`" **pressure**
+   * `"macropore pressure`" **MACROPORE_DOMAIN-pressure**
+   * `"micropore relative permeability`" **relative_permeability**
+   * `"macropore relative permeability`" **MACROPORE_DOMAIN-relative_permeability**
+   * `"permeability`" **permeability**
+
 */
 
 #include "micropore_macropore_flux_evaluator.hh"
@@ -42,7 +68,7 @@ MicroporeMacroporeFluxEvaluator::MicroporeMacroporeFluxEvaluator(const Micropore
     pM_key_(other.pM_key_),
     krM_key_(other.krM_key_),
     krm_key_(other.krm_key_),
-    K_key_(other.K_key_),    
+    K_key_(other.K_key_),
     model_(other.model_) {}
 
 
@@ -59,41 +85,25 @@ void
 MicroporeMacroporeFluxEvaluator::InitializeFromPlist_()
 {
   // Set up my dependencies
-  // - defaults to prefixed via domain
-  std::size_t end = my_key_.find_first_of("_");
-  std::string domain_name = my_key_.substr(0,end);
-
-  std::string my_key_first("micropore");
-  if (domain_name == my_key_first) {
-    domain_name = std::string("");
-  } else {
-    domain_name = domain_name+std::string("_");
-  }
+  Key micro_domain = Keys::getDomain(my_key_);
+  micro_domain = plist_.get<std::string>("micropore domain name", micro_domain);
+  Key macro_domain = plist_.get<std::string>("macropore domain name", "macropore");
 
   // - pull Keys from plist
   // dependency: micropore_pressure
-  pm_key_ = plist_.get<std::string>("micropore pressure key",
-          domain_name+std::string("micropore_pressure"));
+  pm_key_ = Keys::readKey(plist_, micro_domain, "micropore pressure", "pressure");
   dependencies_.insert(pm_key_);
-
   // dependency: pressure
-  pM_key_ = plist_.get<std::string>("pressure key",
-          domain_name+std::string("pressure"));
+  pM_key_ = Keys::readKey(plist_, macro_domain, "macropore pressure", "pressure");
   dependencies_.insert(pM_key_);
-
-  // dependency: relative_permeability
-  krM_key_ = plist_.get<std::string>("relative permeability key",
-          domain_name+std::string("relative_permeability"));
-  dependencies_.insert(krM_key_);
-
   // dependency: micropore_relative_permeability
-  krm_key_ = plist_.get<std::string>("micropore relative permeability key",
-          domain_name+std::string("micropore_relative_permeability"));
+  krm_key_ = Keys::readKey(plist_, micro_domain, "micropore relative permeability", "relative_permeability");
   dependencies_.insert(krm_key_);
-
+  // dependency: relative_permeability
+  krM_key_ = Keys::readKey(plist_, macro_domain, "macropore relative permeability", "relative_permeability");
+  dependencies_.insert(krM_key_);
   // dependency: micropore_absolute_permeability
-  K_key_ = plist_.get<std::string>("micropore absolute permeability key",
-          domain_name+std::string("micropore_absolute_permeability"));
+  K_key_ = Keys::readKey(plist_, macro_domain, "macropore absolute permeability", "permeability");
   dependencies_.insert(K_key_);
 }
 
@@ -102,11 +112,11 @@ void
 MicroporeMacroporeFluxEvaluator::EvaluateField_(const Teuchos::Ptr<State>& S,
         const Teuchos::Ptr<CompositeVector>& result)
 {
-Teuchos::RCP<const CompositeVector> pm = S->GetFieldData(pm_key_);
-Teuchos::RCP<const CompositeVector> pM = S->GetFieldData(pM_key_);
-Teuchos::RCP<const CompositeVector> krM = S->GetFieldData(krM_key_);
-Teuchos::RCP<const CompositeVector> krm = S->GetFieldData(krm_key_);
-Teuchos::RCP<const CompositeVector> K = S->GetFieldData(K_key_);
+  Teuchos::RCP<const CompositeVector> pm = S->GetFieldData(pm_key_);
+  Teuchos::RCP<const CompositeVector> pM = S->GetFieldData(pM_key_);
+  Teuchos::RCP<const CompositeVector> krM = S->GetFieldData(krM_key_);
+  Teuchos::RCP<const CompositeVector> krm = S->GetFieldData(krm_key_);
+  Teuchos::RCP<const CompositeVector> K = S->GetFieldData(K_key_);
 
   for (CompositeVector::name_iterator comp=result->begin();
        comp!=result->end(); ++comp) {
@@ -129,11 +139,11 @@ void
 MicroporeMacroporeFluxEvaluator::EvaluateFieldPartialDerivative_(const Teuchos::Ptr<State>& S,
         Key wrt_key, const Teuchos::Ptr<CompositeVector>& result)
 {
-Teuchos::RCP<const CompositeVector> pm = S->GetFieldData(pm_key_);
-Teuchos::RCP<const CompositeVector> pM = S->GetFieldData(pM_key_);
-Teuchos::RCP<const CompositeVector> krM = S->GetFieldData(krM_key_);
-Teuchos::RCP<const CompositeVector> krm = S->GetFieldData(krm_key_);
-Teuchos::RCP<const CompositeVector> K = S->GetFieldData(K_key_);
+  Teuchos::RCP<const CompositeVector> pm = S->GetFieldData(pm_key_);
+  Teuchos::RCP<const CompositeVector> pM = S->GetFieldData(pM_key_);
+  Teuchos::RCP<const CompositeVector> krM = S->GetFieldData(krM_key_);
+  Teuchos::RCP<const CompositeVector> krm = S->GetFieldData(krm_key_);
+  Teuchos::RCP<const CompositeVector> K = S->GetFieldData(K_key_);
 
   if (wrt_key == pm_key_) {
     for (CompositeVector::name_iterator comp=result->begin();
@@ -219,7 +229,6 @@ Teuchos::RCP<const CompositeVector> K = S->GetFieldData(K_key_);
     AMANZI_ASSERT(0);
   }
 }
-
 
 } //namespace
 } //namespace
