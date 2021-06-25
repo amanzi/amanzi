cbe18d0c630781528201945c6cfd2b381c3cf8fd
lipnikov <lipnikov@lanl.gov>
2021-06-25 16:11:20 -0600

    Fixed a subtle merge issue.

--- src/structured_grid/mpc_pk/PM_setup.cpp
+++ src/structured_grid/mpc_pk/PM_setup.cpp
@@ -39,9 +39,8 @@
 #include <AlquimiaHelper_Structured.H>
 #include <AmanziChemHelper_Structured.H>
 
-#include "SimpleThermoDatabase.hh"
 #include "ActivityModelFactory.hh"
-#include "beaker.hh"
+#include "SimpleThermoDatabase.hh"
 
 #include <TabularFunction.H>
 
