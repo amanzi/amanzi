4f354593dce3d3bd99227f15f47a788b9872385d
Hyun Lim <hylim1988@gmail.com>
2021-07-26 13:29:31 -0600

    add descripation for FILU

--- src/operators/test/verify_operators.cc
+++ src/operators/test/verify_operators.cc
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
   if (argc < 4) {
     std::cout <<
       "Usage: verify_operators  with_pc|direct  mesh_type  mesh_size|mesh_file  scheme  tol\n\n"
-      "  (req) with_pc   = identity|diagonal|ifpack2: ILUT|ifpack2: RILUK\n"
+      "  (req) with_pc   = identity|diagonal|ifpack2: ILUT|ifpack2: RILUK|ifpack2: FIlu\n"
       "                    Hypre: AMG|Hypre: Euclid\n"
       "                    Trilinos: ML|Trilinos: MueLu\n"
       "  (req) direct    = Amesos1: KLU|Amesos2: Basker|Amesos2: SuperLUDist\n\n"
