f6f84a2b25c5c42182b9ab39c93ab04f8dae28a3
Ethan Coon <coonet@ornl.gov>
2021-07-29 16:29:22 -0400

    merges build system from master into tpetra-solvers to ensure new changes there are used when configuring Amanzi on this branch

--- config/SuperBuild/include/Build_HYPRE.cmake
+++ config/SuperBuild/include/Build_HYPRE.cmake
@@ -22,40 +22,50 @@ amanzi_tpl_version_write(FILENAME ${TPL_VERSIONS_INCLUDE_FILE}
 # if (ENABLE_OpenMP)
 #   find_package(OpenMP)
 
-set(hypre_openmp_opt)
-# if (OPENMP_FOUND)
-#   set(hypre_openmp_opt "-DHYPRE_USING_OPENMP:BOOL=TRUE")
-# endif()
-#else()
-#set(hypre_openmp_opt "-DHYPRE_USING_OPENMP:BOOL=FALSE")
-#set(hypre_openmp_opt "--with-openmp")
-#endif()
+if (ENABLE_KOKKOS_OPENMP)
+  set(hypre_openmp_opt "--with-openmp" "--with-LDFLAGS=${OpenMP_C_LIBRARIES}")
+else()
+  set(hypre_openmp_opt)
+endif()
 
 # Locate LAPACK and BLAS
-set(hypre_blas_opt)
-find_package(BLAS)
 if (BLAS_FOUND)
-  set(hypre_blas_opt "--with-blas")
+  if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    set(hypre_blas_opt "--with-blas-lib=-framework Accelerate")
+  else()
+    set(hypre_blas_opt "--with-blas-lib=${BLAS_LIBRARIES}")
+  endif()
+else()
+  set(hypre_blas_opt)
 endif()
 
-set(hypre_lapack_opt)
-find_package(LAPACK)
 if (LAPACK_FOUND)
-  set(hypre_lapack_opt "--with-lapack")
+  if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    set(hypre_lapack_opt "--with-lapack-lib=-framework Accelerate")
+  else()
+    set(hypre_lapack_opt "--with-lapack-lib=${LAPACK_LIBRARIES}")
+  endif()
+else()
+  set(hypre_lapack_opt)
 endif()
 
-set(hypre_kokkos_cuda)
-set(CUDA_HOME)
-set(Hypre_CUDA_SM)
 if(ENABLE_KOKKOS_CUDA)
   find_package(CUDA REQUIRED)
-  set(CUDA_HOME ${CUDA_INCLUDE_DIRS}/..)
+  set(CUDA_HOME ${CUDA_TOOLKIT_ROOT_DIR})
   message(STATUS "CUDA_HOME: ${CUDA_HOME}")
   set(Hypre_CUDA_SM 70)
   set(hypre_kokkos_cuda "--with-cuda" "--enable-cusparse" "--enable-unified-memory")
+else()
+  set(CUDA_HOME)
+  set(Hypre_CUDA_SM)
+  set(hypre_kokkos_cuda)
 endif()
 
 # set(hypre_fortran_opt -"--disable-fortran)
+
+set(Hypre_CC_FLAGS ${CMAKE_C_FLAGS})
+set(Hypre_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+set(Hypre_LINK_FLAGS ${CMAKE_LINK_FLAGS})
 
 # Locate SuperLU and SuperLUDist
 set(hypre_superlu_opt "--with-superlu" 
@@ -66,9 +76,10 @@ set(hypre_superlu_opt "--with-superlu"
                       "--with-dsuperlu-lib=${SuperLUDist_LIBRARY}")
 
 # shared/static libraries (shared FEI is broken in HYPRE)
-set(hypre_shared_opt "no")
 if (BUILD_SHARED_LIBS)
-  set(hypre_shared_opt "yes")
+  set(hypre_shared_opt "--enable-shared")
+else()
+  set(hypre_shared_opt "--disable-shared")
 endif()
 
 # --- Set the name of the patch
@@ -86,44 +97,53 @@ configure_file(${SuperBuild_TEMPLATE_FILES_DIR}/hypre-patch-step.cmake.in
 # --- Set the patch command
 set(HYPRE_PATCH_COMMAND ${CMAKE_COMMAND} -P ${HYPRE_cmake_patch})     
 
+# --- If downloads are disabled point to local repository
+if ( DISABLE_EXTERNAL_DOWNLOAD )
+  STRING(REGEX REPLACE ".*\/" "" HYPRE_GIT_REPOSITORY_LOCAL_DIR ${HYPRE_GIT_REPOSITORY})
+  set (HYPRE_GIT_REPOSITORY_TEMP ${TPL_DOWNLOAD_DIR}/${HYPRE_GIT_REPOSITORY_LOCAL_DIR})
+else()
+  set (HYPRE_GIT_REPOSITORY_TEMP ${HYPRE_GIT_REPOSITORY})
+endif()
+message(STATUS "HYPRE git repository = ${HYPRE_GIT_REPOSITORY_TEMP}")
 
 # --- Add external project build and tie to the ZLIB build target
 ExternalProject_Add(${HYPRE_BUILD_TARGET}
                     DEPENDS   ${HYPRE_PACKAGE_DEPENDS}         # Package dependency target
                     TMP_DIR   ${HYPRE_tmp_dir}                 # Temporary files directory
                     STAMP_DIR ${HYPRE_stamp_dir}               # Timestamp and log directory
-                    # -- Download and URL definitions
-                    DOWNLOAD_DIR   ${TPL_DOWNLOAD_DIR}          
-                    URL            ${HYPRE_URL}                # URL may be a web site OR a local file
-                    URL_MD5        ${HYPRE_MD5_SUM}            # md5sum of the archive file
-                    DOWNLOAD_NAME  ${HYPRE_SAVEAS_FILE}        # file name to store (if not end of URL)
+                    # -- Download and GIT definition
+                    GIT_REPOSITORY ${HYPRE_GIT_REPOSITORY_TEMP}              
+                    GIT_TAG        ${HYPRE_GIT_TAG}   
+                    # -- Update (one way to skip this step is use null command)
+                    UPDATE_COMMAND ""
                     # -- Patch 
                     PATCH_COMMAND  ${HYPRE_PATCH_COMMAND}
                     # -- Configure
                     SOURCE_DIR    ${HYPRE_source_dir}
                     SOURCE_SUBDIR src                          # cmake 3.7+ feature 
                     CONFIGURE_COMMAND
-                                 ${HYPRE_source_dir}/src/configure
-                                       --prefix=${TPL_INSTALL_PREFIX}
-                                       --with-MPI
-                                       --enable-shared=${hypre_shared_opt}
-                                       ${hypre_openmp_opt}
-                                       ${hypre_lapack_opt}
-                                       ${hypre_blas_opt}
-                                       ${hypre_superlu_opt}
-                                       ${hypre_kokkos_cuda}
-                                       CC=${CMAKE_CC_COMPILER}
-                                       CFLAGS=${Hypre_CC_FLAGS}
-                                       CXX=${CMAKE_CXX_COMPILER}
-                                       CXXFLAGS=${Hypre_CXX_FLAGS}
-                                       CUDA_HOME=${CUDA_HOME}
-                                       HYPRE_CUDA_SM=${Hypre_CUDA_SM}               
+                       ${HYPRE_source_dir}/src/configure
+                         --prefix=${TPL_INSTALL_PREFIX}
+                         --with-MPI
+                         ${hypre_shared_opt}
+                         ${hypre_openmp_opt}
+                         ${hypre_lapack_opt}
+                         ${hypre_blas_opt}
+                         ${hypre_superlu_opt}
+                         ${hypre_kokkos_cuda}
+                         CC=${CMAKE_C_COMPILER}
+                         CFLAGS=${Hypre_CC_FLAGS}
+                         CXX=${CMAKE_CXX_COMPILER}
+                         CXXFLAGS=${Hypre_CXX_FLAGS}
+                         LDFLAGS=${Hypre_LINK_FLAGS}
+                         CUDA_HOME=${CUDA_HOME}
+                         HYPRE_CUDA_SM=${Hypre_CUDA_SM}               
                     # -- Build
                     BINARY_DIR       ${HYPRE_source_dir}/src        # Build directory 
-                    BUILD_COMMAND    $(MAKE) VERBOSE=1 
+                    BUILD_COMMAND    $(MAKE)  
                     # -- Install
                     INSTALL_DIR      ${TPL_INSTALL_PREFIX}     # Install directory
-      		          INSTALL_COMMAND  $(MAKE) install
+                    INSTALL_COMMAND  $(MAKE) install
                     # -- Output control
                     ${HYPRE_logging_args})
 
