e0c789286ae086d994bc9f62cee453422a643487
Ethan Coon <coonet@ornl.gov>
2022-11-04 12:27:35 -0400

    changes PFLOTRAN repository to the updated link on gitlab

--- config/SuperBuild/TPLVersions.cmake
+++ config/SuperBuild/TPLVersions.cmake
@@ -510,11 +510,11 @@ set(PFLOTRAN_VERSION_MAJOR 0)
 set(PFLOTRAN_VERSION_MINOR 0)
 set(PFLOTRAN_VERSION_PATCH 1ba735f)
 set(PFLOTRAN_VERSION ${PFLOTRAN_VERSION_MAJOR}.${PFLOTRAN_VERSION_MINOR}.${PFLOTRAN_VERSION_PATCH})
-set(PFLOTRAN_URL_STRING     "https://bitbucket.org/pflotran/pflotran/get/release")
+set(PFLOTRAN_URL_STRING     "https://gitlab.com/pflotran/pflotran")
 set(PFLOTRAN_ARCHIVE_FILE   pflotran-1ba735f-17Jul2020.tar.gz)
 set(PFLOTRAN_SAVEAS_FILE    pflotran-1ba735f-17Jul2020.tar.gz)
 set(PFLOTRAN_MD5_SUM        486b0981a90aa662644b907f0a42e7fe)
-set(PFLOTRAN_GIT_REPOSITORY "https://bitbucket.org/pflotran/pflotran/src/master")
+set(PFLOTRAN_GIT_REPOSITORY "https://gitlab.com/pflotran/pflotran.git")
 set(PFLOTRAN_GIT_TAG        "1ba735f1a64a12315c52a64107a75571c53492b3")
 
 #
