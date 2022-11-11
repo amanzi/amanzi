55f2f91a03e6070ea25c5471a4ebd0fb452853f2
Ethan Coon <coonet@ornl.gov>
2022-11-11 13:31:10 -0500

    updates documentation

--- src/state/evaluators/EvaluatorIndependentFromFile.hh
+++ src/state/evaluators/EvaluatorIndependentFromFile.hh
@@ -7,9 +7,51 @@
   provided in the top-level COPYRIGHT file.
 
   Author: Ethan Coon
+*/
+//! An evaluator with no dependencies specified by discrete data in a file.
 
-  A field evaluator with no dependencies specified by discrete data 
-  in an HDF5 file.
+/*!
+
+This evaluator is typically used for providing data that are functions of space
+and time.  Data is provided, discretely (e.g. with one data point per
+cell/face/node), at a series of time slices.  The time slices are interpolated
+linearly in time to provide the value.
+
+Within the file, data is expected to meet the following (HDF5) layout:
+
+..code::
+
+   /time : a 1D array of length NTIMES, providing the time in seconds.
+   /variable_name.ENTITY.DOF  (group)
+
+      /0 : a 1D array of length NENTITIES, providing the values for each entity
+           at time /time[0]
+      /1 : ...
+      /NTIMES-1 : 1D array at time /time[NTIMES-1]
+
+This evaluator is used by providing the option:
+
+`"evaluator type`" == `"independent variable`"
+
+.. _independent-variable-from-file-evaluator-spec:
+.. admonition:: independent-variable-from-file-evaluator-spec
+
+   * `"filename`" ``[string]`` Path to the file.
+   * `"variable name`" ``[string]`` Name of the dataset to read from the file.
+   * `"domain name`" ``[string]`` **domain** Name of the domain on which the
+      field is defined.
+   * `"component name`" ``[string]`` **cell** Name of the component in the
+     field to populate.
+   * `"mesh entity`" ``[string]`` **cell** Name of the entity on which the
+     component is defined.
+   * `"number of dofs`" ``[int]`` **1** Number of degrees of freedom to read.
+   * `"time function`" ``[function-spec]`` **optional** If provided, time is
+     first manipulated by this function before interpolation.  This is useful
+     for things like cyclic data, which can use a modulo time function to
+     repeat the same data.
+
+ */
+
 
   TODO: This needs a test and documentation! --etc
 */
