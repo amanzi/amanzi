3feda8a1bbbfc33a93ab3c40e09263dc1366d0ce
Ethan Coon <coonet@ornl.gov>
2022-12-14 14:16:16 -0500

    reverts templates in superbuild, updates script for running clean to be clear how this should get run

--- config/SuperBuild/templates/unittest-TestTestMacros.cpp
+++ config/SuperBuild/templates/unittest-TestTestMacros.cpp
@@ -1,12 +1,3 @@
-/*
-  Copyright 2010-202x held jointly by participating institutions.
-  Amanzi is released under the three-clause BSD License.
-  The terms of use and "as is" disclaimer for this license are
-  provided in the top-level COPYRIGHT file.
-
-  Authors:
-*/
-
 #include "../UnitTest++.h"
 #include "../TestMacros.h"
 #include "../TestList.h"
@@ -29,7 +20,7 @@ using namespace std;
    #ifndef __clang__
       /* Test for GCC >= 6 --assuming default language standard, which changed in 6.x */
       #ifdef __GNUC__
-         #if (__GNUC__ >= 6)
+         #if (__GNUC__ >= 6) 
             #define _NOEXCEPT_OP(x) noexcept(x)
          #else
             #define _NOEXCEPT_OP(x)
