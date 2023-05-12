fd56a84871622b62077e7bd5fcd4f992abb7b8ad
AndrewGraus <andrewsgraus@gmail.com>
2023-05-11 21:35:11 -0700

    changing the make so it works

--- config/SuperBuild/include/Build_EcoSIM_new.cmake
+++ config/SuperBuild/include/Build_EcoSIM_new.cmake
@@ -55,10 +55,10 @@ ExternalProject_Add(${ECOSIM_BUILD_TARGET}
                                   -DCMAKE_Fortran_COMPILER:FILEPATH=${CMAKE_Fortran_COMPILER}
                     # -- Build
                     BINARY_DIR       ${TPL_INSTALL_PREFIX}          # Build directory
-                    BUILD_COMMAND    ../ecosim--source/make config ATS=1 CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} FC=${CMAKE_Fortran_COMPILER}
+                    BUILD_COMMAND    make -f ../ecosim--source/Makefile config ATS=1 CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} FC=${CMAKE_Fortran_COMPILER}
                     # -- Install
                     INSTALL_DIR      ${TPL_INSTALL_PREFIX}     # Install directory
-                    INSTALL_COMMAND  ../ecosim--source/make install ATS=1 CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} FC=${CMAKE_Fortran_COMPILER}
+                    INSTALL_COMMAND  make -f ../ecosim--source/Makefile install ATS=1 CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} FC=${CMAKE_Fortran_COMPILER}
                     # -- Output control
                     ${ECOSIM_logging_args})
 
