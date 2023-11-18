d6d6007dfde09e2b08a11d9a70577e9206799423
Konstantin Lipnikov <lipnikov@lanl.gov>
2023-11-18 10:59:58 -0700

    Upgraded VerifyHex for new Kokkos style. closes #777

--- src/executables/VerifyHex.cc
+++ src/executables/VerifyHex.cc
@@ -41,14 +41,16 @@ namespace po = boost::program_options;
  * @param filenameout HDF5/XDMF format file to produce
  */
 void
-dump_output(const int& me, Amanzi::AmanziMesh::Mesh& mesh, const std::string& filenameout)
+dump_output(const int& me,
+            Teuchos::RCP<Amanzi::AmanziMesh::Mesh> mesh,
+            const std::string& filenameout)
 {
-  Amanzi::HDF5_MPI* viz_output = new Amanzi::HDF5_MPI(mesh.getComm());
+  Amanzi::HDF5_MPI* viz_output = new Amanzi::HDF5_MPI(mesh->getComm());
   viz_output->setTrackXdmf(true);
-  viz_output->createMeshFile(Teuchos::rcp(&mesh), filenameout);
+  viz_output->createMeshFile(mesh, filenameout);
   viz_output->createDataFile(filenameout);
 
-  const Epetra_Map& cmap(mesh.getMap(Amanzi::AmanziMesh::Entity_kind::CELL, false));
+  const Epetra_Map& cmap(mesh->getMap(Amanzi::AmanziMesh::Entity_kind::CELL, false));
   Epetra_Vector part(cmap);
   int nmycell(cmap.NumMyElements());
   std::vector<int> myidx(nmycell, 0);
@@ -59,6 +61,7 @@ dump_output(const int& me, Amanzi::AmanziMesh::Mesh& mesh, const std::string& fi
   for (unsigned int i = 0; i < nmycell; i++) myidx[i] = i;
   part.ReplaceMyValues(nmycell, &mypart[0], &myidx[0]);
 
+  viz_output->open_h5file();
   viz_output->writeCellDataReal(part, "Partition");
 
   std::fill(mypart.begin(), mypart.end(), -1);
@@ -67,6 +70,7 @@ dump_output(const int& me, Amanzi::AmanziMesh::Mesh& mesh, const std::string& fi
   viz_output->writeCellDataReal(part, "Block");
 
   viz_output->endTimestep();
+  viz_output->close_h5file();
 
   delete viz_output;
 }
@@ -103,149 +107,156 @@ main(int argc, char** argv)
   bf::path progpath(argv[0]);
   std::string progname = progpath.filename();
 
-
   Teuchos::GlobalMPISession mpiSession(&argc, &argv);
-  auto comm = Amanzi::getDefaultComm();
-  const int me(comm->MyPID());
+  Kokkos::initialize();
 
-  unsigned int xcells(4), ycells(4), zcells(4);
-  double xdelta(1.0), ydelta(1.0), zdelta(1.0);
-  double xorigin(0.0), yorigin(0.0), zorigin(0.0);
-  std::string inname;
-  std::string outname("verify_hex");
-  std::string outfilename;
+  {
+    auto comm = Amanzi::getDefaultComm();
+    const int me(comm->MyPID());
 
-  bool dosimple(false);
-  bool doaudit(true);
+    unsigned int xcells(4), ycells(4), zcells(4);
+    double xdelta(1.0), ydelta(1.0), zdelta(1.0);
+    double xorigin(0.0), yorigin(0.0), zorigin(0.0);
+    std::string inname;
+    std::string outname("verify_hex");
+    std::string outfilename;
 
-  po::options_description desc("Available options");
+    bool dosimple(false);
+    bool doaudit(true);
 
-  try {
-    desc.add_options()("help", "produce this help message")
+    po::options_description desc("Available options");
 
-      ("noaudit", "do not audit the generated mesh")
+    try {
+      desc.add_options()("help", "produce this help message")
 
-        ("simple", "use the Mesh_Simple framework instead of stk::mesh")
+        ("noaudit", "do not audit the generated mesh")
 
-          ("xcells",
-           po::value<unsigned int>()->default_value(xcells),
-           "number of cells in the x-direction")("ycells",
-                                                 po::value<unsigned int>()->default_value(ycells),
-                                                 "number of cells in the y-direction")(
-            "zcells",
-            po::value<unsigned int>()->default_value(zcells),
-            "number of "
-            "cells in "
-            "the "
-            "z-"
-            "direction")
+          ("simple", "use the Mesh_Simple framework instead of stk::mesh")
 
-            ("xdelta", po::value<double>()->default_value(xdelta), "cell size in the x-direction")(
-              "ydelta", po::value<double>()->default_value(ydelta), "cell size in the y-direction")(
-              "zdelta",
-              po::value<double>()->default_value(zdelta),
-              "cell size in the "
-              "z-direction")
+            ("xcells",
+             po::value<unsigned int>()->default_value(xcells),
+             "number of cells in the x-direction")("ycells",
+                                                   po::value<unsigned int>()->default_value(ycells),
+                                                   "number of cells in the y-direction")(
+              "zcells",
+              po::value<unsigned int>()->default_value(zcells),
+              "number of "
+              "cells in "
+              "the "
+              "z-"
+              "direction")
 
-              ("xorigin", po::value<double>()->default_value(xorigin), "x origin")(
-                "yorigin", po::value<double>()->default_value(yorigin), "y origin")(
-                "zorigin", po::value<double>()->default_value(zorigin), "z origin")
+              ("xdelta",
+               po::value<double>()->default_value(xdelta),
+               "cell size in the x-direction")("ydelta",
+                                               po::value<double>()->default_value(ydelta),
+                                               "cell size in the y-direction")(
+                "zdelta",
+                po::value<double>()->default_value(zdelta),
+                "cell size in the "
+                "z-direction")
 
-                ("xml-file", po::value<std::string>(), "XML file from which to parameters")
+                ("xorigin", po::value<double>()->default_value(xorigin), "x origin")(
+                  "yorigin", po::value<double>()->default_value(yorigin), "y origin")(
+                  "zorigin", po::value<double>()->default_value(zorigin), "z origin")
 
-                  ("output",
-                   po::value<std::string>()->default_value(outname),
-                   "output file base name");
+                  ("xml-file", po::value<std::string>(), "XML file from which to parameters")
 
-    po::positional_options_description p;
-    p.add("output", 1);
+                    ("output",
+                     po::value<std::string>()->default_value(outname),
+                     "output file base name");
 
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
-    po::notify(vm);
+      po::positional_options_description p;
+      p.add("output", 1);
 
-    if (vm.count("help")) {
-      std::cerr << "Usage: " << progname << " [options]" << std::endl;
-      std::cerr << desc << std::endl;
+      po::variables_map vm;
+      po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+      po::notify(vm);
+
+      if (vm.count("help")) {
+        std::cerr << "Usage: " << progname << " [options]" << std::endl;
+        std::cerr << desc << std::endl;
+        return 3;
+      }
+
+      if (vm.count("noaudit")) doaudit = false;
+
+      if (vm.count("xml-file") > 0) {
+        inname = vm["xml-file"].as<std::string>();
+      } else {
+        inname.clear();
+
+        xcells = vm["xcells"].as<unsigned int>();
+        ycells = vm["ycells"].as<unsigned int>();
+        zcells = vm["zcells"].as<unsigned int>();
+
+        xdelta = vm["xdelta"].as<double>();
+        ydelta = vm["ydelta"].as<double>();
+        zdelta = vm["zdelta"].as<double>();
+
+        xorigin = vm["xorigin"].as<double>();
+        yorigin = vm["yorigin"].as<double>();
+        zorigin = vm["zorigin"].as<double>();
+      }
+
+      dosimple = (vm.count("simple") > 0);
+
+      outname = vm["output"].as<std::string>();
+      outfilename = outname;
+
+    } catch (po::error& e) {
+      if (me == 0) {
+        std::cerr << progname << ": command line error: " << e.what() << std::endl;
+        std::cerr << "Usage: " << progname << " [options]" << std::endl;
+        std::cerr << desc << std::endl;
+      }
+      return 3;
+    } catch (boost::bad_any_cast& e) {
+      if (me == 0) {
+        std::cerr << progname << ": command line error: " << e.what() << std::endl;
+        std::cerr << "Usage: " << progname << " [options]" << std::endl;
+        std::cerr << desc << std::endl;
+      }
+      return 3;
+    } catch (...) {
+      if (me == 0) {
+        std::cerr << "Usage: " << progname << " [options]" << std::endl;
+        std::cerr << desc << std::endl;
+      }
       return 3;
     }
 
-    if (vm.count("noaudit")) doaudit = false;
+    // generate a mesh
+    Amanzi::AmanziMesh::MeshFactory meshfactory(comm);
+    Amanzi::AmanziMesh::Preference pref;
+    if (dosimple) { pref.push_back(Amanzi::AmanziMesh::Framework::SIMPLE); }
+    meshfactory.set_preference(pref);
 
-    if (vm.count("xml-file") > 0) {
-      inname = vm["xml-file"].as<std::string>();
-    } else {
-      inname.clear();
-
-      xcells = vm["xcells"].as<unsigned int>();
-      ycells = vm["ycells"].as<unsigned int>();
-      zcells = vm["zcells"].as<unsigned int>();
-
-      xdelta = vm["xdelta"].as<double>();
-      ydelta = vm["ydelta"].as<double>();
-      zdelta = vm["zdelta"].as<double>();
-
-      xorigin = vm["xorigin"].as<double>();
-      yorigin = vm["yorigin"].as<double>();
-      zorigin = vm["zorigin"].as<double>();
+    Teuchos::RCP<Amanzi::AmanziMesh::Mesh> mesh;
+    if (inname.empty()) {
+      mesh = meshfactory.create(xorigin,
+                                yorigin,
+                                zorigin,
+                                xorigin + xdelta * xcells,
+                                yorigin + ydelta * ycells,
+                                zorigin + zdelta * zcells,
+                                xcells,
+                                ycells,
+                                zcells);
     }
 
-    dosimple = (vm.count("simple") > 0);
+    // make sure it's OK
+    if (doaudit) {
+      int notok(do_the_audit(me, mesh, outname));
+      if (me == 0) {
+        std::cout << "Mesh \"" << outname << "\" " << (notok ? "has errors" : "OK") << std::endl;
+      }
+    }
 
-    outname = vm["output"].as<std::string>();
-    outfilename = outname;
-
-  } catch (po::error& e) {
-    if (me == 0) {
-      std::cerr << progname << ": command line error: " << e.what() << std::endl;
-      std::cerr << "Usage: " << progname << " [options]" << std::endl;
-      std::cerr << desc << std::endl;
-    }
-    return 3;
-  } catch (boost::bad_any_cast& e) {
-    if (me == 0) {
-      std::cerr << progname << ": command line error: " << e.what() << std::endl;
-      std::cerr << "Usage: " << progname << " [options]" << std::endl;
-      std::cerr << desc << std::endl;
-    }
-    return 3;
-  } catch (...) {
-    if (me == 0) {
-      std::cerr << "Usage: " << progname << " [options]" << std::endl;
-      std::cerr << desc << std::endl;
-    }
-    return 3;
+    // dump it out
+    dump_output(me, mesh, outfilename);
   }
 
-  // generate a mesh
-  Amanzi::AmanziMesh::MeshFactory meshfactory(comm);
-  Amanzi::AmanziMesh::Preference pref;
-  if (dosimple) { pref.push_back(Amanzi::AmanziMesh::Framework::SIMPLE); }
-  meshfactory.set_preference(pref);
-
-  Teuchos::RCP<Amanzi::AmanziMesh::Mesh> mesh;
-  if (inname.empty()) {
-    mesh = meshfactory.create(xorigin,
-                              yorigin,
-                              zorigin,
-                              xorigin + xdelta * xcells,
-                              yorigin + ydelta * ycells,
-                              zorigin + zdelta * zcells,
-                              xcells,
-                              ycells,
-                              zcells);
-  }
-
-  // make sure it's OK
-  if (doaudit) {
-    int notok(do_the_audit(me, mesh, outname));
-    if (me == 0) {
-      std::cout << "Mesh \"" << outname << "\" " << (notok ? "has errors" : "OK") << std::endl;
-    }
-  }
-
-  // dump it out
-  dump_output(me, *mesh, outfilename);
-
+  Kokkos::finalize();
   return 0;
 }
