1614914b8ef4b5422b32c9fadd959668a7e10ae8
Ethan Coon <coonet@ornl.gov>
2023-11-27 22:35:15 -0500

    fixes remaining issue in implementing ColumnMeshFunction from mesh refactor

--- src/mesh_functions/ColumnMeshFunction.hh
+++ src/mesh_functions/ColumnMeshFunction.hh
@@ -30,18 +30,6 @@ Function values u:
    * `"z header`" ``[string]`` name of the z-coordinate data: `z` above.  Depth
      coordinates (positive downward from the surface), [m]
    * `"f header`" ``[string]`` name of the function data: `f` above.
-
-   ONE OF
-
-   * `"surface sideset`" ``[string]`` Region on the surface domain from which
-     to start to determine columns.
-
-   OR
-
-   * `"surface sidesets`" ``[Array(string)]`` Regions on the surface domain
-     from which to start to determine columns.
-
-   END
 */
 
 
@@ -66,7 +54,6 @@ ReadColumnMeshFunction(Teuchos::ParameterList& plist, CompositeVector& v);
 
 void
 ReadColumnMeshFunction_ByDepth(const Function& func,
-                               const Teuchos::Array<std::string> sidesets,
                                CompositeVector& v);
 
 } // namespace Functions
