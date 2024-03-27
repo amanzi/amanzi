34c1bf28ab03a485fed37a241efe155e3d07b28b
David Moulton <moulton@lanl.gov>
2024-03-27 08:14:07 -0600

    Updated deployment script to build TPLs from this branch (refs #792).

--- Docker/deploy-tpls-docker.sh
+++ Docker/deploy-tpls-docker.sh
@@ -6,19 +6,21 @@ MPI_DISTRO=mpich
 MPI_VERSION=4.0.3  # ignored if build_mpi = False
 BUILD_MPI=True
 
-PETSC_VER=3.16
+PETSC_VER=3.20
 TRILINOS_VER=14-2-fc55b9cd
 
-AMANZI_BRANCH=master
-AMANZI_SOURCE_DIR=/ascem/amanzi/repos/amanzi-master
-AMANZI_TPLS_VER=0.98.7
+AMANZI_BRANCH=david/amanzi-tpls-0.98.8
+AMANZI_SOURCE_DIR=/amanzi/repos/amanzi-tpls-0.98.8
+AMANZI_TPLS_VER=0.98.8
 
 LANL_PROXY="--build-arg http_proxy=proxyout.lanl.gov:8080 --build-arg https_proxy=proxyout.lanl.gov:8080"
 
-docker build --no-cache --build-arg petsc_ver=${PETSC_VER} \
+docker build --no-cache --progress=plain --build-arg petsc_ver=${PETSC_VER} \
              --build-arg trilinos_ver=${TRILINOS_VER} \
              --build-arg amanzi_branch=${AMANZI_BRANCH} \
              --build-arg build_mpi=${BUILD_MPI} --build-arg mpi_flavor=${MPI_DISTRO} \
              --build-arg mpi_version=${MPI_VERSION} \
              -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-TPLs \
              -t metsi/amanzi-tpls:${AMANZI_TPLS_VER}-${MPI_DISTRO} .
+
+#docker build --no-cache --progress=plain --build-arg petsc_ver=${PETSC_VER} --build-arg trilinos_ver=${TRILINOS_VER} --build-arg amanzi_branch=${AMANZI_BRANCH} -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-TPLs -t metsi/amanzi-tpls:${AMANZI_TPLS_VER}-${MPI_DISTRO} .
