ea0ac4aaa07d79215236a61982c163594fd28765
Konstantin Lipnikov <lipnikov@lanl.gov>
2024-03-27 18:19:23 -0600

    Fixed a bug in the FV scheme on manifolds.
    Double counting of relative permeability was removed.

--- src/operators/test/operator_diffusion_dfn.cc
+++ src/operators/test/operator_diffusion_dfn.cc
@@ -105,7 +105,7 @@ RunTest(int icase, double gravity)
     surfmesh->getNumEntities(AmanziMesh::Entity_kind::FACE, AmanziMesh::Parallel_kind::ALL);
 
   WhetStone::Tensor Kc(2, 1);
-  Kc(0, 0) = 1.0;
+  Kc(0, 0) = 2.0;
   for (int c = 0; c < ncells_owned; c++) K->push_back(Kc);
 
   AmanziGeometry::Point v(1.0, 2.0, 3.0);
