443aac89100b05ecfda52a161ad7bca6f683a776
gcapodag <encapod@hotmail.com>
2024-04-05 17:39:36 -0600

    Fixed shallow water Thacker issue

--- src/pks/shallow_water/ShallowWater_PK.cc
+++ src/pks/shallow_water/ShallowWater_PK.cc
@@ -931,6 +931,7 @@ ShallowWater_PK::InitializeFields()
 
     S_->GetRecordW(total_depth_key_, Tags::DEFAULT, passwd_).set_initialized();
   }
+
 }
 
 //--------------------------------------------------------------
