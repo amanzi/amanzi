39e4f47bc8c0ff59e154be2e836e2bccac3cbdb6
Rich Fiorella <rfiorella@lanl.gov>
2024-04-05 12:04:22 -0600

    Update deploy scripts for readability and new options
    Updates deploy scripts to have new MULTIARCH and
    OUTPUT_STYLE options, and standardizes some of the
    script appearances to make the docker --build-arg
    options easier to read. Paired with new readme file.
    
    Ref #769

--- Docker/deploy-amanzi-docker.sh
+++ Docker/deploy-amanzi-docker.sh
@@ -4,6 +4,13 @@ AMANZI_BRANCH=master
 AMANZI_SOURCE_DIR=/ascem/amanzi/repos/amanzi-master
 AMANZI_TPLS_VER=0.98.0
 
+# architecture options - if TRUE, build for arm and x86_64,
+# if FALSE, build only for current system architecture.
+MULTIARCH=FALSE
+
+# output style - change to "--progress=plain" for debugging
+OUTPUT_STYLE=""
+
 LANL_PROXY="--build-arg http_proxy=proxyout.lanl.gov:8080 --build-arg https_proxy=proxyout.lanl.gov:8080"
 
 AMANZI_GIT_LATEST_TAG_VER=`(cd $AMANZI_SOURCE_DIR; git tag -l amanzi-* | tail -n1 | sed -e 's/amanzi-//')`
@@ -17,7 +24,22 @@ echo " - global hash      $AMANZI_GIT_GLOBAL_HASH"
 echo " - version string   $AMANZI_VER"
 echo ""
 
-docker build --no-cache --build-arg amanzi_branch=${AMANZI_BRANCH} --build-arg amanzi_tpls_ver=${AMANZI_TPLS_VER} ${LANL_PROXY} --progress=plain -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-Amanzi -t metsi/amanzi:${AMANZI_VER} .
+if $MULTIARCH
+then
+    docker buildx build --platform=linux/amd64,linux/arm64 --no-cache \
+        --build-arg amanzi_branch=${AMANZI_BRANCH} \
+        --build-arg amanzi_tpls_ver=${AMANZI_TPLS_VER} ${LANL_PROXY} \
+        $OUTPUT_STYLE \
+        -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-Amanzi \
+        -t metsi/amanzi:${AMANZI_VER} .
+else
+    docker build --no-cache \
+        --build-arg amanzi_branch=${AMANZI_BRANCH} \
+        --build-arg amanzi_tpls_ver=${AMANZI_TPLS_VER} ${LANL_PROXY} \
+        $OUTPUT_STYLE \
+        -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-Amanzi \
+        -t metsi/amanzi:${AMANZI_VER} .
+fi
 
 docker tag metsi/amanzi:${AMANZI_VER} metsi/amanzi:latest
 
--- Docker/deploy-tpls-docker.sh
+++ Docker/deploy-tpls-docker.sh
@@ -9,18 +9,41 @@ BUILD_MPI=True
 PETSC_VER=3.20
 TRILINOS_VER=14-2-fc55b9cd
 
-AMANZI_BRANCH=david/amanzi-tpls-0.98.8
-AMANZI_SOURCE_DIR=/amanzi/repos/amanzi-tpls-0.98.8
+AMANZI_BRANCH=master
+AMANZI_SOURCE_DIR=/ascem/amanzi/repos/amanzi-master
 AMANZI_TPLS_VER=0.98.8
+
+# architecture options - if TRUE, build for arm and x86_64,
+# if FALSE, build only for current system architecture.
+MULTIARCH=FALSE
+
+# output style - change to "--progress=plain" for debugging
+OUTPUT_STYLE=""
 
 LANL_PROXY="--build-arg http_proxy=proxyout.lanl.gov:8080 --build-arg https_proxy=proxyout.lanl.gov:8080"
 
-docker build --no-cache --progress=plain --build-arg petsc_ver=${PETSC_VER} \
-             --build-arg trilinos_ver=${TRILINOS_VER} \
-             --build-arg amanzi_branch=${AMANZI_BRANCH} \
-             --build-arg build_mpi=${BUILD_MPI} --build-arg mpi_flavor=${MPI_DISTRO} \
-             --build-arg mpi_version=${MPI_VERSION} \
-             -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-TPLs \
-             -t metsi/amanzi-tpls:${AMANZI_TPLS_VER}-${MPI_DISTRO} .
+if $MULTIARCH
+then
+    docker buildx build --platform=linux/amd64,linux/arm64 \
+        --no-cache $OUTPUT_STYLE \
+        --build-arg petsc_ver=${PETSC_VER} \
+        --build-arg trilinos_ver=${TRILINOS_VER} \
+        --build-arg amanzi_branch=${AMANZI_BRANCH} \
+        --build-arg build_mpi=${BUILD_MPI} \
+        --build-arg mpi_flavor=${MPI_DISTRO} \
+        --build-arg mpi_version=${MPI_VERSION} \
+        -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-TPLs \
+        -t metsi/amanzi-tpls:${AMANZI_TPLS_VER}-${MPI_DISTRO} .
+else
+    docker build --no-cache $OUTPUT_STYLE \
+        --build-arg petsc_ver=${PETSC_VER} \
+        --build-arg trilinos_ver=${TRILINOS_VER} \
+        --build-arg amanzi_branch=${AMANZI_BRANCH} \
+        --build-arg build_mpi=${BUILD_MPI} \
+        --build-arg mpi_flavor=${MPI_DISTRO} \
+        --build-arg mpi_version=${MPI_VERSION} \
+        -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-TPLs \
+        -t metsi/amanzi-tpls:${AMANZI_TPLS_VER}-${MPI_DISTRO} .
+fi
 
 #docker build --no-cache --progress=plain --build-arg petsc_ver=${PETSC_VER} --build-arg trilinos_ver=${TRILINOS_VER} --build-arg amanzi_branch=${AMANZI_BRANCH} -f ${AMANZI_SOURCE_DIR}/Docker/Dockerfile-TPLs -t metsi/amanzi-tpls:${AMANZI_TPLS_VER}-${MPI_DISTRO} .
