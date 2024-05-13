592705849d269c0b8e5c1bd705f864218d82d989
Ethan Coon <coonet@ornl.gov>
2024-05-13 13:30:34 -0600

    fixes bug in how we download Trilinos -- use release not git

--- config/SuperBuild/TPLVersions.cmake
+++ config/SuperBuild/TPLVersions.cmake
@@ -512,7 +512,8 @@ set(Trilinos_VERSION ${Trilinos_VERSION_MAJOR}-${Trilinos_VERSION_MINOR}-${Trili
 set(Trilinos_URL_STRING     "https://github.com/trilinos/Trilinos/archive")
 set(Trilinos_ARCHIVE_FILE   trilinos-release-${Trilinos_VERSION}.tar.gz)
 set(Trilinos_SAVEAS_FILE    ${Trilinos_ARCHIVE_FILE})
-set(Trilinos_GIT_REPOSITORY "https://github.com/trilinos/Trilinos")
+set(Trilinos_MD5_SUM       79237697af4fc42eaaf70f23104a8e12)
+#set(Trilinos_GIT_REPOSITORY "https://github.com/trilinos/Trilinos")
 
 
 #
