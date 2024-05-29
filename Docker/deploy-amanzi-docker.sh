a7558c2a5b652011e0585e7b5b55e67a8aaa651e
Rich Fiorella <rfiorella@lanl.gov>
2024-05-29 11:08:56 -0600

    Complete updates to deploy scripts

--- Docker/deploy-amanzi-docker.sh
+++ Docker/deploy-amanzi-docker.sh
@@ -6,6 +6,7 @@ Help()
     echo "Usage: $(basename $0) [-h|--help] [additional options]"
     echo "Options:"
     echo "  -h, --help          Display this help message"
+    echo "  --no_cache          Ignore docker layers in Docker build cache and build from scratch"
     echo "  --amanzi_branch     Which Amanzi branch should be used when building container? (Default: master)"
     echo "  --amanzi_src_dir    Where does the Amanzi repo reside on the current system?"
     echo "                      (Default: /ascem/amanzi/repos/amanzi-master)"
@@ -13,9 +14,23 @@ Help()
     echo "  --output_style      Should we use the condensed or plain version of Docker output (Default: condensed)."
     echo "                      Set --output_style='plain' for expanded output"
     echo "  --multiarch         Build for both linux/amd64 and linux/arm64 instead of only local system architecture"
+    echo "                      Assumes your already have Docker configured to build multiarchitecture images"
+    echo "  --push              Push resulting image to Dockerhub (Requires caution!!)"
+    echo "  --mpi_flavor        Which MPI implementation does the TPL image you want to build from use? (Default: mpich)"
     exit 0
 }
 
+get_tpl_version()
+#
+#  Find version information and provide it for production installs
+#
+{
+   local SBFile=${amanzi_src_dir}/config/SuperBuild/TPLVersions.cmake
+   tpl_version_major=`grep AMANZI_TPLS_VERSION_MAJOR ${SBFile} | tr -cd '[[:digit:]]'`
+   tpl_version_minor=`grep AMANZI_TPLS_VERSION_MINOR ${SBFile} | tr -cd '[[:digit:]]'`
+   tpl_version_patch=`grep AMANZI_TPLS_VERSION_PATCH ${SBFile} | tr -cd '[[:digit:]]'`
+   echo "${tpl_version_major}.${tpl_version_minor}.${tpl_version_patch}"
+}
 # parse command line options, if given
 for i in "$@"
 do
@@ -36,7 +51,7 @@ case $i in
     shift
     ;;
     --use_proxy=*)
-    use_proxy="${i#*=}"
+    use_proxy="--build-arg http_proxy=proxyout.lanl.gov:8080 --build-arg https_proxy=proxyout.lanl.gov:8080"
     shift
     ;;
     --output_style=*)
@@ -45,6 +60,18 @@ case $i in
     ;;
     --multiarch)
     multiarch=True
+    shift
+    ;;
+    --mpi_flavor=*)
+    mpi_flavor="${i#*=}"
+    shift
+    ;;
+    --push)
+    push=True
+    shift
+    ;;
+    --no_cache)
+    cache="--no-cache"
     shift
     ;;
     *)
@@ -56,20 +83,28 @@ done
 # set defaults, if not given on CLI
 amanzi_branch="${amanzi_branch:-master}"
 amanzi_src_dir="${amanzi_src_dir:-/ascem/amanzi/repos/amanzi-master}"
-amanzi_tpls_ver="${amanzi_tpls_ver:-0.98.9}"
-use_proxy="${use_proxy:-False}"
+amanzi_tpls_ver="${amanzi_tpls_ver:-`get_tpl_version`}"
+use_proxy="${use_proxy:-}"
 output_style="${output_style:-}"
 multiarch="${multiarch:-False}"
+push="${push:-False}"
+mpi_flavor="${mpi_flavor:-mpich}"
+cache="${cache:-}"
 
-if "${use_proxy}" ; then
-    LANL_PROXY="--build-arg http_proxy=proxyout.lanl.gov:8080 --build-arg https_proxy=proxyout.lanl.gov:8080"
-else
-    LANL_PROXY=""
-fi
 if [ "${output_style}" = "plain" ]; then
-    OUTPUT_STYLE="--progress=plain"
+    output="--progress=plain"
 else
-    OUTPUT_STYLE=""
+    output=""
+fi
+
+if "${push}" ; then
+    push_arg="--push"
+else
+    if $multiarch ; then
+        push_arg="--load"
+    else
+        push_arg=""
+    fi
 fi
 
 AMANZI_GIT_LATEST_TAG_VER=`(cd $amanzi_src_dir; git tag -l amanzi-* | tail -n1 | sed -e 's/amanzi-//')`
@@ -85,20 +120,26 @@ echo ""
 
 if $multiarch
 then
-    docker buildx build --platform=linux/amd64,linux/arm64 \
-        --no-cache \
+    docker buildx build \
+        --platform=linux/amd64,linux/arm64 \
+        ${cache} \
         --build-arg amanzi_branch=${amanzi_branch} \
         --build-arg amanzi_tpls_ver=${amanzi_tpls_ver} \
-        ${LANL_PROXY} \
-        ${OUTPUT_STYLE} \
+        --build-arg mpi_flavor=${mpi_flavor} \
+        ${use_proxy} \
+        ${output} \
+        ${push_arg} \
         -f ${amanzi_src_dir}/Docker/Dockerfile-Amanzi \
         -t metsi/amanzi:${AMANZI_VER} .
 else
-    docker build --no-cache \
+    docker build \
+        ${cache} \
         --build-arg amanzi_branch=${amanzi_branch} \
         --build-arg amanzi_tpls_ver=${amanzi_tpls_ver} \
-        ${LANL_PROXY} \
-        ${OUTPUT_STYLE} \
+        --build-arg mpi_flavor=${mpi_flavor} \
+        ${use_proxy} \
+        ${output} \
+        ${push_arg} \
         -f ${amanzi_src_dir}/Docker/Dockerfile-Amanzi \
         -t metsi/amanzi:${AMANZI_VER} .
 fi
--- Docker/deploy-tpls-docker.sh
+++ Docker/deploy-tpls-docker.sh
@@ -6,6 +6,7 @@ Help()
     echo "Usage: $(basename $0) [-h|--help] [additional options]"
     echo "Options:"
     echo "  -h, --help          Display this help message"
+    echo "  --no_cache          Ignore docker layers in Docker build cache and build from scratch"
     echo "  --build_mpi         Build MPI implementation (either MPICH or OpenMPI) instead"
     echo "                      of using precompiled binaries in Ubuntu package repository (Default: True)"
     echo "  --mpi_distro        Which MPI implementation to use? (currently OpenMPI or MPICH) (Default: MPICH)"
@@ -17,15 +18,28 @@ Help()
     echo "                      (Default: /ascem/amanzi/repos/amanzi-master)"
     echo "  --amanzi_tpls_ver   Which version of the Amanzi TPLs should we build? (Default: 0.98.9)"
     echo "  --output_style      Should we use the condensed or plain version of Docker output (Default: condensed)"
-    echo "  --multiarch         Build for local system architecture only (false) or for linux/amd64 and linux/arm64"
-    echo "                      (Default: False)"
+    echo "  --multiarch         Build for both linux/amd64 and linux/arm64 instead of only local system architecture"
+    echo "                      Assumes your already have Docker configured to build multiarchitecture images"
+    echo "  --push              Push resulting image to Dockerhub (Requires caution!!)"
     exit 0
 }
 
+get_tpl_version()
+#
+#  Find version information and provide it for production installs
+#
 # rf notes 240528:
 # are --petsc_ver and --trilinos_ver options still necessary?
 # seems like these could be scraped from the TPL versions information,
 # as in deploy-ats-docker.sh
+{
+   local SBFile=${amanzi_src_dir}/config/SuperBuild/TPLVersions.cmake
+   tpl_version_major=`grep AMANZI_TPLS_VERSION_MAJOR ${SBFile} | tr -cd '[[:digit:]]'`
+   tpl_version_minor=`grep AMANZI_TPLS_VERSION_MINOR ${SBFile} | tr -cd '[[:digit:]]'`
+   tpl_version_patch=`grep AMANZI_TPLS_VERSION_PATCH ${SBFile} | tr -cd '[[:digit:]]'`
+   echo "${tpl_version_major}.${tpl_version_minor}.${tpl_version_patch}"
+}
+
 
 # parse command line options, if given
 for i in "$@"
@@ -66,16 +80,24 @@ case $i in
     amanzi_tpls_ver="${i#*=}"
     shift
     ;;
-    --use_proxy=*)
-    use_proxy="${i#*=}"
+    --use_proxy)
+    use_proxy="--build-arg http_proxy=proxyout.lanl.gov:8080 --build-arg https_proxy=proxyout.lanl.gov:8080"
     shift
     ;;
     --output_style=*)
     output_style="${i#*=}"
     shift
     ;;
-    --multiarch=*)
-    multiarch="${i#*=}"
+    --multiarch)
+    multiarch=True
+    shift
+    ;;
+    --push)
+    push=True
+    shift
+    ;;
+    --no_cache)
+    cache="--no-cache"
     shift
     ;;
     *)
@@ -92,42 +114,55 @@ petsc_ver="${petsc_ver:-3.20}"
 trilinos_ver="${trilnos_ver:-15-1-6af5f44}"
 amanzi_branch="${amanzi_branch:-master}"
 amanzi_src_dir="${amanzi_src_dir:-/ascem/amanzi/repos/amanzi-master}"
-amanzi_tpls_ver="${amanzi_tpls_ver:-0.98.9}"
-use_proxy="${use_proxy:-False}"
+amanzi_tpls_ver="${amanzi_tpls_ver:-`get_tpl_version`}"
+use_proxy="${use_proxy:-}"
+push="${push:-False}"
 output_style="${output_style:-}"
 multiarch="${multiarch:-False}"
+cache="${cache:-}"
 
-if "${use_proxy}" ; then
-    LANL_PROXY="--build-arg http_proxy=proxyout.lanl.gov:8080 --build-arg https_proxy=proxyout.lanl.gov:8080"
-else
-    LANL_PROXY=""
-fi
 if [ "${output_style}" = "plain" ]; then
-    OUTPUT_STYLE="--progress=plain"
+    output="--progress=plain"
 else
-    OUTPUT_STYLE=""
+    output=""
+fi
+
+if "${push}" ; then
+    push_arg="--push"
+else
+    if $multiarch ; then
+        push_arg="--load"
+    else
+        push_arg=""
+    fi
 fi
 
 if $multiarch
 then
-    docker buildx build --platform=linux/amd64,linux/arm64 \
-             --no-cache --build-arg petsc_ver=${petsc_ver} \
-             --build-arg trilinos_ver=${trilinos_ver} \
-             --build-arg amanzi_branch=${amanzi_branch} \
-             --build-arg build_mpi=${build_mpi} --build-arg mpi_flavor=${mpi_distro} \
-             --build-arg mpi_version=${mpi_version} \
-             ${LANL_PROXY} \
-             ${OUTPUT_STYLE} \
-             -f ${amanzi_src_dir}/Docker/Dockerfile-TPLs \
-             -t metsi/amanzi-tpls:${amanzi_tpls_ver}-${mpi_distro} .
+    docker buildx build \
+        --platform=linux/amd64,linux/arm64 \
+        ${cache} \
+        --build-arg petsc_ver=${petsc_ver} \
+        --build-arg trilinos_ver=${trilinos_ver} \
+        --build-arg amanzi_branch=${amanzi_branch} \
+        --build-arg build_mpi=${build_mpi} \
+        --build-arg mpi_flavor=${mpi_distro} \
+        --build-arg mpi_version=${mpi_version} \
+        ${use_proxy} \
+        ${output} \
+        -f ${amanzi_src_dir}/Docker/Dockerfile-TPLs \
+        -t metsi/amanzi-tpls:${amanzi_tpls_ver}-${mpi_distro} .
 else
-    docker build --no-cache --build-arg petsc_ver=${petsc_ver} \
-             --build-arg trilinos_ver=${trilinos_ver} \
-             --build-arg amanzi_branch=${amanzi_branch} \
-             --build-arg build_mpi=${build_mpi} --build-arg mpi_flavor=${mpi_distro} \
-             --build-arg mpi_version=${mpi_version} \
-             ${LANL_PROXY} \
-             ${OUTPUT_STYLE} \
-             -f ${amanzi_src_dir}/Docker/Dockerfile-TPLs \
-             -t metsi/amanzi-tpls:${amanzi_tpls_ver}-${mpi_distro} .
+    docker build \
+        ${cache} \
+        --build-arg petsc_ver=${petsc_ver} \
+        --build-arg trilinos_ver=${trilinos_ver} \
+        --build-arg amanzi_branch=${amanzi_branch} \
+        --build-arg build_mpi=${build_mpi} \
+        --build-arg mpi_flavor=${mpi_distro} \
+        --build-arg mpi_version=${mpi_version} \
+        ${use_proxy} \
+        ${output} \
+        -f ${amanzi_src_dir}/Docker/Dockerfile-TPLs \
+        -t metsi/amanzi-tpls:${amanzi_tpls_ver}-${mpi_distro} .
 fi
