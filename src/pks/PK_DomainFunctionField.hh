1d288d55a0731db6380e4df09979237d4fc4fff6
Phong Le <levuvietphong@gmail.com>
2024-08-06 14:33:33 -0400

    Add plist to PK_Domain and clean up

--- src/pks/PK_DomainFunctionField.hh
+++ src/pks/PK_DomainFunctionField.hh
@@ -80,10 +80,12 @@ class PK_DomainFunctionField : public FunctionBase {
  private:
   std::string submodel_;
   Teuchos::RCP<MeshIDs> entity_ids_;
-  AmanziMesh::Entity_kind kind_;
-  Key field_key_;
-  Tag tag_;
+  AmanziMesh::Entity_kind kind_;  
   Key component_key_;
+  int num_cells_;
+  std::vector<Key> field_keys_;
+  std::vector<Tag> tags_;
+
 };
 
 
@@ -96,18 +98,41 @@ PK_DomainFunctionField<FunctionBase>::Init(const Teuchos::ParameterList& plist,
                                            const std::string& keyword)
 {
   Errors::Message msg;
-
   keyword_ = keyword;
-
   submodel_ = "rate";
+
   if (plist.isParameter("submodel")) submodel_ = plist.get<std::string>("submodel");
   std::vector<std::string> regions = plist.get<Teuchos::Array<std::string>>("regions").toVector();
 
   try {
-    Teuchos::ParameterList flist = plist.sublist(keyword);
-    field_key_ = flist.get<std::string>("field key");
-    tag_ = Keys::readTag(flist, "tag");
-    component_key_ = flist.get<std::string>("component", "cell");
+    Teuchos::ParameterList flist = plist.sublist(keyword);    
+    if (flist.isParameter("number of fields")) {
+      // for multiple (>1) field evaluators. This requires "number of fields" sublist 
+      if (flist.isType<int>("number of fields")) {
+        int num_fields = flist.get<int>("number of fields");
+        if (num_fields < 2) { // ERROR -- invalid number of fields
+          std::cout << "Number of fields must be > 1" << std::endl << std::flush;
+          AMANZI_ASSERT(0);
+        }
+
+        // Get corresponding field_keys to global memory
+        for (int lcv = 1; lcv != (num_fields + 1); ++lcv) {
+          std::stringstream sublist_name;
+          sublist_name << "field " << lcv << " info";
+          Key field_key_ = flist.sublist(sublist_name.str()).get<std::string>("field key");
+          Tag tag_ = Keys::readTag(flist.sublist(sublist_name.str()), "tag");
+          field_keys_.push_back(field_key_);
+          tags_.push_back(tag_);
+        }
+      }
+    } else { // for only ONE field evaluator (without number of fields sublist)
+      Key field_key_ = flist.get<std::string>("field key");
+      field_keys_.push_back(field_key_);
+      Tag tag_ = Keys::readTag(flist, "tag");
+      tags_.push_back(tag_);
+      // component_key_ = flist.get<std::string>("component", "cell");
+    }
+    num_cells_ = mesh_->getNumEntities(AmanziMesh::Entity_kind::CELL, AmanziMesh::Parallel_kind::ALL);    
   } catch (Errors::Message& msg) {
     Errors::Message m;
     m << "error in source sublist : " << msg.what();
@@ -140,16 +165,32 @@ template <class FunctionBase>
 void
 PK_DomainFunctionField<FunctionBase>::Compute(double t0, double t1)
 {
-  if (S_->HasEvaluator(field_key_, tag_)) {
-    S_->GetEvaluator(field_key_, tag_).Update(*S_, field_key_);
+  int num_fields = field_keys_.size();      // number of field evaluators
+  std::vector<double> val_vec(num_fields);  // vector values
+  std::vector<std::vector<double>> field_all(num_fields, std::vector<double>(num_cells_));
+
+  // For multiple field evaluators (fe), loop through each fe to retrieve the values for each cell.
+  // Copy the retrieved values to intermediate variables.
+  // Finally, update the source values (value_) for each cell.
+  for (size_t ife = 0; ife < num_fields; ++ife) { // fe counter
+    Key field_key_ = field_keys_[ife];
+    Tag tag_ = tags_[ife];
+
+    if (S_->HasEvaluator(field_key_, tag_)) {
+      S_->GetEvaluator(field_key_, tag_).Update(*S_, field_key_);
+    }
+    // reference to coressponding fe
+    const Epetra_MultiVector& field_vec = *S_->Get<CompositeVector>(field_key_, tag_).ViewComponent("cell", false);    
+
+    // loop through every cell to update the intermediate variables
+    for (auto c : *entity_ids_) {
+      field_all[ife][c] = field_vec[0][c];       
+    }
   }
 
-  const auto& field_vec = *S_->Get<CompositeVector>(field_key_, tag_).ViewComponent("cell", false);
-  int nvalues = field_vec.NumVectors();
-  std::vector<double> val_vec(nvalues);
-
+  // Update source values (value_)
   for (auto c : *entity_ids_) {
-    for (int i = 0; i < nvalues; ++i) { val_vec[i] = field_vec[i][c]; }
+    for (int i = 0; i < num_fields; ++i) { val_vec[i] = field_all[i][c]; }
     value_[c] = val_vec;
   }
 }
