dbdbf6ffda323cbcb31419e5e0ad6dbaf3f2b4fc
ehinrichs <54997013+ehinrichs@users.noreply.github.com>
2024-08-08 08:25:44 -0600

    Updates to fix dark logo and color issues
    Updates to fix dark logo and color issues

--- doc/user_guide/conf.py
+++ doc/user_guide/conf.py
@@ -149,6 +149,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'pydata_sphinx_theme'
+html_context = {
+    "default_mode": "auto"
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
