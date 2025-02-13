2b19ab6e40cea30556e6bdd04899500cbbe81305
Konstantin Lipnikov <lipnikov@lanl.gov>
2025-02-12 22:16:26 -0700

    Changed default floating number to 64

--- config/SuperBuild/include/Build_METIS.cmake
+++ config/SuperBuild/include/Build_METIS.cmake
@@ -16,6 +16,22 @@ amanzi_tpl_version_write(FILENAME ${TPL_VERSIONS_INCLUDE_FILE}
 set(METIS_DIR ${TPL_INSTALL_PREFIX})
 set(METIS_GKLIB_DIR ${METIS_source_dir}/GKlib)
 
+# --- Set the name of the patch
+set(METIS_patch_file metis-realtype.patch)
+
+# --- Configure the bash patch script
+set(METIS_sh_patch ${METIS_prefix_dir}/metis-patch-step.sh)
+configure_file(${SuperBuild_TEMPLATE_FILES_DIR}/metis-patch-step.sh.in
+               ${METIS_sh_patch}
+               @ONLY)
+# --- Configure the CMake patch step
+set(METIS_cmake_patch ${METIS_prefix_dir}/metis-patch-step.cmake)
+configure_file(${SuperBuild_TEMPLATE_FILES_DIR}/metis-patch-step.cmake.in
+               ${METIS_cmake_patch}
+               @ONLY)
+# --- Set the patch command
+set(METIS_PATCH_COMMAND ${CMAKE_COMMAND} -P ${METIS_cmake_patch})     
+
 # --- Define the CMake configure parameters
 # Note:
 #      CMAKE_CACHE_ARGS requires -DVAR:<TYPE>=VALUE syntax
@@ -37,6 +53,8 @@ ExternalProject_Add(${METIS_BUILD_TARGET}
                     DOWNLOAD_DIR ${TPL_DOWNLOAD_DIR}              # Download directory
                     URL          ${METIS_URL}                     # URL may be a web site OR a local file
                     URL_MD5      ${METIS_MD5_SUM}                 # md5sum of the archive file
+                    # -- Patch 
+                    PATCH_COMMAND  ${METIS_PATCH_COMMAND}
                     # -- Configure
                     SOURCE_DIR   ${METIS_source_dir}              # Source directory
                     CMAKE_ARGS   ${AMANZI_CMAKE_CACHE_ARGS}       # Ensure uniform build
--- config/SuperBuild/include/Build_ParMetis.cmake
+++ config/SuperBuild/include/Build_ParMetis.cmake
@@ -15,6 +15,24 @@ amanzi_tpl_version_write(FILENAME ${TPL_VERSIONS_INCLUDE_FILE}
 
 set(ParMetis_GKLIB_DIR ${METIS_source_dir}/GKlib)
 set(ParMetis_METIS_DIR ${METIS_source_dir})
+
+# --- Set the name of the patch
+set(ParMetis_patch_file parmetis-realtype.patch)
+
+# --- Configure the bash patch script
+set(ParMetis_sh_patch ${ParMetis_prefix_dir}/parmetis-patch-step.sh)
+configure_file(${SuperBuild_TEMPLATE_FILES_DIR}/parmetis-patch-step.sh.in
+               ${ParMetis_sh_patch}
+               @ONLY)
+# --- Configure the CMake patch step
+set(ParMetis_cmake_patch ${ParMetis_prefix_dir}/parmetis-patch-step.cmake)
+configure_file(${SuperBuild_TEMPLATE_FILES_DIR}/parmetis-patch-step.cmake.in
+               ${ParMetis_cmake_patch}
+               @ONLY)
+
+# --- Set the patch command
+set(ParMetis_PATCH_COMMAND ${CMAKE_COMMAND} -P ${ParMetis_cmake_patch})     
+
 # --- Define the CMake configure parameters
 # Note:
 #      CMAKE_CACHE_ARGS requires -DVAR:<TYPE>=VALUE syntax
@@ -36,6 +54,8 @@ ExternalProject_Add(${ParMetis_BUILD_TARGET}
                     DOWNLOAD_DIR ${TPL_DOWNLOAD_DIR} 
                     URL          ${ParMetis_URL}                      # URL may be a web site OR a local file
                     URL_MD5      ${ParMetis_MD5_SUM}                  # md5sum of the archive file
+                    # -- Patch 
+                    PATCH_COMMAND  ${ParMetis_PATCH_COMMAND}
                     # -- Configure
                     SOURCE_DIR       ${ParMetis_source_dir}           # Source directory
                     CMAKE_CACHE_ARGS ${AMANZI_CMAKE_CACHE_ARGS}       # Ensure uniform build
