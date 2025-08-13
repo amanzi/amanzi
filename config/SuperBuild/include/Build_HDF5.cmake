bc2aebf55951ddb068db89836d428eea66c29fe1
Ethan Coon <etcoon@gmail.com>
2025-08-13 10:32:49 -0600

    Ecoon/amanzi 1.6 superbuild old patches (#927)
    Cleans up outdated patches for hdf5 and netcdf, and fixes MPI include path issue with Moab.
    
    * removes two no longer useful patches to TPLs
    
    * Fixes MOAB build on OSX. closes #876
    
    ---------
    
    Co-authored-by: Konstantin Lipnikov <lipnikov@lanl.gov>, Ethan Coon <coonet@ornl.gov>

--- config/SuperBuild/include/Build_HDF5.cmake
+++ config/SuperBuild/include/Build_HDF5.cmake
@@ -13,20 +13,6 @@ amanzi_tpl_version_write(FILENAME ${TPL_VERSIONS_INCLUDE_FILE}
   PREFIX HDF5
   VERSION ${HDF5_VERSION_MAJOR} ${HDF5_VERSION_MINOR} ${HDF5_VERSION_PATCH})
 
-# --- Patch the original code
-set(HDF5_patch_file hdf5-1.10.6-rpath.patch)
-set(HDF5_sh_patch ${HDF5_prefix_dir}/hdf5-patch-step.sh)
-configure_file(${SuperBuild_TEMPLATE_FILES_DIR}/hdf5-patch-step.sh.in
-               ${HDF5_sh_patch}
-               @ONLY)
-# configure the CMake patch step
-set(HDF5_cmake_patch ${HDF5_prefix_dir}/hdf5-patch-step.cmake)
-configure_file(${SuperBuild_TEMPLATE_FILES_DIR}/hdf5-patch-step.cmake.in
-               ${HDF5_cmake_patch}
-               @ONLY)
-# set the patch command
-set(HDF5_PATCH_COMMAND ${CMAKE_COMMAND} -P ${HDF5_cmake_patch})     
-
 # --- Define configure parameters
 set(HDF5_CMAKE_CACHE_ARGS "-DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}")
 if ( (${AMANZI_ARCH_NERSC} OR ${AMANZI_ARCH_CHICOMA}) AND (NOT BUILD_SHARED_LIBS))
