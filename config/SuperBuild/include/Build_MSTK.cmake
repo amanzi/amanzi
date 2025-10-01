de9b6331f99b4e3ebd4f24535b79b4f25b8a8592
Ethan Coon <coonet@ornl.gov>
2025-10-01 09:25:34 -0600

    adds MSTK patch to allow prepartioning

--- config/SuperBuild/include/Build_MSTK.cmake
+++ config/SuperBuild/include/Build_MSTK.cmake
@@ -20,8 +20,8 @@ amanzi_tpl_version_write(FILENAME ${TPL_VERSIONS_INCLUDE_FILE}
                          VERSION ${MSTK_VERSION_MAJOR} ${MSTK_VERSION_MINOR} ${MSTK_VERSION_PATCH})
 
 # --- Patch the original code
-set(MSTK_patch_file mstk-cmake2.patch)
-set(MSTK_sh_patch ${MSTK_prefix_dir}/mstk-patch-step.sh)
+set(MSTK_patch_file mstk-cmake2.patch mstk-prepartitioned.patch)
+set(MSTK_sh_patch ${MSTK_prefix_dir}/mstk-patch-step.sh )
 configure_file(${SuperBuild_TEMPLATE_FILES_DIR}/mstk-patch-step.sh.in
               ${MSTK_sh_patch}
               @ONLY)
