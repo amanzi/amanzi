amanzi/amanzi

commit e91f3bf0b45a9e72aea6427b803ddba6212909c1

Added CPTR preconditioner to flow-energy MPC PK.
Added new method to modify the set of local Ops.
Added call SymbolicAssembleMatrix() to the TreeOperator assembly logic.
Added lower-triangular preconditioner to the TreeOperator class.

src/operators/Operator.hh
@@ -352,6 +352,7 @@ class Operator : public Matrix<CompositeVector, CompositeVectorSpace> {
   void OpExtend(op_iterator begin, op_iterator end);
   void OpReplace(const Teuchos::RCP<Op>& op, int index) { ops_[index] = op; }
   void OpErase(op_iterator begin, op_iterator end) { ops_.erase(begin, end); }
+  void OpErase(op_iterator begin) { ops_.erase(begin); }
 
   // quality control
   void Verify() const;

src/operators/TreeOperator.cc
@@ -49,7 +49,8 @@ TreeOperator::TreeOperator()
     inverse_pars_set_(false),
     initialize_complete_(false),
     compute_complete_(false),
-    assembly_complete_(false)
+    assembly_complete_(false),
+    symbolic_assembly_complete_(false)
 {
   vo_ = Teuchos::rcp(new VerboseObject("TreeOperator", Teuchos::ParameterList()));
 }
@@ -111,6 +112,7 @@ TreeOperator::TreeOperator(const TreeOperator& other)
   shift_min_ = other.shift_min_;
   vo_ = other.vo_;
   inv_plist_ = other.inv_plist_;
+  inverse_pars_set_ = other.inverse_pars_set_;
 
   for (int i = 0; i != row_size_; ++i) {
     for (int j = 0; j != col_size_; ++j) {
@@ -413,6 +415,8 @@ TreeOperator::SymbolicAssembleMatrix()
   // create the matrix
   Amat_ = Teuchos::rcp(new MatrixFE(graph));
   A_ = Amat_->Matrix();
+
+  symbolic_assembly_complete_ = true;
 }
 
 
@@ -422,6 +426,8 @@ TreeOperator::SymbolicAssembleMatrix()
 void
 TreeOperator::AssembleMatrix()
 {
+  if (!symbolic_assembly_complete_) SymbolicAssembleMatrix();
+
   AMANZI_ASSERT(leaves_.size() != 0);
   Amat_->PutScalar(0.);
 
@@ -462,6 +468,7 @@ TreeOperator::AssembleMatrix()
 void
 TreeOperator::Init()
 {
+  symbolic_assembly_complete_ = false;
   assembly_complete_ = false;
   compute_complete_ = false;
   for (int i = 0; i != row_size_; ++i) {
@@ -479,6 +486,7 @@ TreeOperator::Init()
 void
 TreeOperator::InitOffdiagonals()
 {
+  symbolic_assembly_complete_ = false;
   assembly_complete_ = false;
   compute_complete_ = false;
   for (int i = 0; i != row_size_; ++i) {

src/operators/TreeOperator.hh
@@ -43,6 +43,7 @@ class MatrixFE;
 class Operator;
 namespace Impl {
 class TreeOperator_BlockDiagonalPreconditioner;
+class TreeOperator_BlockTriangularPreconditioner;
 }
 
 class TreeOperator : public Matrix<TreeVector, TreeVectorSpace> {
@@ -175,6 +176,7 @@ class TreeOperator : public Matrix<TreeVector, TreeVectorSpace> {
 
  protected:
   friend Impl::TreeOperator_BlockDiagonalPreconditioner;
+  friend Impl::TreeOperator_BlockTriangularPreconditioner;
 
   Teuchos::RCP<const TreeVectorSpace> row_map_, col_map_;
   std::size_t row_size_, col_size_;
@@ -197,6 +199,7 @@ class TreeOperator : public Matrix<TreeVector, TreeVectorSpace> {
   bool initialize_complete_;
   bool compute_complete_;
   bool assembly_complete_;
+  bool symbolic_assembly_complete_;
 
   Teuchos::RCP<VerboseObject> vo_;
 };
@@ -230,6 +233,56 @@ class TreeOperator_BlockDiagonalPreconditioner {
   {
     return op_.ApplyInverseBlockDiagonal_(X, Y);
   }
+  void InitializeInverse()
+  {
+    for (int n = 0; n != op_.get_col_size(); ++n) {
+      op_.get_block(n, n)->InitializeInverse();
+    }
+  }
+  void ComputeInverse()
+  {
+    for (int n = 0; n != op_.get_col_size(); ++n) {
+      op_.get_block(n, n)->ComputeInverse();
+    }
+  }
+
+ private:
+  TreeOperator& op_;
+};
+
+
+// This class implements the Gauss-Seidel algorithm.
+class TreeOperator_BlockTriangularPreconditioner {
+ public:
+  using Vector_t = TreeOperator::Vector_t;
+  using VectorSpace_t = TreeOperator::VectorSpace_t;
+
+  TreeOperator_BlockTriangularPreconditioner(TreeOperator& op)
+    : op_(op)
+  {}
+
+  int Apply(const TreeVector& X, TreeVector& Y) const
+  {
+    Exceptions::amanzi_throw("TreeOperator GS Preconditioner does not implement Apply()");
+    return 1;
+  }
+
+  int ApplyInverse(const TreeVector& X, TreeVector& Y) const
+  {
+    for (int i = 0; i != op_.get_col_size(); ++i) {
+      const TreeVector& Xi = *X.SubVector(i);
+      TreeVector Z(Xi), Z1(Xi);
+
+      for (int j = 0; j < i; ++j) {
+        op_.get_block(i, j)->Apply(*Y.SubVector(j), Z1);
+        Z.Update(-1.0, Z1, 1.0);
+      }
+      int ierr = op_.get_block(i, i)->ApplyInverse(Z, *Y.SubVector(i));
+      if (ierr) return ierr;
+    }
+    return 0;
+  }
+
   void InitializeInverse()
   {
     for (int n = 0; n != op_.get_col_size(); ++n) {

src/mpc/test/mpc_supercriticalPH.xml
@@ -214,7 +214,7 @@
         <Parameter name="start period time" type="double" value="0.0"/>
         <Parameter name="end period time" type="double" value="40000000.0"/>
         <Parameter name="maximum cycle number" type="int" value="30"/>
-        <Parameter name="initial timestep" type="double" value="100.0"/>
+        <Parameter name="initial timestep" type="double" value="10.0"/>
         <Parameter name="maximum timestep" type="double" value="1.0e+5"/>
       </ParameterList>
     </ParameterList>
@@ -281,7 +281,7 @@
       </ParameterList>
       <ParameterList name="time integrator">
         <Parameter name="error control options" type="Array(string)" value="{pressure, residual}"/>
-        <Parameter name="linear solver" type="string" value="AztecOO"/>
+        <Parameter name="linear solver" type="string" value="none"/>
         <Parameter name="preconditioner" type="string" value="Hypre AMG"/>
         <ParameterList name="dae constraint">
           <Parameter name="method" type="string" value="projection"/>
@@ -393,7 +393,7 @@
       </ParameterList>
       <ParameterList name="time integrator">
         <Parameter name="error control options" type="Array(string)" value="{energy}"/>
-        <Parameter name="linear solver" type="string" value="AztecOO"/>
+        <Parameter name="linear solver" type="string" value="none"/>
         <Parameter name="preconditioner" type="string" value="Hypre AMG"/>
         <ParameterList name="dae constraint">
           <Parameter name="method" type="string" value="projection"/>
@@ -470,8 +470,9 @@
       <Parameter name="domain name" type="string" value="domain"/>
       <ParameterList name="time integrator">
         <Parameter name="error control options" type="Array(string)" value="{residual}"/>
-        <Parameter name="linear solver" type="string" value="AztecOO"/>
+        <Parameter name="linear solver" type="string" value="none"/>
         <Parameter name="preconditioner" type="string" value="Hypre MGR"/>
+        <Parameter name="use CPTR preconditioner" type="bool" value="true"/>
         <ParameterList name="dae constraint">
           <Parameter name="method" type="string" value="projection"/>
           <Parameter name="inflow krel correction" type="bool" value="true"/>
@@ -500,7 +501,7 @@
             <Parameter name="modify correction" type="bool" value="true"/>
           </ParameterList>
           <Parameter name="extrapolation damping factor" type="double" value="0.8"/>
-          <Parameter name="max preconditioner lag iterations" type="int" value="0"/>
+          <Parameter name="max preconditioner lag iterations" type="int" value="5"/>
           <Parameter name="extrapolate initial guess" type="bool" value="false"/>
           <Parameter name="restart tolerance relaxation factor" type="double" value="1.0"/>
           <Parameter name="restart tolerance relaxation factor damping" type="double" value="1.0"/>
@@ -549,7 +550,7 @@
     <ParameterList name="ILU">
       <Parameter name="preconditioning method" type="string" value="ILU"/>
       <ParameterList name="ILU parameters">
-        <Parameter name="ilu(k) fill level" type="int" value="20"/>
+        <Parameter name="ilu(k) fill level" type="int" value="0"/>
         <Parameter name="rescale rows" type="bool" value="true"/>
         <Parameter name="verbosity" type="int" value="0"/>
       </ParameterList>

src/mpc/test/mpc_supercriticalPH_jacobian.cc
@@ -57,6 +57,8 @@ void RunJacobian(double pMPa, double T, double hkJ, double tol10 = 1e-3)
   tmp4.sublist("function").sublist("domain").sublist("function").sublist("function-exprtk")
     .set<std::string>("formula", std::to_string(hkJ * 18.015) + " - 0.05 * x * (100 - x)");
 
+  plist->sublist("PKs").sublist("transient:mpc1").sublist("time integrator").set<bool>("use CPTR preconditioner", false);
+
   auto comm = Amanzi::getDefaultComm();
   Teuchos::ParameterList region_list = plist->get<Teuchos::ParameterList>("regions");
   auto gm = Teuchos::rcp(new AmanziGeometry::GeometricModel(2, region_list, *comm));

commit b11ebb702b1a487242bdfde19d4d361a44fe6cf3

Changes to how Operators deal with Symbolic Assembly (#972)
Deals with a bug introduced in  e91f3bf but also fixes some nagging conceptual issues.  See #971 for a detailed discussion.

1. {Tree}Operator::AssembleMatrix() now calls SymbolicAssembleMatrix() if that has not been done.
2. Symbolic assembly now "locks" a matrix structure (and a user can do this manually as well.  Once structure is locked, further calls that change the matrix structure error.  
3. Call {Tree}Operator::Clone() to get a new, unlocked, unassembled Operator. 


Closes #971

src/operators/Operator.cc
@@ -63,17 +63,18 @@ namespace Operators {
 * Deprecated constructor: still supported for compatability
 ****************************************************************** */
 Operator::Operator(const Teuchos::RCP<const CompositeVectorSpace>& cvs,
-                   Teuchos::ParameterList& plist,
+                   Teuchos::ParameterList plist,
                    int schema)
   : cvs_row_(cvs),
     cvs_col_(cvs),
-    plist_(plist),
+    plist_(std::move(plist)),
     num_colors_(0),
     coloring_(Teuchos::null),
     inverse_pars_set_(false),
     initialize_complete_(false),
     compute_complete_(false),
     assembly_complete_(false),
+    structure_locked_(false),
     schema_row_(schema),
     schema_col_(schema),
     shift_(0.0),
@@ -106,9 +107,9 @@ Operator::Operator(const Teuchos::RCP<const CompositeVectorSpace>& cvs,
     nedges_wghost = 0;
   }
 
-  vo_ = Teuchos::rcp(new VerboseObject("Operator", plist));
-  shift_ = plist.get<double>("diagonal shift", 0.0);
-  shift_min_ = plist.get<double>("diagonal shift minimum", 0.0);
+  vo_ = Teuchos::rcp(new VerboseObject("Operator", plist_));
+  shift_ = plist_.get<double>("diagonal shift", 0.0);
+  shift_min_ = plist_.get<double>("diagonal shift minimum", 0.0);
 
   apply_calls_ = 0;
 
@@ -126,18 +127,19 @@ Operator::Operator(const Teuchos::RCP<const CompositeVectorSpace>& cvs,
 ****************************************************************** */
 Operator::Operator(const Teuchos::RCP<const CompositeVectorSpace>& cvs_row,
                    const Teuchos::RCP<const CompositeVectorSpace>& cvs_col,
-                   Teuchos::ParameterList& plist,
+                   Teuchos::ParameterList plist,
                    const Schema& schema_row,
                    const Schema& schema_col)
   : cvs_row_(cvs_row),
     cvs_col_(cvs_col),
-    plist_(plist),
+    plist_(std::move(plist)),
     num_colors_(0),
     coloring_(Teuchos::null),
     inverse_pars_set_(false),
     initialize_complete_(false),
     compute_complete_(false),
     assembly_complete_(false),
+    structure_locked_(false),
     schema_row_(schema_row),
     schema_col_(schema_col),
     shift_(0.0),
@@ -170,10 +172,29 @@ Operator::Operator(const Teuchos::RCP<const CompositeVectorSpace>& cvs_row,
     nedges_wghost = 0;
   }
 
-  vo_ = Teuchos::rcp(new VerboseObject("Operator", plist));
-  shift_ = plist.get<double>("diagonal shift", 0.0);
-  shift_min_ = plist.get<double>("diagonal shift minimum", 0.0);
+  vo_ = Teuchos::rcp(new VerboseObject("Operator", plist_));
+  shift_ = plist_.get<double>("diagonal shift", 0.0);
+  shift_min_ = plist_.get<double>("diagonal shift minimum", 0.0);
   apply_calls_ = 0;
+}
+
+
+/* ******************************************************************
+* Copy constructor duplicates plist_ (Operator owns it by value) and
+* shares Ops and rhs_ with the original (shallow copy) -- callers may
+* have already populated rhs_ (e.g. via AddAccumulationDelta/ApplyBCs)
+* before cloning, and expect that data to carry over, just as they
+* expect the Ops themselves to. Assembly/lock state (Amat_, smap_, A_,
+* structure_locked_) is reset fresh by delegating to the general
+* constructor.
+****************************************************************** */
+Operator::Operator(const Operator& other)
+  : Operator(other.cvs_row_, other.cvs_col_, other.plist_, other.schema_row_, other.schema_col_)
+{
+  ops_ = other.ops_;
+  rhs_ = other.rhs_;
+  inv_plist_ = other.inv_plist_;
+  inverse_pars_set_ = other.inverse_pars_set_;
 }
 
 
@@ -216,7 +237,11 @@ Operator::SymbolicAssembleMatrix()
   // create global matrix
   Amat_ = Teuchos::rcp(new MatrixFE(graph));
   A_ = Amat_->Matrix();
+  lockStructure();
+
+  // invalidate later steps
   assembly_complete_ = false;
+  compute_complete_ = false;
 }
 
 
@@ -285,14 +310,7 @@ Operator::SymbolicAssembleMatrixOp(const Op_Diagonal& op,
 void
 Operator::AssembleMatrix()
 {
-  if (Amat_ == Teuchos::null) {
-    Errors::Message msg("Symbolic assembling was not performed.");
-    Exceptions::amanzi_throw(msg);
-  }
-
-  // note, this is called prior to AssembleMatrix() because Schur complements
-  // override this because ApplyAssembled is not valid for them.
-  assembly_complete_ = true;
+  if (!structure_locked_) SymbolicAssembleMatrix();
 
   Amat_->PutScalar(0.);
   AssembleMatrix(*smap_, *Amat_, 0, 0);
@@ -304,6 +322,8 @@ Operator::AssembleMatrix()
     Amat_->DiagonalShiftMin(shift_min_);
   }
 
+  // set flags
+  assembly_complete_ = true;
   compute_complete_ = false;
 
   // std::stringstream filename_s2;
@@ -629,7 +649,7 @@ Operator::ComputeInverse()
   }
   // assembly must be possible now
   AMANZI_ASSERT(preconditioner_.get());
-  preconditioner_->ComputeInverse(); // NOTE: calls this->AssembleMatrix()
+  preconditioner_->ComputeInverse(); // NOTE: calls this->AssembleMatrix() if needed
   compute_complete_ = true;
 }
 
@@ -782,16 +802,57 @@ Operator::FindMatrixOp(int schema_dofs, int matching_rule, bool action)
 
 
 /* ******************************************************************
-* Add more operators to the existing list.
+* Block mutate: these change the operator's structure (the set of Ops
+* it owns) and so are only valid prior to the first
+* SymbolicAssembleMatrix() call, which fixes the matrix structure.
 ****************************************************************** */
+void
+Operator::AssertStructureNotLocked_() const
+{
+  if (structure_locked_) {
+    Errors::Message msg("Operator: cannot change the structure of the operator (add/replace/erase "
+                        "Ops) after SymbolicAssembleMatrix() has been called.");
+    Exceptions::amanzi_throw(msg);
+  }
+}
+
+void
+Operator::OpPushBack(const Teuchos::RCP<Op>& op)
+{
+  AssertStructureNotLocked_();
+  ops_.push_back(op);
+}
+
 void
 Operator::OpExtend(op_iterator begin, op_iterator end)
 {
+  AssertStructureNotLocked_();
   int nops = ops_.size();
   int nnew = nops + std::distance(begin, end);
 
   ops_.reserve(nnew);
   ops_.insert(ops_.end(), begin, end);
+}
+
+void
+Operator::OpReplace(const Teuchos::RCP<Op>& op, int index)
+{
+  AssertStructureNotLocked_();
+  ops_[index] = op;
+}
+
+void
+Operator::OpErase(op_iterator begin, op_iterator end)
+{
+  AssertStructureNotLocked_();
+  ops_.erase(begin, end);
+}
+
+void
+Operator::OpErase(op_iterator begin)
+{
+  AssertStructureNotLocked_();
+  ops_.erase(begin);
 }
 
 

src/operators/Operator.hh
@@ -211,21 +211,36 @@ class Operator : public Matrix<CompositeVector, CompositeVectorSpace> {
 
   // deprecated but yet supported
   Operator(const Teuchos::RCP<const CompositeVectorSpace>& cvs,
-           Teuchos::ParameterList& plist,
+           Teuchos::ParameterList plist,
            int schema);
 
   // general operator (domain may differ from range)
   Operator(const Teuchos::RCP<const CompositeVectorSpace>& cvs_row,
            const Teuchos::RCP<const CompositeVectorSpace>& cvs_col,
-           Teuchos::ParameterList& plist,
+           Teuchos::ParameterList plist,
            const Schema& schema_row,
            const Schema& schema_col);
 
   // bijective operator (domain = range)
   Operator(const Teuchos::RCP<const CompositeVectorSpace>& cvs,
-           Teuchos::ParameterList& plist,
+           Teuchos::ParameterList plist,
            const Schema& schema)
-    : Operator(cvs, cvs, plist, schema, schema) {};
+    : Operator(cvs, cvs, std::move(plist), schema, schema) {};
+
+  // Copy constructor shares (shallow-copies) the Ops and rhs_ with the
+  // original, matching the pre-existing Clone() semantics: the clone's Ops
+  // still see the same underlying data as the original, so updates to the
+  // original's Ops (and any rhs_ contributions already accumulated via
+  // AddAccumulationDelta/ApplyBCs before cloning) are reflected in the
+  // clone. plist_ is duplicated (deep copy), since Operator owns its
+  // plist_ by value. What is NOT copied is the assembled matrix/graph/
+  // supermap or the staging flags -- these are reset to their pre-assembly
+  // state by delegating to the general constructor, so the resulting
+  // Operator starts unlocked and may be extended with more Ops.
+  // Subclasses must implement Clone() by delegating to their own copy
+  // constructor rather than relying on the compiler-generated one (which
+  // would instead shallow-copy Amat_/smap_/structure_locked_ too).
+  Operator(const Operator& other);
 
   virtual ~Operator() = default;
 
@@ -348,11 +363,20 @@ class Operator : public Matrix<CompositeVector, CompositeVectorSpace> {
   op_iterator FindMatrixOp(int schema_dofs, int matching_rule, bool action);
 
   // block mutate
-  void OpPushBack(const Teuchos::RCP<Op>& op) { ops_.push_back(op); }
+  //
+  // NOTE: these change the structure of the operator (the set of Ops it
+  // owns) and so must only be called during construction/setup, before the
+  // first SymbolicAssembleMatrix() call fixes the matrix structure.
+  void OpPushBack(const Teuchos::RCP<Op>& op);
   void OpExtend(op_iterator begin, op_iterator end);
-  void OpReplace(const Teuchos::RCP<Op>& op, int index) { ops_[index] = op; }
-  void OpErase(op_iterator begin, op_iterator end) { ops_.erase(begin, end); }
-  void OpErase(op_iterator begin) { ops_.erase(begin); }
+  void OpReplace(const Teuchos::RCP<Op>& op, int index);
+  void OpErase(op_iterator begin, op_iterator end);
+  void OpErase(op_iterator begin);
+
+  // Locks the structure of the operator (the set of Ops it owns), preventing
+  // any further structural changes. Called automatically by
+  // SymbolicAssembleMatrix(), but may also be called directly.
+  void lockStructure() { structure_locked_ = true; }
 
   // quality control
   void Verify() const;
@@ -630,6 +654,7 @@ class Operator : public Matrix<CompositeVector, CompositeVectorSpace> {
 
  protected:
   int SchemaMismatch_(const std::string& schema1, const std::string& schema2) const;
+  void AssertStructureNotLocked_() const;
 
  protected:
   Teuchos::RCP<const AmanziMesh::Mesh> mesh_;
@@ -637,7 +662,7 @@ class Operator : public Matrix<CompositeVector, CompositeVectorSpace> {
   Teuchos::RCP<const CompositeVectorSpace> cvs_col_;
   Teuchos::ParameterList plist_;
 
-  mutable std::vector<Teuchos::RCP<Op>> ops_;
+  std::vector<Teuchos::RCP<Op>> ops_;
   Teuchos::RCP<CompositeVector> rhs_, rhs_checkpoint_;
 
   int ncells_owned, nfaces_owned, nnodes_owned, nedges_owned;
@@ -646,10 +671,13 @@ class Operator : public Matrix<CompositeVector, CompositeVectorSpace> {
   int num_colors_;
   Teuchos::RCP<std::vector<int>> coloring_;
   Teuchos::ParameterList inv_plist_;
+
+  // lazy staging
   bool inverse_pars_set_;
   bool initialize_complete_;
   bool compute_complete_;
-  mutable bool assembly_complete_;
+  bool assembly_complete_;
+  bool structure_locked_;
 
   Teuchos::RCP<Epetra_CrsMatrix> A_;
   Teuchos::RCP<Matrix<CompositeVector>> preconditioner_;
@@ -667,7 +695,6 @@ class Operator : public Matrix<CompositeVector, CompositeVectorSpace> {
   mutable int apply_calls_;
 
  private:
-  // Operator(const Operator& op);
   Operator& operator=(const Operator& op);
 };
 

src/operators/Operator_Cell.cc
@@ -217,9 +217,8 @@ Operator_Cell::AssembleMatrixOp(const Op_Face_Cell& op,
     }
 
     ierr |= mat.SumIntoMyValues(lid_r.data(), lid_c.data(), op.matrices[f]);
-    AMANZI_ASSERT(ierr >= 0);
   }
-  AMANZI_ASSERT(ierr >= 0);
+  AMANZI_ASSERT(!ierr);
 }
 
 

src/operators/Operator_FaceCellScc.cc
@@ -45,6 +45,8 @@ namespace Operators {
 int
 Operator_FaceCellScc::ApplyInverse(const CompositeVector& X, CompositeVector& Y) const
 {
+  if (!compute_complete_) const_cast<Operator_FaceCellScc*>(this)->ComputeInverse();
+
   int ierr(0);
   Y.PutScalarGhosted(0.0);
 
@@ -355,6 +357,10 @@ Operator_FaceCellScc::SymbolicAssembleMatrix()
   // create global matrix
   Amat_ = Teuchos::rcp(new MatrixFE(graph));
   A_ = Amat_->Matrix();
+  lockStructure();
+
+  assembly_complete_ = false;
+  compute_complete_ = false;
 }
 
 

src/operators/Operator_FaceCellScc.hh
@@ -34,6 +34,16 @@ class Operator_FaceCellScc : public Operator_Cell {
     set_schema_string("FACE+CELL Schur to CELL");
   }
 
+  // Copy constructor: diag_ops_ and schur_ops_ are per-instance derived
+  // scratch state, lazily rebuilt from ops_ on AssembleMatrix, so they
+  // should not be shared with a clone -- left at their default (empty)
+  // state by not being copied here. The base subobject is copied by
+  // delegating to Operator_Cell's (implicit) copy constructor, which
+  // chains up to Operator's.
+  Operator_FaceCellScc(const Operator_FaceCellScc& other)
+    : Operator_Cell(other)
+  {}
+
   virtual Teuchos::RCP<Operator> Clone() const override;
 
   // cannot do an assembled forward apply as the assembled thing is not the full

src/operators/Operator_FaceCellSff.cc
@@ -184,7 +184,10 @@ Operator_FaceCellSff::SymbolicAssembleMatrix()
   // create global matrix
   Amat_ = Teuchos::rcp(new MatrixFE(graph));
   A_ = Amat_->Matrix();
+  lockStructure();
+
   assembly_complete_ = false;
+  compute_complete_ = false;
 }
 
 

src/operators/Operator_FaceCellSff.hh
@@ -40,6 +40,18 @@ class Operator_FaceCellSff : public Operator_FaceCell {
     set_schema_string("FACE+CELL Schur to FACE");
   }
 
+  // Copy constructor: schur_ops_ and schur_inv_ are per-instance derived
+  // scratch state (schur_ops_ is lazily rebuilt from ops_ on AssembleMatrix;
+  // schur_inv_ captures a back-reference to this specific instance via
+  // set_matrix(Teuchos::rcpFromRef(*this)) in InitializeInverse()), so
+  // neither should be shared with a clone -- both are left at their default
+  // (empty / null) state by not being copied here. The base subobject is
+  // copied by delegating to Operator_FaceCell's (implicit) copy
+  // constructor, which chains up to Operator's.
+  Operator_FaceCellSff(const Operator_FaceCellSff& other)
+    : Operator_FaceCell(other)
+  {}
+
   virtual void InitializeInverse() override;
   virtual Teuchos::RCP<Operator> Clone() const override;
 

src/operators/TreeOperator.cc
@@ -50,7 +50,7 @@ TreeOperator::TreeOperator()
     initialize_complete_(false),
     compute_complete_(false),
     assembly_complete_(false),
-    symbolic_assembly_complete_(false)
+    structure_locked_(false)
 {
   vo_ = Teuchos::rcp(new VerboseObject("TreeOperator", Teuchos::ParameterList()));
 }
@@ -130,10 +130,44 @@ TreeOperator::TreeOperator(const TreeOperator& other)
 
 /* ******************************************************************
 * block setters
+*
+* NOTE: these change the structure of the TreeOperator (which
+* sub-operator occupies a block) and so are only valid prior to the
+* first SymbolicAssembleMatrix() call, which fixes the matrix
+* structure (including the flattened leaves_ array).
 ****************************************************************** */
+void
+TreeOperator::AssertStructureNotLocked_() const
+{
+  if (structure_locked_) {
+    Errors::Message msg("TreeOperator: cannot change the structure of the operator (set_block, "
+                        "set_operator) after SymbolicAssembleMatrix() has been called.");
+    Exceptions::amanzi_throw(msg);
+  }
+}
+
+
+/* ******************************************************************
+* Locks the structure of this TreeOperator and, recursively, every
+* sub-TreeOperator block and leaf Operator beneath it.
+****************************************************************** */
+void
+TreeOperator::lockStructure()
+{
+  structure_locked_ = true;
+  for (int i = 0; i != row_size_; ++i) {
+    for (int j = 0; j != col_size_; ++j) {
+      if (blocks_[i][j] != Teuchos::null) blocks_[i][j]->lockStructure();
+    }
+  }
+  if (data_ != Teuchos::null) data_->lockStructure();
+}
+
+
 void
 TreeOperator::set_block(std::size_t i, std::size_t j, const Teuchos::RCP<TreeOperator>& op)
 {
+  AssertStructureNotLocked_();
   blocks_[i][j] = op;
   initialize_complete_ = false;
 }
@@ -142,6 +176,7 @@ TreeOperator::set_block(std::size_t i, std::size_t j, const Teuchos::RCP<TreeOpe
 void
 TreeOperator::set_operator(const Teuchos::RCP<Operator>& op)
 {
+  AssertStructureNotLocked_();
   data_ = op;
   initialize_complete_ = false;
 }
@@ -415,8 +450,10 @@ TreeOperator::SymbolicAssembleMatrix()
   // create the matrix
   Amat_ = Teuchos::rcp(new MatrixFE(graph));
   A_ = Amat_->Matrix();
+  lockStructure();
 
-  symbolic_assembly_complete_ = true;
+  assembly_complete_ = false;
+  compute_complete_ = false;
 }
 
 
@@ -426,9 +463,7 @@ TreeOperator::SymbolicAssembleMatrix()
 void
 TreeOperator::AssembleMatrix()
 {
-  if (!symbolic_assembly_complete_) SymbolicAssembleMatrix();
-
-  AMANZI_ASSERT(leaves_.size() != 0);
+  if (!structure_locked_) SymbolicAssembleMatrix();
   Amat_->PutScalar(0.);
 
   // check that each row has at least one non-null operator block
@@ -468,15 +503,15 @@ TreeOperator::AssembleMatrix()
 void
 TreeOperator::Init()
 {
-  symbolic_assembly_complete_ = false;
-  assembly_complete_ = false;
-  compute_complete_ = false;
   for (int i = 0; i != row_size_; ++i) {
     for (int j = 0; j != col_size_; ++j) {
       if (get_block(i, j) != Teuchos::null) get_block(i, j)->Init();
     }
   }
   if (data_ != Teuchos::null) data_->Init();
+
+  assembly_complete_ = false;
+  compute_complete_ = false;
 }
 
 
@@ -486,14 +521,14 @@ TreeOperator::Init()
 void
 TreeOperator::InitOffdiagonals()
 {
-  symbolic_assembly_complete_ = false;
-  assembly_complete_ = false;
-  compute_complete_ = false;
   for (int i = 0; i != row_size_; ++i) {
     for (int j = 0; j != col_size_; ++j) {
       if (i != j && get_block(i, j) != Teuchos::null) get_block(i, j)->Init();
     }
   }
+
+  assembly_complete_ = false;
+  compute_complete_ = false;
 }
 
 
@@ -523,6 +558,8 @@ TreeOperator::InitializeInverse()
   // if this is a leaf, just initialize the Operator
   if (get_operator() != Teuchos::null) {
     get_operator()->InitializeInverse();
+    initialize_complete_ = true;
+    compute_complete_ = false;
     return;
   }
 

src/operators/TreeOperator.hh
@@ -108,6 +108,13 @@ class TreeOperator : public Matrix<TreeVector, TreeVectorSpace> {
   void set_operator(const Teuchos::RCP<Operator>& op);
   void set_operator_block(std::size_t i, std::size_t j, const Teuchos::RCP<Operator>& op);
 
+  // Locks the structure of the TreeOperator (which sub-operator occupies
+  // each block), preventing any further structural changes. Recursively
+  // locks every sub-TreeOperator block and leaf Operator. Called
+  // automatically by SymbolicAssembleMatrix(), but may also be called
+  // directly.
+  void lockStructure();
+
   bool IsSquare() const { return get_col_size() == get_row_size(); }
   std::size_t get_col_size() const { return col_size_; }
   std::size_t get_row_size() const { return row_size_; }
@@ -173,6 +180,7 @@ class TreeOperator : public Matrix<TreeVector, TreeVectorSpace> {
 
  protected:
   int ApplyInverseBlockDiagonal_(const TreeVector& X, TreeVector& Y) const;
+  void AssertStructureNotLocked_() const;
 
  protected:
   friend Impl::TreeOperator_BlockDiagonalPreconditioner;
@@ -199,7 +207,7 @@ class TreeOperator : public Matrix<TreeVector, TreeVectorSpace> {
   bool initialize_complete_;
   bool compute_complete_;
   bool assembly_complete_;
-  bool symbolic_assembly_complete_;
+  bool structure_locked_;
 
   Teuchos::RCP<VerboseObject> vo_;
 };

src/solvers/InverseHelpers.hh
@@ -29,6 +29,8 @@ struct is_assembled {
 
 //
 // is_assembled::value == true if Operator has method FillComplete()
+// This is only for Epetra_Matrix objects used mostly (only?) in testing
+// where the matrix comes pre-assembled.
 //
 template<typename Operator>
 struct is_assembled<

src/operators/test/operator_magnetic_diffusion.cc
@@ -159,6 +159,12 @@ MagneticDiffusion2D(double dt,
   // CompositeVector B0(B);
   // Epetra_MultiVector& B0f = *B0.ViewComponent("face");
 
+  // Don't try to modify the structure of a matrix after it has been assembled/computed/etc.
+  Teuchos::RCP<PDE_Accumulation> op_acc =
+    Teuchos::rcp(new PDE_Accumulation(AmanziMesh::Entity_kind::NODE, global_op));
+  op_acc->SetBCs(bc1, bc1);
+
+
   int cycle(0);
   double divB0(0.0);
   while (told + dt / 2 < tend) {
@@ -172,9 +178,6 @@ MagneticDiffusion2D(double dt,
     CompositeVector phi(cvs_e);
     phi.PutScalar(1.0 / Kc(0, 0));
 
-    Teuchos::RCP<PDE_Accumulation> op_acc =
-      Teuchos::rcp(new PDE_Accumulation(AmanziMesh::Entity_kind::NODE, global_op));
-    op_acc->SetBCs(bc1, bc1);
     op_acc->AddAccumulationTerm(phi, 1.0, "node");
 
     // update electric boundary conditions